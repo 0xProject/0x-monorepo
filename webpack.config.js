91a34889f94422d02f08f76fb7442b2556424ec5
Leonid Logvinov <logvinov.leon@gmail.com>
2017-05-24 12:08:15 +0200

    Add webpack config and npm scripts

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        '0x': './src/ts/0x.js.ts',
+        '0x.min': './src/ts/0x.js.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, '_bundles'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'zeroEx',
+        umdNamedDefine: true
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: true,
+            include: /\.min\.js$/,
+        })
+    ],
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader',
+            exclude: /node_modules/,
+            query: {
+                declaration: false,
+            }
+        }]
+    }
+}
