c50b32ee3f740139623d4044f8110d8ca2b26062
Leonid Logvinov <logvinov.leon@gmail.com>
2017-05-24 13:37:54 +0200

    Add initial testing config and a dummy test

---
+++ test/0x.js.ts
@@ -0,0 +1,12 @@
+import {ZeroEx} from '../src/ts/0x.js';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('ZeroEx library', () => {
+    describe('#verifySignature', () => {
+        it('should return undefined', () => {
+            const zeroEx = new ZeroEx();
+            expect(zeroEx.verifySignature()).to.be.undefined;
+        });
+    });
+});
