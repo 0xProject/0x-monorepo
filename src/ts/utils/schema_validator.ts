fa559377c80a75ba4169cab0825e5808154ae72b
Fabio Berger <me@fabioberger.com>
2017-05-25 12:06:38 +0200

    Improve ECSignature schema to check signature parameters and that v is 27 or 28

--- src/ts/utils/schema_validator.ts
+++ src/ts/utils/schema_validator.ts
@@ -1,10 +1,11 @@
 import {Validator as V, ValidatorResult} from 'jsonschema';
-import {ECSignatureSchema} from '../schemas/ec_signature_schema';
+import {ECSignatureSchema, ECSignatureParameter} from '../schemas/ec_signature_schema';
 
 export class SchemaValidator {
     private v: V;
     constructor() {
         this.v = new V();
+        this.v.addSchema(ECSignatureParameter, ECSignatureParameter.id);
         this.v.addSchema(ECSignatureSchema, ECSignatureSchema.id);
     }
     public validate(instance: object, schema: Schema): ValidatorResult {
