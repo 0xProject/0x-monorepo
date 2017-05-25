a5550a0b585dfb80176082d57ca0bf3ca1e4d1f9
Fabio Berger <me@fabioberger.com>
2017-05-25 12:09:50 +0200

    rename v to validator for clarity

--- src/ts/utils/schema_validator.ts
+++ src/ts/utils/schema_validator.ts
@@ -1,14 +1,14 @@
-import {Validator as V, ValidatorResult} from 'jsonschema';
+import {Validator, ValidatorResult} from 'jsonschema';
 import {ECSignatureSchema, ECSignatureParameter} from '../schemas/ec_signature_schema';
 
 export class SchemaValidator {
-    private v: V;
+    private validator: Validator;
     constructor() {
-        this.v = new V();
-        this.v.addSchema(ECSignatureParameter, ECSignatureParameter.id);
-        this.v.addSchema(ECSignatureSchema, ECSignatureSchema.id);
+        this.validator = new Validator();
+        this.validator.addSchema(ECSignatureParameter, ECSignatureParameter.id);
+        this.validator.addSchema(ECSignatureSchema, ECSignatureSchema.id);
     }
     public validate(instance: object, schema: Schema): ValidatorResult {
-        return this.v.validate(instance, schema);
+        return this.validator.validate(instance, schema);
     }
 }
