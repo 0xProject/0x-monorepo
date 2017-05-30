cf63e1d1a6606c1b3ab82d5fec56df8ac4128eea
Leonid Logvinov <logvinov.leon@gmail.com>
2017-05-30 12:16:33 +0200

    Change BigNumber schema to Number schema

--- src/utils/schema_validator.ts
+++ src/utils/schema_validator.ts
@@ -1,14 +1,14 @@
 import {Validator, ValidatorResult} from 'jsonschema';
 import {ecSignatureSchema, ecSignatureParameter} from '../schemas/ec_signature_schema';
-import {addressSchema, bigNumberSchema, orderSchema, signedOrderSchema} from '../schemas/signed_order_schema';
+import {addressSchema, numberSchema, orderSchema, signedOrderSchema} from '../schemas/signed_order_schema';
 
 export class SchemaValidator {
     private validator: Validator;
     constructor() {
         this.validator = new Validator();
         this.validator.addSchema(orderSchema, orderSchema.id);
+        this.validator.addSchema(numberSchema, numberSchema.id);
         this.validator.addSchema(addressSchema, addressSchema.id);
-        this.validator.addSchema(bigNumberSchema, bigNumberSchema.id);
         this.validator.addSchema(ecSignatureSchema, ecSignatureSchema.id);
         this.validator.addSchema(signedOrderSchema, signedOrderSchema.id);
         this.validator.addSchema(ecSignatureParameter, ecSignatureParameter.id);
