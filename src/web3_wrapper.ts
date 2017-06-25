a2c495a9830170173e5024b2e8e44e99cc319d06
Fabio Berger <me@fabioberger.com>
2017-06-25 14:48:48 -0700

    Re-wrap BigNumber returned from web3 in `getBalanceInEthAsync`

--- src/web3_wrapper.ts
+++ src/web3_wrapper.ts
@@ -36,7 +36,8 @@ export class Web3Wrapper {
     }
     public async getBalanceInEthAsync(owner: string): Promise<BigNumber.BigNumber> {
         const balanceInWei = await promisify(this.web3.eth.getBalance)(owner);
-        const balanceEth = this.web3.fromWei(balanceInWei, 'ether');
+        let balanceEth = this.web3.fromWei(balanceInWei, 'ether');
+        balanceEth = new BigNumber(balanceEth);
         return balanceEth;
     }
     public async doesContractExistAtAddressAsync(address: string): Promise<boolean> {
