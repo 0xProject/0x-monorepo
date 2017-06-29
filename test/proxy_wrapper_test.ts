1321ea5beb530ca301ff4c7408ada66c44759b12
Leonid Logvinov <logvinov.leon@gmail.com>
2017-06-29 13:15:20 -0700

    Remove unused code from test/proxy_wrapper_test.ts

--- test/proxy_wrapper_test.ts
+++ test/proxy_wrapper_test.ts
@@ -1,29 +1,17 @@
-import * as _ from 'lodash';
-import 'mocha';
 import * as chai from 'chai';
 import {chaiSetup} from './utils/chai_setup';
 import {web3Factory} from './utils/web3_factory';
 import {ZeroEx} from '../src';
-import {BlockchainLifecycle} from './utils/blockchain_lifecycle';
-import {SchemaValidator} from '../src/utils/schema_validator';
-import {tokenSchema} from '../src/schemas/token_schema';
 import {ProxyWrapper} from '../src/contract_wrappers/proxy_wrapper';
 
 chaiSetup.configure();
 const expect = chai.expect;
-const blockchainLifecycle = new BlockchainLifecycle();
 
 describe('ProxyWrapper', () => {
     let zeroEx: ZeroEx;
     before(async () => {
         const web3 = web3Factory.create();
         zeroEx = new ZeroEx(web3.currentProvider);
-    });
-    beforeEach(async () => {
-        await blockchainLifecycle.startAsync();
-    });
-    afterEach(async () => {
-        await blockchainLifecycle.revertAsync();
     });
     describe('#isAuthorizedAsync', () => {
         it('should return false if the address is not authorized', async () => {
