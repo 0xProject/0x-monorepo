bc92968c5eb5714380b217f2a530f1c86130cae7
Leonid Logvinov <logvinov.leon@gmail.com>
2017-06-29 11:21:04 -0700

    Rename Exchange.json to Exchange_v1.json

--- src/exchange_artifacts_by_name.ts
+++ src/exchange_artifacts_by_name.ts
@@ -1,5 +1,5 @@
 import {ContractArtifact} from './types';
-import * as Exchange_v1 from './artifacts/exchange/Exchange.json';
+import * as Exchange_v1 from './artifacts/exchange/Exchange_v1.json';
 
 export const ExchangeArtifactsByName = {
     Exchange_v1: Exchange_v1 as any as ContractArtifact,
