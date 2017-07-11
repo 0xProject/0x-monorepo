2787bdc46b1670fb0444578b62ae85e6aa626264
Leonid Logvinov <logvinov.leon@gmail.com>
2017-07-11 11:33:47 -0700

    Remove exchange artifacts by name

--- src/exchange_artifacts_by_name.ts
+++
@@ -1,6 +0,0 @@
-import {ContractArtifact} from './types';
-import * as Exchange_v1 from './artifacts/exchange/Exchange_v1.json';
-
-export const ExchangeArtifactsByName = {
-    Exchange_v1: Exchange_v1 as any as ContractArtifact,
-};
