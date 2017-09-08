be0b9a1d7c9428fc35b3fdf3cd2a730e9bd5a606
Leonid Logvinov <logvinov.leon@gmail.com>
2017-09-08 14:02:03 +0200

    Add a comment for MethodOpts

--- src/types.ts
+++ src/types.ts
@@ -421,6 +421,12 @@ export interface Artifact {
     }};
 }
 
+/*
+ * defaultBlock: The block up to which to query the blockchain state. Setting this to a historical block number
+ * let's the user query the blockchain's state at an arbitrary point in time. In order for this to work, the
+ * backing  Ethereum node must keep the entire historical state of the chain (e.g setting `--pruning=archive`
+ * flag when  running Parity).
+ */
 export interface MethodOpts {
     defaultBlock?: Web3.BlockParam;
 }
