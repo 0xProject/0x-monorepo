1e5cc3b0e53aeb606140d0f735652aa464c2a4e7
Brandon Millman <brandon.millman@gmail.com>
2017-11-15 18:17:41 -0500

    experiment with calling typedoc programatically

--- packages/0x.js/scripts/postpublish.js
+++ packages/0x.js/scripts/postpublish.js
@@ -2,12 +2,10 @@ const execAsync = require('async-child-process').execAsync;
 const semverSort = require('semver-sort');
 const publishRelease = require('publish-release');
 const promisify = require('es6-promisify');
-const prompt = require('prompt');
+const typedoc = require('typedoc');
 
 const publishReleaseAsync = promisify(publishRelease);
-const promptGetAsync = promisify(prompt.get);
 const subPackageName = '0x.js';
-const subPackagePrefix = subPackageName + '@';
 const githubPersonalAccessToken = process.env.GITHUB_PERSONAL_ACCESS_TOKEN_0X_JS;
 let tag;
 let version;
@@ -33,7 +31,23 @@ getLatestTagAndVersionAsync(subPackageName)
     })
     .then(function(release) {
         console.log('POSTPUBLISH: Release successful, generating docs...');
-        return execAsync('typedoc --excludePrivate --excludeExternals --target ES5 --json ' + __dirname + '/../docs/index.json ' + __dirname + '/..');
+
+        // const rootDir = __dirname + '/../src/index.ts';
+        // const typedocApp = new typedoc.Application({
+        //     excludePrivate: true,
+        //     excludeExternals: true,
+        //     target: 'ES5',
+        // });
+
+        // console.log(typedocApp.options);
+        // typedocApp.options.setValue('excludePrivate', true);
+        // typedocApp.options.setValue('excludeExternals', true);
+        // typedocApp.options.setValue('json', true);
+        // typedocApp.options.setValue('target', 'ES5');
+
+
+        // return typedocApp.generateDocs([rootDir], __dirname + '/../docs/index.json');
+        return execAsync('yarn typedoc --excludePrivate --excludeExternals --target ES5 --json ' + __dirname + '/../docs/index.json ' + __dirname + '/..');
     })
     .then(function(result) {
         if (result.stderr !== '') {
@@ -42,6 +56,8 @@ getLatestTagAndVersionAsync(subPackageName)
         console.log('POSTPUBLISH: Doc generation successful, uploading docs...');
         const s3Url = 's3://0xjs-docs-jsons/v' + version +'.json';
         return execAsync('aws s3 cp ' + __dirname + '/../docs/index.json ' + s3Url + ' --profile 0xproject --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --content-type aplication/json');
+    }).catch (function(error) {
+        throw error;
     });
 
 function getLatestTagAndVersionAsync(subPackageName) {
