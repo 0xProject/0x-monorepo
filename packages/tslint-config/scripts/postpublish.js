5015f2d7d75974a5ce950e8f05d6a5ab481c488a
Fabio Berger <me@fabioberger.com>
2017-11-17 14:10:18 -0600

    Add postpublish scripts for all the subpackages so that they each publish a release to github

---
+++ packages/tslint-config/scripts/postpublish.js
@@ -0,0 +1,13 @@
+const postpublish_utils = require('../../../scripts/postpublish_utils');
+
+const subPackageName = '0xproject/tslint-config';
+
+postpublish_utils.getLatestTagAndVersionAsync(subPackageName)
+    .then(function(result) {
+        const releaseName = subPackageName + ' v' + result.version;
+        const assets = [];
+        return postpublish_utils.publishReleaseNotes(result.tag, releaseName, assets);
+    })
+    .catch (function(err) {
+        throw err;
+    });
