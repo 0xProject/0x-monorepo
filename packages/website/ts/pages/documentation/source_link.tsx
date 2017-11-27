e7b523074ab124f2d4e790d71aac9df7e1e2c4cf
Fabio Berger <me@fabioberger.com>
2017-11-27 12:24:11 -0600

    remove comment

--- packages/website/ts/pages/documentation/source_link.tsx
+++ packages/website/ts/pages/documentation/source_link.tsx
@@ -13,7 +13,6 @@ const SUB_PKG = '0x.js';
 export function SourceLink(props: SourceLinkProps) {
     const src = props.source;
     const url = constants.GITHUB_0X_JS_URL;
-    // https://github.com/0xProject/0x.js/blob/0x.js%400.26.1/packages/0x.js/src/contract_wrappers/exchange_wrapper.ts
     const sourceCodeUrl = `${url}/blob/${SUB_PKG}%40${props.version}/packages/${SUB_PKG}/${src.fileName}#L${src.line}`;
     return (
         <div className="pt2" style={{fontSize: 14}}>
