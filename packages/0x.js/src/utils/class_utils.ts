865ee090c8c23376b018bf0d109912ac20eaafc4
Leonid Logvinov <logvinov.leon@gmail.com>
2017-12-01 22:44:06 -0600

    Add class utils

---
+++ packages/0x.js/src/utils/class_utils.ts
@@ -0,0 +1,17 @@
+import * as _ from 'lodash';
+
+export const classUtils = {
+    bindAll(self: any, exclude: string[] = ['contructor'], thisArg?: any): void {
+        for (const key of Object.getOwnPropertyNames(self)) {
+            const val = self[key];
+            if (!_.includes(exclude, key)) {
+                if (_.isFunction(val)) {
+                    self[key] = val.bind(thisArg || self);
+                } else if (_.isObject(val)) {
+                    classUtils.bindAll(val, exclude, self);
+                }
+            }
+        }
+        return self;
+    },
+};
