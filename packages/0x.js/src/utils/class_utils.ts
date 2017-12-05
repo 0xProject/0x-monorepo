c0cf47138ec2efe4c0a27de9a503bb0d74d99d5f
Leonid Logvinov <logvinov.leon@gmail.com>
2017-12-05 16:59:36 +0300

    Add a comment

--- packages/0x.js/src/utils/class_utils.ts
+++ packages/0x.js/src/utils/class_utils.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 
 export const classUtils = {
+    // This is usefull for classes that have nested methods. Nested methods don't get binded out of the box.
     bindAll(self: any, exclude: string[] = ['contructor'], thisArg?: any): void {
         for (const key of Object.getOwnPropertyNames(self)) {
             const val = self[key];
