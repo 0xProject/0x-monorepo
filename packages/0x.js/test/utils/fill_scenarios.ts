28c409fc6d635ee8ed1430d97d9bbdde2f613e37
Leonid Logvinov <logvinov.leon@gmail.com>
2017-12-14 15:31:37 +0100

    Add a function to init token balances

--- packages/0x.js/test/utils/fill_scenarios.ts
+++ packages/0x.js/test/utils/fill_scenarios.ts
@@ -1,9 +1,14 @@
+import {Web3Wrapper} from '@0xproject/web3-wrapper';
 import BigNumber from 'bignumber.js';
 
 import {SignedOrder, Token, ZeroEx} from '../../src';
+import {artifacts} from '../../src/artifacts';
+import {DummyTokenContract} from '../../src/contract_wrappers/generated/dummy_token';
 import {orderFactory} from '../utils/order_factory';
 
 import {constants} from './constants';
+
+const INITIAL_COINBASE_TOKEN_SUPPLY_IN_UNITS = new BigNumber(100);
 
 export class FillScenarios {
     private zeroEx: ZeroEx;
@@ -20,6 +25,26 @@ export class FillScenarios {
         this.coinbase = userAddresses[0];
         this.zrxTokenAddress = zrxTokenAddress;
         this.exchangeContractAddress = exchangeContractAddress;
+    }
+    public async initTokenBalancesAsync() {
+        const web3Wrapper = (this.zeroEx as any)._web3Wrapper as Web3Wrapper;
+        for (const token of this.tokens) {
+            if (token.symbol === 'WETH') {
+                // _.noop
+            } else if (token.symbol !== 'ZRX') {
+                const contractInstance = web3Wrapper.getContractInstance(
+                    artifacts.DummyTokenArtifact.abi, token.address,
+                );
+                const dummyToken = new DummyTokenContract(contractInstance, {});
+                const tokenSupply = ZeroEx.toBaseUnitAmount(INITIAL_COINBASE_TOKEN_SUPPLY_IN_UNITS, token.decimals);
+                const txHash = await dummyToken.setBalance.sendTransactionAsync(this.coinbase, tokenSupply, {
+                    from: this.coinbase,
+                });
+                await this.zeroEx.awaitTransactionMinedAsync(txHash);
+            } else {
+                // no-op
+            }
+        }
     }
     public async createFillableSignedOrderAsync(makerTokenAddress: string, takerTokenAddress: string,
                                                 makerAddress: string, takerAddress: string,
