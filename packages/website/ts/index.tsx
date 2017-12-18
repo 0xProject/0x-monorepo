f698dd40772eee0b3142028f62b6c56dcfcc5a1e
Fabio Berger <me@fabioberger.com>
2017-12-17 20:55:50 -0500

    Use bigNumberConfigs from @0xproject/utils in website repo

--- packages/website/ts/index.tsx
+++ packages/website/ts/index.tsx
@@ -1,6 +1,7 @@
 // Polyfills
 import 'whatwg-fetch';
 
+import {bigNumberConfigs} from '@0xproject/utils';
 import BigNumber from 'bignumber.js';
 import {colors, getMuiTheme, MuiThemeProvider} from 'material-ui/styles';
 import * as React from 'react';
@@ -22,11 +23,7 @@ import {WebsitePaths} from 'ts/types';
 import {constants} from 'ts/utils/constants';
 injectTapEventPlugin();
 
-// By default BigNumber's `toString` method converts to exponential notation if the value has
-// more then 20 digits. We want to avoid this behavior, so we set EXPONENTIAL_AT to a high number
-BigNumber.config({
-    EXPONENTIAL_AT: 1000,
-});
+bigNumberConfigs.configure();
 
 // Check if we've introduced an update that requires us to clear the tradeHistory local storage entries
 tradeHistoryStorage.clearIfRequired();
