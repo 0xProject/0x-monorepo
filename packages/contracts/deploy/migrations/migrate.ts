e2ef0c5b9419fa1536a1b7e478f7950e69e8ff0e
Leonid Logvinov <logvinov.leon@gmail.com>
2018-01-04 15:12:32 +0100

    Fix linter errors

--- packages/contracts/deploy/migrations/migrate.ts
+++ packages/contracts/deploy/migrations/migrate.ts
@@ -1,5 +1,5 @@
-import { Web3Wrapper } from '@0xproject/web3-wrapper';
 import { BigNumber } from '@0xproject/utils';
+import { Web3Wrapper } from '@0xproject/web3-wrapper';
 import * as _ from 'lodash';
 
 import { Deployer } from './../src/deployer';
