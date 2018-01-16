7040a01cf2d154e16ab97fe0bfcb9297962eb77a
Leonid Logvinov <logvinov.leon@gmail.com>
2018-01-16 14:01:46 +0100

    Remove umd tests

--- packages/0x.js/scripts/test_umd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This script runs umd tests and cleans up after them while preserving the `return_code` for CI
-# UMD tests should only be run after building the commonjs because they reuse some of the commonjs build artifacts
-run-s substitute_umd_bundle run_mocha
-return_code=$?
-exit $return_code
