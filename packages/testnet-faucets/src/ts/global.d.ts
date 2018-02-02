48aa1ad57d91acba93c445c14177cb9e318974d8
Jacob Evans <jacob@dekz.net>
2018-02-01 18:43:32 -0800

    Remove double declaration

--- packages/testnet-faucets/src/ts/global.d.ts
+++ packages/testnet-faucets/src/ts/global.d.ts
@@ -1,8 +1,4 @@
 declare module 'rollbar';
-declare module 'web3-provider-engine';
-declare module 'web3-provider-engine/subproviders/rpc';
-declare module 'web3-provider-engine/subproviders/nonce-tracker';
-declare module 'web3-provider-engine/subproviders/hooked-wallet';
 
 declare module '*.json' {
     const json: any;
@@ -36,3 +32,7 @@ declare module 'web3-provider-engine' {
     }
     export = Web3ProviderEngine;
 }
+
+declare module 'web3-provider-engine/subproviders/rpc';
+declare module 'web3-provider-engine/subproviders/nonce-tracker';
+declare module 'web3-provider-engine/subproviders/hooked-wallet';
