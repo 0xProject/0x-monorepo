2eccb28dee6ad16f8b4a73102d36463067be8262
Jacob Evans <jacob@dekz.net>
2018-02-01 17:08:19 -0800

    Enable CIRCLECI and declare web3

--- packages/testnet-faucets/src/ts/global.d.ts
+++ packages/testnet-faucets/src/ts/global.d.ts
@@ -24,3 +24,15 @@ declare module 'ethereumjs-tx' {
     }
     export = EthereumTx;
 }
+
+declare module 'web3-provider-engine' {
+    class Web3ProviderEngine {
+        public on(event: string, handler: () => void): void;
+        public send(payload: any): void;
+        public sendAsync(payload: any, callback: (error: any, response: any) => void): void;
+        public addProvider(provider: any): void;
+        public start(): void;
+        public stop(): void;
+    }
+    export = Web3ProviderEngine;
+}
