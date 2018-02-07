97cb2025d20a563c7746a0bf2982842e2d42bd23
Remco Bloemen <remco@wicked.ventures>
2018-02-07 13:40:58 -0800

    Copy Exchange and deps to previous as Exchange_v1

---
+++ packages/contracts/src/contracts/current/tokens/Token/IToken.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.18;
+
+contract IToken {
+
+    /// @notice send `value` token to `to` from `msg.sender`
+    /// @param to The address of the recipient
+    /// @param value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address to, uint value)
+        public
+        returns (bool);
+
+    /// @notice send `value` token to `to` from `from` on the condition it is approved by `from`
+    /// @param from The address of the sender
+    /// @param to The address of the recipient
+    /// @param value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address from, address to, uint value)
+        public
+        returns (bool);
+    
+    /// @notice `msg.sender` approves `addr` to spend `value` tokens
+    /// @param spender The address of the account able to transfer the tokens
+    /// @param value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address spender, uint value)
+        public
+        returns (bool);
+
+    /// @param owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address owner)
+        public view
+        returns (uint);
+
+    /// @param owner The address of the account owning tokens
+    /// @param spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address owner, address spender)
+        public view
+        returns (uint);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint value);
+    
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint value);
+}
