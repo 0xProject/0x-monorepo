1ad31ab007c82e9538793b6a331e435344285ef8
Amir Bandeali <abandeali1@gmail.com>
2018-02-08 10:57:33 -0800

    Add fillOrderNoThrow and wrapper functions

--- packages/contracts/src/contracts/current/protocol/Exchange/LibPartialAmount.sol
+++ packages/contracts/src/contracts/current/protocol/Exchange/LibPartialAmount.sol
@@ -27,11 +27,12 @@ contract LibPartialAmount is SafeMath {
     /// @param denominator Denominator.
     /// @param target Value to calculate partial of.
     /// @return Partial value of target.
-    function getPartialAmount(uint numerator, uint denominator, uint target)
+    function getPartialAmount(uint256 numerator, uint256 denominator, uint256 target)
         public pure
-        returns (uint)
+        returns (uint256 partialAmount)
     {
-        return safeDiv(safeMul(numerator, target), denominator);
+        partialAmount = safeDiv(safeMul(numerator, target), denominator);
+        return partialAmount;
     }
 }
 
