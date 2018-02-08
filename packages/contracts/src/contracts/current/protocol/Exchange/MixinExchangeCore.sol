e0786f2ca0d154c370bf70c8a5589e0e8d0836e0
Amir Bandeali <abandeali1@gmail.com>
2018-02-08 12:09:07 -0800

    Run prettier

--- packages/contracts/src/contracts/current/protocol/Exchange/MixinExchangeCore.sol
+++ packages/contracts/src/contracts/current/protocol/Exchange/MixinExchangeCore.sol
@@ -235,7 +235,7 @@ contract MixinExchangeCore is
         return isError;
     }
 
-   /// @dev Calculates the sum of values already filled and cancelled for a given order.
+    /// @dev Calculates the sum of values already filled and cancelled for a given order.
     /// @param orderHash The Keccak-256 hash of the given order.
     /// @return Sum of values already filled and cancelled.
     function getUnavailableTakerTokenAmount(bytes32 orderHash)
