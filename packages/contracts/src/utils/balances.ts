7201a74aeaa6b646e97d7c9c2c4cc979ace5f4f7
Amir Bandeali <abandeali1@gmail.com>
2018-02-08 17:09:34 -0800

    Move utils dir into src

--- packages/contracts/src/utils/balances.ts
+++ packages/contracts/src/utils/balances.ts
@@ -2,7 +2,7 @@ import { BigNumber } from '@0xproject/utils';
 import * as _ from 'lodash';
 import * as Web3 from 'web3';
 
-import { DummyTokenContract } from '../src/contract_wrappers/generated/dummy_token';
+import { DummyTokenContract } from '../contract_wrappers/generated/dummy_token';
 
 import { BalancesByOwner } from './types';
 
