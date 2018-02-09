b53a1b51d6b7e6ec768b974f656329599585bee6
Leonid Logvinov <logvinov.leon@gmail.com>
2018-02-09 10:47:41 +0100

    Add type cast

--- packages/contracts/util/constants.ts
+++ packages/contracts/util/constants.ts
@@ -2,7 +2,7 @@ export const constants = {
     NULL_BYTES: '0x',
     INVALID_OPCODE: 'invalid opcode',
     REVERT: 'revert',
-    TESTRPC_NETWORK_ID: 50,
+    TESTRPC_NETWORK_ID: 50 as 50,
     MAX_ETHERTOKEN_WITHDRAW_GAS: 43000,
     MAX_TOKEN_TRANSFERFROM_GAS: 80000,
     MAX_TOKEN_APPROVE_GAS: 60000,
