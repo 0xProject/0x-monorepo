49a4f0c74c5578abf4b64ea2ed36f4821af10f9f
Fabio Berger <me@fabioberger.com>
2018-03-03 21:33:44 +0100

    Fix styling

--- scripts/postpublish_utils.js
+++ scripts/postpublish_utils.js
@@ -11,23 +11,22 @@ module.exports = {
     getLatestTagAndVersionAsync: function(subPackageName) {
         const subPackagePrefix = subPackageName + '@';
         const gitTagsCommand = 'git tag -l "' + subPackagePrefix + '*"';
-        return execAsync(gitTagsCommand)
-            .then(function(result) {
-                if (result.stderr !== '') {
-                    throw new Error(result.stderr);
-                }
-                const tags = result.stdout.trim().split('\n');
-                const versions = tags.map(function(tag) {
-                    return tag.slice(subPackagePrefix.length);
-                });
-                const sortedVersions = semverSort.desc(versions);
-                const latestVersion = sortedVersions[0];
-                const latestTag = subPackagePrefix + latestVersion;
-                return {
-                    tag: latestTag,
-                    version: latestVersion
-                };
+        return execAsync(gitTagsCommand).then(function(result) {
+            if (result.stderr !== '') {
+                throw new Error(result.stderr);
+            }
+            const tags = result.stdout.trim().split('\n');
+            const versions = tags.map(function(tag) {
+                return tag.slice(subPackagePrefix.length);
             });
+            const sortedVersions = semverSort.desc(versions);
+            const latestVersion = sortedVersions[0];
+            const latestTag = subPackagePrefix + latestVersion;
+            return {
+                tag: latestTag,
+                version: latestVersion,
+            };
+        });
     },
     publishReleaseNotesAsync: function(tag, releaseName, assets) {
         console.log('POSTPUBLISH: Releasing ', releaseName, '...');
@@ -43,7 +42,7 @@ module.exports = {
             reuseRelease: true,
             reuseDraftOnly: false,
             assets: assets,
-         });
+        });
     },
     getReleaseName(subPackageName, version) {
         const releaseName = subPackageName + ' v' + version;
@@ -51,11 +50,13 @@ module.exports = {
     },
     standardPostPublishAsync: function(subPackageName) {
         return this.getLatestTagAndVersionAsync(subPackageName)
-            .then(function(result) {
-                const releaseName = this.getReleaseName(subPackageName, result.version);
-                const assets = [];
-                return this.publishReleaseNotesAsync(result.tag, releaseName, assets);
-            }.bind(this))
+            .then(
+                function(result) {
+                    const releaseName = this.getReleaseName(subPackageName, result.version);
+                    const assets = [];
+                    return this.publishReleaseNotesAsync(result.tag, releaseName, assets);
+                }.bind(this)
+            )
             .catch(function(err) {
                 throw err;
             });
