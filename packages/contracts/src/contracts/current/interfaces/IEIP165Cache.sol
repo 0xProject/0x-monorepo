aa3461a05570e6cb410d931a89579e0725d03474
Remco Bloemen <remco@wicked.ventures>
2018-03-05 17:32:11 -0800

    Add documentation

--- packages/contracts/src/contracts/current/interfaces/IEIP165Cache.sol
+++ packages/contracts/src/contracts/current/interfaces/IEIP165Cache.sol
@@ -20,10 +20,18 @@ contract IEIP165Cache is IEIP165 {
         Supported
     }
     
+    /// When EIP165 support is queried, the result will never be Unknown.
+    /// If EIP165 is supported, no result will ever be Unknown.
+    /// If EIP165 is unsupported, other results may be unknown (but may
+    /// be manually set). 
+    /// @returns Status.Unknown if support could not be determined
+    /// @returns Status.Unsupported if 
     function query(address addr, bytes4 interfaceId)
         public
         returns (Status);
     
+    /// Batch query multiple interfaces. Corresponding output
+    /// bit is set to one iff it has Status.Supported.
     function query(address addr, bytes4[] interfaceIds)
         public
         returns (uint256 r);
