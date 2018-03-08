6196b7166b639d69fc8105850706237b2a345bab
Amir Bandeali <abandeali1@gmail.com>
2018-03-07 16:06:38 -0800

    Add script for starting testrpc with preconfigured private keys

---
+++ packages/contracts/scripts/testrpc.ts
@@ -0,0 +1,43 @@
+import * as testrpc from 'ganache-cli';
+import * as _ from 'lodash';
+
+import { constants } from '../src/utils/constants';
+import { utils } from '../src/utils/utils';
+
+const opts = {
+    accounts: constants.TESTRPC_ACCOUNTS,
+};
+
+const server = testrpc.server(opts);
+
+server.listen(constants.TESTRPC_PORT, (err: any, result: any) => {
+    if (err) {
+        utils.consoleLog(err);
+        return;
+    }
+
+    const state = result ? result : server.provider.manager.state;
+
+    utils.consoleLog('');
+    utils.consoleLog('Available Accounts');
+    utils.consoleLog('==================');
+
+    const accounts = state.accounts;
+    const addresses = _.keys(accounts);
+
+    _.forEach(addresses, (address, index) => {
+        const line = `(${index}) ${address}`;
+        utils.consoleLog(line);
+    });
+
+    utils.consoleLog('');
+    utils.consoleLog('Private Keys');
+    utils.consoleLog('==================');
+
+    _.forEach(addresses, (address, index) => {
+        utils.consoleLog(`(${index}) ${accounts[address].secretKey.toString('hex')}`);
+    });
+
+    utils.consoleLog('');
+    utils.consoleLog('Listening on localhost:' + constants.TESTRPC_PORT);
+});
---
+++ packages/contracts/src/utils/utils.ts
@@ -0,0 +1,7 @@
+export const utils = {
+    consoleLog(message: string): void {
+        /* tslint:disable */
+        console.log(message);
+        /* tslint:enable */
+    },
+};
