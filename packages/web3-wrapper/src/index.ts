7b82a8669d7a7c69d879349a083424d5dcdf79ec
Fabio Berger <me@fabioberger.com>
2018-03-09 15:07:39 +0100

    Fix comment

--- packages/web3-wrapper/src/index.ts
+++ packages/web3-wrapper/src/index.ts
@@ -11,7 +11,7 @@ export class Web3Wrapper {
         if (_.isUndefined((provider as any).sendAsync)) {
             // Web3@1.0 provider doesn't support synchronous http requests,
             // so it only has an async `send` method, instead of a `send` and `sendAsync` in web3@0.x.x`
-            // We re-assign the send method so that Web3@1.0 providers work with 0x.js
+            // We re-assign the send method so that Web3@1.0 providers work with @0xproject/web3-wrapper
             (provider as any).sendAsync = (provider as any).send;
         }
         this._web3 = new Web3();
