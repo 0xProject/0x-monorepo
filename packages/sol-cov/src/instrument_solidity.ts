62f45f7b41fdd984025ba70008c682eeded2fadb
Leonid Logvinov <logvinov.leon@gmail.com>
2018-03-09 14:36:19 +0100

    Cache code parsing

--- packages/sol-cov/src/instrument_solidity.ts
+++ packages/sol-cov/src/instrument_solidity.ts
@@ -1,3 +1,4 @@
+import * as ethUtil from 'ethereumjs-util';
 import * as fs from 'fs';
 import * as _ from 'lodash';
 import * as path from 'path';
@@ -6,11 +7,19 @@ import * as SolidityParser from 'solidity-parser-sc';
 import { ASTVisitor, CoverageEntriesDescription } from './ast_visitor';
 import { getLocationByOffset } from './source_maps';
 
+// Parsing source code for each transaction/code is slow and therefore we cache it
+const coverageEntriesBySourceHash: { [sourceHash: string]: CoverageEntriesDescription } = {};
+
 export const collectCoverageEntries = (contractSource: string, fileName: string) => {
-    const ast = SolidityParser.parse(contractSource);
-    const locationByOffset = getLocationByOffset(contractSource);
-    const astVisitor = new ASTVisitor(locationByOffset);
-    astVisitor.walkAST(ast);
-    const coverageEntries = astVisitor.getCollectedCoverageEntries();
-    return coverageEntries;
+    const time = Date.now();
+    const sourceHash = ethUtil.sha3(contractSource).toString('hex');
+    if (_.isUndefined(coverageEntriesBySourceHash[sourceHash])) {
+        const ast = SolidityParser.parse(contractSource);
+        const locationByOffset = getLocationByOffset(contractSource);
+        const astVisitor = new ASTVisitor(locationByOffset);
+        astVisitor.walkAST(ast);
+        coverageEntriesBySourceHash[sourceHash] = astVisitor.getCollectedCoverageEntries();
+    }
+    const coverageEntriesDescription = coverageEntriesBySourceHash[sourceHash];
+    return coverageEntriesDescription;
 };
