368870a2fc515a63f6485d500ca7e67cd0a5b80e
Leonid Logvinov <logvinov.leon@gmail.com>
2018-03-10 03:10:33 +0100

    Fix sources list generation in a compiler

--- packages/deployer/src/compiler.ts
+++ packages/deployer/src/compiler.ts
@@ -233,7 +233,7 @@ export class Compiler {
         const runtimeBytecode = `0x${compiled.contracts[contractIdentifier].runtimeBytecode}`;
         const sourceMap = compiled.contracts[contractIdentifier].srcmap;
         const sourceMapRuntime = compiled.contracts[contractIdentifier].srcmapRuntime;
-        const sources = compiled.sourceList;
+        const sources = _.keys(compiled.sources);
         const updated_at = Date.now();
         const contractNetworkData: ContractNetworkData = {
             solc_version: contractSpecificSourceData.solcVersion,
