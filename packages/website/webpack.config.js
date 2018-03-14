56eeea1e633b3d0b41b50df6dcd8f76d284d5c73
Fabio Berger <me@fabioberger.com>
2018-03-14 16:37:09 +0100

    Add fix to webpack for issue: https://github.com/trufflesuite/ganache-core/issues/84 and prettier the configs

--- packages/website/webpack.config.js
+++ packages/website/webpack.config.js
@@ -11,16 +11,17 @@ module.exports = {
     },
     devtool: 'source-map',
     resolve: {
-        modules: [
-            path.join(__dirname, '/ts'),
-            'node_modules',
-        ],
+        modules: [path.join(__dirname, '/ts'), 'node_modules'],
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.md'],
         alias: {
             ts: path.join(__dirname, '/ts'),
             less: path.join(__dirname, '/less'),
             md: path.join(__dirname, '/md'),
         },
+    },
+    // HACK: Temporary fix for this issue: https://github.com/trufflesuite/ganache-core/issues/84
+    node: {
+        fs: 'empty',
     },
     module: {
         rules: [
@@ -62,25 +63,28 @@ module.exports = {
                     from: /^\/docs\/.*$/,
                     to: function() {
                         return 'index.html';
-                    }
-                }
-            ]
+                    },
+                },
+            ],
         },
         disableHostCheck: true,
     },
-    plugins: process.env.NODE_ENV === 'production' ? [
-        // Since we do not use moment's locale feature, we exclude them from the bundle.
-        // This reduces the bundle size by 0.4MB.
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: {
-                except: ['BigNumber']
-            }
-        })
-    ] : [],
+    plugins:
+        process.env.NODE_ENV === 'production'
+            ? [
+                  // Since we do not use moment's locale feature, we exclude them from the bundle.
+                  // This reduces the bundle size by 0.4MB.
+                  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+                  new webpack.DefinePlugin({
+                      'process.env': {
+                          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                      },
+                  }),
+                  new webpack.optimize.UglifyJsPlugin({
+                      mangle: {
+                          except: ['BigNumber'],
+                      },
+                  }),
+              ]
+            : [],
 };
