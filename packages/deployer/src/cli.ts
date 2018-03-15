02ede26893d5801c827f68d4ffb24c216b873a09
Leonid Logvinov <logvinov.leon@gmail.com>
2018-03-15 16:22:42 +0100

    Rename onDeployCommand to onDeployCommandAsync and onCompileCommand to onCompileCommandAsync

--- packages/deployer/src/cli.ts
+++ packages/deployer/src/cli.ts
@@ -24,7 +24,7 @@ const DEFAULT_CONTRACTS_LIST = '*';
  * Compiles all contracts with options passed in through CLI.
  * @param argv Instance of process.argv provided by yargs.
  */
-async function onCompileCommand(argv: CliOptions): Promise<void> {
+async function onCompileCommandAsync(argv: CliOptions): Promise<void> {
     const opts: CompilerOptions = {
         contractsDir: argv.contractsDir,
         networkId: argv.networkId,
@@ -38,7 +38,7 @@ async function onCompileCommand(argv: CliOptions): Promise<void> {
  * Deploys a single contract with provided name and args.
  * @param argv Instance of process.argv provided by yargs.
  */
-async function onDeployCommand(argv: CliOptions): Promise<void> {
+async function onDeployCommandAsync(argv: CliOptions): Promise<void> {
     const url = argv.jsonrpcUrl;
     const web3Provider = new Web3.providers.HttpProvider(url);
     const web3Wrapper = new Web3Wrapper(web3Provider);
@@ -142,7 +142,12 @@ function deployCommandBuilder(yargsInstance: any) {
             default: DEFAULT_CONTRACTS_LIST,
             description: 'comma separated list of contracts to compile',
         })
-        .command('compile', 'compile contracts', identityCommandBuilder, onCompileCommand)
-        .command('deploy', 'deploy a single contract with provided arguments', deployCommandBuilder, onDeployCommand)
+        .command('compile', 'compile contracts', identityCommandBuilder, onCompileCommandAsync)
+        .command(
+            'deploy',
+            'deploy a single contract with provided arguments',
+            deployCommandBuilder,
+            onDeployCommandAsync,
+        )
         .help().argv;
 })();
