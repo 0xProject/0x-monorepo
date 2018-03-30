9dc7f1e30c776efe013d50d75a562794d11187e1
Remco Bloemen <remco@wicked.ventures>
2018-03-30 15:33:01 -0700

    Pretty print BigNumbers in console

--- packages/contracts/test/utils/chai_setup.ts
+++ packages/contracts/test/utils/chai_setup.ts
@@ -2,6 +2,7 @@ import * as chai from 'chai';
 import chaiAsPromised = require('chai-as-promised');
 import ChaiBigNumber = require('chai-bignumber');
 import * as dirtyChai from 'dirty-chai';
+import { BigNumber } from 'bignumber.js';
 
 export const chaiSetup = {
     configure() {
@@ -9,5 +10,12 @@ export const chaiSetup = {
         chai.use(ChaiBigNumber());
         chai.use(dirtyChai);
         chai.use(chaiAsPromised);
+        
+        // Node uses '.inspect()' instead of '.toString()' for log messages
+        // HACK: Typescript won't allow me to mess with BigNumber.prototype
+        // directly, so I create an instance and then get the prototype.
+        Object.getPrototypeOf(new BigNumber(0)).inspect = function() {
+           return this.toString();
+        }
     },
 };
