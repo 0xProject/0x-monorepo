b08c616713e153ca767d2f01976da5f1ef903251
Jacob Evans <jacob@dekz.net>
2018-04-11 14:53:58 +1000

    Move type declaration for hdkey to typescript-typings

--- packages/subproviders/src/globals.d.ts
+++ packages/subproviders/src/globals.d.ts
@@ -51,19 +51,6 @@ declare module '@ledgerhq/hw-transport-node-hid' {
     }
 }
 
-// hdkey declarations
-declare module 'hdkey' {
-    class HDNode {
-        public static fromMasterSeed(seed: Buffer): HDNode;
-        public publicKey: Buffer;
-        public privateKey: Buffer;
-        public chainCode: Buffer;
-        public constructor();
-        public derive(path: string): HDNode;
-    }
-    export = HDNode;
-}
-
 declare module '*.json' {
     const json: any;
     /* tslint:disable */
