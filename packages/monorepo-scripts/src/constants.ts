7dd3b2d38b4e4ee2f1daa24c90f503b2e7ad0422
Fabio Berger <me@fabioberger.com>
2018-04-19 11:40:22 +0900

    Add removeGitTags script that can be run after a failed Lerna publish

--- packages/monorepo-scripts/src/constants.ts
+++ packages/monorepo-scripts/src/constants.ts
@@ -3,4 +3,5 @@ import * as path from 'path';
 export const constants = {
     monorepoRootPath: path.join(__dirname, '../../..'),
     stagingWebsite: 'http://staging-0xproject.s3-website-us-east-1.amazonaws.com',
+    lernaExecutable: './node_modules/lerna/bin/lerna.js',
 };
