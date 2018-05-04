91fdd6fc292327c5f021d2718d7e9aa0dd1b39ca
Leonid Logvinov <logvinov.leon@gmail.com>
2018-05-04 19:07:02 +0200

    Make a negligible change to order-utils to publish a new version

--- packages/order-utils/src/index.ts
+++ packages/order-utils/src/index.ts
@@ -1,6 +1,6 @@
 export { getOrderHashHex, isValidOrderHash } from './order_hash';
 export { isValidSignature, signOrderHashAsync } from './signature_utils';
 export { orderFactory } from './order_factory';
-export { generatePseudoRandomSalt } from './salt';
 export { constants } from './constants';
+export { generatePseudoRandomSalt } from './salt';
 export { OrderError } from './types';
