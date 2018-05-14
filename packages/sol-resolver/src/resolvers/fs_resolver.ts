842f2ea5cc75c66397597a21202f4b8cf3082ed6
Leonid Logvinov <logvinov.leon@gmail.com>
2018-05-14 18:34:04 +0200

    Fix a bug in FS resolver causing it to try reading directories

--- packages/sol-resolver/src/resolvers/fs_resolver.ts
+++ packages/sol-resolver/src/resolvers/fs_resolver.ts
@@ -7,7 +7,7 @@ import { Resolver } from './resolver';
 export class FSResolver extends Resolver {
     // tslint:disable-next-line:prefer-function-over-method
     public resolveIfExists(importPath: string): ContractSource | undefined {
-        if (fs.existsSync(importPath)) {
+        if (fs.existsSync(importPath) && fs.lstatSync(importPath).isFile()) {
             const fileContent = fs.readFileSync(importPath).toString();
             return {
                 source: fileContent,
