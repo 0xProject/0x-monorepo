ca08dd9705c2aefb092b10926af359ed8fabac79
Brandon Millman <brandon.millman@gmail.com>
2018-05-15 00:52:51 -0700

    Add hover state to top tokens

--- packages/website/ts/components/relayer_index/relayer_top_tokens.tsx
+++ packages/website/ts/components/relayer_index/relayer_top_tokens.tsx
@@ -26,20 +26,53 @@ export const TopTokens: React.StatelessComponent<TopTokensProps> = (props: TopTo
             {_.map(props.tokens, (tokenInfo: WebsiteBackendTokenInfo, index: number) => {
                 const firstItemStyle = { ...styles.tokenLabel, ...styles.followingTokenLabel };
                 const style = index !== 0 ? firstItemStyle : styles.tokenLabel;
-                return (
-                    <a
-                        key={tokenInfo.address}
-                        href={tokenLinkFromToken(tokenInfo, props.networkId)}
-                        target="_blank"
-                        style={style}
-                    >
-                        {tokenInfo.symbol}
-                    </a>
-                );
+                return <TokenLink tokenInfo={tokenInfo} style={style} networkId={props.networkId} />;
             })}
         </div>
     );
 };
+
+interface TokenLinkProps {
+    tokenInfo: WebsiteBackendTokenInfo;
+    style: React.CSSProperties;
+    networkId: number;
+}
+interface TokenLinkState {
+    isHovering: boolean;
+}
+
+class TokenLink extends React.Component<TokenLinkProps, TokenLinkState> {
+    constructor(props: TokenLinkProps) {
+        super(props);
+        this.state = {
+            isHovering: false,
+        };
+    }
+    public render(): React.ReactNode {
+        const style = {
+            ...this.props.style,
+            cursor: 'pointer',
+            opacity: this.state.isHovering ? 0.5 : 1,
+        };
+        return (
+            <a
+                key={this.props.tokenInfo.address}
+                href={tokenLinkFromToken(this.props.tokenInfo, this.props.networkId)}
+                target="_blank"
+                style={style}
+                onMouseEnter={this._onToggleHover.bind(this, true)}
+                onMouseLeave={this._onToggleHover.bind(this, false)}
+            >
+                {this.props.tokenInfo.symbol}
+            </a>
+        );
+    }
+    private _onToggleHover(isHovering: boolean): void {
+        this.setState({
+            isHovering,
+        });
+    }
+}
 
 function tokenLinkFromToken(tokenInfo: WebsiteBackendTokenInfo, networkId: number): string {
     return sharedUtils.getEtherScanLinkIfExists(tokenInfo.address, networkId, EtherscanLinkSuffixes.Address);
