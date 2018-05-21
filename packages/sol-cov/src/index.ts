08b08ef1d0fbff2da8b64524704cf1bc50f77f30
Leonid Logvinov <logvinov.leon@gmail.com>
2018-05-21 16:56:00 -0700

    Match class names with file names

--- packages/sol-cov/src/index.ts
+++ packages/sol-cov/src/index.ts
@@ -1,5 +1,5 @@
 export { CoverageSubprovider } from './coverage_subprovider';
-export { SolCompilerArtifactAdapter } from './artifact_adapters/0x';
-export { TruffleArtifactAdapter } from './artifact_adapters/truffle';
-export { AbstractArtifactAdapter } from './artifact_adapters/abstract';
+export { SolCompilerArtifactAdapter } from './artifact_adapters/sol_compiler_artifact_adapter';
+export { TruffleArtifactAdapter } from './artifact_adapters/truffle_artifact_adapter';
+export { AbstractArtifactAdapter } from './artifact_adapters/abstract_artifact_adapter';
 export { ContractData } from './types';
