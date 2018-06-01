2004c0d7398a5e77d08e3b4d8030c0f22cb09cc8
Alex Browne <stephenalexbrowne@gmail.com>
2018-06-01 13:19:36 -0700

    Add ability to quickly switch between Geth and Ganache by changing a const

--- packages/contracts/src/utils/web3_wrapper.ts
+++ packages/contracts/src/utils/web3_wrapper.ts
@@ -5,14 +5,27 @@ import { Provider } from 'ethereum-types';
 
 import { coverage } from './coverage';
 
-export const txDefaults = {
+const useGeth = false;
+
+const ganacheTxDefaults = {
     from: devConstants.TESTRPC_FIRST_ADDRESS,
-    // gas: devConstants.GAS_LIMIT,
+    gas: devConstants.GAS_LIMIT,
 };
-const providerConfigs = {
+const gethTxDefaults = {
+    from: devConstants.TESTRPC_FIRST_ADDRESS,
+};
+export const txDefaults = useGeth ? gethTxDefaults : ganacheTxDefaults;
+
+const gethConfigs = {
     shouldUseInProcessGanache: false,
     rpcUrl: 'http://localhost:8501',
+    shouldUseFakeGasEstimate: false,
 };
+const ganacheConfigs = {
+    shouldUseInProcessGanache: true,
+};
+const providerConfigs = useGeth ? gethConfigs : ganacheConfigs;
+
 export const provider = web3Factory.getRpcProvider(providerConfigs);
 const isCoverageEnabled = env.parseBoolean(EnvVars.SolidityCoverage);
 if (isCoverageEnabled) {
