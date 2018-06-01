612fc4a949250beef2274df88d6e7a670812add6
Leonid Logvinov <logvinov.leon@gmail.com>
2018-06-01 15:46:16 -0700

    Fix cherry-pick

--- packages/monorepo-scripts/src/publish.ts
+++ packages/monorepo-scripts/src/publish.ts
@@ -175,9 +175,9 @@ async function updateChangeLogsAsync(updatedPublicLernaPackages: LernaPackage[])
         const packageName = lernaPackage.package.name;
         const changelogJSONPath = path.join(lernaPackage.location, 'CHANGELOG.json');
         const changelogJSON = utils.getChangelogJSONOrCreateIfMissing(changelogJSONPath);
-        let changelogs: Changelog[];
+        let changelog: Changelog;
         try {
-            changelogs = JSON.parse(changelogJSON);
+            changelog = JSON.parse(changelogJSON);
         } catch (err) {
             throw new Error(
                 `${lernaPackage.package.name}'s CHANGELOG.json contains invalid JSON. Please fix and try again.`,
@@ -185,7 +185,7 @@ async function updateChangeLogsAsync(updatedPublicLernaPackages: LernaPackage[])
         }
 
         const currentVersion = lernaPackage.package.version;
-        const shouldAddNewEntry = shouldAddNewChangelogEntry(currentVersion, changelogs);
+        const shouldAddNewEntry = changelogUtils.shouldAddNewChangelogEntry(currentVersion, changelog);
         if (shouldAddNewEntry) {
             // Create a new entry for a patch version with generic changelog entry.
             const nextPatchVersion = utils.getNextPatchVersion(currentVersion);
@@ -198,27 +198,27 @@ async function updateChangeLogsAsync(updatedPublicLernaPackages: LernaPackage[])
                     },
                 ],
             };
-            changelogs = [newChangelogEntry, ...changelogs];
+            changelog = [newChangelogEntry, ...changelog];
             packageToVersionChange[packageName] = semverDiff(currentVersion, nextPatchVersion);
         } else {
             // Update existing entry with timestamp
-            const lastEntry = changelogs[0];
+            const lastEntry = changelog[0];
             if (_.isUndefined(lastEntry.timestamp)) {
                 lastEntry.timestamp = TODAYS_TIMESTAMP;
             }
             // Check version number is correct.
             const proposedNextVersion = lastEntry.version;
             lastEntry.version = updateVersionNumberIfNeeded(currentVersion, proposedNextVersion);
-            changelogs[0] = lastEntry;
+            changelog[0] = lastEntry;
             packageToVersionChange[packageName] = semverDiff(currentVersion, lastEntry.version);
         }
 
         // Save updated CHANGELOG.json
-        fs.writeFileSync(changelogJSONPath, JSON.stringify(changelogs, null, '\t'));
+        fs.writeFileSync(changelogJSONPath, JSON.stringify(changelog, null, '\t'));
         await utils.prettifyAsync(changelogJSONPath, constants.monorepoRootPath);
         utils.log(`${packageName}: Updated CHANGELOG.json`);
         // Generate updated CHANGELOG.md
-        const changelogMd = generateChangelogMd(changelogs);
+        const changelogMd = changelogUtils.generateChangelogMd(changelog);
         const changelogMdPath = path.join(lernaPackage.location, 'CHANGELOG.md');
         fs.writeFileSync(changelogMdPath, changelogMd);
         await utils.prettifyAsync(changelogMdPath, constants.monorepoRootPath);
