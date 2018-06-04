a44842ea3387b932f7853fc7594781ec68db5f1c
Brandon Millman <brandon.millman@gmail.com>
2018-06-04 15:56:03 -0700

    Pipeline package

---
+++ packages/pipeline/src/scripts/query_data.ts
@@ -0,0 +1,66 @@
+import { formatters } from '../utils';
+
+export const dataFetchingQueries: any = {
+    get_missing_txn_hashes: `
+        SELECT
+            a.txn_hash
+        FROM
+            events_raw a
+        WHERE NOT EXISTS
+            (
+                SELECT
+                    *
+                FROM
+                    transactions b
+                WHERE
+                    b.txn_hash = a.txn_hash
+            )
+        AND
+            a.block_number >= $1
+        AND
+            a.block_number < $2`
+    ,
+    get_used_block_numbers: `
+        SELECT DISTINCT
+            a.block_number
+        FROM
+            events_raw a
+        WHERE NOT EXISTS
+            (
+                SELECT
+                    *
+                FROM
+                    blocks b
+                WHERE
+                    b.block_number = a.block_number
+            )
+        AND
+            a.block_number >= $1
+        AND
+            a.block_number < $2`
+    ,
+    get_token_registry: `
+        SELECT
+            *
+        FROM
+            tokens`
+    ,
+    get_max_block: `
+        SELECT
+            MAX(block_number)
+        FROM
+            events_raw`
+    ,
+    get_relayers: `
+        SELECT
+            *
+        FROM
+            relayers`
+    ,
+    get_most_recent_pricing_date: `
+        SELECT
+            MAX(DATE(timestamp))
+        FROM
+            prices
+    `
+};
