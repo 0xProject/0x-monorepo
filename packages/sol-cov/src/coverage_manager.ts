af9caa9c9824cb44cfa5bc91208ebe4fbf5049b3
Leonid Logvinov <logvinov.leon@gmail.com>
2018-06-05 16:06:31 -0700

    Fix a bug with contract code being too long for a regex

--- packages/sol-cov/src/coverage_manager.ts
+++ packages/sol-cov/src/coverage_manager.ts
@@ -121,7 +121,10 @@ export class CoverageManager {
             .replace(/.{86}$/, '')
             // Libraries contain their own address at the beginning of the code and it's impossible to know it in advance
             .replace(/^0x730000000000000000000000000000000000000000/, '0x73........................................');
-        return bytecodeRegex;
+        // HACK: Node regexes can't be longer that 32767 characters. Contracts bytecode can. We jsut truncate the regexes. It's safe in practice.
+        const MAX_REGEX_LENGTH = 32767;
+        const truncatedBytecodeRegex = bytecodeRegex.slice(0, MAX_REGEX_LENGTH);
+        return truncatedBytecodeRegex;
     }
     private static _getContractDataIfExists(contractsData: ContractData[], bytecode: string): ContractData | undefined {
         if (!bytecode.startsWith('0x')) {
