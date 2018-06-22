32d499421953851002492b6eef68fd7cc7b43639
Remco Bloemen <remco@wicked.ventures>
2018-06-22 16:32:19 +0200

    Fix ERC721Proxy TokenId

--- packages/contracts/src/contracts/current/protocol/AssetProxy/ERC721Proxy.sol
+++ packages/contracts/src/contracts/current/protocol/AssetProxy/ERC721Proxy.sol
@@ -30,7 +30,7 @@ contract ERC721Proxy is
     MixinERC721Transfer
 {
     // Id of this proxy.
-    bytes4 constant PROXY_ID = 0xf47261b0;
+    bytes4 constant PROXY_ID = 0x08e937fa;
 
     /// @dev Gets the proxy id associated with the proxy address.
     /// @return Proxy id.
