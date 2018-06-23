04d3e278cd174283e5e878bf3e46470813610e07
perissology <perissology@protonmail.com>
2018-06-23 06:43:02 -0700

    cleanup traceInfo logic

--- packages/sol-cov/src/trace_info_subprovider.ts
+++ packages/sol-cov/src/trace_info_subprovider.ts
@@ -19,58 +19,39 @@ export abstract class TraceInfoSubprovider extends TraceCollectionSubprovider {
         });
         const tracesByContractAddress = getTracesByContractAddress(trace.structLogs, address);
         const subcallAddresses = _.keys(tracesByContractAddress);
-        if (address === constants.NEW_CONTRACT) {
-            for (const subcallAddress of subcallAddresses) {
-                let traceInfo: TraceInfoNewContract | TraceInfoExistingContract;
-                if (subcallAddress === 'NEW_CONTRACT') {
-                    const traceForThatSubcall = tracesByContractAddress[subcallAddress];
-                    traceInfo = {
-                        subtrace: traceForThatSubcall,
-                        txHash,
-                        address: subcallAddress,
-                        bytecode: data as string,
-                    };
-                } else {
-                    const runtimeBytecode = await this._web3Wrapper.getContractCodeAsync(subcallAddress);
-                    const traceForThatSubcall = tracesByContractAddress[subcallAddress];
-                    traceInfo = {
-                        subtrace: traceForThatSubcall,
-                        txHash,
-                        address: subcallAddress,
-                        runtimeBytecode,
-                    };
-                }
-                await this._handleTraceInfoAsync(traceInfo);
+        for (const subcallAddress of subcallAddresses) {
+            let traceInfo: TraceInfoNewContract | TraceInfoExistingContract;
+            const traceForThatSubcall = tracesByContractAddress[subcallAddress];
+            if (subcallAddress === 'NEW_CONTRACT') {
+                traceInfo = {
+                    subtrace: traceForThatSubcall,
+                    txHash,
+                    address: subcallAddress,
+                    bytecode: data as string,
+                };
+            } else if (subcallAddress.startsWith(constants.CREATE_PLACEHOLDER_PREFIX)) {
+                const deployedAddress = subcallAddress.replace(constants.CREATE_PLACEHOLDER_PREFIX, '');
+                const runtimeBytecode = await this._web3Wrapper.getContractCodeAsync(deployedAddress);
+                traceInfo = {
+                    subtrace: traceForThatSubcall,
+                    txHash,
+                    address: 'NEW_CONTRACT',
+                    // we use runtimeBytecode here b/c it is what we can fetch.
+                    // the later call to `utils.getContractDataIfExists` in TraceCollector
+                    // will compare the bytecode to the contractData bytecode & runtimeBytecode
+                    // so the contract will still find a match
+                    bytecode: runtimeBytecode,
+                };
+            } else {
+                const runtimeBytecode = await this._web3Wrapper.getContractCodeAsync(subcallAddress);
+                traceInfo = {
+                    subtrace: traceForThatSubcall,
+                    txHash,
+                    address: subcallAddress,
+                    runtimeBytecode,
+                };
             }
-        } else {
-            for (const subcallAddress of subcallAddresses) {
-                let traceInfo: TraceInfoNewContract | TraceInfoExistingContract;
-                if (subcallAddress.startsWith(constants.CREATE_PLACEHOLDER_PREFIX)) {
-                    const deployedAddress = subcallAddress.replace(constants.CREATE_PLACEHOLDER_PREFIX, '');
-                    const runtimeBytecode = await this._web3Wrapper.getContractCodeAsync(deployedAddress);
-                    const traceForThatSubcall = tracesByContractAddress[subcallAddress];
-                    traceInfo = {
-                        subtrace: traceForThatSubcall,
-                        txHash,
-                        address: 'NEW_CONTRACT',
-                        // we use runtimeBytecode here b/c it is what we can fetch.
-                        // the later call to `utils.getContractDataIfExists` in TraceCollector
-                        // will compare the bytecode to the contractData bytecode & runtimeBytecode
-                        // so the contract will still find a match
-                        bytecode: runtimeBytecode,
-                    };
-                } else {
-                    const runtimeBytecode = await this._web3Wrapper.getContractCodeAsync(subcallAddress);
-                    const traceForThatSubcall = tracesByContractAddress[subcallAddress];
-                    traceInfo = {
-                        subtrace: traceForThatSubcall,
-                        txHash,
-                        address: subcallAddress,
-                        runtimeBytecode,
-                    };
-                }
-                await this._handleTraceInfoAsync(traceInfo);
-            }
+            await this._handleTraceInfoAsync(traceInfo);
         }
     }
 }
