6a39a69afeed7b0737cff70b4d3314e668b67308
Brandon Millman <brandon.millman@gmail.com>
2018-06-26 15:01:11 -0700

    Added Circle component

--- packages/website/ts/pages/jobs/list/list_item.tsx
+++ packages/website/ts/pages/jobs/list/list_item.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
+import { Circle } from 'ts/components/ui/circle';
+
 export interface ListItemProps {
     bulletColor?: string;
 }
 export const ListItem: React.StatelessComponent<ListItemProps> = ({ bulletColor, children }) => {
     return (
         <div className="flex items-center">
-            <svg className="flex-none lg-px2 md-px2 sm-pl2" height="26" width="26">
-                <circle cx="13" cy="13" r="13" fill={bulletColor || 'transparent'} />
-            </svg>
+            <Circle className="flex-none lg-px2 md-px2 sm-pl2" diameter={26} fillColor={bulletColor || 'transparent'} />
             <div className="flex-auto px2">{children}</div>
         </div>
     );
