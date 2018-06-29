36cadaae46eb17261c6dd0ee82cd8cefb3806082
fragosti <francesco.agosti93@gmail.com>
2018-06-29 11:08:48 -0700

    Use Image component where relevant and add back tracked token timestamp logic

--- packages/website/ts/components/onboarding/add_eth_onboarding_step.tsx
+++ packages/website/ts/components/onboarding/add_eth_onboarding_step.tsx
@@ -1,6 +1,7 @@
 import { BigNumber } from '@0xproject/utils';
 import * as React from 'react';
 import { Container } from 'ts/components/ui/container';
+import { Image } from 'ts/components/ui/image';
 import { Text } from 'ts/components/ui/text';
 import { constants } from 'ts/utils/constants';
 import { utils } from 'ts/utils/utils';
@@ -24,17 +25,17 @@ export const AddEthOnboardingStep: React.StatelessComponent<AddEthOnboardingStep
                 in your wallet.
             </Text>
             <Container marginTop="15px" marginBottom="15px">
-                <img src="/images/ether_alt.svg" height="50px" width="50px" />
+                <Image src="/images/ether_alt.svg" height="50px" width="50px" />
             </Container>
         </div>
     ) : (
         <div className="flex items-center flex-column">
             <Text> Before you begin you will need to send some ETH to your wallet.</Text>
             <Container marginTop="15px" marginBottom="15px">
-                <img src="/images/ether_alt.svg" height="50px" width="50px" />
+                <Image src="/images/ether_alt.svg" height="50px" width="50px" />
             </Container>
             <Text className="xs-hide">
-                Click on the <img src="/images/metamask_icon.png" height="20px" width="20px" /> metamask extension in
+                Click on the <Image src="/images/metamask_icon.png" height="20px" width="20px" /> MetaMask extension in
                 your browser and click either <b>BUY</b> or <b>DEPOSIT</b>.
             </Text>
         </div>
--- packages/website/ts/components/onboarding/intro_onboarding_step.tsx
+++ packages/website/ts/components/onboarding/intro_onboarding_step.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Container } from 'ts/components/ui/container';
+import { Image } from 'ts/components/ui/image';
 import { Text } from 'ts/components/ui/text';
 
 export interface IntroOnboardingStepProps {}
@@ -11,15 +12,15 @@ export const IntroOnboardingStep: React.StatelessComponent<IntroOnboardingStepPr
         </Text>
         <Container width="100%" marginTop="25px" marginBottom="15px" className="flex justify-around">
             <div className="flex flex-column items-center">
-                <img src="/images/ether.png" height="50px" width="50px" />
+                <Image src="/images/ether.png" height="50px" width="50px" />
                 <Text> Add ETH </Text>
             </div>
             <div className="flex flex-column items-center">
-                <img src="/images/eth_token.svg" height="50px" width="50x" />
+                <Image src="/images/eth_token.svg" height="50px" width="50x" />
                 <Text> Wrap ETH </Text>
             </div>
             <div className="flex flex-column items-center">
-                <img src="/images/fake_toggle.svg" height="50px" width="50px" />
+                <Image src="/images/fake_toggle.svg" height="50px" width="50px" />
                 <Text> Unlock tokens </Text>
             </div>
         </Container>
