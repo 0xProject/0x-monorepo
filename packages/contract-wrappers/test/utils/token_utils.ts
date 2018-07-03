0f477607f9586a00da4ca1586c34de7b2cd47ec5
Leonid Logvinov <logvinov.leon@gmail.com>
2018-07-03 15:57:37 +0300

    Update dummy tokens addresses in token_utils

--- packages/contract-wrappers/test/utils/token_utils.ts
+++ packages/contract-wrappers/test/utils/token_utils.ts
@@ -10,14 +10,14 @@ import { provider, txDefaults, web3Wrapper } from './web3_wrapper';
 // Those addresses come from migrations. They're deterministic so it's relatively safe to hard-code them here.
 // Before we were fetching them from the TokenRegistry but now we can't as it's deprecated and removed.
 const DUMMY_ERC_20_ADRESSES = [
-    '0x07f96aa816c1f244cbc6ef114bb2b023ba54a2eb',
-    '0x6a4a62e5a7ed13c361b176a5f62c2ee620ac0df8',
     '0x6dfff22588be9b3ef8cf0ad6dc9b84796f9fb45f',
     '0xcfc18cec799fbd1793b5c43e773c98d4d61cc2db',
     '0xf22469f31527adc53284441bae1665a7b9214dba',
+    '0x10add991de718a69dec2117cb6aa28098836511b',
+    '0x8d61158a366019ac78db4149d75fff9dda51160d',
 ];
 
-const DUMMY_ERC_721_ADRESSES = ['0x10add991de718a69dec2117cb6aa28098836511b'];
+const DUMMY_ERC_721_ADRESSES = ['0x131855dda0aaff096f6854854c55a4debf61077a'];
 
 export const tokenUtils = {
     getProtocolTokenAddress(): string {
