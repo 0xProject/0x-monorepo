99883b54038a673cbfc973855996940c3442007e
Remco Bloemen <remco@wicked.ventures>
2018-07-06 13:15:49 +0200

    Add tests for isRoundingError

--- packages/contracts/test/exchange/lib_math.ts
+++ packages/contracts/test/exchange/lib_math.ts
@@ -15,6 +15,9 @@ const blockchainLifecycle = new BlockchainLifecycle(web3Wrapper);
 describe.only('LibMath', () => {
     let libs: TestLibsContract;
 
+    const toHex = (number: BigNumber): string =>
+        `0x${number.toString(16).padStart(64, '0')}`;
+
     const uint256Tests: {[label: string]: BigNumber}  = {
         '0':        new BigNumber(0),
         '1':        new BigNumber(1),
@@ -65,17 +68,131 @@ describe.only('LibMath', () => {
         // TODO: continue with low disrepancy sequence
     };
 
+    function require(constraint: boolean, error: string) {
+        if (!constraint) {
+            throw new Error(error);
+        }
+    }
+
+    // Check if a value is a bignumber
+    function isBigNumber(obj: any) {
+        if (typeof obj !== 'object') {
+            return false;
+        }
+        return Object.getPrototypeOf(obj).constructor.name === 'BigNumber';
+    }
+
+    // Wrapper arround mocha because Mocha doesn't know BigNumber
+    function expectEqual(result: any, expected: any) {
+        if (isBigNumber(expected)) {
+            expect(result).to.be.bignumber.equal(expected);
+        } else {
+            expect(result).to.equal(expected);
+        }
+    }
+
+    // We need to use funGen instead of fund directly because of:
+    // https://github.com/mochajs/mocha/issues/1628
+    function createTest(funcGen: any, reference: any, label: string, args: any) {
+        // TypeScript does not support spread operator
+        // https://github.com/Microsoft/TypeScript/issues/4130
+        try {
+            const expected = reference.apply(null, args);
+            it(`${label} ↦ ${isBigNumber(expected) ? toHex(expected) : expected}`, async () => {
+                const func = funcGen();
+                const result = await func.callAsync.apply(func, args);
+                expectEqual(result, expected);
+            });
+        } catch (e) {
+            const revertString = e.message;
+            it(`${label} ↦ ${revertString}`, async () => {
+                const func = funcGen();
+                const call = func.callAsync.apply(func, args);
+                expect(call).to.rejectedWith(revertString);
+            });
+        }
+    }
+
     before(async () => {
         await blockchainLifecycle.startAsync();
     });
+
     after(async () => {
         await blockchainLifecycle.revertAsync();
     });
+
     before(async () => {
         libs = await TestLibsContract.deployFrom0xArtifactAsync(artifacts.TestLibs, provider, txDefaults);
     });
 
+    describe('getPartialAmount(numerator, denominator, target)', () => {
+
+        function reference(
+            numerator: BigNumber,
+            denominator: BigNumber,
+            target: BigNumber): BigNumber {
+            require(numerator.lte(denominator), 'NUMERATOR_GT_DENOMINATOR');
+            if (denominator.eq(0)) {
+                return new BigNumber(0);
+            }
+            return target.mul(numerator).divToInt(denominator);
+        }
+
+        for (const a in uint256Tests) {
+            for (const b in uint256Tests) {
+                for (const c in uint256Tests) {
+                    createTest(
+                        () => libs.publicGetPartialAmount,
+                        reference,
+                        `(${a}, ${b}, ${c})`,
+                        [
+                            uint256Tests[a],
+                            uint256Tests[b],
+                            uint256Tests[c],
+                        ],
+                    );
+                }
+            }
+        }
+    });
+
+    describe('isRoundingError(numerator, denominator, target)', () => {
+
+        function reference(
+            numerator: BigNumber,
+            denominator: BigNumber,
+            target: BigNumber): boolean {
+            require(numerator.lte(denominator), 'NUMERATOR_GT_DENOMINATOR');
+
+            const error = target.mul(numerator).mod(denominator);
+            const ideal = target.mul(numerator).div(denominator);
+
+            // Return true iff error is >0.1% of ideal
+            const treshold = ideal.mul(0.001);
+            return error.gt(treshold);
+        }
+
+        for (const a in uint256Tests) {
+            for (const b in uint256Tests) {
+                for (const c in uint256Tests) {
+                    createTest(
+                        () => libs.publicIsRoundingError,
+                        reference,
+                        `(${a}, ${b}, ${c})`,
+                        [
+                            uint256Tests[a],
+                            uint256Tests[b],
+                            uint256Tests[c],
+                        ],
+                    );
+                }
+            }
+        }
+
+    });
+
     describe('isRoundingError', () => {
+
         it('should return false if there is a rounding error of 0.1%', async () => {
             const numerator = new BigNumber(20);
             const denominator = new BigNumber(999);
@@ -133,62 +250,4 @@ describe.only('LibMath', () => {
         });
     });
 
-    describe('getPartialAmount(numerator, denominator, target)', () => {
-
-        const tests: {[label: string]: [BigNumber, BigNumber, BigNumber]} = {};
-        for (const a in uint256Tests) {
-            for (const b in uint256Tests) {
-                for (const c in uint256Tests) {
-                    tests[`(${a}, ${b}, ${c})`] = [
-                        uint256Tests[a],
-                        uint256Tests[b],
-                        uint256Tests[c],
-                    ];
-                }
-            }
-        }
-
-        const toHex = (number: BigNumber): string =>
-            `0x${number.toString(16).padStart(64, '0')}`;
-
-        function require(constraint: boolean, error: string) {
-            if (!constraint) {
-                throw new Error(error);
-            }
-        }
-
-        function reference(
-            numerator: BigNumber,
-            denominator: BigNumber,
-            target: BigNumber): BigNumber {
-            require(numerator.lte(denominator), 'NUMERATOR_GT_DENOMINATOR');
-            if (denominator.eq(0)) {
-                return new BigNumber(0);
-            }
-            return target.mul(numerator).divToInt(denominator);
-        }
-
-        for (const test in tests) {
-            // TypeScript does not support spread operator
-            // https://github.com/Microsoft/TypeScript/issues/4130
-            const args = tests[test];
-            try {
-                const expected = reference.apply(null, args);
-                it(`${test} ↦ 0x${expected.toString(16)}`, async () => {
-                    const result = await libs.publicGetPartialAmount.callAsync.apply(
-                        libs.publicGetPartialAmount, args,
-                    );
-                    expect(toHex(result)).to.equal(toHex(expected));
-                });
-            } catch (e) {
-                const revertString = e.message;
-                it(`${test} ↦ ${revertString}`, async () => {
-                    const call = libs.publicGetPartialAmount.callAsync.apply(
-                        libs.publicGetPartialAmount, args,
-                    );
-                    expect(call).to.rejectedWith(revertString);
-                });
-            }
-        }
-    });
 });
