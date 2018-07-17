a3d8858bb1425e43234a071d2e50e78266b8e189
Fabio Berger <me@fabioberger.com>
2018-07-17 16:27:46 +0200

    Fix typing

--- packages/typescript-typings/types/ethereumjs-abi/index.d.ts
+++ packages/typescript-typings/types/ethereumjs-abi/index.d.ts
@@ -1,4 +1,4 @@
-declare module 'ethereumjs-util' {
+declare module 'ethereumjs-abi' {
     export function soliditySHA3(argTypes: string[], args: any[]): Buffer;
     export function soliditySHA256(argTypes: string[], args: any[]): Buffer;
     export function methodID(name: string, types: string[]): Buffer;
