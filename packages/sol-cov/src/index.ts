13520dbd94831cd93e419d000e933bfb75c99e84
Fabio Berger <me@fabioberger.com>
2018-08-03 13:02:29 +0200

    Add missing types to sol-cov index.ts

--- packages/sol-cov/src/index.ts
+++ packages/sol-cov/src/index.ts
@@ -5,4 +5,15 @@ export { RevertTraceSubprovider } from './revert_trace_subprovider';
 export { SolCompilerArtifactAdapter } from './artifact_adapters/sol_compiler_artifact_adapter';
 export { TruffleArtifactAdapter } from './artifact_adapters/truffle_artifact_adapter';
 export { AbstractArtifactAdapter } from './artifact_adapters/abstract_artifact_adapter';
-export { ContractData, TraceInfo, Subtrace, SourceRange, Coverage } from './types';
+export {
+    ContractData,
+    TraceInfo,
+    Subtrace,
+    SourceRange,
+    Coverage,
+    TraceInfoNewContract,
+    TraceInfoExistingContract,
+    SingleFileSourceRange,
+} from './types';
+export { StructLog, JSONRPCRequestPayload, Provider } from 'ethereum-types';
+export { ErrorCallback, NextCallback } from '@0xproject/subproviders';
