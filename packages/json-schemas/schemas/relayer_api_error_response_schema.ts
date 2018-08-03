0390a5ecbfc79dfd435d3f9e911abd610f33d455
fragosti <francesco.agosti93@gmail.com>
2018-08-02 18:08:36 -0700

    Limit error codes with ranges

--- packages/json-schemas/schemas/relayer_api_error_response_schema.ts
+++ packages/json-schemas/schemas/relayer_api_error_response_schema.ts
@@ -2,7 +2,7 @@ export const relayerApiErrorResponseSchema = {
     id: '/relayerApiErrorResponseSchema',
     type: 'object',
     properties: {
-        code: { type: 'number' },
+        code: { type: 'number', minimum: 100, maximum: 103 },
         reason: { type: 'string' },
         validationErrors: {
             type: 'array',
@@ -10,7 +10,7 @@ export const relayerApiErrorResponseSchema = {
                 type: 'object',
                 properties: {
                     field: { type: 'string' },
-                    code: { type: 'number' },
+                    code: { type: 'number', minimum: 1000, maximum: 1006 },
                     reason: { type: 'string' },
                 },
                 required: ['field', 'code', 'reason'],
