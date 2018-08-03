bcc1ad20869d10fb16678619e6fa411e53a07742
Fabio Berger <me@fabioberger.com>
2018-08-03 21:30:57 +0200

    Re-order index.ts and add missing types

--- packages/order-utils/src/index.ts
+++ packages/order-utils/src/index.ts
@@ -1,6 +1,29 @@
 export { orderHashUtils } from './order_hash';
 export { signatureUtils } from './signature_utils';
 export { generatePseudoRandomSalt } from './salt';
+export { assetDataUtils } from './asset_data_utils';
+export { EIP712Utils } from './eip712_utils';
+
+export { OrderStateUtils } from './order_state_utils';
+export { AbstractBalanceAndProxyAllowanceFetcher } from './abstract/abstract_balance_and_proxy_allowance_fetcher';
+export { AbstractOrderFilledCancelledFetcher } from './abstract/abstract_order_filled_cancelled_fetcher';
+
+export { OrderValidationUtils } from './order_validation_utils';
+export { ExchangeTransferSimulator } from './exchange_transfer_simulator';
+export { BalanceAndProxyAllowanceLazyStore } from './store/balance_and_proxy_allowance_lazy_store';
+export { OrderFilledCancelledLazyStore } from './store/order_filled_cancelled_lazy_store';
+
+export { Provider } from 'ethereum-types';
+export {
+    SignedOrder,
+    Order,
+    OrderRelevantState,
+    OrderState,
+    ECSignature,
+    ERC20AssetData,
+    ERC721AssetData,
+    AssetProxyId,
+} from '@0xproject/types';
 export {
     OrderError,
     MessagePrefixType,
@@ -11,15 +34,3 @@ export {
     TradeSide,
     TransferType,
 } from './types';
-export { AbstractBalanceAndProxyAllowanceFetcher } from './abstract/abstract_balance_and_proxy_allowance_fetcher';
-export { AbstractOrderFilledCancelledFetcher } from './abstract/abstract_order_filled_cancelled_fetcher';
-export { BalanceAndProxyAllowanceLazyStore } from './store/balance_and_proxy_allowance_lazy_store';
-export { OrderFilledCancelledLazyStore } from './store/order_filled_cancelled_lazy_store';
-export { RemainingFillableCalculator } from './remaining_fillable_calculator';
-export { OrderStateUtils } from './order_state_utils';
-export { assetDataUtils } from './asset_data_utils';
-export { EIP712Utils } from './eip712_utils';
-export { OrderValidationUtils } from './order_validation_utils';
-export { ExchangeTransferSimulator } from './exchange_transfer_simulator';
-export { Provider } from 'ethereum-types';
-export { SignedOrder, Order, ECSignature, ERC20AssetData, ERC721AssetData, AssetProxyId } from '@0xproject/types';
