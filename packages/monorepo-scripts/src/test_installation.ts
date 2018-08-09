ef248ae890b25a91e951c175486ae0f4f76dcc5d
Alex Browne <stephenalexbrowne@gmail.com>
2018-08-09 16:31:39 -0700

    Update test_installation to run package tests in parallel

--- packages/monorepo-scripts/src/test_installation.ts
+++ packages/monorepo-scripts/src/test_installation.ts
@@ -2,9 +2,13 @@
 
 import * as fs from 'fs';
 import * as _ from 'lodash';
+import * as mkdirp from 'mkdirp';
 import * as path from 'path';
 import { exec as execAsync } from 'promisify-child-process';
 import * as rimraf from 'rimraf';
+import { promisify } from 'util';
+
+import { Package } from './types';
 
 import { utils } from './utils/utils';
 
@@ -15,6 +19,10 @@ const UNRUNNABLE_PACKAGES = [
     '@0xproject/react-shared',
     '@0xproject/react-docs',
 ];
+
+const mkdirpAsync = promisify(mkdirp);
+const rimrafAsync = promisify(rimraf);
+const writeFileAsync = promisify(fs.writeFile);
 
 (async () => {
     const IS_LOCAL_PUBLISH = process.env.IS_LOCAL_PUBLISH === 'true';
@@ -27,53 +35,72 @@ const UNRUNNABLE_PACKAGES = [
     );
     utils.log('Testing packages:');
     _.map(installablePackages, pkg => utils.log(`* ${pkg.packageJson.name}`));
+    const promises: Array<Promise<void>> = [];
     for (const installablePackage of installablePackages) {
-        const changelogPath = path.join(installablePackage.location, 'CHANGELOG.json');
-        const lastChangelogVersion = JSON.parse(fs.readFileSync(changelogPath).toString())[0].version;
-        const packageName = installablePackage.packageJson.name;
-        utils.log(`Testing ${packageName}@${lastChangelogVersion}`);
-        const testDirectory = path.join(monorepoRootPath, '../test-env');
-        rimraf.sync(testDirectory);
-        fs.mkdirSync(testDirectory);
-        await execAsync('yarn init --yes', { cwd: testDirectory });
-        const npmrcFilePath = path.join(testDirectory, '.npmrc');
-        fs.writeFileSync(npmrcFilePath, `registry=${registry}`);
-        utils.log(`Installing ${packageName}@${lastChangelogVersion}`);
-        await execAsync(`npm install --save ${packageName}@${lastChangelogVersion} --registry=${registry}`, {
-            cwd: testDirectory,
-        });
-        const indexFilePath = path.join(testDirectory, 'index.ts');
-        fs.writeFileSync(indexFilePath, `import * as Package from '${packageName}';\nconsole.log(Package);\n`);
-        const tsConfig = {
-            compilerOptions: {
-                typeRoots: ['node_modules/@0xproject/typescript-typings/types', 'node_modules/@types'],
-                module: 'commonjs',
-                target: 'es5',
-                lib: ['es2017', 'dom'],
-                declaration: true,
-                noImplicitReturns: true,
-                pretty: true,
-                strict: true,
-            },
-            include: ['index.ts'],
-        };
-        const tsconfigFilePath = path.join(testDirectory, 'tsconfig.json');
-        fs.writeFileSync(tsconfigFilePath, JSON.stringify(tsConfig, null, '\t'));
-        utils.log(`Compiling ${packageName}`);
-        const tscBinaryPath = path.join(monorepoRootPath, './node_modules/typescript/bin/tsc');
-        await execAsync(tscBinaryPath, { cwd: testDirectory });
-        utils.log(`Successfully compiled with ${packageName} as a dependency`);
-        const isUnrunnablePkg = _.includes(UNRUNNABLE_PACKAGES, packageName);
-        if (!isUnrunnablePkg) {
-            const transpiledIndexFilePath = path.join(testDirectory, 'index.js');
-            utils.log(`Running test script with ${packageName} imported`);
-            await execAsync(`node ${transpiledIndexFilePath}`);
-            utils.log(`Successfilly ran test script with ${packageName} imported`);
-        }
-        rimraf.sync(testDirectory);
+        promises.push(testInstallPackageAsync(monorepoRootPath, registry, installablePackage));
     }
+    await Promise.all(promises);
 })().catch(err => {
+    utils.log(err.message);
     utils.log(err.stderr);
     utils.log(err.stdout);
+    utils.log(err.stack);
     process.exit(1);
 });
+
+async function testInstallPackageAsync(
+    monorepoRootPath: string,
+    registry: string,
+    installablePackage: Package,
+): Promise<void> {
+    const changelogPath = path.join(installablePackage.location, 'CHANGELOG.json');
+    const lastChangelogVersion = JSON.parse(fs.readFileSync(changelogPath).toString())[0].version;
+    const packageName = installablePackage.packageJson.name;
+    utils.log(`Testing ${packageName}@${lastChangelogVersion}`);
+    const packageDirName = path.join(...(packageName + '-test').split('/'));
+    const testDirectory = path.join(
+        monorepoRootPath,
+        'packages',
+        'monorepo-scripts',
+        '.installation-test',
+        packageDirName,
+    );
+    await rimrafAsync(testDirectory);
+    await mkdirpAsync(testDirectory);
+    await execAsync('yarn init --yes', { cwd: testDirectory });
+    const npmrcFilePath = path.join(testDirectory, '.npmrc');
+    await writeFileAsync(npmrcFilePath, `registry=${registry}`);
+    utils.log(`Installing ${packageName}@${lastChangelogVersion}`);
+    await execAsync(`npm install --save ${packageName}@${lastChangelogVersion} --registry=${registry}`, {
+        cwd: testDirectory,
+    });
+    const indexFilePath = path.join(testDirectory, 'index.ts');
+    await writeFileAsync(indexFilePath, `import * as Package from '${packageName}';\nconsole.log(Package);\n`);
+    const tsConfig = {
+        compilerOptions: {
+            typeRoots: ['node_modules/@0xproject/typescript-typings/types', 'node_modules/@types'],
+            module: 'commonjs',
+            target: 'es5',
+            lib: ['es2017', 'dom'],
+            declaration: true,
+            noImplicitReturns: true,
+            pretty: true,
+            strict: true,
+        },
+        include: ['index.ts'],
+    };
+    const tsconfigFilePath = path.join(testDirectory, 'tsconfig.json');
+    await writeFileAsync(tsconfigFilePath, JSON.stringify(tsConfig, null, '\t'));
+    utils.log(`Compiling ${packageName}`);
+    const tscBinaryPath = path.join(monorepoRootPath, './node_modules/typescript/bin/tsc');
+    await execAsync(tscBinaryPath, { cwd: testDirectory });
+    utils.log(`Successfully compiled with ${packageName} as a dependency`);
+    const isUnrunnablePkg = _.includes(UNRUNNABLE_PACKAGES, packageName);
+    if (!isUnrunnablePkg) {
+        const transpiledIndexFilePath = path.join(testDirectory, 'index.js');
+        utils.log(`Running test script with ${packageName} imported`);
+        await execAsync(`node ${transpiledIndexFilePath}`);
+        utils.log(`Successfilly ran test script with ${packageName} imported`);
+    }
+    await rimrafAsync(testDirectory);
+}
