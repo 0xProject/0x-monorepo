d8593998414aa3234533b8fa868b05495ac54457
Brandon Millman <brandon.millman@gmail.com>
2018-08-13 17:45:50 -0700

    Change rateUtils to use Order

--- packages/order-utils/src/rate_utils.ts
+++ packages/order-utils/src/rate_utils.ts
@@ -1,5 +1,5 @@
 import { schemas } from '@0xproject/json-schemas';
-import { SignedOrder } from '@0xproject/types';
+import { Order } from '@0xproject/types';
 import { BigNumber } from '@0xproject/utils';
 
 import { assert } from './assert';
@@ -7,42 +7,42 @@ import { constants } from './constants';
 
 export const rateUtils = {
     /**
-     * Takes a signed order and calculates the fee adjusted rate (takerAsset/makerAsset) by calculating how much takerAsset
+     * Takes an order and calculates the fee adjusted rate (takerAsset/makerAsset) by calculating how much takerAsset
      * is required to cover the fees (feeRate * takerFee), adding the takerAssetAmount and dividing by makerAssetAmount
-     * @param   signedOrder     An object that conforms to the signedOrder interface
-     * @param   feeRate         The market rate of ZRX denominated in takerAssetAmount
-     *                          (ex. feeRate is 0.1 takerAsset/ZRX if it takes 1 unit of takerAsset to buy 10 ZRX)
-     *                          Defaults to 0
+     * @param   order       An object that conforms to the order interface
+     * @param   feeRate     The market rate of ZRX denominated in takerAssetAmount
+     *                      (ex. feeRate is 0.1 takerAsset/ZRX if it takes 1 unit of takerAsset to buy 10 ZRX)
+     *                      Defaults to 0
      * @return  The rate (takerAsset/makerAsset) of the order adjusted for fees
      */
-    getFeeAdjustedRateOfOrder(signedOrder: SignedOrder, feeRate: BigNumber = constants.ZERO_AMOUNT): BigNumber {
-        assert.doesConformToSchema('signedOrder', signedOrder, schemas.signedOrderSchema);
+    getFeeAdjustedRateOfOrder(order: Order, feeRate: BigNumber = constants.ZERO_AMOUNT): BigNumber {
+        assert.doesConformToSchema('order', order, schemas.orderSchema);
         assert.isBigNumber('feeRate', feeRate);
         assert.assert(
             feeRate.gte(constants.ZERO_AMOUNT),
             `Expected feeRate: ${feeRate} to be greater than or equal to 0`,
         );
-        const takerAssetAmountNeededToPayForFees = signedOrder.takerFee.mul(feeRate);
-        const totalTakerAssetAmount = takerAssetAmountNeededToPayForFees.plus(signedOrder.takerAssetAmount);
-        const rate = totalTakerAssetAmount.div(signedOrder.makerAssetAmount);
+        const takerAssetAmountNeededToPayForFees = order.takerFee.mul(feeRate);
+        const totalTakerAssetAmount = takerAssetAmountNeededToPayForFees.plus(order.takerAssetAmount);
+        const rate = totalTakerAssetAmount.div(order.makerAssetAmount);
         return rate;
     },
     /**
-     * Takes a signed fee order (makerAssetData corresponds to ZRX and takerAssetData corresponds to WETH) and calculates
+     * Takes a fee order (makerAssetData corresponds to ZRX and takerAssetData corresponds to WETH) and calculates
      * the fee adjusted rate (WETH/ZRX) by dividing the takerAssetAmount by the makerAmount minus the takerFee
-     * @param   signedFeeOrder    An object that conforms to the signedOrder interface
+     * @param   feeOrder    An object that conforms to the order interface
      * @return  The rate (WETH/ZRX) of the fee order adjusted for fees
      */
-    getFeeAdjustedRateOfFeeOrder(signedFeeOrder: SignedOrder): BigNumber {
-        assert.doesConformToSchema('signedFeeOrder', signedFeeOrder, schemas.signedOrderSchema);
-        const zrxAmountAfterFees = signedFeeOrder.makerAssetAmount.sub(signedFeeOrder.takerFee);
+    getFeeAdjustedRateOfFeeOrder(feeOrder: Order): BigNumber {
+        assert.doesConformToSchema('feeOrder', feeOrder, schemas.orderSchema);
+        const zrxAmountAfterFees = feeOrder.makerAssetAmount.sub(feeOrder.takerFee);
         assert.assert(
             zrxAmountAfterFees.greaterThan(constants.ZERO_AMOUNT),
-            `Expected takerFee: ${JSON.stringify(
-                signedFeeOrder.takerFee,
-            )} to be less than makerAssetAmount: ${JSON.stringify(signedFeeOrder.makerAssetAmount)}`,
+            `Expected takerFee: ${JSON.stringify(feeOrder.takerFee)} to be less than makerAssetAmount: ${JSON.stringify(
+                feeOrder.makerAssetAmount,
+            )}`,
         );
-        const rate = signedFeeOrder.takerAssetAmount.div(zrxAmountAfterFees);
+        const rate = feeOrder.takerAssetAmount.div(zrxAmountAfterFees);
         return rate;
     },
 };
