b0b3f9e339cc470860f8c1080ab6dad145d70624
Brandon Millman <brandon.millman@gmail.com>
2018-08-22 14:58:03 -0700

    Add assertions to factory

--- packages/forwarder-helper/src/forwarder_helper_factory.ts
+++ packages/forwarder-helper/src/forwarder_helper_factory.ts
@@ -1,3 +1,5 @@
+import { assert } from '@0xproject/assert';
+import { schemas } from '@0xproject/json-schemas';
 import { SignedOrder } from '@0xproject/types';
 
 import { ForwarderHelperImpl, ForwarderHelperImplConfig } from './forwarder_helper_impl';
@@ -7,10 +9,12 @@ export const forwarderHelperFactory = {
     /**
      * Given an array of orders and an array of feeOrders
      * @param   orders      An array of objects conforming to SignedOrder. Each order should specify the same makerAssetData and takerAssetData
-     * @param   orders      An array of objects conforming to SignedOrder. Each order should specify ZRX as makerAssetData WETH as takerAssetData
+     * @param   feeOrders   An array of objects conforming to SignedOrder. Each order should specify ZRX as makerAssetData WETH as takerAssetData
      * @return  A ForwarderHelper, see type for definition
      */
     getForwarderHelperForOrders(orders: SignedOrder[], feeOrders: SignedOrder[] = []): ForwarderHelper {
+        assert.doesConformToSchema('orders', orders, schemas.signedOrdersSchema);
+        assert.doesConformToSchema('feeOrders', orders, schemas.signedOrdersSchema);
         const config: ForwarderHelperImplConfig = {
             orders,
             feeOrders,
