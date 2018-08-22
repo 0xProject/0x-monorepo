04e4fe251a4c791239ec893b94ae7f4a7f9d0b53
Fabio Berger <me@fabioberger.com>
2018-08-23 00:08:21 +0100

    Prettier fix

--- packages/react-shared/src/utils/utils.ts
+++ packages/react-shared/src/utils/utils.ts
@@ -35,7 +35,7 @@ export const utils = {
         return text.replace(/-/g, ' ');
     },
     convertCamelCaseToSpaces(text: string): string {
-       return changeCase.snake(text).replace(/_/g, ' ');
+        return changeCase.snake(text).replace(/_/g, ' ');
     },
     getEtherScanLinkIfExists(
         addressOrTxHash: string,
