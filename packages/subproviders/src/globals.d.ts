ba6351841d3cf3ff800b3cd1143b363a64344d2d
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-04 15:26:47 +0200

    Add temporarily missing web3-provider-engine types in subproviders

--- packages/subproviders/src/globals.d.ts
+++ packages/subproviders/src/globals.d.ts
@@ -4,3 +4,21 @@ declare module '*.json' {
     export default json;
     /* tslint:enable */
 }
+declare module 'web3-provider-engine/util/rpc-cache-utils' {
+    class ProviderEngineRpcUtils {
+        public static blockTagForPayload(payload: any): string | null;
+    }
+    export = ProviderEngineRpcUtils;
+}
+declare module 'web3-provider-engine/subproviders/fixture' {
+    import { JSONRPCRequestPayload, JSONRPCResponsePayload } from 'ethereum-types';
+    class FixtureSubprovider {
+        constructor(staticResponses: any);
+        public handleRequest(
+            payload: JSONRPCRequestPayload,
+            next: () => void,
+            end: (err: Error | null, data?: JSONRPCResponsePayload) => void,
+        ): void;
+    }
+    export = FixtureSubprovider;
+}
