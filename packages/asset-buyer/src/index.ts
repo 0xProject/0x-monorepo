d57619b5db8d4b5a8d31f095d8dfef04faef44c9
Brandon Millman <brandon.millman@gmail.com>
2018-09-15 14:16:08 +0200

    Export the order fetchers

--- packages/asset-buyer/src/index.ts
+++ packages/asset-buyer/src/index.ts
@@ -3,6 +3,8 @@ export { SignedOrder } from '@0xproject/types';
 export { BigNumber } from '@0xproject/utils';
 
 export { AssetBuyer } from './asset_buyer';
+export { ProvidedOrderFetcher } from './order_fetchers/provided_order_fetcher';
+export { StandardRelayerAPIOrderFetcher } from './order_fetchers/standard_relayer_api_order_fetcher';
 export {
     AssetBuyerError,
     BuyQuote,
