65aecc0024fdaaa82abd2db440c5a1f24e18400d
Jacob Evans <jacob@dekz.net>
2018-09-17 23:23:27 +0200

    Rename to v1->v0 v2->v1

--- packages/website/ts/containers/zero_ex_js_documentation.ts
+++ packages/website/ts/containers/zero_ex_js_documentation.ts
@@ -9,17 +9,17 @@ import { DocPackages } from 'ts/types';
 import { Translate } from 'ts/utils/translate';
 
 /* tslint:disable:no-var-requires */
-const IntroMarkdownV1 = require('md/docs/0xjs/0.0.1/introduction');
-const InstallationMarkdownV1 = require('md/docs/0xjs/0.0.1/installation');
-const AsyncMarkdownV1 = require('md/docs/0xjs/0.0.1/async');
-const ErrorsMarkdownV1 = require('md/docs/0xjs/0.0.1/errors');
-const versioningMarkdownV1 = require('md/docs/0xjs/0.0.1/versioning');
+const IntroMarkdownV0 = require('md/docs/0xjs/0.0.1/introduction');
+const InstallationMarkdownV0 = require('md/docs/0xjs/0.0.1/installation');
+const AsyncMarkdownV0 = require('md/docs/0xjs/0.0.1/async');
+const ErrorsMarkdownV0 = require('md/docs/0xjs/0.0.1/errors');
+const versioningMarkdownV0 = require('md/docs/0xjs/0.0.1/versioning');
 
-const IntroMarkdownV2 = require('md/docs/0xjs/1.0.0/introduction');
-const InstallationMarkdownV2 = require('md/docs/0xjs/1.0.0/installation');
-const AsyncMarkdownV2 = require('md/docs/0xjs/1.0.0/async');
-const ErrorsMarkdownV2 = ErrorsMarkdownV1;
-const versioningMarkdownV2 = require('md/docs/0xjs/1.0.0/versioning');
+const IntroMarkdownV1 = require('md/docs/0xjs/1.0.0/introduction');
+const InstallationMarkdownV1 = require('md/docs/0xjs/1.0.0/installation');
+const AsyncMarkdownV1 = require('md/docs/0xjs/1.0.0/async');
+const ErrorsMarkdownV1 = ErrorsMarkdownV0;
+const versioningMarkdownV1 = require('md/docs/0xjs/1.0.0/versioning');
 
 /* tslint:enable:no-var-requires */
 
@@ -45,18 +45,18 @@ const docsInfoConfig: DocsInfoConfig = {
     },
     sectionNameToMarkdownByVersion: {
         '0.0.1': {
+            [markdownSections.introduction]: IntroMarkdownV0,
+            [markdownSections.installation]: InstallationMarkdownV0,
+            [markdownSections.versioning]: versioningMarkdownV0,
+            [markdownSections.async]: AsyncMarkdownV0,
+            [markdownSections.errors]: ErrorsMarkdownV0,
+        },
+        '1.0.0': {
             [markdownSections.introduction]: IntroMarkdownV1,
             [markdownSections.installation]: InstallationMarkdownV1,
             [markdownSections.versioning]: versioningMarkdownV1,
             [markdownSections.async]: AsyncMarkdownV1,
             [markdownSections.errors]: ErrorsMarkdownV1,
-        },
-        '1.0.0': {
-            [markdownSections.introduction]: IntroMarkdownV2,
-            [markdownSections.installation]: InstallationMarkdownV2,
-            [markdownSections.versioning]: versioningMarkdownV2,
-            [markdownSections.async]: AsyncMarkdownV2,
-            [markdownSections.errors]: ErrorsMarkdownV2,
         },
     },
     markdownSections,
