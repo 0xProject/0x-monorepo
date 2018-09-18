fd4b4f8f8289d1e5a873ff5aa0aa9f7ac6a2ba08
fragosti <francesco.agosti93@gmail.com>
2018-09-18 23:56:07 +0200

    Make ForwaderWrapper take in a number for feePercentage instead of BigNumber

--- packages/contract-wrappers/src/utils/utils.ts
+++ packages/contract-wrappers/src/utils/utils.ts
@@ -1,4 +1,7 @@
 import { BigNumber } from '@0xproject/utils';
+import { Web3Wrapper } from '@0xproject/web3-wrapper';
+
+import { constants } from './constants';
 
 export const utils = {
     getCurrentUnixTimestampSec(): BigNumber {
@@ -7,5 +10,8 @@ export const utils = {
     },
     getCurrentUnixTimestampMs(): BigNumber {
         return new BigNumber(Date.now());
+    },
+    numberPercentageToEtherTokenAmountPercentage(percentage: number): BigNumber {
+        return Web3Wrapper.toBaseUnitAmount(constants.ONE_AMOUNT, constants.ETHER_TOKEN_DECIMALS).mul(percentage);
     },
 };
