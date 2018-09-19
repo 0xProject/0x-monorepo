cd7e41bd5bbf1abae4c855d6a666e6d9f7da2294
Alex Browne <stephenalexbrowne@gmail.com>
2018-09-19 11:25:46 -0700

    Restructure pipeline package. Create data-sources dir

---
+++ packages/pipeline/src/index.ts
@@ -0,0 +1,8 @@
+import { Etherscan } from './data-sources/etherscan';
+
+const etherscan = new Etherscan(process.env.ETHERSCAN_API_KEY as string);
+
+(async () => {
+    const events = await etherscan.getContractEventsAsync('0x4f833a24e1f95d70f028921e27040ca56e09ab0b');
+    console.log(events);
+})();
