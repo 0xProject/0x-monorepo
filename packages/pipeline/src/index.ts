ed3737c1f14182f75815d2d7d761b3c84602a5c6
Alex Browne <stephenalexbrowne@gmail.com>
2018-09-19 12:09:36 -0700

    Make contractAbi a parameter of getContractEventsAsync

--- packages/pipeline/src/index.ts
+++ packages/pipeline/src/index.ts
@@ -1,7 +1,13 @@
 import { Etherscan } from './data-sources/etherscan';
 
+import { artifacts } from './artifacts';
+
 const etherscan = new Etherscan(process.env.ETHERSCAN_API_KEY as string);
 
 (async () => {
-    await etherscan.getContractEventsAsync('0x4f833a24e1f95d70f028921e27040ca56e09ab0b');
+    const events = await etherscan.getContractEventsAsync(
+        '0x4f833a24e1f95d70f028921e27040ca56e09ab0b',
+        artifacts.Exchange.compilerOutput.abi,
+    );
+    console.log(events);
 })();
