abd308455abb1eb1bdb739e0b4c86887c573dc45
Philippe Castonguay <ph.castonguay@gmail.com>
2018-09-19 20:17:00 -0400

    Add BigNumber[] support in ContractEventArg
    Currently, my contract has events that take a BigNumber array and typescript complains that ContractEventArg does not support this.

--- packages/ethereum-types/src/index.ts
+++ packages/ethereum-types/src/index.ts
@@ -238,7 +238,7 @@ export enum AbiType {
     Fallback = 'fallback',
 }
 
-export type ContractEventArg = string | BigNumber | number | boolean;
+export type ContractEventArg = string | BigNumber | BigNumber[] |number | boolean;
 
 export interface DecodedLogArgs {
     [argName: string]: ContractEventArg;
