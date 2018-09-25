97fc4257a068fe96fc6b382a3564b1bc054bd2fd
Alex Browne <stephenalexbrowne@gmail.com>
2018-09-25 15:31:41 -0700

    Use multi-primary keys for event and filter null logIndexes

--- packages/pipeline/src/index.ts
+++ packages/pipeline/src/index.ts
@@ -1,12 +1,12 @@
+import * as R from 'ramda';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 
 import { Etherscan } from './data_sources/etherscan';
+import { parseExchangeEvents } from './data_types/events/exchange_events';
 import { ExchangeCancelEvent } from './entities/ExchangeCancelEvent';
 import { ExchangeFillEvent } from './entities/ExchangeFillEvent';
 import { config } from './ormconfig';
-
-import { parseExchangeEvents } from './data_types/events/exchange_events';
 
 const etherscan = new Etherscan(process.env.ETHERSCAN_API_KEY as string);
 const EXCHANGE_ADDRESS = '0x4f833a24e1f95d70f028921e27040ca56e09ab0b';
