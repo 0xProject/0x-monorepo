1ae3592e45d163682d8ef2ca076ee57bc170d7ad
Alex Browne <stephenalexbrowne@gmail.com>
2018-09-25 16:03:54 -0700

    Implement scraping and parsing exchange CancelUpTo events

--- packages/pipeline/src/index.ts
+++ packages/pipeline/src/index.ts
@@ -5,6 +5,7 @@ import { createConnection } from 'typeorm';
 import { Etherscan } from './data_sources/etherscan';
 import { parseExchangeEvents } from './data_types/events/exchange_events';
 import { ExchangeCancelEvent } from './entities/ExchangeCancelEvent';
+import { ExchangeCancelUpToEvent } from './entities/ExchangeCancelUpToEvent';
 import { ExchangeFillEvent } from './entities/ExchangeFillEvent';
 import { config } from './ormconfig';
 
@@ -15,14 +16,12 @@ const EXCHANGE_ADDRESS = '0x4f833a24e1f95d70f028921e27040ca56e09ab0b';
     const connection = await createConnection(config);
     const fillRepository = connection.getRepository(ExchangeFillEvent);
     const cancelRepository = connection.getRepository(ExchangeCancelEvent);
-    console.log(`found ${await fillRepository.count()} existing fill events`);
-    console.log(`found ${await cancelRepository.count()} existing cancel events`);
+    const cancelUpToRepository = connection.getRepository(ExchangeCancelUpToEvent);
+    console.log(`found ${(await fillRepository.count()) + (await cancelRepository.count())} existing events`);
     const rawEvents = await etherscan.getContractEventsAsync(EXCHANGE_ADDRESS);
     const events = parseExchangeEvents(rawEvents);
-    console.log(`got ${events.length} parsed events`);
     for (const event of events) {
         await event.save();
     }
-    console.log(`now ${await fillRepository.count()} total fill events`);
-    console.log(`now ${await cancelRepository.count()} total cancel events`);
+    console.log(`now there are ${(await fillRepository.count()) + (await cancelRepository.count())} total events`);
 })();
