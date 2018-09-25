fe523e1f3f765077bdaf4dfc345c9dca67693668
Alex Browne <stephenalexbrowne@gmail.com>
2018-09-25 12:54:10 -0700

    Re-organize event parsing and decoding

--- packages/pipeline/src/data_types/events/event_handlers/exchange_event_handler.ts
+++ packages/pipeline/src/data_types/events/event_handlers/exchange_event_handler.ts
@@ -5,70 +5,28 @@ import { AbiDecoder, BigNumber } from '@0xproject/utils';
 import { AbiDefinition, LogEntry, LogWithDecodedArgs } from 'ethereum-types';
 import * as R from 'ramda';
 
-import { ExchangeFillEvent } from '../../entities/ExchangeFillEvent';
+import { ExchangeFillEvent } from '../../../entities/ExchangeFillEvent';
+import { decodeLogEntry } from '../event_utils';
+
+import { BaseEventHandler } from './base_event_handler';
 
 // TODO(albrow): Union with other exchange event entity types
 export type ExchangeEventEntity = ExchangeFillEvent;
 
-// Raw events response from etherescan.io
-export interface EventsResponse {
-    status: string;
-    message: string;
-    result: EventsResponseResult[];
+export class ExchangeEventHandler extends BaseEventHandler<ExchangeEventEntity> {
+    public convertLogEntryToEventEntity(logEntry: LogEntry): ExchangeEventEntity {
+        const decodedLogEntry = decodeLogEntry<ExchangeEventArgs>(this._abi, logEntry);
+        return _convertToEntity(decodedLogEntry);
+    }
 }
-
-// Events as represented in the response from etherscan.io
-export interface EventsResponseResult {
-    address: string;
-    topics: string[];
-    data: string;
-    blockNumber: string;
-    timeStamp: string;
-    gasPrice: string;
-    gasUsed: string;
-    logIndex: string;
-    transactionHash: string;
-    transactionIndex: string;
-}
-
-const hexRadix = 16;
-
-function hexToInt(hex: string): number {
-    return parseInt(hex.replace('0x', ''), hexRadix);
-}
-
-// Converts a raw event response to a LogEntry
-// tslint:disable-next-line:completed-docs
-export function _convertResponseToLogEntry(result: EventsResponseResult): LogEntry {
-    return {
-        logIndex: hexToInt(result.logIndex),
-        transactionIndex: hexToInt(result.transactionIndex),
-        transactionHash: result.transactionHash,
-        blockHash: '',
-        blockNumber: hexToInt(result.blockNumber),
-        address: result.address,
-        data: result.data,
-        topics: result.topics,
-    };
-}
-
-// Decodes a LogEntry into a LogWithDecodedArgs
-// tslint:disable-next-line:completed-docs
-export const _decodeLogEntry = R.curry((contractAbi: AbiDefinition[], log: LogEntry): LogWithDecodedArgs<
-    ExchangeEventArgs
-> => {
-    const abiDecoder = new AbiDecoder([contractAbi]);
-    const logWithDecodedArgs = abiDecoder.tryToDecodeLogOrNoop(log);
-    // tslint:disable-next-line:no-unnecessary-type-assertion
-    return logWithDecodedArgs as LogWithDecodedArgs<ExchangeEventArgs>;
-});
 
 export function _convertToEntity(eventLog: LogWithDecodedArgs<ExchangeEventArgs>): ExchangeEventEntity {
     switch (eventLog.event) {
         case 'Fill':
             return _convertToExchangeFillEvent(eventLog as LogWithDecodedArgs<ExchangeFillEventArgs>);
         default:
-            throw new Error('unexpected eventLog.event type: ' + eventLog.event);
+            return new ExchangeFillEvent();
+        // throw new Error('unexpected eventLog.event type: ' + eventLog.event);
     }
 }
 
@@ -115,22 +73,4 @@ function filterEventLogs(
     eventLogs: Array<LogWithDecodedArgs<ExchangeEventArgs>>,
 ): Array<LogWithDecodedArgs<ExchangeEventArgs>> {
     return R.filter(eventLog => eventLog.event === 'Fill', eventLogs);
-}
-
-/**
- * Parses and abi-decodes the raw events response from etherscan.io.
- * @param contractAbi The ABI for the contract that the events where emited from.
- * @param rawEventsResponse The raw events response from etherescan.io.
- * @returns Parsed and decoded event entities, ready to be saved to database.
- */
-export function parseRawEventsResponse(
-    contractAbi: AbiDefinition[],
-    rawEventsResponse: EventsResponse,
-): ExchangeEventEntity[] {
-    return R.pipe(
-        R.map(_convertResponseToLogEntry),
-        R.map(_decodeLogEntry(contractAbi)),
-        filterEventLogs,
-        R.map(_convertToEntity),
-    )(rawEventsResponse.result);
 }
