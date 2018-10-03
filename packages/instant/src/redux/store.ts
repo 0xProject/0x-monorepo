48e7aa6e77a71f694f75ab8b2fc86f337700113d
fragosti <francesco.agosti93@gmail.com>
2018-10-03 15:16:59 -0700

    Add Inter UI font

--- packages/instant/src/redux/store.ts
+++ packages/instant/src/redux/store.ts
@@ -3,6 +3,4 @@ import { applyMiddleware, createStore, Store as ReduxStore } from 'redux';
 
 import { reducer, State } from './reducer';
 
-const ONE_SECOND = 1000;
-
 export const store: ReduxStore<State> = createStore(reducer);
--- packages/instant/src/style/theme.ts
+++ packages/instant/src/style/theme.ts
@@ -8,12 +8,18 @@ const {
     ThemeProvider,
 } = styledComponents as styledComponents.ThemedStyledComponentsModule<IThemeInterface>;
 
+// Inject the inter-ui font into the page
+styledComponents.injectGlobal`
+    @import url('https://rsms.me/inter/inter-ui.css');
+`;
+
 export interface IThemeInterface {
     primaryColor: string;
     black: string;
     white: string;
     darkGrey: string;
     lightGrey: string;
+    fontFamily: string;
 }
 
 export const theme: IThemeInterface = {
@@ -22,6 +28,7 @@ export const theme: IThemeInterface = {
     lightGrey: '#999999',
     darkGrey: '#333333',
     white: 'white',
+    fontFamily: 'Inter UI, sans-serif',
 };
 
 export { styled, css, injectGlobal, keyframes, ThemeProvider };
