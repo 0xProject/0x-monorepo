8cffe65047ed193b52ccacc346ce8ad1983f02f4
Fabio Berger <me@fabioberger.com>
2018-10-16 13:22:15 +0100

    fix(monorepo-scripts): Move the creation of the `.installation-test` directory OUTSIDE of the monorepo root, so that the installed packages can't reference the hoisted node_modules folder

--- packages/monorepo-scripts/src/test_installation.ts
+++ packages/monorepo-scripts/src/test_installation.ts
@@ -104,13 +104,7 @@ async function testInstallPackageAsync(
     const packageName = installablePackage.packageJson.name;
     utils.log(`Testing ${packageName}@${lastChangelogVersion}`);
     const packageDirName = path.join(...(packageName + '-test').split('/'));
-    const testDirectory = path.join(
-        monorepoRootPath,
-        'packages',
-        'monorepo-scripts',
-        '.installation-test',
-        packageDirName,
-    );
+    const testDirectory = path.join(monorepoRootPath, '..', '.installation-test', packageDirName);
     await rimrafAsync(testDirectory);
     await mkdirpAsync(testDirectory);
     await execAsync('yarn init --yes', { cwd: testDirectory });
