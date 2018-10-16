cc7b86ac21bad5dc47f5f895ee7807697a96678e
Ari Kardasis <ari.kardasis@gmail.com>
2018-10-16 14:46:25 -0400

    provide the option of using environment variables for constants in dev-utils/src/constants.ts

--- packages/dev-utils/src/constants.ts
+++ packages/dev-utils/src/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
-    RPC_URL: 'http://localhost:8545',
-    RPC_PORT: 8545,
-    GAS_LIMIT: 7000000,
-    TESTRPC_FIRST_ADDRESS: '0x5409ed021d9299bf6814279a6a1411a7e866a631',
+    RPC_URL: process.env.RPC_URL || 'http://localhost:8545',
+    RPC_PORT: process.env.RPC_PORT || 8545,
+    GAS_LIMIT: process.env.GAS_LIMIT || 7000000,
+    TESTRPC_FIRST_ADDRESS: process.env.TESTRPC_FIRST_ADDRESS || '0x5409ed021d9299bf6814279a6a1411a7e866a631',
 };
