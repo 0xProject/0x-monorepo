8c0dfc1936340a08ff627db3c6203bf6836c2992
Alex Browne <stephenalexbrowne@gmail.com>
2018-10-16 14:59:59 -0700

    Rebase and update to use new modularized artifacts

--- packages/pipeline/src/artifacts.ts
+++
@@ -1,7 +0,0 @@
-import { ContractArtifact } from 'ethereum-types';
-
-import * as Exchange from './artifacts/Exchange.json';
-
-export const artifacts = {
-    Exchange: (Exchange as any) as ContractArtifact,
-};
