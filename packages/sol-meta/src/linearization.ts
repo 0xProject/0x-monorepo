fe4cc6eafb64e06430b246d2a3b1864d862f38bb
Remco Bloemen <remco@wicked.ventures>
2018-10-15 22:22:29 -0700

    Add C3 Linearization function

---
+++ packages/sol-meta/src/linearization.ts
@@ -0,0 +1,49 @@
+// Implements C3 Linearization as used in Solidity
+// see: https://www.python.org/download/releases/2.3/mro/
+
+// Produce an array such that each input array is a subarray of the result.
+// In other words, if the inputs specify lists sorted according to some partial
+// order produce a sorted list of all elements compatible.
+//
+// NOTE: merge(a.reverse(), b.reversed(), ...) == merge(a, b, ...).reversed()
+//      (or at least equal up to the implied partial order)
+export function merge<T>(...ilists: T[][]): T[] {
+    
+    // Only consider non-empty lists
+    const lists = ilists.filter(x => x.length > 0);
+    if (lists.length === 0) {
+        return [];
+    }
+    
+    // The first item of each list are heads, the remainders are tails
+    const heads = lists.map(([head, ...tail]) => head);
+    const tails = lists.map(([head, ...tail]) => tail);
+    
+    // A good head is one that does not occur in any tail
+    const goodHead = heads.find(head =>
+        tails.every(tail => !tail.includes(head)));
+    
+    // If there is no  valid head the hierarchy can not be linearized.
+    if (goodHead === undefined) {
+        throw new Error("Hierarchy can not be linearized.");
+    }
+    
+    // Remove the good head from the lists
+    const newLists = lists.map(list => list.filter(elem => elem !== goodHead));
+    
+    // Prepend head to the linearization and repeat
+    return [goodHead, ...merge(...newLists)];
+}
+
+// Given a final element and an parents function, compute the C3 linearization
+// of all ancestors of the final element.
+// NOTE: Solidity has its ancestors in reverse order (base to most derived)
+//       compared to the Python C3 Linearization algorithm (most derived to
+//       base). This version uses the Solidity convention.
+export function linearize<T>(final: T, parents: (_:T) => T[]) : T[] {
+    // TODO: Memoization
+    // TODO: Throw on cycles (instead of stack overflowing)
+    const p = parents(final);
+    const recurse = p.map(a => linearize<T>(a, parents));
+    return [...merge(p, ...recurse), final];
+}
