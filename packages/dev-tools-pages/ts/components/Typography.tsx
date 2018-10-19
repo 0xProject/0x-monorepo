a9b4c1cba87168531752c48b8fe8e58f267eb98c
Megan Pearson <megan.e.pearson@gmail.com>
2018-10-19 14:30:38 +0200

    Responsive font sizes in typography and globalstyles

--- packages/dev-tools-pages/ts/components/Typography.tsx
+++ packages/dev-tools-pages/ts/components/Typography.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
+import { media } from '../variables';
 
 const Alpha = styled.h2`
     font-size: 1.75rem;
     line-height: 1;
+
+    ${media.small`font-size: 1.5rem;`};
 `;
 
 const Beta = styled.h3`
     font-size: 1.25rem;
     line-height: 1.65;
+
+    ${media.small`font-size: 1rem;`};
 `;
 
 const Gamma = styled.h4`
     font-size: 1rem;
+
+    ${media.small`font-size: 0.875rem;`};
 `;
 
 const Small = styled.p`
