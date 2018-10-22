4bf1ca0d1777d56126b3c5113a129738ba3c8d0d
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-10-22 14:27:38 -0700

    Making SecondaryButton accept children instead of text prop

---
+++ packages/instant/src/components/placing_order_button.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { ColorOption } from '../style/theme';
+
+import { Button } from './ui/button';
+import { Container } from './ui/container';
+import { Spinner } from './ui/spinner';
+import { Text } from './ui/text';
+
+export const PlacingOrderButton: React.StatelessComponent<{}> = props => (
+    <Button isDisabled={true} width="100%">
+        <Container display="inline-block" position="relative" top="2px" marginRight="5px">
+            <Spinner widthPx={20} heightPx={20} />
+        </Container>
+        <Text fontColor={ColorOption.white} fontWeight={600} fontSize="20px">
+            Placing Order&hellip;
+        </Text>
+    </Button>
+);
