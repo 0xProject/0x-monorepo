194cbc3ba941023a0b6c1845a867ccfd619b754f
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-10-22 15:57:16 -0700

    Make icon a bit better

--- packages/instant/src/components/placing_order_button.tsx
+++ packages/instant/src/components/placing_order_button.tsx
@@ -9,7 +9,7 @@ import { Text } from './ui/text';
 
 export const PlacingOrderButton: React.StatelessComponent<{}> = props => (
     <Button isDisabled={true} width="100%">
-        <Container display="inline-block" position="relative" top="2px" marginRight="5px">
+        <Container display="inline-block" position="relative" top="3px" marginRight="8px">
             <Spinner widthPx={20} heightPx={20} />
         </Container>
         <Text fontColor={ColorOption.white} fontWeight={600} fontSize="20px">
