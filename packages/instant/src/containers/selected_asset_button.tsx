579a49ba918ef3c10f3eee0c63ff8d8f8be61fdf
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-10-22 14:28:22 -0700

    Placing order button

--- packages/instant/src/containers/selected_asset_button.tsx
+++ packages/instant/src/containers/selected_asset_button.tsx
@@ -6,6 +6,8 @@ import { SecondaryButton } from '../components/secondary_button';
 import { State } from '../redux/reducer';
 import { AsyncProcessState } from '../types';
 
+import { PlacingOrderButton } from '../components/placing_order_button';
+
 import { SelectedAssetBuyButton } from './selected_asset_buy_button';
 import { SelectedAssetRetryButton } from './selected_asset_retry_button';
 
@@ -23,9 +25,9 @@ const SelectedAssetButtonPresentationComponent: React.StatelessComponent<{
     if (props.buyOrderState === AsyncProcessState.FAILURE) {
         return <SelectedAssetRetryButton />;
     } else if (props.buyOrderState === AsyncProcessState.SUCCESS) {
-        return <SecondaryButton text="Success" isDisabled={true} />;
+        return <SecondaryButton isDisabled={true}>Success</SecondaryButton>;
     } else if (props.buyOrderState === AsyncProcessState.PENDING) {
-        return <SecondaryButton text="Processing" isDisabled={true} />;
+        return <PlacingOrderButton />;
     }
 
     return <SelectedAssetBuyButton />;
