071f9a5a73eef54b657adce66d938ee444bd1d72
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-10-22 14:28:02 -0700

    Spinner: change prop names and fix svg props

--- packages/instant/src/components/ui/spinner.tsx
+++ packages/instant/src/components/ui/spinner.tsx
@@ -3,26 +3,26 @@ import * as React from 'react';
 import { FullRotation } from '../animations/full_rotation';
 
 export interface SpinnerProps {
-    pxWidth: number;
-    pxHeight: number;
+    widthPx: number;
+    heightPx: number;
 }
 export const Spinner: React.StatelessComponent<SpinnerProps> = props => {
     return (
-        <FullRotation width={`${props.pxWidth}px`} height={`${props.pxHeight}px`}>
+        <FullRotation width={`${props.widthPx}px`} height={`${props.heightPx}px`}>
             <svg
-                width={props.pxWidth}
-                height={props.pxHeight}
+                width={props.widthPx}
+                height={props.heightPx}
                 viewBox="0 0 34 34"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
             >
-                <circle cx="17" cy="17" r="15" stroke="white" stroke-opacity="0.2" stroke-width="4" />
+                <circle cx="17" cy="17" r="15" stroke="white" strokeOpacity="0.2" strokeWidth="4" />
                 <path
                     d="M17 32C25.2843 32 32 25.2843 32 17C32 8.71573 25.2843 2 17 2"
                     stroke="white"
-                    stroke-width="4"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
             </svg>
         </FullRotation>
