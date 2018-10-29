25ca3d4c29ad98b75e256424246a38a72e5a26da
Brandon Millman <brandon.millman@gmail.com>
2018-10-29 12:51:38 -0700

    feat(instant): add default gasPrice of 6 gwei to transactions

--- packages/instant/src/constants.ts
+++ packages/instant/src/constants.ts
@@ -3,3 +3,5 @@ export const BIG_NUMBER_ZERO = new BigNumber(0);
 export const ETH_DECIMALS = 18;
 export const DEFAULT_ZERO_EX_CONTAINER_SELECTOR = '#zeroExInstantContainer';
 export const WEB_3_WRAPPER_TRANSACTION_FAILED_ERROR_MSG_PREFIX = 'Transaction failed';
+export const GWEI_IN_WEI = new BigNumber(1000000000);
+export const DEFAULT_GAS_PRICE = GWEI_IN_WEI.mul(6);
