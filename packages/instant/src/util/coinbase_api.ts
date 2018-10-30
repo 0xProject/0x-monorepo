2ad2644b6bcb3c1f4da5891d79856af46e808eab
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-10-29 17:11:15 -0700

    Use polyfilled fetch from utils

--- packages/instant/src/util/coinbase_api.ts
+++ packages/instant/src/util/coinbase_api.ts
@@ -1,10 +1,10 @@
-import { BigNumber } from '@0x/utils';
+import { BigNumber, fetchAsync } from '@0x/utils';
 
 import { COINBASE_API_BASE_URL } from '../constants';
 
 export const coinbaseApi = {
     getEthUsdPrice: async (): Promise<BigNumber> => {
-        const res = await fetch(`${COINBASE_API_BASE_URL}/prices/ETH-USD/buy`);
+        const res = await fetchAsync(`${COINBASE_API_BASE_URL}/prices/ETH-USD/buy`);
         const resJson = await res.json();
         return new BigNumber(resJson.data.amount);
     },
