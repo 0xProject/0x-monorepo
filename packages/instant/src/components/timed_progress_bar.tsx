f82d16a5b06a47f3ede917165ba75dfd73f48137
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-11-01 11:15:07 -0700

    Use primary color instead of black

--- packages/instant/src/components/timed_progress_bar.tsx
+++ packages/instant/src/components/timed_progress_bar.tsx
@@ -70,7 +70,7 @@ export const TimedProgress =
     styled.div <
     TimedProgressProps >
     `
-    background-color: black;
+    background-color: ${props => props.theme[ColorOption.primaryColor]};
     border-radius: 6px;
     height: 6px;
     animation: ${props => expandingWidthKeyframes(props.fromWidth, props.toWidth)}
