61a1a0be974c8610623f2bcb9a43797532cc78d9
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-11-01 11:29:33 -0700

    Move BuyOrderProgress to its own component

--- packages/instant/src/containers/selected_asset_progress.tsx
+++ packages/instant/src/containers/selected_asset_progress.tsx
@@ -1,49 +1,15 @@
-import * as React from 'react';
+// TODO: rename file and props to SelectedAssetBuyOrderProgress and use .ts
 
 import { connect } from 'react-redux';
 
-import { TimedProgressBar } from '../components/timed_progress_bar';
-
-import { TimeCounter } from '../components/time_counter';
-import { Container } from '../components/ui';
+import { BuyOrderProgress } from '../components/buy_order_progress';
 import { State } from '../redux/reducer';
-import { OrderProcessState, OrderState, SimulatedProgress } from '../types';
-
-interface SelectedAssetProgressComponentProps {
-    buyOrderState: OrderState;
-}
-// TODO: rename this component and move to seperate file, and get props using mapStateToProps
-export const SelectedAssetSimulatedProgressComponent: React.StatelessComponent<
-    SelectedAssetProgressComponentProps
-> = props => {
-    const { buyOrderState } = props;
-
-    if (
-        buyOrderState.processState === OrderProcessState.PROCESSING ||
-        buyOrderState.processState === OrderProcessState.SUCCESS ||
-        buyOrderState.processState === OrderProcessState.FAILURE
-    ) {
-        const progress = buyOrderState.progress;
-        const hasEnded = buyOrderState.processState !== OrderProcessState.PROCESSING;
-        const expectedTimeMs = progress.expectedEndTimeUnix - progress.startTimeUnix;
-        return (
-            <Container padding="20px 20px 0px 20px" width="100%">
-                <Container marginBottom="5px">
-                    <TimeCounter estimatedTimeMs={expectedTimeMs} hasEnded={hasEnded} key={progress.startTimeUnix} />
-                </Container>
-                <TimedProgressBar expectedTimeMs={expectedTimeMs} hasEnded={hasEnded} key={progress.startTimeUnix} />
-            </Container>
-        );
-    }
-
-    return null;
-};
+import { OrderState } from '../types';
 
 interface ConnectedState {
     buyOrderState: OrderState;
-    simulatedProgress?: SimulatedProgress;
 }
 const mapStateToProps = (state: State, _ownProps: {}): ConnectedState => ({
     buyOrderState: state.buyOrderState,
 });
-export const SelectedAssetProgress = connect(mapStateToProps)(SelectedAssetSimulatedProgressComponent);
+export const SelectedAssetProgress = connect(mapStateToProps)(BuyOrderProgress);
