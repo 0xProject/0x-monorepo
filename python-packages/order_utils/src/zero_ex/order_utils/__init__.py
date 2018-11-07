0cdaeca021ad149393b775cf71c932f6f9654491
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2018-11-07 09:53:35 -0500

    squashme: grammar
    "just such this purpose" -> "just this purpose"
    "contracts deployed in it" -> "contracts deployed on it"

--- python-packages/order_utils/src/zero_ex/order_utils/__init__.py
+++ python-packages/order_utils/src/zero_ex/order_utils/__init__.py
@@ -3,9 +3,9 @@
 Some methods require the caller to pass in a `Web3.HTTPProvider` object.  For
 local testing one may construct such a provider pointing at an instance of
 `ganache-cli <https://www.npmjs.com/package/ganache-cli>`_ which has the 0x
-contracts deployed in it.  For convenience, a docker container is provided for
-just such this purpose.  To start it: ``docker run -d -p 8545:8545
-0xorg/ganache-cli --gasLimit 10000000 --db /snapshot --noVMErrorsOnRPCResponse
--p 8545 --networkId 50 -m "concert load couple harbor equip island argue ramp
-clarify fence smart topic"``.
+contracts deployed on it.  For convenience, a docker container is provided for
+just this purpose.  To start it: ``docker run -d -p 8545:8545 0xorg/ganache-cli
+--gasLimit 10000000 --db /snapshot --noVMErrorsOnRPCResponse -p 8545
+--networkId 50 -m "concert load couple harbor equip island argue ramp clarify
+fence smart topic"``.
 """
