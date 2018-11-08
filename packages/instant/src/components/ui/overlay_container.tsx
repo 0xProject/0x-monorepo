37dfc88007bcfcc66d39f719d698fea69a9c9a9c
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-11-07 20:53:18 -0800

    Cleanup so onClick doesn't trigger

--- packages/instant/src/components/ui/overlay_container.tsx
+++ packages/instant/src/components/ui/overlay_container.tsx
@@ -1,38 +1,43 @@
 import * as React from 'react';
 
-import { media, generateMediaWrapper } from '../../style/media';
+import { generateMediaWrapper, ScreenWidths } from '../../style/media';
 import { styled } from '../../style/theme';
 
 import { Container, ContainerProps } from './container';
 
 // TODO: move z-index to constant
+interface OverlayProps {
+    showMaxWidthEm?: ScreenWidths;
+}
+const Overlay =
+    styled.div <
+    OverlayProps >
+    `
+    top: 0;
+    left: 0;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    display: ${props => (props.showMaxWidthEm ? 'none' : 'block')};
+    position: absolute;
+    background: black;
+    opacity: 0.6;
+    ${props => props.showMaxWidthEm && generateMediaWrapper(props.showMaxWidthEm)`display: block;`}
+`;
 
-// Todo: hide when Sm, Md, or Lg, or keep like this use generateMediaWrapper
 interface PlainOverlayContainerProps extends ContainerProps {
     showMaxWidthEm?: number;
 }
-const PlainOverlayContainer =
-    styled(Container) <
-    PlainOverlayContainerProps >
-    `
-    :after {
-        content: '';
-        top: 0;
-        left: 0;
-        z-index: 10;
-        width: 100%;
-        height: 100%;
-        display: ${props => (props.showMaxWidthEm ? 'none' : 'block')};
-        position: absolute;
-        background: black;
-        opacity: 0.6;
-       ${props => props.showMaxWidthEm && generateMediaWrapper(props.showMaxWidthEm)`display: block;`}
-    }
-`;
+// TODO: rename onClick to onOverlayClick
 export interface OverlayContainerProps extends PlainOverlayContainerProps {
     onClick: () => void;
 }
 export const OverlayContainer: React.StatelessComponent<OverlayContainerProps> = props => {
-    const { onClick, ...otherProps } = props;
-    return <PlainOverlayContainer showMaxWidthEm={props.showMaxWidthEm} onClick={onClick} {...otherProps} />;
+    const { onClick, showMaxWidthEm, ...otherProps } = props;
+    return (
+        <React.Fragment>
+            <Container {...otherProps} />
+            <Overlay onClick={onClick} showMaxWidthEm={showMaxWidthEm} />
+        </React.Fragment>
+    );
 };
