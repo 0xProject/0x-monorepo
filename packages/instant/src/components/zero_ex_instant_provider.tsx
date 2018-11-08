8493d619b45d84eedf9f8d349e45a4d3e759c538
Brandon Millman <brandon.millman@gmail.com>
2018-11-08 10:34:36 -0800

    chore(instant): fix linter

--- packages/instant/src/components/zero_ex_instant_provider.tsx
+++ packages/instant/src/components/zero_ex_instant_provider.tsx
@@ -92,6 +92,7 @@ export class ZeroExInstantProvider extends React.Component<ZeroExInstantProvider
             // tslint:disable-next-line:no-floating-promises
             asyncData.fetchAvailableAssetDatasAndDispatchToStore(this._store);
         }
+        // tslint:disable-next-line:no-floating-promises
         asyncData.fetchCurrentBuyQuoteAndDispatchToStore(this._store);
         // warm up the gas price estimator cache just in case we can't
         // grab the gas price estimate when submitting the transaction
