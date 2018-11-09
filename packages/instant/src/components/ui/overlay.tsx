d6960369ab3f62393e1ba2bae6d79b3ea06f6570
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-11-09 09:27:08 -0800

    Use existing overlay instead of generating new one

--- packages/instant/src/components/ui/overlay.tsx
+++ packages/instant/src/components/ui/overlay.tsx
@@ -1,11 +1,15 @@
 import * as _ from 'lodash';
 
+import { generateMediaWrapper, ScreenWidths } from '../../style/media';
 import { overlayBlack, styled } from '../../style/theme';
 import { zIndex } from '../../style/z_index';
 
 export interface OverlayProps {
     zIndex?: number;
     backgroundColor?: string;
+    width?: string;
+    height?: string;
+    showMaxWidth?: ScreenWidths;
 }
 
 export const Overlay =
@@ -20,6 +24,10 @@ export const Overlay =
         left: 0;
         z-index: ${props => props.zIndex}
         background-color: ${props => props.backgroundColor};
+        ${props => props.width && `width: ${props.width};`}
+        ${props => props.height && `height: ${props.height};`}
+        display: ${props => (props.showMaxWidth ? 'none' : 'block')};
+        ${props => props.showMaxWidth && generateMediaWrapper(props.showMaxWidth)`display: block;`}
     }
 `;
 
--- packages/instant/src/components/ui/overlay_container.tsx
+++ packages/instant/src/components/ui/overlay_container.tsx
@@ -1,37 +1,13 @@
 import * as React from 'react';
 
-import { generateMediaWrapper, ScreenWidths } from '../../style/media';
-import { styled } from '../../style/theme';
 import { zIndex } from '../../style/z_index';
 
 import { Container, ContainerProps } from './container';
+import { Overlay } from './overlay';
 
-interface OverlayProps {
-    showMaxWidth?: ScreenWidths;
-}
-const Overlay =
-    styled.div <
-    OverlayProps >
-    `
-    && {
-        top: 0;
-        left: 0;
-        z-index: ${zIndex.containerOverlay};
-        width: 100%;
-        height: 100%;
-        display: ${props => (props.showMaxWidth ? 'none' : 'block')};
-        position: absolute;
-        background: black;
-        opacity: 0.6;
-        ${props => props.showMaxWidth && generateMediaWrapper(props.showMaxWidth)`display: block;`}
-    }
-`;
-
-interface PlainOverlayContainerProps extends ContainerProps {
+export interface OverlayContainerProps extends ContainerProps {
     showOverlay: boolean;
     showMaxWidth?: number;
-}
-export interface OverlayContainerProps extends PlainOverlayContainerProps {
     onOverlayClick?: () => void;
 }
 export const OverlayContainer: React.StatelessComponent<OverlayContainerProps> = props => {
@@ -39,7 +15,9 @@ export const OverlayContainer: React.StatelessComponent<OverlayContainerProps> =
     return (
         <React.Fragment>
             <Container {...otherProps} />
-            {props.showOverlay && <Overlay onClick={onOverlayClick} showMaxWidth={showMaxWidth} />}
+            {props.showOverlay && (
+                <Overlay onClick={onOverlayClick} zIndex={zIndex.containerOverlay} showMaxWidth={showMaxWidth} />
+            )}
         </React.Fragment>
     );
 };
