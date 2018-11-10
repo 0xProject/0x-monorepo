ba292ead45f451e4d7d2fac74582b8406cd4a586
fragosti <francesco.agosti93@gmail.com>
2018-11-09 16:19:12 -0800

    fix: set all button font sizes in buy flow to 16px

--- packages/instant/src/components/placing_order_button.tsx
+++ packages/instant/src/components/placing_order_button.tsx
@@ -7,9 +7,9 @@ import { Container } from './ui/container';
 import { Spinner } from './ui/spinner';
 
 export const PlacingOrderButton: React.StatelessComponent<{}> = props => (
-    <Button isDisabled={true} width="100%" fontColor={ColorOption.white} fontSize="20px">
+    <Button isDisabled={true} width="100%" fontColor={ColorOption.white} fontSize="16px">
         <Container display="inline-block" position="relative" top="3px" marginRight="8px">
-            <Spinner widthPx={20} heightPx={20} />
+            <Spinner widthPx={16} heightPx={16} />
         </Container>
         Placing Order&hellip;
     </Button>
