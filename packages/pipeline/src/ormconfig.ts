bfd4be559477425233c59b836e19a1b2d4a68ad6
Alex Browne <stephenalexbrowne@gmail.com>
2018-11-12 16:40:20 -0800

    Configure TypeORM for migrations. Add new package.json scripts.

--- packages/pipeline/src/ormconfig.ts
+++ packages/pipeline/src/ormconfig.ts
@@ -20,28 +20,13 @@ const entities = [
     Transaction,
 ];
 
-export const testConfig: ConnectionOptions = {
-    type: 'sqlite',
-    database: 'database.sqlite',
-    synchronize: true,
-    logging: false,
-    entities,
-    migrations: ['./lib/src/migrations/**/*.js'],
-    cli: {
-        entitiesDir: 'lib/src/entities',
-        migrationsDir: 'lib/src/migrations',
-    },
-};
-
-export const deployConfig: ConnectionOptions = {
+const config: ConnectionOptions = {
     type: 'postgres',
     url: process.env.ZEROEX_DATA_PIPELINE_DB_URL,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities,
-    migrations: ['./lib/src/migrations/**/*.js'],
-    cli: {
-        entitiesDir: 'lib/src/entities',
-        migrationsDir: 'lib/src/migrations',
-    },
+    migrations: ['./lib/migrations/**/*.js'],
 };
+
+module.exports = config as ConnectionOptions;
