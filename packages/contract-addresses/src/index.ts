d4462bd80b3e9c63048c45378167a36d49c50637
Amir Bandeali <abandeali1@gmail.com>
2018-11-13 17:03:15 -0800

    feat: import contract addresses from artifacts

--- packages/contract-addresses/src/index.ts
+++ packages/contract-addresses/src/index.ts
@@ -1,3 +1,14 @@
+import {
+    AssetProxyOwner,
+    ERC20Proxy,
+    ERC721Proxy,
+    Exchange,
+    Forwarder,
+    OrderValidator,
+    WETH9,
+    ZRXToken,
+} from '@0x/contract-artifacts';
+import { ContractArtifact } from 'ethereum-types';
 import * as _ from 'lodash';
 
 export interface ContractAddresses {
@@ -19,34 +30,34 @@ export enum NetworkId {
 
 const networkToAddresses: { [networkId: number]: ContractAddresses } = {
     1: {
-        erc20Proxy: '0x2240dab907db71e64d3e0dba4800c83b5c502d4e',
-        erc721Proxy: '0x208e41fb445f1bb1b6780d58356e81405f3e6127',
-        zrxToken: '0xe41d2489571d322189246dafa5ebde1f4699f498',
-        etherToken: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-        exchange: '0x4f833a24e1f95d70f028921e27040ca56e09ab0b',
-        assetProxyOwner: '0x17992e4ffb22730138e4b62aaa6367fa9d3699a6',
-        forwarder: '0x5468a1dc173652ee28d249c271fa9933144746b1',
-        orderValidator: '0x9463e518dea6810309563c81d5266c1b1d149138',
+        erc20Proxy: (ERC20Proxy as ContractArtifact).networks[NetworkId.Mainnet].address,
+        erc721Proxy: (ERC721Proxy as ContractArtifact).networks[NetworkId.Mainnet].address,
+        zrxToken: (ZRXToken as ContractArtifact).networks[NetworkId.Mainnet].address,
+        etherToken: (WETH9 as ContractArtifact).networks[NetworkId.Mainnet].address,
+        exchange: (Exchange as ContractArtifact).networks[NetworkId.Mainnet].address,
+        assetProxyOwner: (AssetProxyOwner as ContractArtifact).networks[NetworkId.Mainnet].address,
+        forwarder: (Forwarder as ContractArtifact).networks[NetworkId.Mainnet].address,
+        orderValidator: (OrderValidator as ContractArtifact).networks[NetworkId.Mainnet].address,
     },
     3: {
-        erc20Proxy: '0xb1408f4c245a23c31b98d2c626777d4c0d766caa',
-        erc721Proxy: '0xe654aac058bfbf9f83fcaee7793311dd82f6ddb4',
-        zrxToken: '0xff67881f8d12f372d91baae9752eb3631ff0ed00',
-        etherToken: '0xc778417e063141139fce010982780140aa0cd5ab',
-        exchange: '0x4530c0483a1633c7a1c97d2c53721caff2caaaaf',
-        assetProxyOwner: '0xf5fa5b5fed2727a0e44ac67f6772e97977aa358b',
-        forwarder: '0x2240dab907db71e64d3e0dba4800c83b5c502d4e',
-        orderValidator: '0x90431a90516ab49af23a0530e04e8c7836e7122f',
+        erc20Proxy: (ERC20Proxy as ContractArtifact).networks[NetworkId.Ropsten].address,
+        erc721Proxy: (ERC721Proxy as ContractArtifact).networks[NetworkId.Ropsten].address,
+        zrxToken: (ZRXToken as ContractArtifact).networks[NetworkId.Ropsten].address,
+        etherToken: (WETH9 as ContractArtifact).networks[NetworkId.Ropsten].address,
+        exchange: (Exchange as ContractArtifact).networks[NetworkId.Ropsten].address,
+        assetProxyOwner: (AssetProxyOwner as ContractArtifact).networks[NetworkId.Ropsten].address,
+        forwarder: (Forwarder as ContractArtifact).networks[NetworkId.Ropsten].address,
+        orderValidator: (OrderValidator as ContractArtifact).networks[NetworkId.Ropsten].address,
     },
     42: {
-        erc20Proxy: '0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e',
-        erc721Proxy: '0x2a9127c745688a165106c11cd4d647d2220af821',
-        zrxToken: '0x2002d3812f58e35f0ea1ffbf80a75a38c32175fa',
-        etherToken: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
-        exchange: '0x35dd2932454449b14cee11a94d3674a936d5d7b2',
-        assetProxyOwner: '0x2c824d2882baa668e0d5202b1e7f2922278703f8',
-        forwarder: '0x17992e4ffb22730138e4b62aaa6367fa9d3699a6',
-        orderValidator: '0xb389da3d204b412df2f75c6afb3d0a7ce0bc283d',
+        erc20Proxy: (ERC20Proxy as ContractArtifact).networks[NetworkId.Kovan].address,
+        erc721Proxy: (ERC721Proxy as ContractArtifact).networks[NetworkId.Kovan].address,
+        zrxToken: (ZRXToken as ContractArtifact).networks[NetworkId.Kovan].address,
+        etherToken: (WETH9 as ContractArtifact).networks[NetworkId.Kovan].address,
+        exchange: (Exchange as ContractArtifact).networks[NetworkId.Kovan].address,
+        assetProxyOwner: (AssetProxyOwner as ContractArtifact).networks[NetworkId.Kovan].address,
+        forwarder: (Forwarder as ContractArtifact).networks[NetworkId.Kovan].address,
+        orderValidator: (OrderValidator as ContractArtifact).networks[NetworkId.Kovan].address,
     },
 };
 
