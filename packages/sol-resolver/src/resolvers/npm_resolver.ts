9ac5d5fd2c9ebf8a31d2ac40a84e7b0ad5d37990
Jaime van Oers <jaimevanoers@gmail.com>
2018-11-15 12:01:08 +0000

    Fix *nix-like hardcoded paths in NPM Resolver
    The NPM resolver currently assumes a hardcoded root directory
    of '/' and a path separator of '/'.
    
    Currently it uses a while loop to recursively move up directories
    from the current project working directory to the root directory,
    searching for dependencies in possible node_modules locations.
    
    The while loop breaks if 'trial directory === the hardcoded root'
    i.e. trial as a result of moving up directories  === '/'
    
    On windows machines this statement will never be true, as the root
    path of the file system is 'X:/' (where X is any letter [A-Z]),
    thus this loop is infinitely recursive.
    
    Additionally, it hardcodes a path separator of '/' which on Windows
    is instead '\'.
    
    This fix utilises Node's idiomatic path library to gather the current
    system root directory and separator.
    
    It uses 'path.parse(...).root' for the system root and 'path.sep'.

--- packages/sol-resolver/src/resolvers/npm_resolver.ts
+++ packages/sol-resolver/src/resolvers/npm_resolver.ts
@@ -7,16 +7,20 @@ import { Resolver } from './resolver';
 
 export class NPMResolver extends Resolver {
     private readonly _packagePath: string;
+    private readonly _packageParsedPath: path.ParsedPath;
+
     constructor(packagePath: string) {
         super();
         this._packagePath = packagePath;
+        this._packageParsedPath = path.parse(packagePath);
     }
     public resolveIfExists(importPath: string): ContractSource | undefined {
-        if (!importPath.startsWith('/')) {
-            const [packageName, ...other] = importPath.split('/');
+        if (!importPath.startsWith(this._packageParsedPath.root)) {
+            const [packageName, ...other] = importPath.split(path.sep);
             const pathWithinPackage = path.join(...other);
             let currentPath = this._packagePath;
-            const ROOT_PATH = '/';
+            const ROOT_PATH = this._packageParsedPath.root;
+
             while (currentPath !== ROOT_PATH) {
                 const lookupPath = path.join(currentPath, 'node_modules', packageName, pathWithinPackage);
                 if (fs.existsSync(lookupPath) && fs.lstatSync(lookupPath).isFile()) {
