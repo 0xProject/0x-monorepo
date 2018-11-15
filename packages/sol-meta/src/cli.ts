ef502299631c06744c3e0bf7644a1d41b6459385
Remco Bloemen <remco@wicked.ventures>
2018-11-15 19:46:04 +0100

    Refactor CLI

--- packages/sol-meta/src/cli.ts
+++ packages/sol-meta/src/cli.ts
@@ -1,56 +1,142 @@
 import 'source-map-support/register';
 
-import { logUtils } from '@0xproject/utils';
-import * as _ from 'lodash';
+import * as pathUtils from 'path';
+import * as process from 'process';
+import * as S from 'solidity-parser-antlr';
 import * as yargs from 'yargs';
 
-import { Compiler } from './compiler';
-
-const SEPARATOR = ',';
+import { ContractMockOptions, mockContract, mockContractName } from './contractMocker';
+import { compile } from './solcWrapper';
+import { readSources, SourceReaderOptions } from './source_reader';
+import { unparse } from './unparser';
+import * as utils from './utils';
+import { visit, Visitor } from './visitor';
 
 (async () => {
     // Parse command line arguments and handle help
     const argv = yargs
+        .usage('Usage: $0 <sources> [options]')
+        .help()
+        .option('config', {
+            type: 'string',
+            description: 'config file',
+        })
         .option('remapping', {
             type: 'string',
-            description: 'search path remappings for contracts',
+            array: true,
+            description: 'path remappings for import statements',
         })
         .option('includes', {
             type: 'string',
-            description: 'search path for contracts',
-        })
-        .option('sources', {
-            type: 'string',
-            description: 'comma separated list of Solidity files to process',
+            array: true,
+            description: 'search paths for imported source files',
         })
         .option('output', {
             type: 'string',
             description: 'directory to output too',
         })
-        .help().argv;
+        .options('test', {
+            type: 'boolean',
+            description: 'run all generated mock contracts through solc',
+        }).argv;
+
+    // TODO: CommandLineOptions object
+    let options: Partial<SourceReaderOptions> = {};
+    let defaults: ContractMockOptions = {
+        constructors: {},
+        scripted: {},
+    };
+    let contracts: { [contractName: string]: ContractMockOptions } = {};
+    let outputPath = `${process.cwd()}/out`;
+
+    // Handle config file
+    if (argv.config) {
+        const config = JSON.parse(await utils.readFileAsync(argv.config));
+        if (config.options) {
+            options = { ...options, ...config.options };
+        }
+        if (config.constructors) {
+            defaults.constructors = { ...defaults.constructors, ...config.constructors };
+        }
+        if (config.scripted) {
+            defaults.scripted = { ...defaults.scripted, ...config.scripted };
+        }
+        if (config.contracts) {
+            contracts = { ...contracts, ...config.contracts };
+        }
+    }
 
     // Handle command line arguments
-    const options = Compiler.defaultOptions;
-    if (argv.paths) {
-        options.includes = argv.paths.split(SEPARATOR) as string[];
+    if (argv.remapping) {
+        options.remapping = (argv.remapping as string[])
+            .map((str: string) => str.split('=', 2))
+            .reduce((a, [from, to]) => ({ ...a, [from]: to }), {});
     }
-    if (argv.sources) {
-        options.sources = argv.sources.split(SEPARATOR) as string[];
+    if (argv.includes) {
+        options.includes = argv.includes;
     }
-    if (argv.output) {
-        options.output = argv.output as string;
-    }
+    options.sources = [...options.sources, ...argv._];
 
-    // Instantiate and run compiler
-    console.time('Compilation');
-    const compiler = new Compiler(options);
-    await compiler.compileAsync();
-    console.timeEnd('Compilation');
+    // Parse all sources
+    console.time('Parsing sources');
+    const sources = await readSources(options.sources, options);
+    console.timeEnd('Parsing sources');
+
+    for (const contractName in contracts) {
+        if (contracts.hasOwnProperty(contractName)) {
+            console.log(`\n${contractName}`);
+            const spec = {
+                constructors: {
+                    ...defaults.constructors,
+                    ...contracts[contractName].constructors,
+                },
+                scripted: {
+                    ...defaults.scripted,
+                    ...contracts[contractName].scripted,
+                },
+            };
+
+            // Find path
+            const path = Object.keys(sources).find(path => contractName in sources[path].contracts);
+            if (path === undefined) {
+                throw new Error(`Could not find contract ${contractName}.`);
+            }
+
+            // Create mock contract
+            console.time('Generating mocks');
+            const mock = mockContract(sources, path, contractName, spec);
+            console.timeEnd('Generating mocks');
+
+            // Optionally test
+            if (argv.test) {
+                console.time('Test compile');
+                await compile(sources, mock);
+                console.timeEnd('Test compile');
+            }
+
+            // Make import paths relative
+            mock.children = mock.children.map(node =>
+                visit(node, {
+                    ImportDirective: importDirective => ({
+                        ...importDirective,
+                        path: pathUtils.relative(outputPath, importDirective.path),
+                    }),
+                    SourceMember: n => n,
+                }),
+            );
+
+            // Write file
+            console.time('Writing mocks');
+            await utils.writeFileAsync(`${outputPath}/${mockContractName(contractName)}.sol`, unparse(mock));
+            console.timeEnd('Writing mocks');
+        }
+    }
 
     // Exit successfully
     process.exit(0);
+
+    // Catch errors, log and exit with failure
 })().catch(err => {
-    // Catch and log errorsm exit with failure
-    logUtils.log(err);
+    console.error(err);
     process.exit(1);
 });
