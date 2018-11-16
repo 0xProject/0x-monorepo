f8681a7780d8db0a1f941982bc5b311dfe93f7c3
fragosti <francesco.agosti93@gmail.com>
2018-11-15 16:20:06 -0800

    fix: deploy commands

--- packages/instant/webpack.config.js
+++ packages/instant/webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const ip = require('ip');
 // The common js bundle (not this one) is built using tsc.
 // The umd bundle (this one) has a different entrypoint.
-
+const outputPath = process.env.WEBPACK_OUTPUT_PATH || 'umd';
 const config = {
     entry: {
         instant: './src/index.umd.ts',
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'umd'),
+        path: path.resolve(__dirname, outputPath),
         library: 'zeroExInstant',
         libraryTarget: 'umd',
     },
