0b46f44ad37afb17341b394b16e3317e3bbbd285
August Skare <post@augustskare.no>
2018-11-19 09:32:53 +0100

    fix typings for components in header

--- packages/dev-tools-pages/ts/components/Header.tsx
+++ packages/dev-tools-pages/ts/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.StatelessComponent<{}> = () => (
             <StyledHeader>
                 <Container>
                     <LogoMark>
-                        <Logo as={icon} />
+                        <Logo as={icon as 'svg'} />
                         <Title>{title}</Title>
                     </LogoMark>
 
@@ -50,7 +50,7 @@ const LogoMark = styled.div`
     align-items: center;
 `;
 
-const StyledLogo = styled.div`
+const Logo = styled.div`
     color: ${props => props.theme.colors.main};
     width: 1.75rem;
     height: 100%;
@@ -67,13 +67,10 @@ const Title = styled.h1`
     ${media.small`font-size: 1.25rem;`};
 `;
 
-const StyledLink = styled(Small)`
+const Link = styled(Small)`
     :hover {
         text-decoration: underline;
     }
 `;
-
-const Link = StyledLink as any;
-const Logo = StyledLogo as any;
 
 export { Header };
