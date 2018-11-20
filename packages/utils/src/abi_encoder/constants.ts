a1809c5fd9a76f9062224882d8596ee325a0f4cc
Greg Hysen <greg.hysen@gmail.com>
2018-11-20 10:08:17 -0800

    Moved global constants to separate file

---
+++ packages/utils/src/abi_encoder/constants.ts
@@ -0,0 +1,6 @@
+export const EVM_WORD_WIDTH_IN_BYTES = 32;
+export const HEX_BASE = 16;
+export const BIN_BASE = 2;
+export const HEX_SELECTOR_LENGTH_IN_CHARS = 10;
+export const HEX_SELECTOR_LENGTH_IN_BYTES = 4;
+export const HEX_SELECTOR_BYTE_OFFSET_IN_CALLDATA = 0;
