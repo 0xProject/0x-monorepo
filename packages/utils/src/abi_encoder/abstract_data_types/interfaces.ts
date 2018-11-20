56953539e0214721205a7e4ceed56f3dd6b068fe
Greg Hysen <greg.hysen@gmail.com>
2018-11-20 13:40:46 -0800

    Merge above

---
+++ packages/utils/src/abi_encoder/abstract_data_types/interfaces.ts
@@ -0,0 +1,16 @@
+import { DataItem } from 'ethereum-types';
+
+import { RawCalldata } from '../calldata';
+
+import { DataType } from './data_type';
+
+export interface DataTypeFactory {
+    create: (dataItem: DataItem, parentDataType?: DataType) => DataType;
+    mapDataItemToDataType: (dataItem: DataItem) => DataType;
+}
+
+export interface DataTypeStaticInterface {
+    matchType: (type: string) => boolean;
+    encodeValue: (value: any) => Buffer;
+    decodeValue: (rawCalldata: RawCalldata) => any;
+}
