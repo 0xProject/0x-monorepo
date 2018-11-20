ce0acb261b6c6722a4d09b033b6ec9e87413e3de
Greg Hysen <greg.hysen@gmail.com>
2018-11-20 14:18:01 -0800

    moved abi encoder constants into utils dir

--- packages/utils/src/abi_encoder/evm_data_types/bool.ts
+++ packages/utils/src/abi_encoder/evm_data_types/bool.ts
@@ -6,7 +6,7 @@ import * as _ from 'lodash';
 import { BigNumber } from '../../configured_bignumber';
 import { DataTypeFactory, PayloadDataType } from '../abstract_data_types';
 import { RawCalldata } from '../calldata';
-import * as Constants from '../constants';
+import * as Constants from '../utils/constants';
 
 export class Bool extends PayloadDataType {
     private static readonly _SIZE_KNOWN_AT_COMPILE_TIME: boolean = true;
