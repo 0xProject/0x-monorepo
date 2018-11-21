61b3305af3a7a599f83370aa8ebe8157738506fb
Fabio Berger <me@fabioberger.com>
2018-11-21 13:26:58 +0000

    Fix prettier

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -1,3 +1,11 @@
-export { Provider, TxData, JSONRPCRequestPayload, JSONRPCErrorCallback, TxDataPayable, JSONRPCResponsePayload, JSONRPCResponseError } from 'ethereum-types';
+export {
+    Provider,
+    TxData,
+    JSONRPCRequestPayload,
+    JSONRPCErrorCallback,
+    TxDataPayable,
+    JSONRPCResponsePayload,
+    JSONRPCResponseError,
+} from 'ethereum-types';
 export { ContractAddresses } from '@0x/contract-addresses';
 export { runMigrationsAsync, runMigrationsOnceAsync } from './migration';
