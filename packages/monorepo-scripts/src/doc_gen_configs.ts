72bb35256a67a1cc2f92008735ccdc24cab286a4
Leonid Logvinov <logvinov.leon@gmail.com>
2018-11-26 14:06:37 +0100

    Add ethers FunctionFragment to EXTERNAL_TYPE_TO_LINK mapping

--- packages/monorepo-scripts/src/doc_gen_configs.ts
+++ packages/monorepo-scripts/src/doc_gen_configs.ts
@@ -24,6 +24,8 @@ export const docGenConfigs: DocGenConfigs = {
             'https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/ganache-core/index.d.ts#L8',
         'lightwallet.keystore':
             'https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/eth-lightwallet/index.d.ts#L36',
+        'ethers.utils.FunctionFragment':
+            'https://github.com/ethers-io/ethers.js/blob/061b0eae1d4c570aedd9bee1971afa43fcdae1a6/utils/abi-coder.d.ts#L16',
     },
     // If a 0x package re-exports an external package, we should add a link to it's exported items here
     EXTERNAL_EXPORT_TO_LINK: {
