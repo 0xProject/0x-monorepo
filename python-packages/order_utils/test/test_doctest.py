750467543b71b32f833d7ad01ed51c8109ab7b8d
Daniel Pyrathon <daniel@coffeemeetsbagel.com>
2018-11-28 01:51:44 -0800

    Followed up on PR comments

--- python-packages/order_utils/test/test_doctest.py
+++ python-packages/order_utils/test/test_doctest.py
@@ -8,13 +8,7 @@ import zero_ex
 
 
 def test_all_doctests():
-    """Gather zero_ex.* modules and doctest them.
-
-    NOTE: we now use `importlib` instead of `imp` because `imp` is deprecated
-    and also `load_module()` will reload modules that are already loaded,
-    causing tests to faily non-deterministically.
-    Ref https://bit.ly/2zwgyrZ
-    """
+    """Gather zero_ex.* modules and doctest them."""
     for (_, modname, _) in pkgutil.walk_packages(
         path=zero_ex.__path__, prefix="zero_ex."
     ):
