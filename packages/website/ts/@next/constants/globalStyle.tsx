5294c31997a20e4ff9daf25b62f5b78e42419daf
Fred Carlsen <fred@sjelfull.no>
2018-11-29 15:57:03 +0100

    Format styles

--- packages/website/ts/@next/constants/globalStyle.tsx
+++ packages/website/ts/@next/constants/globalStyle.tsx
@@ -1,11 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
-import { cssReset } from 'ts/@next/constants/cssReset';
-import { colors } from 'ts/style/colors';
+import {createGlobalStyle} from 'styled-components';
+import {cssReset} from 'ts/@next/constants/cssReset';
+import {colors} from 'ts/style/colors';
 
-// Not sure if cssReset is already imported into index.tsx
-// Also: currently createglobalStyle from styled-components is
-// throwing a warning about how there's not typing exported from styled-comps
-const GlobalStyles = createGlobalStyle`
+// Not sure if cssReset is already imported into index.tsx Also: currently
+// createglobalStyle from styled-components is throwing a warning about how
+// there's not typing exported from styled-comps
+const GlobalStyles = createGlobalStyle `
   ${cssReset};
 
   @font-face {
@@ -32,7 +32,17 @@ const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     color: #fff;
   }
+
+  .visuallyHidden {
+    position: absolute !important;
+    clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+    clip: rect(1px, 1px, 1px, 1px);
+    padding:0 !important;
+    border:0 !important;
+    height: 1px !important;
+    width: 1px !important;
+    overflow: hidden;
+  }
 `;
 
-
-export { GlobalStyles }
+export {GlobalStyles};
