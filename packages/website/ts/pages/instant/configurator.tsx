ea61c7a5dbf35586e74f6fd1ccb20dffe6e7c8f4
fragosti <francesco.agosti93@gmail.com>
2018-11-29 11:23:16 -0800

    feat: have basic code syntax highlighting working

--- packages/website/ts/pages/instant/configurator.tsx
+++ packages/website/ts/pages/instant/configurator.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Container } from 'ts/components/ui/container';
+import { CodeDemo } from 'ts/pages/instant/code_demo';
 import { colors } from 'ts/style/colors';
 
 export interface ConfiguratorProps {
@@ -8,5 +9,9 @@ export interface ConfiguratorProps {
 }
 
 export const Configurator = (props: ConfiguratorProps) => (
-    <Container id={props.hash} height="400px" backgroundColor={colors.instantTertiaryBackground} />
+    <Container id={props.hash} height="400px" backgroundColor={colors.instantTertiaryBackground}>
+        <Container width="50%">
+            <CodeDemo />
+        </Container>
+    </Container>
 );
