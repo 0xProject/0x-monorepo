800149c7270cc17d5596d41aa2e432b1f1ab04c8
xianny <xianny@gmail.com>
2018-11-28 17:55:10 -0800

    add entity test for ohlcv_external entity

--- packages/pipeline/src/entities/ohlcv_external.ts
+++ packages/pipeline/src/entities/ohlcv_external.ts
@@ -1,13 +1,15 @@
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
+import { numberToBigIntTransformer } from '../utils';
+
 @Entity({ name: 'ohlcv_external', schema: 'raw' })
 export class OHLCVExternal {
     @PrimaryColumn() public exchange!: string;
 
     @PrimaryColumn({ name: 'from_symbol', type: 'varchar' }) public fromSymbol!: string;
     @PrimaryColumn({ name: 'to_symbol', type: 'varchar' }) public toSymbol!: string;
-    @PrimaryColumn({ name: 'start_time' }) public startTime!: number;
-    @PrimaryColumn({ name: 'end_time' }) public endTime!: number;
+    @PrimaryColumn({ name: 'start_time', transformer: numberToBigIntTransformer }) public startTime!: number;
+    @PrimaryColumn({ name: 'end_time', transformer: numberToBigIntTransformer }) public endTime!: number;
 
     @Column() public open!: number;
     @Column() public close!: number;
@@ -17,5 +19,5 @@ export class OHLCVExternal {
     @Column({ name: 'volume_to' }) public volumeTo!: number;
 
     @PrimaryColumn() public source!: string;
-    @PrimaryColumn({ name: 'observed_timestamp' }) public observedTimestamp!: number;
+    @PrimaryColumn({ name: 'observed_timestamp', transformer: numberToBigIntTransformer }) public observedTimestamp!: number;
 }
