f00ae95cfd01529772f23f6db2a63455c88aa902
Remco Bloemen <remco@wicked.ventures>
2018-11-30 21:15:20 +0100

    test mock generator

--- packages/sol-meta/test/index.ts
+++ packages/sol-meta/test/index.ts
@@ -1,21 +1,26 @@
 import * as chai from 'chai';
 import * as fs from 'fs';
 import * as glob from 'glob';
+import * as _ from 'lodash';
 import 'mocha';
 import * as path from 'path';
+import * as S from 'solidity-parser-antlr';
 
 import { parse } from '../src/parser';
+import { readSources, SourceInfo, SourceCollection } from '../src/source_reader';
 import { unparse } from '../src/unparser';
+import { compile } from '../src/solc_wrapper';
+import { mockContract } from '../src/contract_mocker';
 
 const expect = chai.expect;
 
 const findContracts = (searchPath: string) =>
     glob.sync(searchPath).map(file => ({
-        name: path.basename(file, '.sol') + ` (${file})`,
+        name: path.basename(file, '.sol'),
         source: fs.readFileSync(file, 'utf8'),
     }));
 
-const contracts = findContracts('../contracts/src/**/*.sol');
+const contracts = findContracts('../contracts/contracts/**/*.sol');
 
 describe('Parser', () => {
     it('should have test contracts', () => {
@@ -42,4 +47,40 @@ describe('Unparser', () => {
             // syntax, even though they should be identical in meaning.
         }),
     );
+});
+
+describe('Mocker', () => {
+    const sourcePath = '../contracts/contracts/protocol/Exchange/';
+    const toMock = ['Exchange', 'MixinExchangeCore', 'MixinSignatureValidator', 'MixinWrapperFunctions'];
+    const path = (name: string) => `${sourcePath}/${name}.sol`;
+    let sources: SourceCollection;
+    let mocks: { [name: string]: S.SourceUnit } = {};
+
+    it('should read sources', async () => {
+        sources = await readSources(_.map(toMock, path));
+        _.map(toMock, name => expect(_.keys(sources).some(absPath => absPath.endsWith(`${name}.sol`))));
+    });
+    _.map(toMock, name =>
+        it(`should generate mocks for ${name}`, () => {
+            mocks[name] = mockContract(
+                sources,
+                _.keys(sources).find(absPath => absPath.endsWith(`${name}.sol`)) || '',
+                name,
+                {
+                    constructors: {
+                        LibConstants: ['"ZRXASSETSTRING"'],
+                        Exchange: ['"ZRXASSETSTRING"'],
+                    },
+                    scripted: {},
+                },
+            );
+        }),
+    );
+    // Note(recmo): These tests are slow
+    describe.skip('Compiling', () =>
+        _.map(toMock, name =>
+            it(`should compile mock for ${name}`, async () => {
+                await compile(sources, mocks[name]);
+            }).timeout(30000),
+        ));
 });
