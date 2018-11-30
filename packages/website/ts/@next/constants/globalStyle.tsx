9b6d738ab102b47a133260f8515071396a0fe6cc
Fred Carlsen <fred@sjelfull.no>
2018-11-30 12:05:21 +0100

    Tweak global styles

--- packages/website/ts/@next/constants/globalStyle.tsx
+++ packages/website/ts/@next/constants/globalStyle.tsx
@@ -6,43 +6,53 @@ import {colors} from 'ts/style/colors';
 // createglobalStyle from styled-components is throwing a warning about how
 // there's not typing exported from styled-comps
 const GlobalStyles = createGlobalStyle `
-  ${cssReset};
+    ${cssReset};
 
-  @font-face {
-    font-family: "Formular";
-    src: url("/fonts/Formular-Light.woff2") format("woff2"), url("/fonts/Formular-Light.woff") format("woff");
-    font-weight: 300;
-    font-display: swap;
-  }
+    @font-face {
+        font-family: "Formular";
+        src: url("/fonts/Formular-Light.woff2") format("woff2"), url("/fonts/Formular-Light.woff") format("woff");
+        font-weight: 300;
+        font-display: swap;
+    }
 
-  @font-face {
-    font-family: "Formular";
-    src: url("/fonts/Formular-Regular.woff2") format("woff2"), url("/fonts/Formular-Regular.woff") format("woff");
-    font-weight: 400;
-    font-display: swap;
-  }
+    @font-face {
+        font-family: "Formular";
+        src: url("/fonts/Formular-Regular.woff2") format("woff2"), url("/fonts/Formular-Regular.woff") format("woff");
+        font-weight: 400;
+        font-display: swap;
+    }
 
-  html {
-    font-size: 18px;
-    background-color: ${colors.backgroundBlack};
-  }
+    html {
+        font-size: 18px;
+        background-color: ${colors.backgroundBlack};
+    }
 
-  body {
-    font-family: 'Formular', sans-serif !important;
-    -webkit-font-smoothing: antialiased;
-    color: #fff;
-  }
+    body {
+        font-family: 'Formular', sans-serif !important;
+        -webkit-font-smoothing: antialiased;
+        color: ${colors.white};
+    }
 
-  .visuallyHidden {
-    position: absolute !important;
-    clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-    clip: rect(1px, 1px, 1px, 1px);
-    padding:0 !important;
-    border:0 !important;
-    height: 1px !important;
-    width: 1px !important;
-    overflow: hidden;
-  }
+    .visuallyHidden {
+        position: absolute !important;
+        clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+        clip: rect(1px, 1px, 1px, 1px);
+        padding:0 !important;
+        border:0 !important;
+        height: 1px !important;
+        width: 1px !important;
+        overflow: hidden;
+    }
+
+    p {
+        font-size: 1rem;
+        line-height: 1.444444444em; // 26px
+    }
+
+    :root a {
+        color: inherit;
+        text-decoration: none;
+    }
 `;
 
 export {GlobalStyles};
