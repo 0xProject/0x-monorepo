b6be92e40b3f12e04a583400f32b76dcf6ccb4d6
Fred Carlsen <fred@sjelfull.no>
2018-12-03 14:43:19 +0100

    Updated press content

--- packages/website/ts/@next/pages/about/press.tsx
+++ packages/website/ts/@next/pages/about/press.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 import { colors } from 'ts/style/colors';
 
@@ -18,59 +19,52 @@ export const NextAboutPress = () => (
     <Section>
       <Wrap>
          <Column colWidth="1/3">
-            <ChapterLink href="#">Our Mission</ChapterLink>
-            <ChapterLink href="#">Team</ChapterLink>
-            <ChapterLink href="#">Press</ChapterLink>
-            <ChapterLink href="#">Jobs</ChapterLink>
+            <ChapterLink to="/next/about/mission">Our Mission</ChapterLink>
+            <ChapterLink to="/next/about/team">Team</ChapterLink>
+            <ChapterLink to="/next/about/press">Press</ChapterLink>
+            <ChapterLink to="/next/about/jobs">Jobs</ChapterLink>
         </Column>
         <Column colWidth="2/3">
-            <Heading size="medium">Creating a tokenized world where all value can flow freely.</Heading>
-            <Paragraph size="medium">0x Protocol is an important infrastructure layer for the emerging crypto economy and enables markets to be created that couldn't have existed before. As more assets become tokenized, public blockchains provide the opportunity to establish a new financial stack that is more efficient, transparent, and equitable than any system in the past.</Paragraph>
-            <Paragraph>Our missions and values (arrow)</Paragraph>
-        </Column>
-      </Wrap>
-    </Section>
+            <Heading size="medium">Press Highlights</Heading>
+            <Paragraph size="medium">Want to write about 0x? Get in touch, or download our press kit.</Paragraph>
 
-    <Section
-        isFullWidth={true}
-        isNoPadding={true}>
-        <Wrap width="full">
-            <Image src="/images/@next/about/about-mission@2x.jpg" height="320" alt="" center />
-        </Wrap>
-    </Section>
-
-    <Section>
-      <Wrap>
-        <Column colWidth="1/3">
-            <Heading size="medium">Core<br/>Values</Heading>
-        </Column>
-
-        <Column colWidth="2/3">
             <Wrap>
                 <Column colWidth="1/3">
-                    <RightThingIcon width="100" />
+                    <img src="/images/@next/press/logo-fortune.png" width="100" alt="Fortune"/>
                 </Column>
                 <Column colWidth="2/3">
-                    <Heading size="medium">Do The Right Thing</Heading>
-                    <Paragraph isMuted={true}>We acknowledge the broad subjectivity behind doing “the right thing,” and are committed to rigorously exploring its nuance in our decision making. We believe this responsibility drives our decision making above all else, and pledge to act in the best interest of our peers, community, and society as a whole.</Paragraph>
+                    <Paragraph isMuted={false}>The difference is that 0x is decentralized, operating as a series of anonymous nodes...</Paragraph>
+                    <a href="#">Read Article</a>
                 </Column>
             </Wrap>
+
             <Wrap>
                 <Column colWidth="1/3">
-                    <ConsistentlyShipIcon width="100" />
+                    <img src="/images/@next/press/logo-fortune.png" width="100" alt="Fortune"/>
                 </Column>
                 <Column colWidth="2/3">
-                    <Heading size="medium">Consistently Ship</Heading>
-                    <Paragraph isMuted={true}>Achieving our mission requires dedication and diligence. We aspire to be an organization that consistently ships. We set high-impact goals that are rooted in data and pride ourselves in consistently outputting outstanding results across the organization.</Paragraph>
+                    <Paragraph isMuted={false}>The difference is that 0x is decentralized, operating as a series of anonymous nodes...</Paragraph>
+                    <a href="#">Read Article</a>
                 </Column>
             </Wrap>
+
             <Wrap>
                 <Column colWidth="1/3">
-                    <LongTermImpactIcon width="100" />
+                    <img src="/images/@next/press/logo-fortune.png" width="100" alt="Fortune"/>
                 </Column>
                 <Column colWidth="2/3">
-                    <Heading size="medium">Focus on long-term Impact</Heading>
-                    <Paragraph isMuted={true}>We anticipate that over time, awareness of the fundamentally disruptive nature of frictionless global exchange will cause some to see this technology as a threat. There will be setbacks, some will claim that this technology is too disruptive, and we will face adversity. Persistence and a healthy long-term focus will see us through these battles.</Paragraph>
+                    <Paragraph isMuted={false}>The difference is that 0x is decentralized, operating as a series of anonymous nodes...</Paragraph>
+                    <a href="#">Read Article</a>
+                </Column>
+            </Wrap>
+
+            <Wrap>
+                <Column colWidth="1/3">
+                    <img src="/images/@next/press/logo-fortune.png" width="100" alt="Fortune"/>
+                </Column>
+                <Column colWidth="2/3">
+                    <Paragraph isMuted={false}>The difference is that 0x is decentralized, operating as a series of anonymous nodes...</Paragraph>
+                    <a href="#">Read Article</a>
                 </Column>
             </Wrap>
         </Column>
@@ -79,7 +73,7 @@ export const NextAboutPress = () => (
   </SiteWrap>
 );
 
-const ChapterLink = styled.a`
+const ChapterLink = styled(ReactRouterLink)`
     font-size: 1.222222222rem;
     display: block;
     opacity: 0.8;
