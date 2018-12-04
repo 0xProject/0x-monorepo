340493a0b14b0c70524407b4c66f61d115845c33
fragosti <francesco.agosti93@gmail.com>
2018-12-04 13:36:47 -0800

    polish: reduce vertical padding in instant

--- packages/instant/src/components/payment_method.tsx
+++ packages/instant/src/components/payment_method.tsx
@@ -26,7 +26,7 @@ export interface PaymentMethodProps {
 export class PaymentMethod extends React.Component<PaymentMethodProps> {
     public render(): React.ReactNode {
         return (
-            <Container padding="20px" width="100%" height="133px">
+            <Container width="100%" height="120px" padding="20px 20px 0px 20px">
                 <Container marginBottom="12px">
                     <Flex justify="space-between">
                         <Text
