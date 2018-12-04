492ecb0829cb324f7a9d06b5d65adb0ec71112cf
xianny <xianny@gmail.com>
2018-12-04 13:26:15 -0800

    prettier

--- packages/pipeline/src/entities/ohlcv_external.ts
+++ packages/pipeline/src/entities/ohlcv_external.ts
@@ -6,18 +6,25 @@ import { numberToBigIntTransformer } from '../utils';
 export class OHLCVExternal {
     @PrimaryColumn() public exchange!: string;
 
-    @PrimaryColumn({ name: 'from_symbol', type: 'varchar' }) public fromSymbol!: string;
-    @PrimaryColumn({ name: 'to_symbol', type: 'varchar' }) public toSymbol!: string;
-    @PrimaryColumn({ name: 'start_time', transformer: numberToBigIntTransformer }) public startTime!: number;
-    @PrimaryColumn({ name: 'end_time', transformer: numberToBigIntTransformer }) public endTime!: number;
+    @PrimaryColumn({ name: 'from_symbol', type: 'varchar' })
+    public fromSymbol!: string;
+    @PrimaryColumn({ name: 'to_symbol', type: 'varchar' })
+    public toSymbol!: string;
+    @PrimaryColumn({ name: 'start_time', transformer: numberToBigIntTransformer })
+    public startTime!: number;
+    @PrimaryColumn({ name: 'end_time', transformer: numberToBigIntTransformer })
+    public endTime!: number;
 
     @Column() public open!: number;
     @Column() public close!: number;
     @Column() public low!: number;
     @Column() public high!: number;
-    @Column({ name: 'volume_from' }) public volumeFrom!: number;
-    @Column({ name: 'volume_to' }) public volumeTo!: number;
+    @Column({ name: 'volume_from' })
+    public volumeFrom!: number;
+    @Column({ name: 'volume_to' })
+    public volumeTo!: number;
 
     @PrimaryColumn() public source!: string;
-    @PrimaryColumn({ name: 'observed_timestamp', transformer: numberToBigIntTransformer }) public observedTimestamp!: number;
+    @PrimaryColumn({ name: 'observed_timestamp', transformer: numberToBigIntTransformer })
+    public observedTimestamp!: number;
 }
