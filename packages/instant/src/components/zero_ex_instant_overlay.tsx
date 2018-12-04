8b659dbd77294f3d44f4c1fe75a62d6fec3fea15
Brandon Millman <brandon.millman@gmail.com>
2018-12-04 15:20:55 -0800

    feat(instant): give a stable class name to the close button on the overlay

--- packages/instant/src/components/zero_ex_instant_overlay.tsx
+++ packages/instant/src/components/zero_ex_instant_overlay.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { ZeroExInstantContainer } from '../components/zero_ex_instant_container';
-import { MAIN_CONTAINER_DIV_CLASS, OVERLAY_DIV_CLASS } from '../constants';
+import { MAIN_CONTAINER_DIV_CLASS, OVERLAY_CLOSE_BUTTON_DIV_CLASS, OVERLAY_DIV_CLASS } from '../constants';
 import { ColorOption } from '../style/theme';
 
 import { Container } from './ui/container';
@@ -22,14 +22,16 @@ export const ZeroExInstantOverlay: React.StatelessComponent<ZeroExInstantOverlay
             <Overlay zIndex={zIndex} className={OVERLAY_DIV_CLASS}>
                 <Flex height="100vh">
                     <Container position="absolute" top="0px" right="0px" display={{ default: 'initial', sm: 'none' }}>
-                        <Icon
-                            height={18}
-                            width={18}
-                            color={ColorOption.white}
-                            icon="closeX"
-                            onClick={onClose}
-                            padding="2em 2em"
-                        />
+                        <Container className={OVERLAY_CLOSE_BUTTON_DIV_CLASS}>
+                            <Icon
+                                height={18}
+                                width={18}
+                                color={ColorOption.white}
+                                icon="closeX"
+                                onClick={onClose}
+                                padding="2em 2em"
+                            />
+                        </Container>
                     </Container>
                     <Container
                         width={{ default: 'auto', sm: '100%' }}
