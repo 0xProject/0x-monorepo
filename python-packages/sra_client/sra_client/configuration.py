72a3cc68cc9de4b7588a39a608f5ee99f7c51ad6
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2018-12-08 17:53:31 -0800

    Change test config to hit 0x-launch-kit

--- python-packages/sra_client/sra_client/configuration.py
+++ python-packages/sra_client/sra_client/configuration.py
@@ -46,7 +46,7 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
     def __init__(self):
         """Constructor"""
         # Default Base url
-        self.host = "http://localhost"
+        self.host = "http://localhost:3000"
         # Temp file folder for downloading files
         self.temp_folder_path = None
 
