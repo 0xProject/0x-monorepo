0ff13edb585bb2cf1ef5799cabed800d57f354f7
Fred Carlsen <fred@sjelfull.no>
2018-12-10 14:28:40 +0100

    Tweak icons on why

--- packages/website/ts/@next/pages/why.tsx
+++ packages/website/ts/@next/pages/why.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { colors } from 'ts/style/colors';
 
 import { Banner } from 'ts/@next/components/banner';
+import { Icon } from 'ts/@next/components/icon';
 import { Column, Section, Wrap, WrapCentered } from 'ts/@next/components/layout';
 import { Link } from 'ts/@next/components/link';
 import { SiteWrap } from 'ts/@next/components/siteWrap';
@@ -18,31 +19,31 @@ export const NextWhy = () => (
     <Section>
       <WrapCentered>
         <Column colWidth="2/3">
-          <Heading size="medium" isCentered={true} style={{ '--desktopMaxWidth': '570px' }}>The exchange layer for the crypto economy</Heading>
-          <Paragraph size="medium" isMuted={true} isCentered={true}>The world's assets are becoming tokenized on public blockchains. 0x Protocol is free, open-source infrastructure that allows anyone in the world to build products that enable the purchasing and trading of crypto tokens.</Paragraph>
-          <Link href="/docs" isCentered={true}>Build on 0x</Link>
+            <Heading size="medium" isCentered={true} style={{ '--desktopMaxWidth': '570px' }}>The exchange layer for the crypto economy</Heading>
+            <Paragraph size="medium" isMuted={true} isCentered={true}>The world's assets are becoming tokenized on public blockchains. 0x Protocol is free, open-source infrastructure that allows anyone in the world to build products that enable the purchasing and trading of crypto tokens.</Paragraph>
+            <Link href="/docs" isCentered={true}>Build on 0x</Link>
         </Column>
       </WrapCentered>
     </Section>
 
-    <Section bgColor={colors.backgroundDark}>
+    <Section bgColor={colors.backgroundDark} isPadLarge={true}>
       <Wrap>
         <Column colWidth="1/3">
-            <ProtocolIcon width="150" />
-            <Heading>Support for all Ethereum Standards</Heading>
-            <Paragraph>0x Protocol facilitates the decentralized exchange of a growing number of Ethereum-based tokens, including all ERC-20 and ERC-721 assets. Additional ERC standards can be added to the protocol...</Paragraph>
+            <Icon name="coin" size="large" margin={[0, 0, 32, 0]} />
+            <Heading size="small">Support for all Ethereum Standards</Heading>
+            <Paragraph isMuted={true}>0x Protocol facilitates the decentralized exchange of a growing number of Ethereum-based tokens, including all ERC-20 and ERC-721 assets. Additional ERC standards can be added to the protocol...</Paragraph>
         </Column>
 
         <Column colWidth="1/3">
-            <ProtocolIcon width="150" />
-            <Heading>Shared Networked Liquidity</Heading>
-            <Paragraph>0x is building a layer of networked liquidity that will lower the barriers to entry. By enabling businesses to tap into a shared pool of digital assets, it will create a more stable financial system.</Paragraph>
+            <Icon name="coin" size="large" margin={[0, 0, 32, 0]} />
+            <Heading size="small">Shared Networked Liquidity</Heading>
+            <Paragraph isMuted={true}>0x is building a layer of networked liquidity that will lower the barriers to entry. By enabling businesses to tap into a shared pool of digital assets, it will create a more stable financial system.</Paragraph>
         </Column>
 
         <Column colWidth="1/3">
-            <CustomizeIcon width="150" />
-            <Heading>Customize the User Experience</Heading>
-            <Paragraph>Relayers are businesses around the world that utilize 0x to integrate exchange functionality into a wide variety of products including order books, games, and digital art marketplaces.</Paragraph>
+            <Icon name="coin" size="large" margin={[0, 0, 32, 0]} />
+            <Heading size="small">Customize the User Experience</Heading>
+            <Paragraph isMuted={true}>Relayers are businesses around the world that utilize 0x to integrate exchange functionality into a wide variety of products including order books, games, and digital art marketplaces.</Paragraph>
         </Column>
       </Wrap>
     </Section>
@@ -56,43 +57,44 @@ export const NextWhy = () => (
         </Column>
 
         <Column colWidth="2/3">
-          <Heading size="medium">What 0x offers</Heading>
-          <CoinIcon width="150" />
-          <Heading>A Standard for Exchange</Heading>
-          <Paragraph isMuted={true}>0x provides developers with a technical standard for trading Ethereum-based tokens such as ERC 20 and ERC 721.</Paragraph>
+            <Heading size="medium">What 0x offers</Heading>
 
-          <CoinIcon width="150" />
-          <Heading>Robust Smart Contracts</Heading>
-          <Paragraph isMuted={true}>0x Protocol's smart contracts have been put through two rounds of rigorous security audits.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">A Standard for Exchange</Heading>
+            <Paragraph isMuted={true}>0x provides developers with a technical standard for trading Ethereum-based tokens such as ERC 20 and ERC 721.</Paragraph>
 
-          <CoinIcon width="150" />
-          <Heading>Extensible Architecture</Heading>
-          <Paragraph isMuted={true}>0x's modular pipeline enables you to plug in your own smart contracts through an extensible API.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Robust Smart Contracts</Heading>
+            <Paragraph isMuted={true}>0x Protocol's smart contracts have been put through two rounds of rigorous security audits.</Paragraph>
 
-          <CoinIcon width="150" />
-          <Heading>Efficient Design</Heading>
-          <Paragraph isMuted={true}>0x’s off-chain order relay with on-chain settlement is a gas efficient approach to p2p exchange, reducing blockchain bloat.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Extensible Architecture</Heading>
+            <Paragraph isMuted={true}>0x's modular pipeline enables you to plug in your own smart contracts through an extensible API.</Paragraph>
 
-          <Heading size="medium">Use Cases</Heading>
-          <Paragraph isMuted={true}>slider</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Efficient Design</Heading>
+            <Paragraph isMuted={true}>0x’s off-chain order relay with on-chain settlement is a gas efficient approach to p2p exchange, reducing blockchain bloat.</Paragraph>
 
-          <Heading size="medium">Exchange Functionality</Heading>
+            <Heading size="small">Use Cases</Heading>
+            <Paragraph isMuted={true}>slider</Paragraph>
 
-          <CoinIcon width="150" />
-          <Heading>Secure Non-custodial Trading</Heading>
-          <Paragraph isMuted={true}>Enable tokens to be traded wallet-to-wallet with no deposits or withdrawals.</Paragraph>
+            <Heading size="small">Exchange Functionality</Heading>
 
-          <CoinIcon width="150" />
-          <Heading>Flexible Order Types</Heading>
-          <Paragraph isMuted={true}>Choose to sell assets at a specific “buy it now” price or allow potential buyers to submit bids.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Secure Non-custodial Trading</Heading>
+            <Paragraph isMuted={true}>Enable tokens to be traded wallet-to-wallet with no deposits or withdrawals.</Paragraph>
 
-          <CoinIcon width="150" />
-          <Heading>Build a Business</Heading>
-          <Paragraph isMuted={true}>Monetize your product by taking fees on each transaction and join a growing number of relayers in the 0x ecosystem.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Flexible Order Types</Heading>
+            <Paragraph isMuted={true}>Choose to sell assets at a specific “buy it now” price or allow potential buyers to submit bids.</Paragraph>
 
-          <CoinIcon width="150" />
-          <Heading>Networked Liquidity</Heading>
-          <Paragraph isMuted={true}>Allow your assets to appear on other 0x-based marketplaces by sharing your liquidity through an open order book.</Paragraph>
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Build a Business</Heading>
+            <Paragraph isMuted={true}>Monetize your product by taking fees on each transaction and join a growing number of relayers in the 0x ecosystem.</Paragraph>
+
+            <Icon name="coin" size="medium" margin={[0, 0, 22, 0]} />
+            <Heading size="small">Networked Liquidity</Heading>
+            <Paragraph isMuted={true}>Allow your assets to appear on other 0x-based marketplaces by sharing your liquidity through an open order book.</Paragraph>
         </Column>
       </Wrap>
     </Section>
