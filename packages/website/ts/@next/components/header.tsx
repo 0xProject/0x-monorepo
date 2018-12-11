26c9cd3cb2dcc21763977543e83c54f4bc32d679
Ezekiel Aquino <ezekiel@bakkenbaeck.no>
2018-12-11 17:02:29 +0100

    dropdown z-index

--- packages/website/ts/@next/components/header.tsx
+++ packages/website/ts/@next/components/header.tsx
@@ -254,6 +254,7 @@ const DropdownWrap = styled.div`
     opacity: 0;
     transform: translate3d(0, -10px, 0);
     transition: opacity 0.35s, transform 0.35s, visibility 0s 0.35s;
+    z-index: 9999;
 
     &:after, &:before {
     	bottom: 100%;
