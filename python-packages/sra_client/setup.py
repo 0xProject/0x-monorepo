f5d27f0a914e7120096eee6814c40dae547e1afa
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2018-12-11 15:38:03 -0800

    Add shorcuts for publishing

--- python-packages/sra_client/setup.py
+++ python-packages/sra_client/setup.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 
+import subprocess
+import distutils.command.build_py
+
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "0x-sra-client"
@@ -18,6 +21,33 @@ with open("README.md", "r") as file_handle:
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 
+class TestPublishCommand(distutils.command.build_py.build_py):
+    """Custom command to publish to test.pypi.org."""
+
+    description = (
+        "Publish dist/* to test.pypi.org. Run sdist & bdist_wheel first."
+    )
+
+    def run(self):
+        """Run twine to upload to test.pypi.org."""
+        subprocess.check_call(  # nosec
+            (
+                "twine upload --repository-url https://test.pypi.org/legacy/"
+                + " --verbose dist/*"
+            ).split()
+        )
+
+
+class PublishCommand(distutils.command.build_py.build_py):
+    """Custom command to publish to pypi.org."""
+
+    description = "Publish dist/* to pypi.org. Run sdist & bdist_wheel first."
+
+    def run(self):
+        """Run twine to upload to pypi.org."""
+        subprocess.check_call("twine upload dist/*".split())  # nosec
+
+
 setup(
     name=NAME,
     version=VERSION,
@@ -30,4 +60,8 @@ setup(
     include_package_data=True,
     long_description=README_MD,
     long_description_content_type="text/markdown",
+    cmdclass={
+        "test_publish": TestPublishCommand,
+        "publish": PublishCommand,
+    },
 )
