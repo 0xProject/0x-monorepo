73605bec4f909e9361c3d682ddb855f9cf33a0d7
Alex Browne <stephenalexbrowne@gmail.com>
2018-12-12 17:37:05 -0800

    Pull approval events for ZRX and DAI

--- packages/pipeline/src/scripts/pull_erc20_events.ts
+++ packages/pipeline/src/scripts/pull_erc20_events.ts
@@ -1,10 +1,10 @@
-// tslint:disable:no-console
 import { getContractAddressesForNetworkOrThrow } from '@0x/contract-addresses';
 import { web3Factory } from '@0x/dev-utils';
 import { Web3ProviderEngine } from '@0x/subproviders';
+import { logUtils } from '@0x/utils';
 import { Web3Wrapper } from '@0x/web3-wrapper';
 import 'reflect-metadata';
-import { Connection, ConnectionOptions, createConnection, Repository } from 'typeorm';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 
 import { ERC20EventsSource } from '../data_sources/contract-wrappers/erc20_events';
 import { ERC20ApprovalEvent } from '../entities';
@@ -16,33 +16,65 @@ const NETWORK_ID = 1;
 const START_BLOCK_OFFSET = 100; // Number of blocks before the last known block to consider when updating fill events.
 const BATCH_SAVE_SIZE = 1000; // Number of events to save at once.
 const BLOCK_FINALITY_THRESHOLD = 10; // When to consider blocks as final. Used to compute default endBlock.
-const WETH_START_BLOCK = 4719568; // Block number when the WETH contract was deployed.
 
 let connection: Connection;
+
+interface Token {
+    // name is used for logging only.
+    name: string;
+    address: string;
+    defaultStartBlock: number;
+}
+
+const tokensToGetApprovalEvents: Token[] = [
+    {
+        name: 'WETH',
+        address: getContractAddressesForNetworkOrThrow(NETWORK_ID).etherToken,
+        defaultStartBlock: 4719568, // Block when the WETH contract was deployed.
+    },
+    {
+        name: 'ZRX',
+        address: getContractAddressesForNetworkOrThrow(NETWORK_ID).zrxToken,
+        defaultStartBlock: 4145415, // Block when the ZRX contract was deployed.
+    },
+    {
+        name: 'DAI',
+        address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+        defaultStartBlock: 4752008, // Block when the DAI contract was deployed.
+    },
+];
 
 (async () => {
     connection = await createConnection(ormConfig as ConnectionOptions);
     const provider = web3Factory.getRpcProvider({
         rpcUrl: INFURA_ROOT_URL,
     });
+    provider.stop();
+    (provider as any)._ready.go();
     const endBlock = await calculateEndBlockAsync(provider);
-    await getAndSaveWETHApprovalEventsAsync(provider, endBlock);
+    for (const token of tokensToGetApprovalEvents) {
+        await getAndSaveApprovalEventsAsync(provider, token, endBlock);
+    }
     process.exit(0);
 })().catch(handleError);
 
-async function getAndSaveWETHApprovalEventsAsync(provider: Web3ProviderEngine, endBlock: number): Promise<void> {
-    console.log('Checking existing approval events...');
+async function getAndSaveApprovalEventsAsync(
+    provider: Web3ProviderEngine,
+    token: Token,
+    endBlock: number,
+): Promise<void> {
+    logUtils.log(`Getting approval events for ${token.name}...`);
+    logUtils.log('Checking existing approval events...');
     const repository = connection.getRepository(ERC20ApprovalEvent);
-    const startBlock = (await getStartBlockAsync(repository)) || WETH_START_BLOCK;
+    const startBlock = (await getStartBlockAsync(token)) || token.defaultStartBlock;
 
-    console.log(`Getting WETH approval events starting at ${startBlock}...`);
-    const wethTokenAddress = getContractAddressesForNetworkOrThrow(NETWORK_ID).etherToken;
-    const eventsSource = new ERC20EventsSource(provider, NETWORK_ID, wethTokenAddress);
+    logUtils.log(`Getting approval events starting at ${startBlock}...`);
+    const eventsSource = new ERC20EventsSource(provider, NETWORK_ID, token.address);
     const eventLogs = await eventsSource.getApprovalEventsAsync(startBlock, endBlock);
 
-    console.log(`Parsing ${eventLogs.length} WETH approval events...`);
+    logUtils.log(`Parsing ${eventLogs.length} approval events...`);
     const events = parseERC20ApprovalEvents(eventLogs);
-    console.log(`Retrieved and parsed ${events.length} total WETH approval events.`);
+    logUtils.log(`Retrieved and parsed ${events.length} total approval events.`);
     await repository.save(events, { chunk: Math.ceil(events.length / BATCH_SAVE_SIZE) });
 }
 
@@ -52,15 +84,15 @@ async function calculateEndBlockAsync(provider: Web3ProviderEngine): Promise<num
     return currentBlock - BLOCK_FINALITY_THRESHOLD;
 }
 
-async function getStartBlockAsync(repository: Repository<ERC20ApprovalEvent>): Promise<number | null> {
-    const fillEventCount = await repository.count();
-    if (fillEventCount === 0) {
-        console.log(`No existing approval events found.`);
+async function getStartBlockAsync(token: Token): Promise<number | null> {
+    const queryResult = await connection.query(
+        `SELECT block_number FROM raw.erc20_approval_events WHERE token_address = $1 ORDER BY block_number DESC LIMIT 1`,
+        [token.address],
+    );
+    if (queryResult.length === 0) {
+        logUtils.log(`No existing approval events found for ${token.name}.`);
         return null;
     }
-    const queryResult = await connection.query(
-        `SELECT block_number FROM raw.erc20_approval_events ORDER BY block_number DESC LIMIT 1`,
-    );
     const lastKnownBlock = queryResult[0].block_number;
     return lastKnownBlock - START_BLOCK_OFFSET;
 }
