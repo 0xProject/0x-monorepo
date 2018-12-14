106df4cdaf002d949a2ba1aa73a5cdbb5f443f03
Fred Carlsen <fred@sjelfull.no>
2018-12-14 11:41:05 +0100

    Linting fix

--- packages/website/ts/@next/components/header.tsx
+++ packages/website/ts/@next/components/header.tsx
@@ -127,7 +127,7 @@ class HeaderBase extends React.Component<HeaderProps, HeaderState> {
 
 export const Header = withTheme(HeaderBase);
 
-const NavItem = (props: { link: NavItem; key: string }): React.ReactNode => {
+const NavItem = (props: { link: NavItem; key: string }) => {
     const { link } = props;
     const Subnav = link.dropdownComponent;
 
