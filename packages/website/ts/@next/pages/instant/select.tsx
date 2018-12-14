2b4c557a3fd97f966e375966ff39dd28ba550ef8
Ezekiel Aquino <ezekiel@bakkenbaeck.no>
2018-12-14 18:53:43 +0100

    Cleanup select

--- packages/website/ts/@next/pages/instant/select.tsx
+++ packages/website/ts/@next/pages/instant/select.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-import { colors } from 'ts/style/colors';
-
-import {Column, Section, Wrap, WrapCentered} from 'ts/@next/components/layout';
-import {Heading, Paragraph} from 'ts/@next/components/text';
-
 export interface SelectItemConfig {
     label: string;
     value?: string;
@@ -49,14 +44,6 @@ const StyledSelect = styled.select`
     font-size: 1rem;
     width: 100%;
     padding: 20px 20px 20px 20px;
-`;
-
-const SelectAllButton = styled.button`
-    appearance: none;
-    border: 0;
-    font-size: 0.777777778rem;
-    display: block;
-    opacity: 0.75;
 `;
 
 const Caret = styled.svg`
