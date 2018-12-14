d410c44b0744524c13d731d9b43dedf1add55f53
Fred Carlsen <fred@sjelfull.no>
2018-12-14 15:35:19 +0100

    Add icon

--- packages/website/ts/@next/pages/about/mission.tsx
+++ packages/website/ts/@next/pages/about/mission.tsx
@@ -22,6 +22,7 @@ const values = [
     {
         title: 'Focus on long-term Impact',
         description: 'We anticipate that over time, awareness of the fundamentally disruptive nature of frictionless global exchange will cause some to see this technology as a threat. There will be setbacks, some will claim that this technology is too disruptive, and we will face adversity. Persistence and a healthy long-term focus will see us through these battles.',
+        icon: 'long-term-impact',
     },
 ];
 
