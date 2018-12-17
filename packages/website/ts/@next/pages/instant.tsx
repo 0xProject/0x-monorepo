33e06fdf40f04a64dbe3c42264a5518d52a5119c
Steve Klebanoff <steve.klebanoff@gmail.com>
2018-12-17 13:16:19 -0800

    Run prettifier

--- packages/website/ts/@next/pages/instant.tsx
+++ packages/website/ts/@next/pages/instant.tsx
@@ -2,16 +2,16 @@ import * as _ from 'lodash';
 import * as React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-import {colors} from 'ts/style/colors';
+import { colors } from 'ts/style/colors';
 
-import {Banner} from 'ts/@next/components/banner';
-import {Hero} from 'ts/@next/components/hero';
+import { Banner } from 'ts/@next/components/banner';
+import { Hero } from 'ts/@next/components/hero';
 
-import {Button} from 'ts/@next/components/button';
-import {Definition} from 'ts/@next/components/definition';
-import {Section, SectionProps} from 'ts/@next/components/newLayout';
-import {SiteWrap} from 'ts/@next/components/siteWrap';
-import {Heading, Paragraph} from 'ts/@next/components/text';
+import { Button } from 'ts/@next/components/button';
+import { Definition } from 'ts/@next/components/definition';
+import { Section, SectionProps } from 'ts/@next/components/newLayout';
+import { SiteWrap } from 'ts/@next/components/siteWrap';
+import { Heading, Paragraph } from 'ts/@next/components/text';
 import { Configurator } from 'ts/@next/pages/instant/configurator';
 import { ModalContact } from '../components/modals/modal_contact';
 
@@ -19,7 +19,8 @@ const featuresData = [
     {
         title: 'Support ERC-20 and ERC-721 tokens',
         icon: 'supportForAllEthereumStandards-large',
-        description: 'Seamlessly integrate token purchasing into your product experience by offering digital assets ranging from in-game items to stablecoins.',
+        description:
+            'Seamlessly integrate token purchasing into your product experience by offering digital assets ranging from in-game items to stablecoins.',
         links: [
             {
                 label: 'Get Started',
@@ -34,7 +35,8 @@ const featuresData = [
     {
         title: 'Generate revenue for your business',
         icon: 'generateRevenueForYourBusiness-large',
-        description: 'With just a few lines of code, you can earn up to 5% in affiliate fees on every transaction from your crypto wallet or dApp.',
+        description:
+            'With just a few lines of code, you can earn up to 5% in affiliate fees on every transaction from your crypto wallet or dApp.',
         links: [
             {
                 label: 'Learn about affiliate fees',
@@ -45,7 +47,8 @@ const featuresData = [
     {
         title: 'Easy and flexible integration',
         icon: 'flexibleIntegration0xInstant',
-        description: 'Use our out-of-the-box design or customize the user interface by integrating via the AssetBuyer engine.. You can also tap into 0x networked liquidity or choose your own liquidity pool.',
+        description:
+            'Use our out-of-the-box design or customize the user interface by integrating via the AssetBuyer engine.. You can also tap into 0x networked liquidity or choose your own liquidity pool.',
         links: [
             {
                 label: 'Explore AssetBuyer',
@@ -72,7 +75,7 @@ export class Next0xInstant extends React.Component<Props> {
         isContactModalOpen: false,
     };
     public render(): React.ReactNode {
-        return  (
+        return (
             <SiteWrap>
                 <Hero
                     title="Introducing 0x Instant"
@@ -81,15 +84,15 @@ export class Next0xInstant extends React.Component<Props> {
                 />
 
                 <Section isFullWidth={true} isPadded={false} padding="30px 0">
-                <MarqueeWrap>
-                    <div>
-                        {[...Array(18)].map((item, index) => (
-                            <Card key={`card-${index}`} index={index}>
-                            <img src={`/images/@next/0x-instant/widget-${(index % 6) + 1}.png`} />
-                            </Card>
-                        ))}
-                    </div>
-                </MarqueeWrap>
+                    <MarqueeWrap>
+                        <div>
+                            {[...Array(18)].map((item, index) => (
+                                <Card key={`card-${index}`} index={index}>
+                                    <img src={`/images/@next/0x-instant/widget-${index % 6 + 1}.png`} />
+                                </Card>
+                            ))}
+                        </div>
+                    </MarqueeWrap>
                 </Section>
 
                 <Section>
@@ -106,7 +109,12 @@ export class Next0xInstant extends React.Component<Props> {
                     ))}
                 </Section>
 
-                <ConfiguratorSection id="configurator" maxWidth="1386px" padding="0 58px 70px" bgColor={colors.backgroundDark}>
+                <ConfiguratorSection
+                    id="configurator"
+                    maxWidth="1386px"
+                    padding="0 58px 70px"
+                    bgColor={colors.backgroundDark}
+                >
                     <Heading>0x Instant Configurator</Heading>
                     <Configurator />
                 </ConfiguratorSection>
@@ -120,8 +128,18 @@ export class Next0xInstant extends React.Component<Props> {
                 <ModalContact isOpen={this.state.isContactModalOpen} onDismiss={this._onDismissContactModal} />
 
                 <Section maxWidth="1170px" isPadded={false} padding="60px 0">
-                    <Paragraph size="small" isMuted={0.5}>Disclaimer: The laws and regulations applicable to the use and exchange of digital assets and blockchain-native tokens, including through any software developed using the licensed work created by ZeroEx Intl. (the “Work”), vary by jurisdiction. As set forth in the Apache License, Version 2.0 applicable to the Work, developers are “solely responsible for determining the appropriateness of using or redistributing the Work,” which includes responsibility for ensuring compliance with any such applicable laws and regulations.</Paragraph>
-                    <Paragraph size="small" isMuted={0.5}>See the Apache License, Version 2.0 for the specific language governing all applicable permissions and limitations.</Paragraph>
+                    <Paragraph size="small" isMuted={0.5}>
+                        Disclaimer: The laws and regulations applicable to the use and exchange of digital assets and
+                        blockchain-native tokens, including through any software developed using the licensed work
+                        created by ZeroEx Intl. (the “Work”), vary by jurisdiction. As set forth in the Apache License,
+                        Version 2.0 applicable to the Work, developers are “solely responsible for determining the
+                        appropriateness of using or redistributing the Work,” which includes responsibility for ensuring
+                        compliance with any such applicable laws and regulations.
+                    </Paragraph>
+                    <Paragraph size="small" isMuted={0.5}>
+                        See the Apache License, Version 2.0 for the specific language governing all applicable
+                        permissions and limitations.
+                    </Paragraph>
                 </Section>
             </SiteWrap>
         );
@@ -129,11 +147,11 @@ export class Next0xInstant extends React.Component<Props> {
 
     public _onOpenContactModal = (): void => {
         this.setState({ isContactModalOpen: true });
-    }
+    };
 
     public _onDismissContactModal = (): void => {
         this.setState({ isContactModalOpen: false });
-    }
+    };
 }
 
 // scroll animation calc is simply (imageWidth * totalRepetitions) / 2
@@ -159,7 +177,10 @@ const fadeUp = keyframes`
   }
 `;
 
-const ConfiguratorSection = styled(Section)<SectionProps>`
+const ConfiguratorSection =
+    styled(Section) <
+    SectionProps >
+    `
   @media (max-width: 1050px) {
       display: none;
   }
@@ -180,21 +201,24 @@ const MarqueeWrap = styled.div`
     }
 
     @media (min-width: 768px) {
-      > div {
-          width: 6660px;
-          animation: ${scroll} 70s linear infinite;
-      }
+        > div {
+            width: 6660px;
+            animation: ${scroll} 70s linear infinite;
+        }
     }
 
     @media (max-width: 768px) {
-      > div {
-          width: 5400px;
-          animation: ${scrollMobile} 70s linear infinite;
-      }
+        > div {
+            width: 5400px;
+            animation: ${scrollMobile} 70s linear infinite;
+        }
     }
 `;
 
-const Card = styled.div<{index: number}>`
+const Card =
+    styled.div <
+    { index: number } >
+    `
   opacity: 0;
   flex-shrink: 0;
   transform: translateY(10px);
