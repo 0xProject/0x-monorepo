2a2260de45925bc309b6d6baf990d5ea6a171a90
Amir Bandeali <abandeali1@gmail.com>
2018-12-18 15:55:24 -0800

    Use more efficient check for overflow

--- contracts/protocol/contracts/protocol/AssetProxy/MultiAssetProxy.sol
+++ contracts/protocol/contracts/protocol/AssetProxy/MultiAssetProxy.sol
@@ -181,11 +181,11 @@ contract MultiAssetProxy is
                     let amountsElement := calldataload(add(amountsContentsStart, i))
                     let totalAmount := mul(amountsElement, amount)
 
-                    // Revert if multiplication resulted in an overflow
-                    if and(
-                        gt(amount, 0),
-                        iszero(eq(div(totalAmount, amount), amountsElement))
-                    ) {
+                    // Revert if `amount` != 0 and multiplication resulted in an overflow 
+                    if iszero(or(
+                        iszero(amount),
+                        eq(div(totalAmount, amount), amountsElement)
+                    )) {
                         // Revert with `Error("UINT256_OVERFLOW")`
                         mstore(0, 0x08c379a000000000000000000000000000000000000000000000000000000000)
                         mstore(32, 0x0000002000000000000000000000000000000000000000000000000000000000)
