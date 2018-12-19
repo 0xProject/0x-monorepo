8ddf925a8feb41e240871fe0d57e0f40b9bb4896
Leonid Logvinov <logvinov.leon@gmail.com>
2018-12-19 14:07:20 +0100

    Add SpyResolver

---
+++ packages/sol-resolver/src/resolvers/spy_resolver.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import * as _ from 'lodash';
+import * as path from 'path';
+
+import { ContractSource } from '../types';
+
+import { Resolver } from './resolver';
+
+export class SpyResolver extends Resolver {
+    public resolvedContractSources: ContractSource[] = [];
+    private _resolver: Resolver;
+    constructor(resolver: Resolver) {
+        super();
+        this._resolver = resolver;
+    }
+    public resolveIfExists(importPath: string): ContractSource | undefined {
+        const contractSourceIfExists = this._resolver.resolveIfExists(importPath);
+        if (!_.isUndefined(contractSourceIfExists)) {
+            this.resolvedContractSources.push(contractSourceIfExists);
+        }
+        return contractSourceIfExists;
+    }
+}
