e83a108c52f9414658cf2ef6eae50eda39d11dd1
Greg Hysen <greg.hysen@gmail.com>
2018-12-21 17:23:46 -0800

    added dutch auction address for testnets

--- packages/contract-addresses/src/index.ts
+++ packages/contract-addresses/src/index.ts
@@ -41,7 +41,7 @@ const networkToAddresses: { [networkId: number]: ContractAddresses } = {
         assetProxyOwner: '0xf5fa5b5fed2727a0e44ac67f6772e97977aa358b',
         forwarder: '0x2240dab907db71e64d3e0dba4800c83b5c502d4e',
         orderValidator: '0x90431a90516ab49af23a0530e04e8c7836e7122f',
-        dutchAuction: '0x',
+        dutchAuction: '0x2df6b59309f35ada230ec7d61d7d97355017a1df',
     },
     4: {
         exchange: '0xbce0b5f6eb618c565c3e5f5cd69652bbc279f44e',
@@ -52,7 +52,7 @@ const networkToAddresses: { [networkId: number]: ContractAddresses } = {
         assetProxyOwner: '0xe1703da878afcebff5b7624a826902af475b9c03',
         forwarder: '0x2d40589abbdee84961f3a7656b9af7adb0ee5ab4',
         orderValidator: '0x0c5173a51e26b29d6126c686756fb9fbef71f762',
-        dutchAuction: '0x',
+        dutchAuction: '0xdd7bd6437e67c422879364740ab5855fe3dc41f7',
     },
     42: {
         erc20Proxy: '0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e',
@@ -63,7 +63,7 @@ const networkToAddresses: { [networkId: number]: ContractAddresses } = {
         assetProxyOwner: '0x2c824d2882baa668e0d5202b1e7f2922278703f8',
         forwarder: '0x17992e4ffb22730138e4b62aaa6367fa9d3699a6',
         orderValidator: '0xb389da3d204b412df2f75c6afb3d0a7ce0bc283d',
-        dutchAuction: '0x',
+        dutchAuction: '0xe11667fb51f34c5367f40d7e379327ce32ee7150',
     },
     // NetworkId 50 represents our Ganache snapshot generated from migrations.
     50: {
