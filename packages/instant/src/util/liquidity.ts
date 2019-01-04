50b365ec8a192100f360a87089a11fbe5a034924
Steve Klebanoff <steve.klebanoff@gmail.com>
2019-01-03 17:29:50 -0800

    WIP: checkErc20TokenLiquidityAsync

--- packages/instant/src/util/liquidity.ts
+++ packages/instant/src/util/liquidity.ts
@@ -1,8 +1,24 @@
 import { assert } from '@0x/assert';
+import { HttpClient } from '@0x/connect';
+import { assetDataUtils } from '@0x/order-utils';
 
-export const checkErc20TokenLiquidity = (sraApiUrl: string, erc20TokenAddress: string) => {
-    // assert valid Url
+import { Network } from '../types';
+
+// TODO: move to constants
+const MAINNET_WETH_ASSET_DATA = '0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+export const checkErc20TokenLiquidityAsync = async (sraApiUrl: string, erc20TokenAddress: string) => {
     assert.isWebUri('sraApiUrl', sraApiUrl);
-    // assert valid erc20tokenaddress
     assert.isETHAddressHex('erc20TokenAddress', erc20TokenAddress);
+
+    const tokenAssetData = assetDataUtils.encodeERC20AssetData(erc20TokenAddress);
+    const httpClient = new HttpClient(sraApiUrl);
+    const response = await httpClient.getAssetPairsAsync({
+        networkId: Network.Mainnet,
+        assetDataA: MAINNET_WETH_ASSET_DATA,
+        assetDataB: tokenAssetData,
+        perPage: 1000,
+    });
+    const supported = response.records.length > 0;
+    return { supported };
 };
--- packages/instant/test/util/liquidity.test.ts
+++ packages/instant/test/util/liquidity.test.ts
@@ -1,20 +1,58 @@
-import { checkErc20TokenLiquidity } from '../../src/util/liquidity';
+import * as fetchMock from 'fetch-mock';
+
+import { checkErc20TokenLiquidityAsync } from '../../src/util/liquidity';
 
 const ZRX_ADDRESS = '0xe41d2489571d322189246dafa5ebde1f4699f498';
+const OMG_ADDRESS = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
 const SRA_URL = 'https://api.radarrelay.com/0x/v2/';
 
 describe('liquidity', () => {
+    beforeEach(() => {
+        fetchMock.restore();
+    });
     describe('checkErc20TokenLiquidity', () => {
         describe('validation', () => {
             it('should raise if invalid url', () => {
-                expect(() => {
-                    checkErc20TokenLiquidity('abc', ZRX_ADDRESS);
-                }).toThrowError('Expected sraApiUrl to be of type web uri, encountered: abc');
+                expect(checkErc20TokenLiquidityAsync('abc', ZRX_ADDRESS)).rejects.toThrowError(
+                    'Expected sraApiUrl to be of type web uri, encountered: abc',
+                );
             });
             it('should raise if invalid token address', () => {
-                expect(() => {
-                    checkErc20TokenLiquidity(SRA_URL, 'xyz');
-                }).toThrowError('Expected erc20TokenAddress to be of type ETHAddressHex, encountered: xyz');
+                expect(checkErc20TokenLiquidityAsync(SRA_URL, 'xyz')).rejects.toThrowError(
+                    'Expected erc20TokenAddress to be of type ETHAddressHex, encountered: xyz',
+                );
+            });
+            describe('supported', () => {
+                const OMG_ASSET_PAIR_REQUEST_URL =
+                    'https://api.radarrelay.com/0x/v2/asset_pairs?assetDataA=0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&assetDataB=0xf47261b0000000000000000000000000d26114cd6ee289accf82350c8d8487fedb8a0c07&networkId=1&perPage=1000';
+                const OMG_ASSET_PAIR_RESPONSE = {
+                    total: 1,
+                    page: 1,
+                    perPage: 1000,
+                    records: [
+                        {
+                            assetDataA: {
+                                assetData: '0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+                                precision: 8,
+                                minAmount: '0',
+                                maxAmount: '999999999999999999999',
+                            },
+                            assetDataB: {
+                                assetData: '0xf47261b0000000000000000000000000d26114cd6ee289accf82350c8d8487fedb8a0c07',
+                                precision: 8,
+                                minAmount: '0',
+                                maxAmount: '999999999999999999999',
+                            },
+                        },
+                    ],
+                };
+
+                it('should return true if SRA does support support', async () => {
+                    fetchMock.get(OMG_ASSET_PAIR_REQUEST_URL, OMG_ASSET_PAIR_RESPONSE);
+
+                    const result = await checkErc20TokenLiquidityAsync(SRA_URL, OMG_ADDRESS);
+                    expect(result.supported).toEqual(true);
+                });
             });
         });
     });
