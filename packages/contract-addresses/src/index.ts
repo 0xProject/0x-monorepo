b5420fde529a27eb144307777292c4c5f582015d
Jacob Evans <jacob@dekz.net>
2019-01-14 10:09:12 +1100

    Update contract addresses

--- packages/contract-addresses/src/index.ts
+++ packages/contract-addresses/src/index.ts
@@ -75,10 +75,10 @@ const networkToAddresses: { [networkId: number]: ContractAddresses } = {
         zrxToken: '0x871dd7c2b4b25e1aa18728e9d5f2af4c4e431f5c',
         etherToken: '0x0b1ba0af832d7c05fd64161e0db78e85978e8082',
         exchange: '0x48bacb9266a570d521063ef5dd96e61686dbe788',
-        assetProxyOwner: '0x7e3f4e1deb8d3a05d9d2da87d9521268d0ec3239',
-        forwarder: '0xb7c9b454221e26880eb9c3101b3295ca7d8279ef',
-        orderValidator: '0x6000eca38b8b5bba64986182fe2a69c57f6b5414',
-        dutchAuction: '0x32eecaf51dfea9618e9bc94e9fbfddb1bbdcba15',
+        assetProxyOwner: '0x04b5dadd2c0d6a261bfafbc964e0cac48585def3',
+        forwarder: '0x6000eca38b8b5bba64986182fe2a69c57f6b5414',
+        orderValidator: '0x32eecaf51dfea9618e9bc94e9fbfddb1bbdcba15',
+        dutchAuction: '0x7e3f4e1deb8d3a05d9d2da87d9521268d0ec3239',
     },
 };
 
