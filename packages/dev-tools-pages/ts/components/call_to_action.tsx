7b2a9ba698034dfe9dde909753223fcd4a520337
Fabio Berger <me@fabioberger.com>
2019-01-17 16:38:30 +0100

    Fix linter errors

--- packages/dev-tools-pages/ts/components/call_to_action.tsx
+++ packages/dev-tools-pages/ts/components/call_to_action.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { ContextInterface, ThemeContext } from 'ts/context';
-import { media } from 'ts/variables';
 
 import { Button } from './button';
-import { Beta } from './typography';
 
 const CallToAction: React.StatelessComponent<ContextInterface> = ({ children }) => (
     <ThemeContext.Consumer>
@@ -37,25 +35,6 @@ const StyledCallToAction = styled.section`
 const CallToActionContainer = styled.div`
     margin: 0 auto;
     max-width: 590px;
-`;
-
-const Subtitle = styled.h2`
-    font-size: 3.75rem;
-    line-height: 1.16;
-    margin-bottom: 1.5rem;
-
-    ${media.small`
-        font-size: 2.25rem;
-        line-height: 1.1;
-        margin-bottom: 1.375rem;
-    `};
-`;
-
-const Tagline = styled(Beta)`
-    margin-bottom: 2rem;
-    ${media.small`
-        margin-bottom: 1.25rem;
-    `};
 `;
 
 export { CallToAction };
