00e7683315063a868f614e56821cf1d447e5b5d2
xianny <xianny@gmail.com>
2019-01-29 18:58:10 +0900

    add failing CI for dependencies with multiple versions

--- packages/monorepo-scripts/src/deps_versions.ts
+++ packages/monorepo-scripts/src/deps_versions.ts
@@ -3,7 +3,7 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 import { sync as globSync } from 'glob';
-import * as _ from 'lodash';
+import * as path from 'path';
 
 import { utils } from './utils/utils';
 
@@ -17,11 +17,18 @@ interface VersionsByDependency {
     [depName: string]: Versions;
 }
 
-const PACKAGE_JSON_GLOB = '../*/package.json';
+const PACKAGE_JSON_GLOB = '../../*/package.json';
+const excludes: string[] = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../../../package.json')).toString(),
+).config.dependenciesWithMultipleVersions.split(' ');
 
-// tslint:disable:no-unused-variable
-function getDependencies(path: string): Dependencies {
-    const file = fs.readFileSync(path).toString();
+if (require.main === module) {
+    const versions = getVersionsByDependency();
+    printVersionsByDependency(versions);
+}
+
+function getDependencies(_path: string): Dependencies {
+    const file = fs.readFileSync(_path).toString();
     const parsed = JSON.parse(file);
     const dependencies = {
         ...parsed.dependencies,
@@ -30,27 +37,51 @@ function getDependencies(path: string): Dependencies {
     return dependencies;
 }
 
-const files = globSync(PACKAGE_JSON_GLOB);
-const versionsByDependency: VersionsByDependency = {};
-files.map(path => {
-    const [_1, packageName, _2] = path.split('/');
-    const dependencies = getDependencies(path);
-    _.map(dependencies, (version: string, depName: string) => {
-        if (_.isUndefined(versionsByDependency[depName])) {
-            versionsByDependency[depName] = {};
-        }
-        versionsByDependency[depName][packageName] = version;
-    });
-});
-
-_.map(versionsByDependency, (versions: Versions, depName: string) => {
-    if (_.uniq(_.values(versions)).length === 1) {
-        delete versionsByDependency[depName];
-    } else {
-        utils.log(chalk.bold(depName));
-        _.map(versions, (version: string, packageName: string) => {
-            utils.log(`├── ${packageName} -> ${version}`);
+export function getVersionsByDependency(): VersionsByDependency {
+    const files = globSync(path.join(__dirname, PACKAGE_JSON_GLOB));
+    console.log(files);
+    const versionsByDependency: VersionsByDependency = {};
+    files.map(_path => {
+        const pathParts = _path.split('/');
+        const packageName = pathParts[pathParts.length - 2];
+        const dependencies = getDependencies(_path);
+        Object.keys(dependencies).forEach((depName: string) => {
+            if (versionsByDependency[depName] === undefined) {
+                versionsByDependency[depName] = {};
+            }
+            const version = dependencies[depName];
+            versionsByDependency[depName][packageName] = version;
         });
-    }
-});
-// tslint:disable:no-unused-variable
+    });
+    return versionsByDependency;
+}
+
+export function hasAnyMultipleVersions(versionsByDependency: VersionsByDependency): boolean {
+    Object.keys(versionsByDependency)
+        .filter((depName: string) => !excludes.includes(depName))
+        .forEach((depName: string): false | undefined => {
+            const versions = versionsByDependency[depName];
+            if (hasMultipleVersions(versions)) {
+                return false;
+            }
+            return;
+        });
+    return true;
+}
+
+export function printVersionsByDependency(versionsByDependency: VersionsByDependency): void {
+    Object.keys(versionsByDependency).forEach(depName => {
+        const versions = versionsByDependency[depName];
+        if (!excludes.includes(depName) && hasMultipleVersions(versions)) {
+            utils.log(chalk.bold(depName));
+            Object.keys(versions).forEach(packageName => {
+                utils.log(`├── ${packageName} -> ${versions[packageName]}`);
+            });
+        }
+    });
+}
+
+function hasMultipleVersions(versions: Versions): boolean {
+    const uniques = new Set(Object.values(versions));
+    return uniques.size > 1;
+}
