7ce6334685987b981f6ada242804feaa76ce668b
Krzysztof Adamek <squadack@o2.pl>
2019-01-31 09:58:52 -0500

    added new SpyResolver that mimics behaviour of old one

---
+++ packages/sol-compiler/src/utils/spy_resolver.ts
@@ -0,0 +1,18 @@
+import { ResolverEngine } from '@resolver-engine/core';
+import { ImportFile } from '@resolver-engine/imports';
+
+export class SpyResolver extends ResolverEngine<ImportFile> {
+    public resolvedContractSources: ImportFile[] = [];
+    private readonly _resolver: ResolverEngine<ImportFile>;
+
+    constructor(resolver: ResolverEngine<ImportFile>) {
+        super();
+        this._resolver = resolver;
+    }
+
+    public async require(uri: string, workingDir?: string): Promise<ImportFile> {
+        const resolvedFile = await this._resolver.require(uri, workingDir);
+        this.resolvedContractSources.push(resolvedFile);
+        return resolvedFile;
+    }
+}
