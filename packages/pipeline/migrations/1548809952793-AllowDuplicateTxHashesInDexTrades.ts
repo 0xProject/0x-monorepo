4f3cab42131f0bbe5e3f08cc04a1d7f60c6d48a0
fragosti <francesco.agosti93@gmail.com>
2019-02-01 15:50:04 -0800

    add row deletion step to migration

--- packages/pipeline/migrations/1548809952793-AllowDuplicateTxHashesInDexTrades.ts
+++ packages/pipeline/migrations/1548809952793-AllowDuplicateTxHashesInDexTrades.ts
@@ -6,7 +6,9 @@ export class AllowDuplicateTxHashesInDexTrades1548809952793 implements Migration
     public async up(queryRunner: QueryRunner): Promise<any> {
         const dexTradesTable = await queryRunner.getTable(DEX_TRADES_TABLE_NAME);
         if (dexTradesTable) {
-            // Composite key goes from (source_url, tx_hash) to (trade_index, source_url, tx_hash)
+            // Need new primary key to be non-null. No default value makes sense, so drop table.
+            await queryRunner.query(`DELETE from ${DEX_TRADES_TABLE_NAME}`);
+            // Composite key goes from (source_url, tx_hash) to (source_url, tx_hash, trade_index)
             await queryRunner.addColumn(
                 DEX_TRADES_TABLE_NAME,
                 new TableColumn({
