263d5159b9b3724caf332a755b020e335f175e20
Amir Bandeali <abandeali1@gmail.com>
2019-02-07 10:24:05 -0800

    Make constants internal

--- contracts/exchange-libs/contracts/src/LibExchangeErrors.sol
+++ contracts/exchange-libs/contracts/src/LibExchangeErrors.sol
@@ -25,46 +25,46 @@ pragma solidity ^0.5.3;
 contract LibExchangeErrors {
 
     /// Order validation errors ///
-    string constant ORDER_UNFILLABLE = "ORDER_UNFILLABLE";                              // Order cannot be filled.
-    string constant INVALID_MAKER = "INVALID_MAKER";                                    // Invalid makerAddress.
-    string constant INVALID_TAKER = "INVALID_TAKER";                                    // Invalid takerAddress.
-    string constant INVALID_SENDER = "INVALID_SENDER";                                  // Invalid `msg.sender`.
-    string constant INVALID_ORDER_SIGNATURE = "INVALID_ORDER_SIGNATURE";                // Signature validation failed. 
+    string constant internal ORDER_UNFILLABLE = "ORDER_UNFILLABLE";                              // Order cannot be filled.
+    string constant internal INVALID_MAKER = "INVALID_MAKER";                                    // Invalid makerAddress.
+    string constant internal INVALID_TAKER = "INVALID_TAKER";                                    // Invalid takerAddress.
+    string constant internal INVALID_SENDER = "INVALID_SENDER";                                  // Invalid `msg.sender`.
+    string constant internal INVALID_ORDER_SIGNATURE = "INVALID_ORDER_SIGNATURE";                // Signature validation failed. 
     
     /// fillOrder validation errors ///
-    string constant INVALID_TAKER_AMOUNT = "INVALID_TAKER_AMOUNT";                      // takerAssetFillAmount cannot equal 0.
-    string constant ROUNDING_ERROR = "ROUNDING_ERROR";                                  // Rounding error greater than 0.1% of takerAssetFillAmount. 
+    string constant internal INVALID_TAKER_AMOUNT = "INVALID_TAKER_AMOUNT";                      // takerAssetFillAmount cannot equal 0.
+    string constant internal ROUNDING_ERROR = "ROUNDING_ERROR";                                  // Rounding error greater than 0.1% of takerAssetFillAmount. 
     
     /// Signature validation errors ///
-    string constant INVALID_SIGNATURE = "INVALID_SIGNATURE";                            // Signature validation failed. 
-    string constant SIGNATURE_ILLEGAL = "SIGNATURE_ILLEGAL";                            // Signature type is illegal.
-    string constant SIGNATURE_UNSUPPORTED = "SIGNATURE_UNSUPPORTED";                    // Signature type unsupported.
+    string constant internal INVALID_SIGNATURE = "INVALID_SIGNATURE";                            // Signature validation failed. 
+    string constant internal SIGNATURE_ILLEGAL = "SIGNATURE_ILLEGAL";                            // Signature type is illegal.
+    string constant internal SIGNATURE_UNSUPPORTED = "SIGNATURE_UNSUPPORTED";                    // Signature type unsupported.
     
     /// cancelOrdersUptTo errors ///
-    string constant INVALID_NEW_ORDER_EPOCH = "INVALID_NEW_ORDER_EPOCH";                // Specified salt must be greater than or equal to existing orderEpoch.
+    string constant internal INVALID_NEW_ORDER_EPOCH = "INVALID_NEW_ORDER_EPOCH";                // Specified salt must be greater than or equal to existing orderEpoch.
 
     /// fillOrKillOrder errors ///
-    string constant COMPLETE_FILL_FAILED = "COMPLETE_FILL_FAILED";                      // Desired takerAssetFillAmount could not be completely filled. 
+    string constant internal COMPLETE_FILL_FAILED = "COMPLETE_FILL_FAILED";                      // Desired takerAssetFillAmount could not be completely filled. 
 
     /// matchOrders errors ///
-    string constant NEGATIVE_SPREAD_REQUIRED = "NEGATIVE_SPREAD_REQUIRED";              // Matched orders must have a negative spread.
+    string constant internal NEGATIVE_SPREAD_REQUIRED = "NEGATIVE_SPREAD_REQUIRED";              // Matched orders must have a negative spread.
 
     /// Transaction errors ///
-    string constant REENTRANCY_ILLEGAL = "REENTRANCY_ILLEGAL";                          // Recursive reentrancy is not allowed. 
-    string constant INVALID_TX_HASH = "INVALID_TX_HASH";                                // Transaction has already been executed. 
-    string constant INVALID_TX_SIGNATURE = "INVALID_TX_SIGNATURE";                      // Signature validation failed. 
-    string constant FAILED_EXECUTION = "FAILED_EXECUTION";                              // Transaction execution failed. 
+    string constant internal REENTRANCY_ILLEGAL = "REENTRANCY_ILLEGAL";                          // Recursive reentrancy is not allowed. 
+    string constant internal INVALID_TX_HASH = "INVALID_TX_HASH";                                // Transaction has already been executed. 
+    string constant internal INVALID_TX_SIGNATURE = "INVALID_TX_SIGNATURE";                      // Signature validation failed. 
+    string constant internal FAILED_EXECUTION = "FAILED_EXECUTION";                              // Transaction execution failed. 
     
     /// registerAssetProxy errors ///
-    string constant ASSET_PROXY_ALREADY_EXISTS = "ASSET_PROXY_ALREADY_EXISTS";          // AssetProxy with same id already exists.
+    string constant internal ASSET_PROXY_ALREADY_EXISTS = "ASSET_PROXY_ALREADY_EXISTS";          // AssetProxy with same id already exists.
 
     /// dispatchTransferFrom errors ///
-    string constant ASSET_PROXY_DOES_NOT_EXIST = "ASSET_PROXY_DOES_NOT_EXIST";          // No assetProxy registered at given id.
-    string constant TRANSFER_FAILED = "TRANSFER_FAILED";                                // Asset transfer unsuccesful.
+    string constant internal ASSET_PROXY_DOES_NOT_EXIST = "ASSET_PROXY_DOES_NOT_EXIST";          // No assetProxy registered at given id.
+    string constant internal TRANSFER_FAILED = "TRANSFER_FAILED";                                // Asset transfer unsuccesful.
 
     /// Length validation errors ///
-    string constant LENGTH_GREATER_THAN_0_REQUIRED = "LENGTH_GREATER_THAN_0_REQUIRED";  // Byte array must have a length greater than 0.
-    string constant LENGTH_GREATER_THAN_3_REQUIRED = "LENGTH_GREATER_THAN_3_REQUIRED";  // Byte array must have a length greater than 3.
-    string constant LENGTH_0_REQUIRED = "LENGTH_0_REQUIRED";                            // Byte array must have a length of 0.
-    string constant LENGTH_65_REQUIRED = "LENGTH_65_REQUIRED";                          // Byte array must have a length of 65.
+    string constant internal LENGTH_GREATER_THAN_0_REQUIRED = "LENGTH_GREATER_THAN_0_REQUIRED";  // Byte array must have a length greater than 0.
+    string constant internal LENGTH_GREATER_THAN_3_REQUIRED = "LENGTH_GREATER_THAN_3_REQUIRED";  // Byte array must have a length greater than 3.
+    string constant internal LENGTH_0_REQUIRED = "LENGTH_0_REQUIRED";                            // Byte array must have a length of 0.
+    string constant internal LENGTH_65_REQUIRED = "LENGTH_65_REQUIRED";                          // Byte array must have a length of 65.
 }
