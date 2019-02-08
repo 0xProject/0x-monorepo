459fd4ca5cf80cb0c2a25084d951f26af86d769f
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-02-08 09:13:24 -0500

    pull all supported NFT's, not just cryptokitties

--- packages/pipeline/src/scripts/pull_nft_trades.ts
+++ packages/pipeline/src/scripts/pull_nft_trades.ts
@@ -2,7 +2,7 @@
 import 'reflect-metadata';
 import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 
-import { getTradesAsync } from '../data_sources/nonfungible_dot_com';
+import { getTradesAsync, knownPublishers } from '../data_sources/nonfungible_dot_com';
 import { NftTrade } from '../entities';
 import * as ormConfig from '../ormconfig';
 import { parseNonFungibleDotComTrades } from '../parsers/non_fungible_dot_com';
@@ -21,21 +21,23 @@ let connection: Connection;
 
 async function getAndSaveTrades(): Promise<void> {
     const tradesRepository = connection.getRepository(NftTrade);
-    console.log('Getting latest NFT trades...');
-    // TODO(albrow): Pull NFT trades for all publishers, not just cryptokitties.
-    const tradeWithHighestBlockNumber = await tradesRepository
-        .createQueryBuilder('nonfungible_dot_com')
-        .where('nonfungible_dot_com.publisher = :publisher', { publisher: 'cryptokitties' })
-        .orderBy({ 'nonfungible_dot_com.block_number': 'DESC' })
-        .getOne();
-    const highestExistingBlockNumber =
-        tradeWithHighestBlockNumber === undefined ? 0 : tradeWithHighestBlockNumber.blockNumber;
-    console.log(`Highest block number in existing trades: ${highestExistingBlockNumber}`);
-    const rawTrades = await getTradesAsync('cryptokitties', highestExistingBlockNumber);
-    console.log(`Parsing ${rawTrades.length} trades...`);
-    const trades = parseNonFungibleDotComTrades(rawTrades, 'cryptokitties');
-    console.log(`Saving ${rawTrades.length} trades...`);
-    await tradesRepository.save(trades, { chunk: Math.ceil(trades.length / BATCH_SAVE_SIZE) });
+
+    for (const publisher of knownPublishers) {
+        console.log(`Getting latest trades for NFT ${publisher}...`);
+        const tradeWithHighestBlockNumber = await tradesRepository
+            .createQueryBuilder('nonfungible_dot_com')
+            .where('nonfungible_dot_com.publisher = :publisher', { publisher })
+            .orderBy({ 'nonfungible_dot_com.block_number': 'DESC' })
+            .getOne();
+        const highestExistingBlockNumber =
+            tradeWithHighestBlockNumber === undefined ? 0 : tradeWithHighestBlockNumber.blockNumber;
+        console.log(`Highest block number in existing trades: ${highestExistingBlockNumber}`);
+        const rawTrades = await getTradesAsync(publisher, highestExistingBlockNumber);
+        console.log(`Parsing ${rawTrades.length} trades...`);
+        const trades = parseNonFungibleDotComTrades(rawTrades, publisher);
+        console.log(`Saving ${rawTrades.length} trades...`);
+        await tradesRepository.save(trades, { chunk: Math.ceil(trades.length / BATCH_SAVE_SIZE) });
+    }
     const newTotalTrades = await tradesRepository.count();
     console.log(`Done saving trades. There are now ${newTotalTrades} total NFT trades.`);
 }
