35f3446bb0543280d2475734ba8eb5ecf103ba29
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-02-08 09:06:34 -0500

    pull data from initial dump first, then API

--- packages/pipeline/src/data_sources/nonfungible_dot_com/index.ts
+++ packages/pipeline/src/data_sources/nonfungible_dot_com/index.ts
@@ -1,3 +1,5 @@
+import { logUtils } from '@0x/utils';
+
 import axios from 'axios';
 
 // URL to use for getting nft trades from nonfungible.com.
@@ -52,35 +54,132 @@ export interface NonfungibleDotComTrade {
 }
 
 /**
- * Gets and returns all trades for the given publisher and starting offset.
+ * Gets and returns all trades for the given publisher, starting at the given block number.
  * Automatically handles pagination.
  * @param publisher A valid "publisher" for the nonfungible.com API. (e.g. "cryptokitties")
- * @param start The offset to start querying from.
+ * @param blockNumberStart The block number to start querying from.
  */
-export async function getTradesAsync(publisher: string, start: number): Promise<NonfungibleDotComTrade[]> {
+export async function getTradesAsync(publisher: string, blockNumberStart: number): Promise<NonfungibleDotComTrade[]> {
+    const allTrades: NonfungibleDotComTrade[] = [];
+
+    /**
+     * due to high data volumes and rate limiting, we procured an initial data
+     * dump from nonfungible.com.  If the requested starting block number is
+     * contained in that initial dump, then pull relevant trades from there
+     * first.  Later (below) we'll get the more recent trades from the API itself.
+     */
+
+    if (blockNumberStart < highestBlockNumbersInIntialDump[publisher]) {
+        logUtils.log('getting trades from one-time dump');
+        // caller needs trades that are in the initial data dump, so get them
+        // from there, then later go to the API for the rest.
+        const initialDumpResponse = await axios.get<NonfungibleDotComHistoryResponse>(getInitialDumpUrl(publisher));
+        const initialDumpTrades = initialDumpResponse.data.data;
+        for (const initialDumpTrade of initialDumpTrades) {
+            if (shouldProcessTrade(initialDumpTrade, allTrades) === false) {
+                continue;
+            }
+
+            // these fields need to be set in order to avoid non-null
+            // constraint exceptions upon database insertion.
+            if (initialDumpTrade.logIndex === undefined) {
+                // for cryptopunks
+                initialDumpTrade.logIndex = 0;
+            }
+            if (initialDumpTrade.assetDescriptor === undefined) {
+                // for cryptopunks
+                initialDumpTrade.assetDescriptor = '';
+            }
+            if (initialDumpTrade.meta === undefined) {
+                // for cryptopunks
+                initialDumpTrade.meta = new Object();
+            }
+            if (initialDumpTrade.marketAddress === null) {
+                // for decentraland_estate
+                initialDumpTrade.marketAddress = '';
+            }
+
+            allTrades.push(initialDumpTrade);
+        }
+        logUtils.log(`got ${allTrades.length} from one-time dump`);
+    }
+
     const fullUrl = getFullUrlForPublisher(publisher);
 
-    // TODO(albrow): The code below should work but will hit rate limits almost
-    // immediately. Take another look after we talk more with nonfungible.com.
+    /**
+     * API returns trades in reverse chronological order, so highest block
+     * numbers first.  The `start` query parameter indicates how far back in
+     * time (in number of trades) the results should start.  Here we iterate
+     * over both start parameter values and block numbers simultaneously.
+     * Start parameter values count up from zero.  Block numbers count down
+     * until reaching the highest block number in the initial dump.
+     */
 
-    // let totalTradesAvailable = Number.MAX_SAFE_INTEGER;
-    // let allTrades: NonfungibleDotComTrade[] = [];
+    const blockNumberStop = Math.max(highestBlockNumbersInIntialDump[publisher] + 1, blockNumberStart);
+    for (
+        let startParam = 0, blockNumber = Number.MAX_SAFE_INTEGER;
+        blockNumber > blockNumberStop;
+        startParam += MAX_TRADES_PER_QUERY
+    ) {
+        const response = await _getTradesWithOffsetAsync(fullUrl, publisher, startParam);
+        const tradesFromApi = response.data;
+        logUtils.log(
+            `got ${
+                tradesFromApi.length
+            } trades from API. blockNumber=${blockNumber}. blockNumberStop=${blockNumberStop}`,
+        );
+        for (const tradeFromApi of tradesFromApi) {
+            if (tradeFromApi.blockNumber <= blockNumberStop) {
+                blockNumber = blockNumberStop;
+                break;
+            }
+            if (shouldProcessTrade(tradeFromApi, allTrades) === false) {
+                continue;
+            }
+            allTrades.push(tradeFromApi);
+            blockNumber = tradeFromApi.blockNumber;
+        }
+    }
 
-    // // Paginate by getting MAX_TRADES_PER_QUERY trades per request and
-    // // incrementing offset. The API reports the total number of trades available
-    // // as part of the response. We are finished after we send exactly one
-    // // request where offset > totalTradesAvailable.
-    // for (let offset = start; offset < totalTradesAvailable + MAX_TRADES_PER_QUERY; offset += MAX_TRADES_PER_QUERY) {
-    //     const resp = await _getTradesWithOffsetAsync(fullUrl, publisher, offset);
-    //     allTrades = allTrades.concat(resp.data);
-    //     totalTradesAvailable = resp.recordsFiltered;
-    //     console.log(`total trades available: ${totalTradesAvailable}`);
-    // }
-    // return allTrades;
-
-    const resp = await _getTradesWithOffsetAsync(fullUrl, publisher, start);
-    return resp.data;
+    return allTrades;
 }
+
+function shouldProcessTrade(trade: NonfungibleDotComTrade, existingTrades: NonfungibleDotComTrade[]): boolean {
+    // check to see if this trade is already in existingTrades
+    const existingTradeIndex = existingTrades.findIndex(
+        // HACK! making assumptions about composition of primary key
+        e =>
+            e.transactionHash === trade.transactionHash &&
+            e.logIndex === trade.logIndex &&
+            e.blockNumber === trade.blockNumber,
+    );
+    if (existingTradeIndex !== -1) {
+        logUtils.log("we've already captured this trade. deciding whether to use the existing record or this one.");
+        if (trade.blockNumber > existingTrades[existingTradeIndex].blockNumber) {
+            logUtils.log('throwing out existing trade');
+            existingTrades.splice(existingTradeIndex, 1);
+        } else {
+            logUtils.log('letting existing trade stand, and skipping processing of this trade');
+            return false;
+        }
+    }
+    return true;
+}
+
+const highestBlockNumbersInIntialDump: { [publisher: string]: number } = {
+    axieinfinity: 7065913,
+    cryptokitties: 4658171,
+    cryptopunks: 7058897,
+    cryptovoxels: 7060783,
+    decentraland_estate: 7065181,
+    decentraland: 6938962,
+    etherbots: 5204980,
+    etheremon: 7065370,
+    ethtown: 7064126,
+    knownorigin: 7065160,
+    mythereum: 7065311,
+    superrare: 7065955,
+};
 
 async function _getTradesWithOffsetAsync(
     url: string,
@@ -99,4 +198,8 @@ async function _getTradesWithOffsetAsync(
 
 function getFullUrlForPublisher(publisher: string): string {
     return `${NONFUNGIBLE_DOT_COM_URL}/market/${publisher}/history`;
+}
+
+function getInitialDumpUrl(publisher: string): string {
+    return `https://nonfungible-dot-com-one-time-data-dump.s3.amazonaws.com/sales_summary_${publisher}.json`;
 }
