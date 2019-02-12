d56931314c0f6e9c01ba5875bc3baf7e3dac6b22
Fred Carlsen <fred@sjelfull.no>
2019-02-12 22:53:06 +0100

    Update content base

--- packages/v0x/webpack.config.js
+++ packages/v0x/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, '/src'),
     compress: true,
     host: '0.0.0.0',
     port: 3000,
