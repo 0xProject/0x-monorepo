718feb687ff6cb9ddd9f841d5f6f3b04eac4fbe5
Fred Carlsen <fred@sjelfull.no>
2019-02-14 21:05:00 +0100

    Fix countdown and format in locale aware date

--- packages/website/ts/pages/governance/countdown.tsx
+++ packages/website/ts/pages/governance/countdown.tsx
@@ -2,21 +2,32 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 import * as React from 'react';
 
-import { number } from 'prop-types';
 import { Paragraph } from 'ts/components/text';
 
 interface Props {
     deadline: number;
 }
 
+interface TimeStructure {
+    year?: number;
+    month?: number;
+    week?: number;
+    day?: number;
+    hour?: number;
+    minute?: number;
+    second?: number;
+    [key: string]: number;
+}
+
 const now = moment();
 
 export const Countdown: React.StatelessComponent<Props> = ({ deadline }) => {
-    const time = moment(deadline, 'X');
+    const pstOffset = '-0800';
+    const time = moment(deadline, 'X').utcOffset(pstOffset);
     const isPassed = time.isBefore(now);
     const voteTextPrefix = isPassed ? `Voting ended: ` : `Vote ends: `;
     const timeText = !isPassed ? ` • ${getRelativeTime(time)}` : '';
-    const voteText = `${voteTextPrefix}: ${time.format('YYYY/MM/DD h:mm:ss Z')}${timeText}`;
+    const voteText = `${voteTextPrefix} ${time.format('L LT')} PST ${timeText}`;
 
     return (
         <div>
@@ -34,7 +45,7 @@ export const VoteDeadline: React.StatelessComponent<Props> = ({ deadline }) => {
     return (
         <div>
             <div style={{ display: 'flex', marginBottom: '10px' }}>
-                <Paragraph style={{ marginRight: '10px' }}>
+                <Paragraph isMuted={0.5} style={{ marginRight: '10px' }}>
                     Vote {isPassed ? 'ended' : 'ends'}: {time.format('YYYY/MM/DD h:mm:ss Z')}
                 </Paragraph>
             </div>
@@ -43,50 +54,20 @@ export const VoteDeadline: React.StatelessComponent<Props> = ({ deadline }) => {
 };
 
 function getRelativeTime(deadline: moment.Moment): string {
-    const parts = [];
     const diff = moment().diff(deadline);
     const duration = moment.duration(diff);
-    const days = duration.asDays();
-    const hours = duration.asHours();
-    const minutes = duration.asMinutes();
-
-    // @todo: how to apply correct timezone?
-    // ${time.format('YYYY/MM/DD h:mm:ss Z')}
-
-    if (days > 0) {
-        parts.push(`${days} days`);
-    }
-
-    if (hours > 0) {
-        parts.push(`${hours} hours`);
-    }
-
-    if (minutes > 0) {
-        parts.push(`${minutes} mins`);
-    }
 
     return millisToDaysHoursMinutes(duration.asMilliseconds());
 }
 
-interface TimeStructure {
-    year?: number;
-    month?: number;
-    week?: number;
-    day?: number;
-    hour?: number;
-    minute?: number;
-    second?: number;
-    [key: string]: number;
-}
-
 function millisToDaysHoursMinutes(futureDateMs: number): string {
-    let delta = Math.abs(futureDateMs - now.milliseconds()) / 1000;                           // delta
-    const result: TimeStructure = {};                                                                // result
-    const structure: TimeStructure = {                                                                  // structure
+    let delta = Math.abs(futureDateMs - now.milliseconds()) / 1000;
+    const result: TimeStructure = {};
+    const structure: TimeStructure = {
         // year: 31536000,
         // month: 2592000,
-        // week: 604800, // uncomment row to ignore
-        day: 86400,   // feel free to add your own row
+        // week: 604800,
+        day: 86400,
         hour: 3600,
         minute: 60,
         second: 1,
