0aa26d791de79351ec2fe4ddb9d89dc38bd99b70
David Sun <dvsuner@protonmail.com>
2019-02-14 20:14:37 -0500

    fix for credit lead form data

--- packages/website/ts/components/modals/modal_contact.tsx
+++ packages/website/ts/components/modals/modal_contact.tsx
@@ -374,8 +374,8 @@ export class ModalContact extends React.Component<Props> {
             jsonBody = {
                 name: this.nameRef.current.value,
                 email: this.emailRef.current.value,
-                projectOrCompany: this.companyProjectRef.current.value,
-                comments: this.commentsRef.current.value,
+                project_name: this.companyProjectRef.current.value,
+                project_description: this.commentsRef.current.value,
                 services: this.state.creditLeadsServices,
             };
         } else {
