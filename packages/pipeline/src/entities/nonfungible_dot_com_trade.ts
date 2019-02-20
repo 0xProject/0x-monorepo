1232a9a03ddba1b29c0100c2c8d79945c1ac4733
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-02-19 19:07:42 -0800

    Integrate one-time dump and API for nonfungible.com (#1603)
    * Add script for pulling NFT trade data from nonfungible.com
    
    * corrections for current state of API
    
    * change data model to match data source
    
    * change primary key
    
    * pull data from initial dump first, then API
    
    * pull all supported NFT's, not just cryptokitties
    
    * disable problematic data sources
    
    * rename function to satisfy linter
    
    * Rename table to nonfungible_dot_com_trades
    
    * rename parser module to nonfungible_dot_com from non_fungible_dot_com, for consistency
    
    * correct mistaken reference to Bloxy
    
    * rename NonfungibleDotComTrade to ...TradeResponse
    
    * `NftTrade` -> `NonfungibleDotComTrade`
    
    * rename files to match prior object renaming
    
    * use fetchAsync instead of axios
    
    * improve fetchAsync error message: include URL
    
    * avoid non-null contraints in API trades too, not just for trades from the one-time dump
    
    * disable mythereum publisher

---
+++ packages/pipeline/src/entities/nonfungible_dot_com_trade.ts
@@ -0,0 +1,35 @@
+import { BigNumber } from '@0x/utils';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+import { bigNumberTransformer, numberToBigIntTransformer } from '../utils';
+
+@Entity({ name: 'nonfungible_dot_com_trades', schema: 'raw' })
+export class NonfungibleDotComTrade {
+    @PrimaryColumn({ name: 'transaction_hash' })
+    public transactionHash!: string;
+    @PrimaryColumn({ name: 'publisher' })
+    public publisher!: string;
+    @PrimaryColumn({ name: 'block_number', type: 'bigint', transformer: numberToBigIntTransformer })
+    public blockNumber!: number;
+    @PrimaryColumn({ name: 'log_index' })
+    public logIndex!: number;
+    @PrimaryColumn({ name: 'asset_id' })
+    public assetId!: string;
+
+    @Column({ name: 'block_timestamp', type: 'bigint', transformer: numberToBigIntTransformer })
+    public blockTimestamp!: number;
+    @Column({ name: 'asset_descriptor' })
+    public assetDescriptor!: string;
+    @Column({ name: 'market_address' })
+    public marketAddress!: string;
+    @Column({ name: 'total_price', type: 'numeric', transformer: bigNumberTransformer })
+    public totalPrice!: BigNumber;
+    @Column({ name: 'usd_price', type: 'numeric', transformer: bigNumberTransformer })
+    public usdPrice!: BigNumber;
+    @Column({ name: 'buyer_address' })
+    public buyerAddress!: string;
+    @Column({ name: 'seller_address' })
+    public sellerAddress!: string;
+    @Column({ type: 'jsonb' })
+    public meta!: object;
+}
