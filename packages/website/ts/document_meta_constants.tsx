426190181b322baf566a8c525399dad35294a3a8
David Sun <dvsuner@protonmail.com>
2019-02-19 23:33:48 -0500

    added metadata for pages (incomplete)

---
+++ packages/website/ts/document_meta_constants.tsx
@@ -0,0 +1,63 @@
+export interface DocumentMetadata {
+    title: string;
+    description: string;
+    keywords: string;
+}
+
+export const documentConstants: { [s: string]: DocumentMetadata } = {
+    LANDING: {
+        title: '0x: Powering the decentralized exchange of tokens on Ethereum',
+        description: '0x is an open protocol that enables the peer-to-peer exchange of assets on the Ethereum blockchain.',
+        keywords: 'DEX, Decentralized Exchange, Ethereum, 0x, 0x Protocol, Tokens, ZRX, Crypto Exchange, Cryptocurrency, Crypto, DeFi, Decentralized Finance, Open Finance, NFTs, Non Fungible Tokens, Exchange Protocol, Relayer, Exchange Functionality, Crypto Developers, 0x Instant, Crypto Market Maker, Networked Liquidity, 0x Home, Crypto Infrastructure, Build on 0x, Crypto',
+    },
+    WHY: {
+        title: '0x: Features and Benefits',
+        description: '0x Protocol is free, open-source infrastructure that developers and businesses utilize to build products that enable the purchasing and trading of crypto tokens.',
+        keywords: '',
+    },
+    INSTANT: {
+        title: '0x Instant: Quick and secure crypto purchasing',
+        description: 'A free and flexible way to offer simple crypto purchasing in any app or website.',
+        keywords: '',
+    },
+    LAUNCH_KIT: {
+        title: '0x Launch Kit: Launch a relayer in under a minute',
+        description: 'Launch Kit includes a set of tools and documentation to build a relayer on 0x.',
+        keywords: '',
+    },
+    ABOUT: {
+        title: '0x: Our Mission and Values',
+        description: 'As more assets become tokenized, 0x provides the critical exchange layer in a new financial stack that is more efficient, transparent, and equitable than any system in the past.',
+        keywords: '',
+    },
+    TEAM: {
+        title: '0x: Our Team',
+        description: 'The 0x Core Team is passionate about accelerating the adoption decentralized technology and believe in its potential to be an equalizing force in the world. Join us and do the most impactful work of your life.',
+        keywords: '',
+    },
+    PRESS: {
+        title: '0x: Press Highlights',
+        description: '0x has been featured in major publications such as Forbes, Fortune, VentureBeat, and TechCrunch.',
+        keywords: '',
+    },
+    JOBS: {
+        title: 'Jobs at 0x',
+        description: 'Join us in building infrastructure upon which the exchange of all tokenized assets will take place.',
+        keywords: '',
+    },
+    ECOSYSTEM_PROGRAM: {
+        title: '0x Ecosystem Acceleration Program: Jumpstart your Business on 0x',
+        description: 'The Ecosystem Acceleration Program gives teams access to a variety of services including funding, dedicated technical support, and recruiting assistance.',
+        keywords: '',
+    },
+    MARKET_MAKER_PROGRAM: {
+        title: '0x Market Maker Program: Bring liquidity to the markets of the future',
+        description: 'The Market Making Program provides a set of resources that help onboard MMs to bring liquidity to the 0x network. The Program includes tutorials, monetary incentives, and 1:1 support from the 0x team.',
+        keywords: '',
+    },
+    INFRASTRUCTURE_CREDITS: {
+        title: 'Free Credits - 0x',
+        description: '',
+        keywords: '',
+    },
+};
