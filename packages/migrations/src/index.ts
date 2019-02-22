c8f5f6045ec01f1252cd33f345f207bf93eb32d6
Fabio Berger <me@fabioberger.com>
2019-02-21 16:27:38 -0800

    Fix CI failures

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -1,4 +1,4 @@
-export { TxData, TxDataPayable } from 'ethereum-types';
+export { TxData, TxDataPayable, SupportedProvider } from 'ethereum-types';
 export { ContractAddresses } from '@0x/contract-addresses';
 export { runMigrationsAsync, runMigrationsOnceAsync } from './migration';
 export import Web3ProviderEngine = require('web3-provider-engine');
