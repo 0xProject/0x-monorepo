e77a608f45ed29c848dc33a93ea669ace768335e
Greg Hysen <greg.hysen@gmail.com>
2019-03-11 19:06:06 -0700

    Got build working on erc1155

--- contracts/erc1155/src/artifacts.ts
+++ contracts/erc1155/src/artifacts.ts
@@ -5,21 +5,21 @@
  */
 import { ContractArtifact } from 'ethereum-types';
 
-import * as Address from '../generated-artifacts/Address.json';
 import * as DummyERC1155Receiver from '../generated-artifacts/DummyERC1155Receiver.json';
 import * as ERC1155 from '../generated-artifacts/ERC1155.json';
 import * as ERC1155Mintable from '../generated-artifacts/ERC1155Mintable.json';
 import * as IERC1155 from '../generated-artifacts/IERC1155.json';
+import * as IERC1155Mintable from '../generated-artifacts/IERC1155Mintable.json';
 import * as IERC1155Receiver from '../generated-artifacts/IERC1155Receiver.json';
 import * as MixinNonFungibleToken from '../generated-artifacts/MixinNonFungibleToken.json';
-import * as SafeMath from '../generated-artifacts/SafeMath.json';
+import * as MNonFungibleToken from '../generated-artifacts/MNonFungibleToken.json';
 export const artifacts = {
+    DummyERC1155Receiver: DummyERC1155Receiver as ContractArtifact,
     ERC1155: ERC1155 as ContractArtifact,
+    MNonFungibleToken: MNonFungibleToken as ContractArtifact,
     ERC1155Mintable: ERC1155Mintable as ContractArtifact,
     MixinNonFungibleToken: MixinNonFungibleToken as ContractArtifact,
-    IERC1155: IERC1155 as ContractArtifact,
+    IERC1155Mintable: IERC1155Mintable as ContractArtifact,
     IERC1155Receiver: IERC1155Receiver as ContractArtifact,
-    Address: Address as ContractArtifact,
-    SafeMath: SafeMath as ContractArtifact,
-    DummyERC1155Receiver: DummyERC1155Receiver as ContractArtifact,
+    IERC1155: IERC1155 as ContractArtifact,
 };
