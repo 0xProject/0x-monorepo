c23cdb1b381f89019b78b87bbbed9923f89913fd
michaelhly <michaelhly@gmail.com>
2019-03-21 03:58:12 -0500

    Add tests for erc_20_wrapper

---
+++ python-packages/contract_wrappers/test/test_erc_20_wrapper.py
@@ -0,0 +1,34 @@
+from decimal import Decimal
+
+
+MAX_ALLOWANCE = "{:.0f}".format(Decimal(2) ** 256 - 1)
+
+
+def test_erc20_wrapper__balance_of(
+    accounts, erc20_wrapper, weth_address, zrx_address
+):
+    acc1_weth_balance = erc20_wrapper.balance_of(weth_address, accounts[0])
+    acc1_zrx_balance = erc20_wrapper.balance_of(zrx_address, accounts[0])
+    acc2_zrx_balance = erc20_wrapper.balance_of(zrx_address, accounts[1])
+
+    assert acc1_weth_balance > 0
+    assert acc1_zrx_balance > 0
+    assert acc2_zrx_balance > 0
+
+
+def test_erc20_wrapper__approve(
+    accounts, erc20_proxy_address, erc20_wrapper, zrx_address
+):
+    erc20_wrapper.approve(
+        zrx_address,
+        erc20_proxy_address,
+        MAX_ALLOWANCE,
+        tx_opts={"from_": accounts[0]},
+    )
+
+    erc20_wrapper.approve(
+        zrx_address,
+        erc20_proxy_address,
+        MAX_ALLOWANCE,
+        tx_opts={"from_": accounts[1]},
+    )
