6e32f6cb95162d5a6e6c372cdac77c9a4eebffae
michaelhly <michaelhly@gmail.com>
2019-03-21 03:57:52 -0500

    Update for conftest.py

--- python-packages/contract_wrappers/test/conftest.py
+++ python-packages/contract_wrappers/test/conftest.py
@@ -1,46 +1,62 @@
 """Fixtures for pytest"""
 
 import pytest
-from eth_utils import to_checksum_address, remove_0x_prefix
-from hexbytes import HexBytes
+from eth_utils import remove_0x_prefix
 from web3 import Web3
-from web3.middleware import construct_sign_and_send_raw_middleware
 from zero_ex.order_utils import asset_data_utils as adu
 from zero_ex.contract_addresses import NETWORK_TO_ADDRESSES, NetworkId
+from zero_ex.contract_wrappers.erc_20_wrapper import ERC20Wrapper
 
 
 @pytest.fixture
-def local_account_2():
-    return to_checksum_address("0x6ecbe1db9ef729cbe972c83fb886247691fb6beb")
+def ganache_provider():
+    return Web3.HTTPProvider(endpoint_uri="http://127.0.0.1:8545")
+
 
 @pytest.fixture
-def local_private_key_2():
-    return "5d862464fe9303452126c8bc94274b8c5f9874cbd219789b3eb2128075a76f72"
-    
-@pytest.fixture
-def local_account_1():
-    return to_checksum_address("0x5409ed021d9299bf6814279a6a1411a7e866a631")
+def accounts(ganache_provider):
+    return Web3(ganache_provider).eth.accounts
+
 
 @pytest.fixture
-def local_private_key_1():
-    return "f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d"
+def current_time(ganache_provider):
+    return Web3(ganache_provider).eth.getBlock("latest").timestamp
+
 
 @pytest.fixture
-def ganache_rpc_url():
-    return "http://127.0.0.1:8545"
+def get_tx_receipt(ganache_provider):
+    return Web3(ganache_provider).eth.getTransactionReceipt
+
 
 @pytest.fixture
-def ganache_provider(ganache_rpc_url):
-    return Web3.HTTPProvider(endpoint_uri=ganache_rpc_url)
+def erc20_proxy_address():
+    return NETWORK_TO_ADDRESSES[NetworkId.GANACHE].erc20_proxy
+
 
 @pytest.fixture
-def zrx_asset_data_ganache():
-    return bytes.fromhex(remove_0x_prefix(
-        adu.encode_erc20_asset_data(NETWORK_TO_ADDRESSES[NetworkId.GANACHE].zrx_token)
-    ))
+def erc20_wrapper(ganache_provider):
+    return ERC20Wrapper(ganache_provider)
+
 
 @pytest.fixture
-def weth_asset_data_ganache():
-    return bytes.fromhex(remove_0x_prefix(
-        adu.encode_erc20_asset_data(NETWORK_TO_ADDRESSES[NetworkId.GANACHE].ether_token)
-    ))
+def zrx_address():
+    return NETWORK_TO_ADDRESSES[NetworkId.GANACHE].zrx_token
+
+
+@pytest.fixture
+def weth_address():
+    return NETWORK_TO_ADDRESSES[NetworkId.GANACHE].ether_token
+
+
+@pytest.fixture
+def zrx_asset_data(zrx_address):
+    return bytes.fromhex(
+        remove_0x_prefix(adu.encode_erc20_asset_data(zrx_address))
+    )
+
+
+@pytest.fixture
+def weth_asset_data(weth_address):
+    return bytes.fromhex(
+        remove_0x_prefix(adu.encode_erc20_asset_data(weth_address))
+    )
