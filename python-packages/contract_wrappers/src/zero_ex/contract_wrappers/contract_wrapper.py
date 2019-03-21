be97d8246cf134b0e8cc243a7e6d67e54355882b
michaelhly <michaelhly@gmail.com>
2019-03-20 23:25:43 -0500

    Refactor contract wrapper out of exchange wrapper

---
+++ python-packages/contract_wrappers/src/zero_ex/contract_wrappers/contract_wrapper.py
@@ -0,0 +1,84 @@
+from eth_utils import to_checksum_address
+from hexbytes import HexBytes
+from web3 import Web3
+from web3.providers.base import BaseProvider
+
+
+class ContractWrapper:
+
+    __name__ = "ContractWrapper"
+
+    def __init__(
+        self,
+        provider: BaseProvider,
+        account_address: str = None,
+        private_key: str = None,
+    ):
+        self._provider = provider
+        self._web3 = Web3(provider)
+
+        if self._web3.eth.defaultAccount:
+            self._can_send_tx = True
+        else:
+            if hasattr(
+                self._web3, "middleware_stack"
+            ) and self._web3.middleware_stack.get("sign_and_send_raw_middleware"):
+                if account_address:
+                    self._web3.eth.defaultAccount = to_checksum_address(
+                        account_address
+                    )
+                    self._can_send_tx = True
+                else:
+                    raise Exception(
+                        "Please provide your wallet address associated\
+                        with your private key to use the\
+                        sign_and_send_raw_middleware"
+                    )
+            elif private_key:
+                self._private_key = private_key
+                self._web3.eth.defaultAccount = to_checksum_address(
+                    self._web3.eth.account.privateKeyToAccount(private_key).address
+                )
+                self._can_send_tx = True
+            else:
+                self._can_send_tx = False
+
+    def get_account_address(self):
+        return self._web3.eth.defaultAccount
+
+    def _invoke_function_call(
+        self, 
+        func, 
+        tx_opts,
+        validate_only,
+    ):
+        if validate_only or not self._can_send_tx:
+            return func.call()
+
+        prefill_tx_params = self._get_tx_params(**tx_opts)
+        tx_params = {
+            k: v for k, v in prefill_tx_params.items() if v is not None
+        }
+
+        if self._private_key:
+            return self._sign_and_send_raw_direct(func, tx_params)
+        else:
+            return func.transact(tx_params)
+
+    def _get_tx_params(
+        self, from_=None, gas_price=None, gas_limit=None, value=0, nonce=None
+    ):
+        return {
+            "from": from_,
+            "value": value,
+            "gas": gas_limit,
+            "gasPrice": gas_price,
+            "nonce": nonce,
+        }
+
+    def _sign_and_send_raw_direct(self, func, tx_params):
+        transaction = func.buildTransaction(tx_params)
+        signed_tx = self._web3.eth.account.signTransaction(
+            transaction, private_key=self._private_key
+        )
+        return self._web3.eth.sendRawTransaction(signed_tx.rawTransaction)
--- python-packages/contract_wrappers/src/zero_ex/contract_wrappers/exchange_wrapper.py
+++ python-packages/contract_wrappers/src/zero_ex/contract_wrappers/exchange_wrapper.py
@@ -1,93 +1,41 @@
-import web3.exceptions
 from eth_utils import to_checksum_address, remove_0x_prefix
-from web3 import Web3
 from web3.providers.base import BaseProvider
-from web3.utils import datatypes
+from zero_ex.contract_addresses import NETWORK_TO_ADDRESSES, NetworkId
+from zero_ex.contract_artifacts import abi_by_name
+from zero_ex.contract_wrappers.contract_wrapper import ContractWrapper
+from zero_ex.json_schemas import assert_valid
 from zero_ex.order_utils import (
     generate_order_hash_hex,
     is_valid_signature,
-    jsdict_order_to_struct,
+    order_to_jsdict,
     Order,
 )
-from zero_ex.contract_addresses import NETWORK_TO_ADDRESSES, NetworkId
-from zero_ex.contract_artifacts import abi_by_name
-from zero_ex.json_schemas import assert_valid
 
 
-class ExchangeWrapper:
+class ExchangeWrapper(ContractWrapper):
+    __name__ = "ExchangeWrapper"
+
     def __init__(
         self,
         provider: BaseProvider,
         account_address: str = None,
         private_key: str = None,
     ):
-        self._web3 = Web3(provider)
+        super(ExchangeWrapper, self).__init__(
+            provider=provider,
+            account_address=account_address,
+            private_key=private_key,
+        )
         self._exchange_address = NETWORK_TO_ADDRESSES[
             NetworkId(int(self._web3.net.version))
         ].exchange
-        self._exchange_instance = self._web3.eth.contract(
+        self._exchange = self._web3.eth.contract(
             address=to_checksum_address(self._exchange_address),
             abi=abi_by_name("exchange"),
         )
-
-        if self._web3.eth.defaultAccount:
-            self._can_send_tx = True
-        else:
-            if hasattr(
-                self._web3, "middlewares"
-            ) and self._web3.middlewares.get("sign_and_send_raw_middleware"):
-                if account_address:
-                    self._web3.eth.defaultAccount = to_checksum_address(
-                        account_address
-                    )
-                    self._can_send_tx = True
-                else:
-                    raise Exception(
-                        "Please provide your wallet address associated\
-                        with your private key to use the\
-                        sign_and_send_raw_middleware"
-                    )
-            elif private_key:
-                self._private_key = private_key
-                self._web3.eth.defaultAccount = to_checksum_address(
-                    self._web3.eth.account.privateKeyToAccount(private_key)
-                )
-                self._can_send_tx = True
-            else:
-                self._can_send_tx = False
-
-    def get_account(self):
-        return self._web3.eth.defaultAccount
-
-    def _invoke_function_call(self, func, tx_opts, validate_only=False):
-        if validate_only or not self._can_send_tx:
-            return func.call()
-
-        prefill_tx_params = self._get_tx_params(**tx_opts)
-        tx_params = {k: v for k, v in prefill_tx_params.items() if v is not None}
-
-        if self._private_key:
-            return self._sign_and_send_raw_direct(func, tx_params)
-        else:
-            return func.transact(tx_params)
-
-    def _get_tx_params(
-        self, from_=None, gas_price=None, gas_limit=None, value=0, nonce=None
-    ):
-        return {
-            "from": from_,
-            "value": value,
-            "gas": gas_limit,
-            "gasPrice": gas_price,
-            "nonce": nonce,
-        }
-
-    def _sign_and_send_raw_direct(self, func, tx_params):
-        transaction = func.buildTransaction(tx_params)
-        signed_tx = self._web3.eth.account.signTransaction(
-            transaction, private_key=self._private_key
-        )
-        return self._web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    
+    def exchange_address(self):
+        return self._exchange_address
 
     def fill_or_kill_order(
         self,
@@ -97,21 +45,26 @@ class ExchangeWrapper:
         tx_opts={},
         validate_only=False,
     ):
-        assert_valid(order, "/orderSchema")
+        assert_valid(
+            order_to_jsdict(order, self._exchange_address), 
+            "/orderSchema"
+        )
         is_valid_signature(
-            self._web3,
-            generate_order_hash_hex(order),
+            self._provider,
+            generate_order_hash_hex(order, self._exchange_address),
             signature,
-            order.makerAddress,
+            order["makerAddress"],
         )
         taker_fill_amount = int(taker_fill_amount)
         signature = bytes.fromhex(remove_0x_prefix(signature))
-        order = jsdict_order_to_struct(order)
-        func = self._exchange_instance.functions.fillOrKillOrder(
+        func = self._exchange.functions.fillOrKillOrder(
             order, taker_fill_amount, signature
         )
+
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, 
+            tx_opts=tx_opts, 
+            validate_only=validate_only
         )
 
     # def batch_fill_orders(
@@ -121,7 +74,7 @@ class ExchangeWrapper:
     #     order_transaction_opts
     # ):
     # signatures = [x.signature for x in signed_orders]
-    # func = self._exchange_instance.functions.batchFillOrders(
+    # func = self._exchange.functions.batchFillOrders(
     #     signed_orders, taker_asset_fill_amounts, signatures
     # )
     # return _send_transaction(func, gas_price, gas_limit)
