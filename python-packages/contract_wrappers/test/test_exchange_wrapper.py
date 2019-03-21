2327f683f4b8cd2bd8fadd45dbea2e3216158801
michaelhly <michaelhly@gmail.com>
2019-03-21 03:57:26 -0500

    Pass test_exchange_wrapper__fill_order

--- python-packages/contract_wrappers/test/test_exchange_wrapper.py
+++ python-packages/contract_wrappers/test/test_exchange_wrapper.py
@@ -1,32 +1,29 @@
-import pytest
 import random
+import pytest
+from eth_utils import remove_0x_prefix
 from zero_ex.contract_wrappers.exchange_wrapper import ExchangeWrapper
 from zero_ex.json_schemas import assert_valid
-from zero_ex.order_utils import (
-    generate_order_hash_hex,
-    Order,
-    order_to_jsdict,
-    sign_hash
-)
+from zero_ex.order_utils import generate_order_hash_hex, Order, sign_hash
+
 
 @pytest.fixture
-def exchange_wrapper_with_ganache(ganache_provider, local_private_key_1):
-    return ExchangeWrapper(
-        provider=ganache_provider,
-        private_key=local_private_key_1
-    )
+def exchange_wrapper(ganache_provider):
+    return ExchangeWrapper(provider=ganache_provider)
 
-def create_test_order(maker_address, maker_asset_data, taker_asset_data):
+
+def create_test_order(
+    current_time, maker_address, maker_asset_data, taker_asset_data
+):
     order: Order = {
-        "makerAddress": maker_address,
+        "makerAddress": maker_address.lower(),
         "takerAddress": "0x0000000000000000000000000000000000000000",
         "feeRecipientAddress": "0x0000000000000000000000000000000000000000",
         "senderAddress": "0x0000000000000000000000000000000000000000",
-        "makerAssetAmount": 1000000000000000000,
-        "takerAssetAmount": 1000000000000000000,
+        "makerAssetAmount": 2,
+        "takerAssetAmount": 2,
         "makerFee": 0,
         "takerFee": 0,
-        "expirationTimeSeconds": 1000000000,
+        "expirationTimeSeconds": current_time + 1000000000,
         "salt": random.randint(1, 1000000000),
         "makerAssetData": maker_asset_data,
         "takerAssetData": taker_asset_data,
@@ -34,21 +31,47 @@ def create_test_order(maker_address, maker_asset_data, taker_asset_data):
     return order
 
 
-def test_exchange_wrapper__fill_or_kill_order(
-    exchange_wrapper_with_ganache, 
-    ganache_provider, 
-    local_account_2,
-    weth_asset_data_ganache,
-    zrx_asset_data_ganache,
+def test_exchange_wrapper__fill_order(
+    accounts,
+    current_time,
+    exchange_wrapper,
+    ganache_provider,
+    get_tx_receipt,
+    weth_asset_data,
+    zrx_asset_data,
 ):
-    exchange_address = exchange_wrapper_with_ganache.exchange_address()
-    order: Order = create_test_order(
-        local_account_2.lower(), weth_asset_data_ganache, zrx_asset_data_ganache
+    taker = accounts[0]
+    maker = accounts[1]
+
+    exchange_address = exchange_wrapper.exchange_address
+    order = create_test_order(
+        current_time, maker, zrx_asset_data, weth_asset_data
     )
-    order_hash = generate_order_hash_hex(order=order, exchange_address=exchange_address)
-    order_signature = sign_hash(ganache_provider, local_account_2, order_hash)
-    transaction = exchange_wrapper_with_ganache.fill_or_kill_order(
-        order, 0, order_signature,
+    order_hash = generate_order_hash_hex(
+        order=order, exchange_address=exchange_address
     )
-    print(transaction)
-    assert transaction == 1
+    order_signature = sign_hash(ganache_provider, maker, order_hash)
+
+    transaction_hash = exchange_wrapper.fill_order(
+        order=order,
+        taker_fill_amount=order["takerAssetAmount"],
+        signature=order_signature,
+        tx_opts={"from_": taker},
+    )
+    assert_valid(transaction_hash.hex(), "/hexSchema")
+
+    reciept = get_tx_receipt(transaction_hash)
+    fill_event = exchange_wrapper.get_fill_event(reciept)
+    fill_log = fill_event[0].args
+
+    assert fill_log.makerAddress == maker
+    assert fill_log.takerAddress == taker
+    assert fill_log.feeRecipientAddress == order["feeRecipientAddress"]
+    assert fill_log.senderAddress == taker
+    assert fill_log.orderHash == bytes.fromhex(remove_0x_prefix(order_hash))
+    assert fill_log.makerAssetFilledAmount == order["makerAssetAmount"]
+    assert fill_log.takerAssetFilledAmount == order["takerAssetAmount"]
+    assert fill_log.makerFeePaid == order["makerFee"]
+    assert fill_log.takerFeePaid == order["takerFee"]
+    assert fill_log.makerAssetData == order["makerAssetData"]
+    assert fill_log.takerAssetData == order["takerAssetData"]
