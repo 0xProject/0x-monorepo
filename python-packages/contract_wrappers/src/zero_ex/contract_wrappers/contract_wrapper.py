6cf2245208f21613921277427abedf4e95423f45
cryptomaster <officialcryptomaster@gmail.com>
2019-03-23 19:04:33 -0700

    Refactor, add docstrings and fix all linting

--- python-packages/contract_wrappers/src/zero_ex/contract_wrappers/contract_wrapper.py
+++ python-packages/contract_wrappers/src/zero_ex/contract_wrappers/contract_wrapper.py
@@ -1,20 +1,21 @@
-"""Lorem Ipsum
+"""Base wrapper class for accessing ethereum smart contracts."""
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-ullamco laboris nisi ut aliquip ex ea commodo consequat.
-"""
-
-from typing import Dict
 from eth_utils import to_checksum_address
 from web3 import Web3
 from web3.providers.base import BaseProvider
-from zero_ex.json_schemas import assert_valid
 
 
 class ContractWrapper:
-    """Lorem ipsum dolor sit amet, consectetur..."""
+    """This is the base class for wrapping ethereum smart contracts.
+
+    It provides functionality for instantiating a contract instance,
+    calling view functions, and calling functions which require
+    transactions.
+
+    :param provider: instance of :class:`web3.providers.base.BaseProvider`
+    :param account_address: default None, str of account address
+    :param private_key: default None, str of private_key
+    """
 
     def __init__(
         self,
@@ -22,35 +23,33 @@ class ContractWrapper:
         account_address: str = None,
         private_key: str = None,
     ):
+        """Create an instance of ContractWrapper."""
         self._provider = provider
+        self._account_address = account_address
+        self._private_key = private_key
         self._web3 = Web3(provider)
+        self._web3_eth = self._web3.eth  # pylint: disable=no-member
 
-        if (
-            self._web3.eth.defaultAccount
-            or len(self._web3.eth.accounts) is not 0
-        ):
+        if self._web3_eth.defaultAccount or self._web3_eth.accounts:
             self._can_send_tx = True
         else:
-            if hasattr(
-                self._web3, "middleware_stack"
-            ) and self._web3.middleware_stack.get(
-                "sign_and_send_raw_middleware"
-            ):
+            middleware_stack = getattr(self._web3, "middleware_stack")
+            if middleware_stack.get("sign_and_send_raw_middleware"):
                 if account_address:
-                    self._web3.eth.defaultAccount = to_checksum_address(
+                    self._web3_eth.defaultAccount = to_checksum_address(
                         account_address
                     )
                     self._can_send_tx = True
                 else:
                     raise Exception(
-                        "Please provide your wallet address associated\
-                        with your private key to use the\
-                        sign_and_send_raw_middleware"
+                        "Please provide the wallet address associated"
+                        " with your private key to use the"
+                        " sign_and_send_raw_middleware"
                     )
             elif private_key:
                 self._private_key = private_key
-                self._web3.eth.defaultAccount = to_checksum_address(
-                    self._web3.eth.account.privateKeyToAccount(
+                self._web3_eth.defaultAccount = to_checksum_address(
+                    self._web3_eth.account.privateKeyToAccount(
                         private_key
                     ).address
                 )
@@ -59,7 +58,14 @@ class ContractWrapper:
                 self._can_send_tx = False
 
     def _contract_instance(self, address, abi):
-        return self._web3.eth.contract(
+        """Get a contract instance.
+
+        :param address: string address of contract
+        :param abi: dict contract ABI
+
+        :returns: instance of contract
+        """
+        return self._web3_eth.contract(
             address=to_checksum_address(address), abi=abi
         )
 
@@ -68,8 +74,8 @@ class ContractWrapper:
             raise TypeError("Invalid address provided: {}".format(address))
         return to_checksum_address(address)
 
-    def _invoke_function_call(self, func, tx_opts, validate_only):
-        if validate_only or not self._can_send_tx:
+    def _invoke_function_call(self, func, tx_opts, view_only):
+        if view_only or not self._can_send_tx:
             return func.call()
 
         prefilled_tx_params = self._get_tx_params(**tx_opts)
@@ -78,11 +84,11 @@ class ContractWrapper:
         tx_params = {
             k: v for k, v in prefilled_tx_params.items() if v is not None
         }
-
-        if hasattr(self, "_private_key"):
-            return self._sign_and_send_raw_direct(func, tx_params)
+        if self._private_key:
+            res = self._sign_and_send_raw_direct(func, tx_params)
         else:
-            return func.transact(tx_params)
+            res = func.transact(tx_params)
+        return res
 
     def _get_tx_params(
         self, from_=None, gas_price=None, gas_limit=None, value=0, nonce=None
@@ -99,27 +105,40 @@ class ContractWrapper:
 
     def _sign_and_send_raw_direct(self, func, tx_params):
         transaction = func.buildTransaction(tx_params)
-        signed_tx = self._web3.eth.account.signTransaction(
+        signed_tx = self._web3_eth.account.signTransaction(
             transaction, private_key=self._private_key
         )
-        return self._web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return self._web3_eth.sendRawTransaction(signed_tx.rawTransaction)
 
     def execute_method(
-        self, address, abi, method, args, tx_opts=None, validate_only=False
+        self, address, abi, method, args, tx_opts=None, view_only=False
     ):
+        """Execute the method on a contract instance.
+
+        :param address: string of contract address
+        :param abi: json of contract ABI
+        :param method: string name of method to call
+        :param args: dict of arguments for the method
+        :param tx_opts: default None, dictionary of transaction options
+        :param view_only: default False, boolean of whether the transaction
+            should only be validated.
+
+        :returns: result of transacting with the given contract's function.
+        """
         contract_instance = self._contract_instance(address=address, abi=abi)
         if hasattr(contract_instance.functions, method):
             func = getattr(contract_instance.functions, method)(*args)
             return self._invoke_function_call(
-                func=func, tx_opts=tx_opts, validate_only=validate_only
+                func=func, tx_opts=tx_opts, view_only=view_only
             )
-        else:
-            raise Exception(
-                "No method {} found on contract {}.".format(address, method)
-            )
+        raise Exception(
+            "No method {} found on contract {}.".format(address, method)
+        )
 
     def get_accounts(self):
-        return self._web3.eth.accounts
+        """Get a list of all accounts associated with the Web3 instance."""
+        return self._web3_eth.accounts
 
     def get_default_account(self):
-        return self._web3.eth.defaultAccount or self.get_accounts()[0]
+        """Get the default account associated with the Web3 instance."""
+        return self._web3_eth.defaultAccount or self.get_accounts()[0]
--- python-packages/contract_wrappers/src/zero_ex/contract_wrappers/exchange_wrapper.py
+++ python-packages/contract_wrappers/src/zero_ex/contract_wrappers/exchange_wrapper.py
@@ -1,6 +1,9 @@
+"""Wrapper for 0x Exchange Contract (Version 2)."""
+
 from itertools import repeat
 from eth_utils import remove_0x_prefix
 from web3.providers.base import BaseProvider
+
 from zero_ex.contract_addresses import NETWORK_TO_ADDRESSES, NetworkId
 from zero_ex.contract_artifacts import abi_by_name
 from zero_ex.contract_wrappers.contract_wrapper import ContractWrapper
@@ -9,53 +12,66 @@ from zero_ex.order_utils import (
     generate_order_hash_hex,
     is_valid_signature,
     order_to_jsdict,
-    Order,
 )
 
 
+class CancelDisallowedError(Exception):
+    """Exception for when Cancel is not allowed."""
+
+
 class Exchange(ContractWrapper):
+    """Wrapper class for 0x V2 Exchange smart contract.
+
+    :param provider: instance of :class:`web3.providers.base.BaseProvider`
+    :param account_address: default None, str of account address
+    :param private_key: default None, str of private_key
+    """
+
     def __init__(
         self,
         provider: BaseProvider,
         account_address: str = None,
         private_key: str = None,
     ):
+        """Get an instance of the 0x V2 Exchange smart contract wrapper."""
         super(Exchange, self).__init__(
             provider=provider,
             account_address=account_address,
             private_key=private_key,
         )
+        self._web3_net = self._web3.net  # pylint: disable=no-member
         self.address = NETWORK_TO_ADDRESSES[
-            NetworkId(int(self._web3.net.version))
+            NetworkId(int(self._web3_net.version))
         ].exchange
         self._exchange = self._contract_instance(
             address=self.address, abi=abi_by_name("exchange")
         )
 
-    def _match_order_maker_with_cancellor(
-        self, maker_address, cancellor_address
-    ):
-        if maker_address != cancellor_address:
-            raise Exception(
-                "Order with makerAddress {} can not be cancelled by {}".format(
-                    maker_address, cancellor_address
-                )
-            )
-
-    def get_fill_event(self, tx_reciept):
-        return self._exchange.events.Fill().processReceipt(tx_reciept)
-
-    def get_cancel_event(self, tx_reciept):
-        return self._exchange.events.Cancel().processReceipt(tx_reciept)
-
     def fill_order(
-        self,
-        order,
-        amount_in_wei,
-        signature,
-        tx_opts=None,
-        validate_only=False,
+        self, order, taker_amount, signature, tx_opts=None, view_only=False
     ):
+        """Fill a signed order with given amount of taker asset.
+
+        This is the most basic way to fill an order. All of the other methods
+        call fillOrder under the hood with additional logic. This function
+        will attempt to fill the amount specified by the caller. However, if
+        the remaining fillable amount is less than the amount specified, the
+        remaining amount will be filled. Partial fills are allowed when
+        filling orders.
+
+        See the specification docs for `fillOrder
+        <https://github.com/0xProject/0x-protocol-specification/blob/master
+        /v2/v2-specification.md#fillorder>`_.
+
+        :param order: instance of :class:`zero_ex.order_utils.Order`
+        :param taker_amount: integer taker amount in Wei (1 Wei is 10e-18 ETH)
+        :param signature: str or hexstr or bytes of order hash signature
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         assert_valid(order_to_jsdict(order, self.address), "/orderSchema")
         is_valid_signature(
             self._provider,
@@ -63,29 +79,35 @@ class Exchange(ContractWrapper):
             signature,
             order["makerAddress"],
         )
-        taker_fill_amount = int(amount_in_wei)
+        taker_fill_amount = int(taker_amount)
         normalized_signature = bytes.fromhex(remove_0x_prefix(signature))
         func = self._exchange.functions.fillOrder(
             order, taker_fill_amount, normalized_signature
         )
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
 
     def batch_fill_orders(
-        self,
-        orders,
-        amounts_in_wei,
-        signatures,
-        tx_opts=None,
-        validate_only=False,
+        self, orders, taker_amounts, signatures, tx_opts=None, view_only=False
     ):
+        """Call `fillOrder` sequentially for orders, amounts and signatures.
+
+        :param orders: list of instances of :class:`zero_ex.order_utils.Order`
+        :param taker_amounts: list of integer taker amounts in Wei
+        :param signatures: list of str|hexstr|bytes of order hash signature
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         order_jsdicts = [
             order_to_jsdict(order, self.address) for order in orders
         ]
         map(assert_valid, order_jsdicts, repeat("/orderSchema"))
         normalized_fill_amounts = [
-            int(taker_fill_amount) for taker_fill_amount in amounts_in_wei
+            int(taker_fill_amount) for taker_fill_amount in taker_amounts
         ]
         normalized_signatures = [
             bytes.fromhex(remove_0x_prefix(signature))
@@ -95,17 +117,23 @@ class Exchange(ContractWrapper):
             orders, normalized_fill_amounts, normalized_signatures
         )
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
 
     def fill_or_kill_order(
-        self,
-        order,
-        amount_in_wei,
-        signature,
-        tx_opts=None,
-        validate_only=False,
+        self, order, taker_amount, signature, tx_opts=None, view_only=False
     ):
+        """Attemp to `fillOrder`, revert if fill is not exact amount.
+
+        :param order: instance of :class:`zero_ex.order_utils.Order`
+        :param taker_amount: integer taker amount in Wei (1 Wei is 10e-18 ETH)
+        :param signature: str or hexstr or bytes of order hash signature
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         assert_valid(order_to_jsdict(order, self.address), "/orderSchema")
         is_valid_signature(
             self._provider,
@@ -113,29 +141,35 @@ class Exchange(ContractWrapper):
             signature,
             order["makerAddress"],
         )
-        taker_fill_amount = int(amount_in_wei)
+        taker_fill_amount = int(taker_amount)
         normalized_signature = bytes.fromhex(remove_0x_prefix(signature))
         func = self._exchange.functions.fillOrKillOrder(
             order, taker_fill_amount, normalized_signature
         )
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
 
     def batch_fill_or_kill_orders(
-        self,
-        orders,
-        amounts_in_wei,
-        signatures,
-        tx_opts=None,
-        validate_only=False,
+        self, orders, taker_amounts, signatures, tx_opts=None, view_only=False
     ):
+        """Call `fillOrKillOrder` sequentially for orders.
+
+        :param orders: list of instances of :class:`zero_ex.order_utils.Order`
+        :param taker_amounts: list of integer taker amounts in Wei
+        :param signatures: list of str|hexstr|bytes of order hash signature
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         order_jsdicts = [
             order_to_jsdict(order, self.address) for order in orders
         ]
         map(assert_valid, order_jsdicts, repeat("/orderSchema"))
         normalized_fill_amounts = [
-            int(taker_fill_amount) for taker_fill_amount in amounts_in_wei
+            int(taker_fill_amount) for taker_fill_amount in taker_amounts
         ]
         normalized_signatures = [
             bytes.fromhex(remove_0x_prefix(signature))
@@ -145,48 +179,51 @@ class Exchange(ContractWrapper):
             orders, normalized_fill_amounts, normalized_signatures
         )
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
 
-    def cancel_order(self, order, tx_opts=None, validate_only=False):
+    def cancel_order(self, order, tx_opts=None, view_only=False):
+        """Cancel an order.
+
+        See the specification docs for `cancelOrder
+        <https://github.com/0xProject/0x-protocol-specification/blob/master
+        /v2/v2-specification.md#cancelorder>`_.
+
+        :param order: instance of :class:`zero_ex.order_utils.Order`
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         assert_valid(order_to_jsdict(order, self.address), "/orderSchema")
         maker_address = self._validate_and_checksum_address(
             order["makerAddress"]
         )
         if tx_opts.get("from_"):
-            self._match_order_maker_with_cancellor(
+            self._raise_if_maker_not_canceller(
                 maker_address,
                 self._validate_and_checksum_address(tx_opts["from_"]),
             )
-        elif self._web3.eth.defaultAccount:
-            self._match_order_maker_with_cancellor(
-                maker_address, self._web3.eth.defaultAccount
+        elif self._web3_eth.defaultAccount:
+            self._raise_if_maker_not_canceller(
+                maker_address, self._web3_eth.defaultAccount
             )
         func = self._exchange.functions.cancelOrder(order)
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
 
-    def cancel_order(self, order, tx_opts=None, validate_only=False):
-        assert_valid(order_to_jsdict(order, self.address), "/orderSchema")
-        maker_address = self._validate_and_checksum_address(
-            order["makerAddress"]
-        )
-        if tx_opts.get("from_"):
-            self._match_order_maker_with_cancellor(
-                maker_address,
-                self._validate_and_checksum_address(tx_opts["from_"]),
-            )
-        elif self._web3.eth.defaultAccount:
-            self._match_order_maker_with_cancellor(
-                maker_address, self._web3.eth.defaultAccount
-            )
-        func = self._exchange.functions.cancelOrder(order)
-        return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
-        )
+    def batch_cancel_orders(self, orders, tx_opts=None, view_only=False):
+        """Call `cancelOrder` sequentially for provided orders.
 
-    def batch_cancel_orders(self, orders, tx_opts=None, validate_only=False):
+        :param orders: list of instance of :class:`zero_ex.order_utils.Order`
+        :param tx_opts: default None, dict of transaction options
+        :param view_only: default False, boolean of whether to transact or
+            view only
+
+        :returns: transaction hash
+        """
         order_jsdicts = [
             order_to_jsdict(order, self.address) for order in orders
         ]
@@ -197,17 +234,43 @@ class Exchange(ContractWrapper):
         ]
         if tx_opts.get("from_"):
             map(
-                self._match_order_maker_with_cancellor,
+                self._raise_if_maker_not_canceller,
                 maker_addresses,
                 repeat(tx_opts["from_"]),
             )
-        elif self._web3.eth.defaultAccount:
+        elif self._web3_eth.defaultAccount:
             map(
-                self._match_order_maker_with_cancellor,
+                self._raise_if_maker_not_canceller,
                 maker_addresses,
-                repeat(self._web3.eth.defaultAccount),
+                repeat(self._web3_eth.defaultAccount),
             )
         func = self._exchange.functions.batchCancelOrders(orders)
         return self._invoke_function_call(
-            func=func, tx_opts=tx_opts, validate_only=validate_only
+            func=func, tx_opts=tx_opts, view_only=view_only
         )
+
+    def get_fill_event(self, tx_reciept):
+        """Get fill event for a fill transaction.
+
+        :param tx_receipt: str hash of fill transaction
+
+        :returns: `FillResults` instance.
+        """
+        return self._exchange.events.Fill().processReceipt(tx_reciept)
+
+    def get_cancel_event(self, tx_reciept):
+        """Get cancel event for cancel transaction.
+
+        :param tx_receipt: str hash of cancel transaction
+        """
+        return self._exchange.events.Cancel().processReceipt(tx_reciept)
+
+    @staticmethod
+    def _raise_if_maker_not_canceller(maker_address, canceller_address):
+        """Raise exception is maker is not same as canceller."""
+        if maker_address != canceller_address:
+            raise CancelDisallowedError(
+                "Order with makerAddress {} can not be cancelled by {}".format(
+                    maker_address, canceller_address
+                )
+            )
