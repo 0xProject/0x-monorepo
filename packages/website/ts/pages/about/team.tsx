a54aa77d283f0718bc405303226a609f844b46f8
rsingireddy <rahulsingireddy@Rahuls-MacBook-Pro.local>
2019-03-26 16:51:07 -0700

    adds lawrence forman to the website. changes mel role and alex browne role

--- packages/website/ts/pages/about/team.tsx
+++ packages/website/ts/pages/about/team.tsx
@@ -91,12 +91,12 @@ const team: TeamMember[] = [
     {
         imageUrl: '/images/team/melo.jpg',
         name: 'Mel Oberto',
-        title: 'people operations associate',
+        title: 'people operations specialist',
     },
     {
         imageUrl: '/images/team/alexb.jpg',
         name: 'Alex Browne',
-        title: 'engineer in residence',
+        title: 'senior engineer',
     },
     {
         imageUrl: '/images/team/peterz.jpg',
@@ -161,6 +161,11 @@ const team: TeamMember[] = [
     {
         imageUrl: '/images/team/danielp.png',
         name: 'Daniel Pyrathon',
+        title: 'engineer',
+    },
+    {
+        imageUrl: '/images/team/lawrencef.jpg',
+        name: 'Lawrence Forman',
         title: 'engineer',
     },
 
