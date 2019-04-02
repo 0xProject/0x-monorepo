21d47168d7da28664b8a8a26f3bab547a9850cbf
Lawrence Forman <lawrence@0xproject.com>
2019-04-02 17:47:35 -0400

    Add `OrderStatus` to `@0x/types`

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -768,3 +768,13 @@ export interface EIP712DomainWithDefaultSchema {
     chainId: number;
     verifyingContractAddress: string;
 }
+
+export enum OrderStatus {
+    Invalid,
+    InvalidMakerAssetAmount,
+    InvalidTakerAssetAmount,
+    Fillable,
+    Expired,
+    FullyFilled,
+    Cancelled,
+}
