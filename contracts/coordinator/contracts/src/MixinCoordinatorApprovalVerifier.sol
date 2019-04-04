32f9b993684e10c3065cd4fcb4a7f34ab0872b02
Amir Bandeali <abandeali1@gmail.com>
2019-04-03 22:27:39 -0700

    Fix inconsistent pragma for ABIEncoderV2

--- contracts/coordinator/contracts/src/MixinCoordinatorApprovalVerifier.sol
+++ contracts/coordinator/contracts/src/MixinCoordinatorApprovalVerifier.sol
@@ -17,7 +17,7 @@
 */
 
 pragma solidity ^0.5.5;
-pragma experimental "ABIEncoderV2";
+pragma experimental ABIEncoderV2;
 
 import "@0x/contracts-exchange-libs/contracts/src/LibExchangeSelectors.sol";
 import "@0x/contracts-exchange-libs/contracts/src/LibOrder.sol";
