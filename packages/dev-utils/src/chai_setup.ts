e00ac37cb2fa9eebc268dd388231e5524d046aa1
Lawrence Forman <lawrence@0xproject.com>
2019-04-04 09:53:46 -0400

    In `@0x/dev-utils` fix the `RevertError` chai helper's `equal` override not passing all arguments to previous handler. In `@0x/dev-utils` add more `RevertError` chai helper tests for backwards compatibility with `rejectedWith`. In `@0x/dev-utils` instead of overriding `rejectedWith`, add a new method `revertWith`. In `@0x/dev-utils` clean up the code for the `RevertError` chai helper.

--- packages/dev-utils/src/chai_setup.ts
+++ packages/dev-utils/src/chai_setup.ts
@@ -10,29 +10,29 @@ export const chaiSetup = {
         // Order matters.
         chai.use(ChaiBigNumber());
         chai.use(chaiAsPromised);
-        chai.use(richRevertExtension);
+        chai.use(revertErrorHelper);
         chai.use(dirtyChai);
     },
 };
 
 // tslint:disable: only-arrow-functions prefer-conditional-expression
 
-type ChaiPromiseHandler = (x: any) => Promise<void>;
-type ChaiAssertHandler = (x: any) => void;
+type ChaiPromiseHandler = (x: any, ...rest: any[]) => Promise<void>;
+type ChaiAssertHandler = (x: any, ...rest: any[]) => void;
 
 interface Chai {
-    Assertion: any;
+    Assertion: ChaiAssertionType;
 }
 
-interface ChaiUtils {
-    flag: (assertion: any, name: string, value?: any) => any;
-    overwriteMethod: (ctx: any, name: string, _super: (expected: any) => any) => void;
-}
-
-interface ChaiExtension {
+interface ChaiAssertionInstance {
     assert: ChaiAssert;
     _obj: any;
     __flags: any;
+}
+
+interface ChaiAssertionType {
+    overwriteMethod: (name: string, _super: (expected: any) => any) => void;
+    new (): ChaiAssertionInstance;
 }
 
 type ChaiAssert = (
@@ -43,71 +43,77 @@ type ChaiAssert = (
     actual?: any,
 ) => void;
 
-function richRevertExtension(_chai: Chai, utils: ChaiUtils): void {
-    utils.overwriteMethod(_chai.Assertion.prototype, 'rejectedWith', function(
-        _super: ChaiPromiseHandler,
-    ): ChaiPromiseHandler {
-        return async function(this: ChaiExtension, expected: any): Promise<void> {
+function revertErrorHelper(_chai: Chai): void {
+    const proto = _chai.Assertion;
+    proto.overwriteMethod('revertWith', function(_super: ChaiPromiseHandler): ChaiPromiseHandler {
+        return async function(this: ChaiAssertionInstance, expected: any, ...rest: any[]): Promise<void> {
             const maybePromise = this._obj;
             // Make sure we're working with a promise.
-            new _chai.Assertion().assert(maybePromise instanceof Promise, `Expected ${maybePromise} to be a promise`);
+            chaiAssert(_chai, maybePromise instanceof Promise, `Expected ${maybePromise} to be a promise`);
             // Wait for the promise to reject.
-            let err: any;
+            let resolveValue;
+            let rejectValue: any;
             let didReject = false;
             try {
-                await maybePromise;
-            } catch (_err) {
-                err = _err;
+                resolveValue = await maybePromise;
+            } catch (err) {
+                rejectValue = err;
                 didReject = true;
             }
             if (!didReject) {
-                new _chai.Assertion().assert(false, `Expected promise to reject`);
+                chaiFail(_chai, `Expected promise to reject but instead resolved with: ${resolveValue}`);
             }
-            return compareRichRevertReasons.call(this, _chai, _super, err, expected);
+            if (!compareRevertErrors.call(this, _chai, rejectValue, expected)) {
+                // Wasn't handled by the comparison function so call the previous handler.
+                _super.call(this, expected, ...rest);
+            }
         };
     });
-    utils.overwriteMethod(_chai.Assertion.prototype, 'become', function(
-        _super: ChaiPromiseHandler,
-    ): ChaiPromiseHandler {
-        return async function(this: ChaiExtension, expected: any): Promise<void> {
+    proto.overwriteMethod('become', function(_super: ChaiPromiseHandler): ChaiPromiseHandler {
+        return async function(this: ChaiAssertionInstance, expected: any, ...rest: any[]): Promise<void> {
             const maybePromise = this._obj;
             // Make sure we're working with a promise.
-            new _chai.Assertion().assert(maybePromise instanceof Promise, `Expected ${maybePromise} to be a promise`);
+            chaiAssert(_chai, maybePromise instanceof Promise, `Expected ${maybePromise} to be a promise`);
             // Wait for the promise to resolve.
-            return compareRichRevertReasons.call(this, _chai, _super, await maybePromise, expected);
+            if (!compareRevertErrors.call(this, _chai, await maybePromise, expected)) {
+                // Wasn't handled by the comparison function so call the previous handler.
+                _super.call(this, expected, ...rest);
+            }
         };
     });
-    utils.overwriteMethod(_chai.Assertion.prototype, 'equal', function(_super: ChaiAssertHandler): ChaiAssertHandler {
-        return function(this: ChaiExtension, expected: any): void {
-            compareRichRevertReasons.call(this, _chai, _super, this._obj, expected);
-        };
-    });
-    utils.overwriteMethod(_chai.Assertion.prototype, 'eql', function(_super: ChaiAssertHandler): ChaiAssertHandler {
-        return function(this: ChaiExtension, expected: any): void {
-            compareRichRevertReasons.call(this, _chai, _super, this._obj, expected);
+    proto.overwriteMethod('equal', function(_super: ChaiAssertHandler): ChaiAssertHandler {
+        return function(this: ChaiAssertionInstance, expected: any, ...rest: any[]): void {
+            if (!compareRevertErrors.call(this, _chai, this._obj, expected)) {
+                // Wasn't handled by the comparison function so call the previous handler.
+                _super.call(this, expected, ...rest);
+            }
         };
     });
 }
 
-function compareRichRevertReasons(
-    this: ChaiExtension,
-    _chai: Chai,
-    _super: ChaiAssertHandler,
-    _actual: any,
-    _expected: any,
-): void {
+/**
+ * Compare two values as compatible RevertError types.
+ * @return `true` if the comparison was fully evaluated. `false` indicates that
+ *         it should be deferred to another handler.
+ */
+function compareRevertErrors(this: ChaiAssertionInstance, _chai: Chai, _actual: any, _expected: any): boolean {
     let actual = _actual;
     let expected = _expected;
     // If either subject is a RevertError, try to coerce the other into the same.
+    // Some of this is for convenience, some is for backwards-compatibility.
+    // TODO: Remove coercion of `actual` when all contracts and tests are upgraded
+    // to explicitly use RevertErrors.
     if (expected instanceof RevertError || actual instanceof RevertError) {
         // `actual` can be a RevertError, string, or an Error type.
         if (!(actual instanceof RevertError)) {
             if (typeof actual === 'string') {
                 actual = new StringRevertError(actual);
             } else if (actual instanceof Error) {
+                // `BaseContract` will throw a plain `Error` type for `StringRevertErrors`
+                // for backwards compatibility. So coerce it into a StringRevertError.
                 actual = new StringRevertError(actual.message);
             } else {
-                new _chai.Assertion().assert(false, `Result is not of type RevertError: ${actual}`);
+                chaiAssert(_chai, false, `Result is not of type RevertError: ${actual}`);
             }
         }
         // `expected` can be a RevertError or string.
@@ -124,7 +130,18 @@ function compareRichRevertReasons(
             expected,
             actual,
         );
-        return;
+        // Return true to signal we handled it.
+        return true;
     }
-    _super.call(this, _expected);
+    return false;
+}
+
+function chaiAssert(_chai: Chai, condition: boolean, failMessage?: string, expected?: any, actual?: any): void {
+    const assert = new _chai.Assertion();
+    assert.assert(condition, failMessage, undefined, expected, actual);
+}
+
+function chaiFail(_chai: Chai, failMessage?: string, expected?: any, actual?: any): void {
+    const assert = new _chai.Assertion();
+    assert.assert(false, failMessage, undefined, expected, actual);
 }
