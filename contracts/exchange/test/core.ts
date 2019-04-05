0468f6295e52be9d5cf9df311e51e58dc25c03f2
Lawrence Forman <lawrence@0xproject.com>
2019-04-05 00:34:24 -0400

    In `@0x/contracts-exchange`: Update tests for rich reverts

--- contracts/exchange/test/core.ts
+++ contracts/exchange/test/core.ts
@@ -54,7 +54,6 @@ const blockchainLifecycle = new BlockchainLifecycle(web3Wrapper);
 
 // tslint:disable:no-unnecessary-type-assertion
 describe('Exchange core', () => {
-    const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
     let chainId: number;
     let makerAddress: string;
     let owner: string;
