a690939b96cf4f78bdea082b744ae09eaa09ab43
Amir Bandeali <abandeali1@gmail.com>
2019-04-22 15:27:04 -0700

    Move constants and exchangeDataEncoder into test-utils package

--- contracts/coordinator/test/utils/index.ts
+++ contracts/coordinator/test/utils/index.ts
@@ -1,5 +1,3 @@
 export { hashUtils } from './hash_utils';
 export { ApprovalFactory } from './approval_factory';
-export { constants } from './constants';
-export { exchangeDataEncoder } from './exchange_data_encoder';
 export * from './types';
--- contracts/test-utils/src/index.ts
+++ contracts/test-utils/src/index.ts
@@ -28,6 +28,7 @@ export { OrderFactory } from './order_factory';
 export { bytes32Values, testCombinatoriallyWithReferenceFuncAsync, uint256Values } from './combinatorial_utils';
 export { TransactionFactory } from './transaction_factory';
 export { testWithReferenceFuncAsync } from './test_with_reference';
+export { exchangeDataEncoder } from './exchange_data_encoder';
 export {
     MarketBuyOrders,
     MarketSellOrders,
