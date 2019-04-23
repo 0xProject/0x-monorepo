c6873859744e6a9d0ed8cbdb230eecce2095e7f7
fragosti <francesco.agosti93@gmail.com>
2019-04-22 18:21:32 -0700

    Fix token amount bug

--- packages/instant/src/components/zero_ex_instant_provider.tsx
+++ packages/instant/src/components/zero_ex_instant_provider.tsx
@@ -57,10 +57,14 @@ export class ZeroExInstantProvider extends React.PureComponent<ZeroExInstantProv
                       networkId,
                   );
 
-        let selectedAssetUnitAmount: BigNumber | undefined = new BigNumber(1);
-        if (selectedAsset !== undefined && selectedAsset.metaData.assetProxyId === AssetProxyId.ERC20) {
-            selectedAssetUnitAmount =
-                props.defaultAssetBuyAmount === undefined ? undefined : new BigNumber(props.defaultAssetBuyAmount);
+        let selectedAssetUnitAmount: BigNumber | undefined;
+        if (selectedAsset !== undefined) {
+            if (selectedAsset.metaData.assetProxyId === AssetProxyId.ERC20) {
+                selectedAssetUnitAmount =
+                    props.defaultAssetBuyAmount === undefined ? undefined : new BigNumber(props.defaultAssetBuyAmount);
+            } else if (selectedAsset.metaData.assetProxyId === AssetProxyId.ERC721) {
+                selectedAssetUnitAmount = new BigNumber(1);
+            }
         }
 
         // construct the final state
