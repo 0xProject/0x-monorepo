e23fc14133778ee7d6eb7e31290688ca1999e639
Lawrence Forman <lawrence@0xproject.com>
2019-05-13 10:55:47 -0400

    Update `order-utils` for ZEIP-28

--- packages/order-utils/src/remaining_fillable_calculator.ts
+++ packages/order-utils/src/remaining_fillable_calculator.ts
@@ -55,21 +55,21 @@ export class RemainingFillableCalculator {
         }
     }
     private _calculatePartiallyFillableAssetAmount(): BigNumber {
-        // Given an order for 200 wei for 2 ZRXwei fee, find 100 wei for 1 ZRXwei. Order ratio is then 100:1
+        // Given an order for 200 wei for 2 Fee wei fee, find 100 wei for 1 Fee wei. Order ratio is then 100:1
         const orderToFeeRatio = this._orderAssetAmount.dividedBy(this._orderFee);
         // The number of times the trader (maker or taker) can fill the order, if each fill only required the transfer of a single
         // baseUnit of fee tokens.
-        // Given 2 ZRXwei, the maximum amount of times trader can fill this order, in terms of fees, is 2
+        // Given 2 Fee wei, the maximum amount of times trader can fill this order, in terms of fees, is 2
         const fillableTimesInFeeBaseUnits = BigNumber.min(this._transferrableFeeAmount, this._remainingOrderFeeAmount);
         // The number of times the trader can fill the order, given the traders asset Balance
         // Assuming a balance of 150 wei, and an orderToFeeRatio of 100:1, trader can fill this order 1 time.
         let fillableTimesInAssetUnits = this._transferrableAssetAmount.dividedBy(orderToFeeRatio);
         if (this._isPercentageFee) {
-            // If ZRX is the trader asset, the Fee and the trader fill amount need to be removed from the same pool;
-            // 200 ZRXwei for 2ZRXwei fee can only be filled once (need 202 ZRXwei)
+            // If Fee is the trader asset, the Fee and the trader fill amount need to be removed from the same pool;
+            // 200 Fee wei for 2Fee wei fee can only be filled once (need 202 Fee wei)
             const totalAssetPooled = this._transferrableAssetAmount;
             // The purchasing power here is less as the tokens are taken from the same Pool
-            // For every one number of fills, we have to take an extra ZRX out of the pool
+            // For every one number of fills, we have to take an extra Fee out of the pool
             fillableTimesInAssetUnits = totalAssetPooled.dividedBy(orderToFeeRatio.plus(new BigNumber(1)));
         }
         // When Ratio is not fully divisible there can be remainders which cannot be represented, so they are floored.
