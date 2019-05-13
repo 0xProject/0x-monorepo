89ef7adf79644673b88d05eea9c789f03c78a927
xianny <xianny@gmail.com>
2019-05-13 16:05:17 -0400

    add private key option to migration script

--- packages/migrations/src/cli.ts
+++ packages/migrations/src/cli.ts
@@ -17,6 +17,11 @@ const args = yargs
         type: 'string',
         demandOption: true,
     })
+    .option('pk', {
+        describe: 'Private key for the `from` address',
+        type: 'string',
+        demandOption: true,
+    }).
     .example(
         '$0 --rpc-url http://localhost:8545 --from 0x5409ed021d9299bf6814279a6a1411a7e866a631',
         'Full usage example',
