0071bd341453c78416506b47788218c32374a176
Lawrence Forman <lawrence@0xproject.com>
2019-05-16 16:09:27 -0400

    Ran prettier and fixed linter errors

--- contracts/exchange/contracts/src/MixinWrapperFunctions.sol
+++ contracts/exchange/contracts/src/MixinWrapperFunctions.sol
@@ -73,6 +73,7 @@ contract MixinWrapperFunctions is
     {
         // ABI encode calldata for `fillOrder`
         bytes memory fillOrderCalldata = abi.encodeWithSelector(
+            // solhint-disable-next-line max-line-length
             // bytes4(keccak256("fillOrder((address,address,address,address,uint256,uint256,uint256,uint256,uint256,uint256,bytes,bytes,bytes,bytes),uint256,bytes)"))
             // = 0x9b44d556
             0x9b44d556,
