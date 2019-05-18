c14acf3b869326d503e7b95d7c2c80d331303257
Lawrence Forman <lawrence@0xproject.com>
2019-05-18 02:06:49 -0400

    `@0x/order-utils`: Update `RevertError` types for new `RevertError` constructor.

--- packages/order-utils/src/order_validation_utils.ts
+++ packages/order-utils/src/order_validation_utils.ts
@@ -61,6 +61,16 @@ export class OrderValidationUtils {
             signedOrder.takerAssetAmount,
             signedOrder.makerAssetAmount,
         );
+        const makerFeeAmount = utils.getPartialAmountFloor(
+            fillTakerAssetAmount,
+            signedOrder.takerAssetAmount,
+            signedOrder.makerFee,
+        );
+        const takerFeeAmount = utils.getPartialAmountFloor(
+            fillTakerAssetAmount,
+            signedOrder.takerAssetAmount,
+            signedOrder.takerFee,
+        );
         await exchangeTradeEmulator.transferFromAsync(
             signedOrder.makerAssetData,
             signedOrder.makerAddress,
@@ -77,11 +87,6 @@ export class OrderValidationUtils {
             TradeSide.Taker,
             TransferType.Trade,
         );
-        const makerFeeAmount = utils.getPartialAmountFloor(
-            fillTakerAssetAmount,
-            signedOrder.takerAssetAmount,
-            signedOrder.makerFee,
-        );
         await exchangeTradeEmulator.transferFromAsync(
             signedOrder.makerFeeAssetData,
             signedOrder.makerAddress,
@@ -89,11 +94,6 @@ export class OrderValidationUtils {
             makerFeeAmount,
             TradeSide.Maker,
             TransferType.Fee,
-        );
-        const takerFeeAmount = utils.getPartialAmountFloor(
-            fillTakerAssetAmount,
-            signedOrder.takerAssetAmount,
-            signedOrder.takerFee,
         );
         await exchangeTradeEmulator.transferFromAsync(
             signedOrder.takerFeeAssetData,
