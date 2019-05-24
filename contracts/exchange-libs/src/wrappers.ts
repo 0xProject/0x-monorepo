741a731ecbd7318a87ff14206ca07169de23a837
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-05-24 11:27:53 -0400

    Transaction decoder for Coordinator (#1802)
    * Remove unused dependency from erc20/package.json
    
    * Add LibTransactionDecoder
    
    Yet unsupported functions include:
        cancelOrdersUpTo
        executeTransaction

--- contracts/exchange-libs/src/wrappers.ts
+++ contracts/exchange-libs/src/wrappers.ts
@@ -10,4 +10,5 @@ export * from '../generated-wrappers/lib_e_i_p712';
 export * from '../generated-wrappers/lib_fill_results';
 export * from '../generated-wrappers/lib_math';
 export * from '../generated-wrappers/lib_order';
+export * from '../generated-wrappers/lib_transaction_decoder';
 export * from '../generated-wrappers/test_libs';
