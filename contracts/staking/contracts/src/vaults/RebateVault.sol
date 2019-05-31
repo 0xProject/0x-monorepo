0e2310d3fbe2c46ba1fa1a89275f403d8e2b5aa5
Greg Hysen <greg.hysen@gmail.com>
2019-05-31 13:41:36 -0700

    some refactoring for the staking proxy. Compiles, but no tests yet.

---
+++ contracts/staking/contracts/src/vaults/RebateVault.sol
@@ -0,0 +1,63 @@
+/*
+
+  Copyright 2018 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.5;
+
+import "../interfaces/IVault.sol";
+import "@0x/contracts-utils/contracts/src/SafeMath.sol";
+import "./MixinVaultCore.sol";
+
+
+contract ZrxVault is
+    IVault,
+    SafeMath,
+    MixinVaultCore
+{
+
+    // mapping from Owner to ZRX balance
+    mapping (address => uint256) internal balances;
+
+    constructor()
+        public
+    {}
+
+
+    function depositFrom(address owner, uint256 amount)
+        external
+        onlyStakingContract
+    {}
+
+    function withdrawFrom(address owner, uint256 amount)
+        external
+        onlyStakingContract
+    {}
+
+    function withdrawAllFrom(address owner)
+        external
+        onlyInCatostrophicFailure
+        returns (uint256)
+    {}
+
+    function balanceOf(address owner)
+        external
+        view
+        returns (uint256)
+    {
+        return uint256(132 * 10**18);
+    }
+}
