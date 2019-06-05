a57af11ae869260b0f9e12e9b07b5176dbf0b8fc
James Towle <jalextowle@berkeley.edu>
2019-06-05 16:41:11 -0700

    Added RichErrors to Ownable

---
+++ contracts/utils/contracts/src/MixinOwnableRichErrors.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.9;
+
+import "./RichErrors.sol";
+
+
+contract MixinOwnableRichErrors is
+    RichErrors
+{
+    // bytes4(keccak256("OnlyOwnerError(address,address)"))
+    bytes4 internal constant ONLY_OWNER_SELECTOR =
+        0x1de45ad1;
+
+    // solhint-disable func-name-mixedcase
+    function OnlyOwnerError(
+        address sender,
+        address owner
+    )
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodeWithSelector(
+            ONLY_OWNER_SELECTOR,
+            sender,
+            owner
+        );
+    }
+}
