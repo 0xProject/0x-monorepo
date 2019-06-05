12afeb30aed5c6f31eaa817348d1598fa42056c1
Amir Bandeali <abandeali1@gmail.com>
2019-06-05 09:06:01 -0700

    Move LibAssetData to DevUtils package

--- contracts/asset-proxy/src/wrappers.ts
+++ contracts/asset-proxy/src/wrappers.ts
@@ -9,6 +9,5 @@ export * from '../generated-wrappers/erc721_proxy';
 export * from '../generated-wrappers/i_asset_data';
 export * from '../generated-wrappers/i_asset_proxy';
 export * from '../generated-wrappers/i_authorizable';
-export * from '../generated-wrappers/lib_asset_data';
 export * from '../generated-wrappers/mixin_authorizable';
 export * from '../generated-wrappers/multi_asset_proxy';
