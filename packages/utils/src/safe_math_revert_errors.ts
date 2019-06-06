bbd13f70e8595708eb302835c11dff3b199d3eca
James Towle <jalextowle@berkeley.edu>
2019-06-06 12:47:21 -0700

    Updated the erc1155 tests to expect RichErrors

---
+++ packages/utils/src/safe_math_revert_errors.ts
@@ -0,0 +1,32 @@
+import { BigNumber } from './configured_bignumber';
+import { RevertError } from './revert_error';
+
+// tslint:disable:max-classes-per-file
+
+export class Uint256OverflowError extends RevertError {
+    constructor(a?: BigNumber | number | string, b?: BigNumber | number | string) {
+        super('Uint256OverflowError', 'Uint256OverflowError(uint256 a, uint256 b)', {
+            a,
+            b,
+        });
+    }
+}
+
+export class Uint256UnderflowError extends RevertError {
+    constructor(a?: BigNumber | number | string, b?: BigNumber | number | string) {
+        super('Uint256UnderflowError', 'Uint256UnderflowError(uint256 a, uint256 b)', {
+            a,
+            b,
+        });
+    }
+}
+
+const types = [
+    Uint256OverflowError,
+    Uint256UnderflowError,
+]
+
+// Register the types we've defined.
+for (const type of types) {
+    RevertError.registerType(type);
+}
