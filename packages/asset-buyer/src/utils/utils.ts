8d83080b85117e662cf33cefd3f1c41d5f2600c2
David Sun <davidsun@Davids-MacBook-Pro-2.local>
2019-06-06 14:27:33 -0700

    finished forwarder consumer

--- packages/asset-buyer/src/utils/utils.ts
+++ packages/asset-buyer/src/utils/utils.ts
@@ -1,5 +1,7 @@
 import { BigNumber } from '@0x/utils';
 import { Web3Wrapper } from '@0x/web3-wrapper';
+import { AbiDefinition, ContractAbi, MethodAbi } from 'ethereum-types';
+import * as _ from 'lodash';
 
 import { constants } from '../constants';
 
@@ -8,5 +10,15 @@ export const utils = {
         return Web3Wrapper.toBaseUnitAmount(constants.ONE_AMOUNT, constants.ETHER_TOKEN_DECIMALS).multipliedBy(
             percentage,
         );
+    },
+    getMethodAbiFromContractAbi(abi: ContractAbi, name: string): MethodAbi | undefined {
+        return _.find(abi, (def: AbiDefinition): boolean => {
+            if (def.type === `'function'`) {
+                const methodDef = def as MethodAbi;
+                return methodDef.name === name;
+            } else {
+                return false;
+            }
+        }) as MethodAbi | undefined;
     },
 };
