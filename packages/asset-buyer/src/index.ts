ee45e708b47904f84271f304bc49639f5119c932
David Sun <davidsun@Davids-MacBook-Pro-2.local>
2019-06-06 12:52:13 -0700

    renamed asset-buyer + adding consumers

--- packages/asset-buyer/src/index.ts
+++ packages/asset-buyer/src/index.ts
@@ -18,19 +18,19 @@ export {
 export { SignedOrder } from '@0x/types';
 export { BigNumber } from '@0x/utils';
 
-export { AssetBuyer } from './asset_buyer';
+export { AssetSwapQuoter } from './asset_buyer';
 export { InsufficientAssetLiquidityError } from './errors';
 
 export { BasicOrderProvider } from './order_providers/basic_order_provider';
 export { StandardRelayerAPIOrderProvider } from './order_providers/standard_relayer_api_order_provider';
 
 export {
-    AssetBuyerError,
-    AssetBuyerOpts,
-    BuyQuote,
-    BuyQuoteExecutionOpts,
-    BuyQuoteInfo,
-    BuyQuoteRequestOpts,
+    AssetSwapQuoterError,
+    AssetSwapQuoterOpts,
+    SwapQuote,
+    SwapQuoteExecutionOpts,
+    SwapQuoteInfo,
+    SwapQuoteRequestOpts,
     LiquidityForAssetData,
     LiquidityRequestOpts,
     OrdersAndFillableAmounts,
