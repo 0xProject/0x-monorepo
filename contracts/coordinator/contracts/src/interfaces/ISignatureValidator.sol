3629de4c0470c291938ff0ed41314959cbb30e8d
James Towle <jalextowle@berkeley.edu>
2019-06-07 17:10:09 -0700

    Removed enums from Mixins in the coordinator package

--- contracts/coordinator/contracts/src/interfaces/ISignatureValidator.sol
+++ contracts/coordinator/contracts/src/interfaces/ISignatureValidator.sol
@@ -21,6 +21,20 @@ pragma solidity ^0.5.9;
 
 contract ISignatureValidator {
 
+   // Allowed signature types.
+    enum SignatureType {
+        Illegal,                // 0x00, default value
+        Invalid,                // 0x01
+        EIP712,                 // 0x02
+        EthSign,                // 0x03
+        Wallet,                 // 0x04
+        Validator,              // 0x05
+        PreSigned,              // 0x06
+        OrderValidator,         // 0x07
+        WalletOrderValidator,   // 0x08
+        NSignatureTypes         // 0x09, number of signature types. Always leave at end.
+    }
+
     /// @dev Recovers the address of a signer given a hash and signature.
     /// @param hash Any 32 byte hash.
     /// @param signature Proof that the hash has been signed by signer.
