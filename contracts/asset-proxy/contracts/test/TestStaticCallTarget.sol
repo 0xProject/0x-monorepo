7d9e43b2e103b4c5a08bdec4f64a4c894fcfea58
Amir Bandeali <abandeali1@gmail.com>
2019-06-08 16:36:46 -0700

    Add TestStaticCallTarget for testing StaticCallProxy

---
+++ contracts/asset-proxy/contracts/test/TestStaticCallTarget.sol
@@ -0,0 +1,73 @@
+/*
+
+  Copyright 2018 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.5;
+
+
+contract TestStaticCallTarget {
+
+    uint256 internal _state;
+ 
+    function updateState()
+        external
+    {
+        _state++;
+    }
+
+    function assertEvenNumber(uint256 target)
+        external
+        pure
+    {
+        require(
+            target % 2 == 0,
+            "TARGET_NOT_EVEN"
+        );
+    }
+
+    function isOddNumber(uint256 target)
+        external
+        pure
+        returns (bool isOdd)
+    {
+        isOdd = target % 2 == 1;
+        return isOdd;
+    }
+
+    function noInputFunction()
+        external
+        pure
+    {}
+
+    function dynamicInputFunction(bytes calldata a)
+        external
+        pure
+    {}
+
+    function returnComplexType(uint256 a, uint256 b)
+        external
+        view
+        returns (bytes memory result)
+    {
+        result = abi.encodePacked(
+            address(this),
+            a,
+            b
+        );
+        return result;
+    }
+}
