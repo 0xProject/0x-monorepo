55246c5d874ecdef573ef7792e31506eede81829
James Towle <jalextowle@berkeley.edu>
2019-06-13 09:37:53 -0700

    Removed unnecessary import

--- contracts/exchange/contracts/src/interfaces/IExchangeCore.sol
+++ contracts/exchange/contracts/src/interfaces/IExchangeCore.sol
@@ -21,7 +21,6 @@ pragma experimental ABIEncoderV2;
 
 import "@0x/contracts-exchange-libs/contracts/src/LibOrder.sol";
 import "@0x/contracts-exchange-libs/contracts/src/LibFillResults.sol";
-import "@0x/contracts-exchange-libs/contracts/src/LibOrder.sol";
 
 
 contract IExchangeCore {
