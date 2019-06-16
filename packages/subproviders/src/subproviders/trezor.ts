085093063ab01832f615fc7f5adb15e763025881
Rick Morty <rickmorty@MacBook.local>
2019-06-15 21:23:40 -0700

    Hex should be true since the data returned expects it to be hex encoded

--- packages/subproviders/src/subproviders/trezor.ts
+++ packages/subproviders/src/subproviders/trezor.ts
@@ -140,7 +140,7 @@ export class TrezorSubprovider extends BaseWalletSubprovider {
         const response: TrezorConnectResponse = await this._trezorConnectClientApi.ethereumSignMessage({
             path: fullDerivationPath,
             message: data,
-            hex: false,
+            hex: true,
         });
 
         if (response.success) {
