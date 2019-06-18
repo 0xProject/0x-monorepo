14549ec5ad5e3d86f22428e44af9813a6865a0fc
James Towle <jalextowle@gmail.com>
2019-06-17 23:14:19 -0700

    Update contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
    Co-Authored-By: Lawrence Forman <lawrence@0xproject.com>

--- contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
+++ contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.9;
 contract IMixinLibMathRichErrors {
 
     // bytes4(keccak256("DivisionByZeroError()"))
-    bytes4 internal constant DIVISION_BY_ZERO =
+    bytes4 internal constant DIVISION_BY_ZERO_SELECTOR =
         0xa791837c;
 
     // bytes4(keccak256("RoundingError(uint256,uint256,uint256)"))
