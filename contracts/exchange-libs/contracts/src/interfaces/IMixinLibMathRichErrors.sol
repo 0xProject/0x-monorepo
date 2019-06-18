6478b42a4fc2aba1ce68943eeda1a040117b9e2a
James Towle <jalextowle@gmail.com>
2019-06-17 23:14:28 -0700

    Update contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
    Co-Authored-By: Lawrence Forman <lawrence@0xproject.com>

--- contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
+++ contracts/exchange-libs/contracts/src/interfaces/IMixinLibMathRichErrors.sol
@@ -8,6 +8,6 @@ contract IMixinLibMathRichErrors {
         0xa791837c;
 
     // bytes4(keccak256("RoundingError(uint256,uint256,uint256)"))
-    bytes4 internal constant ROUNDING_ERROR =
+    bytes4 internal constant ROUNDING_ERROR_SELECTOR =
         0x339f3de2;
 }
