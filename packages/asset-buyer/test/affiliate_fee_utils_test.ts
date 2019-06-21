dda72ce77ae4ac1ccf9f123a72bcd8969fa5ede0
David Sun <davidsun@Davids-MacBook-Pro-2.local>
2019-06-21 14:11:42 -0700

    working on testing affiliate fee

--- packages/asset-buyer/test/affiliate_fee_utils_test.ts
+++ packages/asset-buyer/test/affiliate_fee_utils_test.ts
@@ -12,16 +12,29 @@ import { constants } from '../src/constants';
 import { LiquidityForAssetData, OrderProvider, OrdersAndFillableAmounts } from '../src/types';
 
 import { chaiSetup } from './utils/chai_setup';
-import {
-    mockAvailableMakerAssetDatas,
-    mockAvailableTakerAssetDatas,
-    mockedSwapQuoterWithOrdersAndFillableAmounts,
-    orderProviderMock,
-} from './utils/mocks';
+import { getFullyFillableSwapQuoteWithNoFees, getSignedOrdersWithNoFees } from './utils/swap_quote';
 
 chaiSetup.configure();
 const expect = chai.expect;
 
-describe('affiliateFeeUtils', () => {
+const FAKE_TAKER_ASSET_DATA = '0xf47261b00000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48';
+const FAKE_MAKER_ASSET_DATA = '0xf47261b00000000000000000000000009f5B0C7e1623793bF0620569b9749e79DF6D0bC5';
+const FAKE_FILLABLE_AMOUNTS = [new BigNumber(5), new BigNumber(1), new BigNumber(4)];
+const NULL_ADDRESS = constants.NULL_ADDRESS;
 
+describe('affiliateFeeUtils', () => {
+    const signedOrders = getSignedOrdersWithNoFees(
+        FAKE_MAKER_ASSET_DATA,
+        FAKE_TAKER_ASSET_DATA,
+        NULL_ADDRESS,
+        NULL_ADDRESS,
+        FAKE_FILLABLE_AMOUNTS,
+        );
+    const swapQuote = getFullyFillableSwapQuoteWithNoFees(FAKE_MAKER_ASSET_DATA, FAKE_TAKER_ASSET_DATA, signedOrders);
+
+    describe('getSwapQuoteWithAffiliateFee', () => {
+        it('should return unchanged swapQuote if feePercentage is 0', () => {
+
+        });
+    });
 });
