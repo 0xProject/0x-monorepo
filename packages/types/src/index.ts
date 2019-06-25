c7324121edd48a4617872d01b2b32456df357f20
Lawrence Forman <lawrence@0xproject.com>
2019-06-25 19:33:34 -0400

    `@0x/types`: Consolidate signature types.

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -166,10 +166,7 @@ export enum SignatureType {
     Wallet,
     Validator,
     PreSigned,
-    OrderValidator,
-    OrderWallet,
     EIP1271Wallet,
-    EIP1271OrderWallet,
     NSignatureTypes,
 }
 
