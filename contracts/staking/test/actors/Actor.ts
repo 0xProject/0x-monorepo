7f0d17dca3f3df81fe75680f22655224476061ad
Greg Hysen <greg.hysen@gmail.com>
2019-06-26 14:15:01 -0700

    Ran `yarn fix`

--- contracts/staking/test/actors/Actor.ts
+++
@@ -1,5 +0,0 @@
-import { StakingWrapper } from '../utils/staking_wrapper';
-
-import { DelegatorActor } from './delegator_actor';
-import { MakerActor } from './maker_actor';
-import { PoolOperatorActor } from './pool_operator_actor';
