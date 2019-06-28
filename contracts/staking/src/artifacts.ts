4a9f5820767fa061bf439b13607f396c3c3c1bd6
Greg Hysen <greg.hysen@gmail.com>
2019-06-28 14:43:38 -0700

    Ran contracts:gen

--- contracts/staking/src/artifacts.ts
+++ contracts/staking/src/artifacts.ts
@@ -6,7 +6,6 @@
 import { ContractArtifact } from 'ethereum-types';
 
 import * as LibFeeMathTest from '../generated-artifacts/LibFeeMathTest.json';
-import * as LibZrxToken from '../generated-artifacts/LibZrxToken.json';
 import * as Staking from '../generated-artifacts/Staking.json';
 import * as StakingPoolRewardVault from '../generated-artifacts/StakingPoolRewardVault.json';
 import * as StakingProxy from '../generated-artifacts/StakingProxy.json';
@@ -14,7 +13,6 @@ import * as ZrxVault from '../generated-artifacts/ZrxVault.json';
 export const artifacts = {
     Staking: Staking as ContractArtifact,
     StakingProxy: StakingProxy as ContractArtifact,
-    LibZrxToken: LibZrxToken as ContractArtifact,
     StakingPoolRewardVault: StakingPoolRewardVault as ContractArtifact,
     ZrxVault: ZrxVault as ContractArtifact,
     LibFeeMathTest: LibFeeMathTest as ContractArtifact,
