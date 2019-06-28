4f8164dc43b233486a72ffedce9167bf79f936ea
Fred Carlsen <fred@sjelfull.no>
2019-06-28 17:19:20 +0200

    Pass filter groups as props

--- packages/website/ts/components/docs/sidebar/filters.tsx
+++ packages/website/ts/components/docs/sidebar/filters.tsx
@@ -3,18 +3,20 @@ import * as CopyToClipboard from 'react-copy-to-clipboard';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
 import { Button } from 'ts/components/button';
+import { Filter } from 'ts/components/docs/sidebar/filter';
 import { Heading } from 'ts/components/text';
 import { Container } from 'ts/components/ui/container';
-import { Filter } from 'ts/components/docs/sidebar/filter';
 import { colors } from 'ts/style/colors';
 import { styled } from 'ts/style/theme';
 import { zIndex } from 'ts/style/z_index';
 
-export interface FiltersProps {}
+export interface FiltersProps {
+    groups: FilterGroup[];
+}
 
 export interface FiltersState {}
 
-interface Group {
+export interface FilterGroup {
     heading: string;
     name: string;
     filters: FilterProps[];
@@ -25,56 +27,14 @@ interface FilterProps {
     label: string;
 }
 
-const groups: Group[] = [
-    {
-        heading: 'Topic',
-        name: 'topic',
-        filters: [
-            {
-                value: 'mesh',
-                label: 'Mesh',
-            },
-            {
-                value: 'testing',
-                label: 'Testing',
-            },
-            {
-                value: 'mesh',
-                label: 'Mesh',
-            },
-            {
-                value: 'testing',
-                label: 'Testing',
-            },
-        ],
-    },
-    {
-        heading: 'Level',
-        name: 'level',
-        filters: [
-            {
-                value: 'beginner',
-                label: 'Beginner',
-            },
-            {
-                value: 'intermediate',
-                label: 'Intermediate',
-            },
-            {
-                value: 'advanced',
-                label: 'Advanced',
-            },
-        ],
-    },
-];
-
 export class Filters extends React.Component<FiltersProps, FiltersState> {
     public static defaultProps = {};
     public state: FiltersState = {};
     public render(): React.ReactNode {
+        const { groups } = this.props;
         return (
             <Wrapper>
-                {groups.map(({ heading, name, filters }: Group, index) => (
+                {groups.map(({ heading, name, filters }: FilterGroup, index) => (
                     <GroupWrapper key={`filter-group-${index}`}>
                         <GroupHeading asElement="h3">{heading}</GroupHeading>
                         {filters.map(({ value, label }: FilterProps, index) => (
@@ -85,9 +45,6 @@ export class Filters extends React.Component<FiltersProps, FiltersState> {
             </Wrapper>
         );
     }
-    /*private readonly _handleCopyClick = () => {
-        this.setState({ didCopyFilters: true });
-    };*/
 }
 
 const Wrapper = styled.div`
