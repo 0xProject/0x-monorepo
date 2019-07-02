4619f34cf893cb559dfab593d8a9e1594c60d7f8
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-07-02 14:26:54 -0400

    abi-gen: refine pipeline for testing gen'd code
    - generate wrappers for test fixture contracts (dummy, etc) not in `yarn
    build` but in `yarn test`
    
    - rename folder test/generated-test/generated-wrappers to
    test/generated-test/known-good, and stop writing test output to there,
    instead writing it to a new test/generated-test/output folder.
    
    - sol-compile test fixture contracts in every test run
    
    - run unit tests separately from tests of generated and built TypeScript
    wrappers.  The existing `yarn run_mocha` will run unit tests, and
    there's a new `yarn run_contract_wrapper_tests`.

--- packages/abi-gen/diff.sh
+++ packages/abi-gen/diff.sh
@@ -1,5 +1,5 @@
 diff $1 $2
 if [ $? -ne 0 ]; then
-    echo "ERROR: Run 'yarn generate_contract_wrappers' to update ./test/generated_test/generated-wrappers"
+    echo "ERROR: Freshly generated code does not match known-good, commited output.  If you're confident that the freshly generated code should be considered known-good, then copy it into there and commit it."
     exit 1
 fi
