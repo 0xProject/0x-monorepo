a6f233267bba124f18cefb35ba48a8612f452f46
Piotr Janosz <janosz128@gmail.com>
2019-07-03 16:41:14 +0200

    Cleaned up code and tabs components in docs

--- packages/website/ts/components/docs/tabs.tsx
+++ packages/website/ts/components/docs/tabs.tsx
@@ -1,41 +1,31 @@
-import * as _ from 'lodash';
-import * as React from 'react';
-import {
-    Tab as OriginalTab,
-    TabList as OriginalTabList,
-    TabPanel as OriginalTabPanel,
-    Tabs as OriginalTabs,
-} from 'react-tabs';
-import styled, { withTheme } from 'styled-components';
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import { Tab as ReactTab, TabList as ReactTabList, TabPanel as ReactTabPanel, Tabs as ReactTabs } from 'react-tabs';
+
 import { colors } from 'ts/style/colors';
 
-export const Tabs = styled(OriginalTabs).attrs({
+interface ITabProps {
+    children: ReactNode;
+    selectedTabClassName?: string;
+}
+
+const activeClass = {
     selectedTabClassName: 'is-active',
-})`
+};
+
+export const Tabs = styled(ReactTabs).attrs(activeClass)<ITabProps>`
     margin-bottom: 1.875rem;
 
     .is-active {
-        background-color: #f3f6f4;
+        background-color: ${colors.backgroundLight};
         color: ${colors.brandDark};
     }
 `;
 
-export const Tab = styled(OriginalTab)`
-    background-color: transparent;
-    border-radius: 4px 4px 0 0;
-    cursor: pointer;
-    padding: 12px 12px 13px;
-    font-size: 1rem;
-    color: #5c5c5c;
-    font-weight: 300;
-`;
-
-export const TabPanel = styled(OriginalTabPanel).attrs({
-    selectedClassName: 'is-active',
-})`
-    background-color: #f3f6f4;
+export const TabPanel = styled(ReactTabPanel).attrs(activeClass)<ITabProps>`
+    background-color: ${colors.backgroundLight};
     border-radius: 4px;
-    padding: 12px 12px;
     display: none;
 
     &.is-active {
@@ -43,6 +33,16 @@ export const TabPanel = styled(OriginalTabPanel).attrs({
     }
 `;
 
-export const TabList = styled(OriginalTabList)`
+export const TabList = styled(ReactTabList)<ITabProps>`
     display: flex;
+`;
+
+export const Tab = styled(ReactTab)<ITabProps>`
+    background-color: transparent;
+    border-radius: 4px 4px 0 0;
+    cursor: pointer;
+    padding: 12px;
+    font-size: 1rem;
+    color: ${colors.textDarkSecondary};
+    font-weight: 300;
 `;
