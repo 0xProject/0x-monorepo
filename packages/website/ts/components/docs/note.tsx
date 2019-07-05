b80f1743a435acfb61557969e9c1b58b47dee960
Piotr Janosz <janosz128@gmail.com>
2019-07-05 19:54:56 +0200

    Refactored Note / Notification components

--- packages/website/ts/components/docs/note.tsx
+++ packages/website/ts/components/docs/note.tsx
@@ -1,29 +1,22 @@
-import * as _ from 'lodash';
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { Heading, Paragraph } from 'ts/components/text';
 import { colors } from 'ts/style/colors';
 
-export interface NoteProps {
+export interface INoteProps {
     heading: string;
     description?: string;
 }
 
-interface WrapperProps {}
-
-export const Note: React.FunctionComponent<NoteProps> = (props: NoteProps) => (
-    <>
-        <Wrapper>
-            <Content>
-                <NoteHeading marginBottom="6px">{props.heading}</NoteHeading>
-                <Description>{props.description}</Description>
-            </Content>
-        </Wrapper>
-    </>
+export const Note: React.FunctionComponent<INoteProps> = props => (
+    <NoteWrapper>
+        <NoteHeading marginBottom="6px">{props.heading}</NoteHeading>
+        <NoteDescription>{props.description}</NoteDescription>
+    </NoteWrapper>
 );
 
-const Wrapper = styled.div`
+const NoteWrapper = styled.div`
     background-color: ${colors.backgroundLight};
     border: 1px solid #dbdfdd;
     padding: 20px 14px;
@@ -35,14 +28,12 @@ const Wrapper = styled.div`
     margin-bottom: 30px;
 `;
 
-const Content = styled.div``;
-
 const NoteHeading = styled(Heading).attrs({ color: colors.brandDark, asElement: 'h4' })`
     font-size: 0.944444444rem !important;
     margin-bottom: 6px;
 `;
 
-const Description = styled(Paragraph)`
+const NoteDescription = styled(Paragraph)`
     font-size: 0.888888889rem;
     margin-bottom: 0;
     line-height: 1.25;
