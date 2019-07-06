72e76e16461949ba6ca70b0a0e0e1dacf2978b13
Piotr Janosz <janosz128@gmail.com>
2019-07-06 19:30:13 +0200

    Removed suprflous code from table, tutorial steps, unordered list

--- packages/website/ts/components/docs/unordered_list.tsx
+++ packages/website/ts/components/docs/unordered_list.tsx
@@ -1,21 +1,7 @@
-import * as _ from 'lodash';
-import * as React from 'react';
-import styled, { withTheme } from 'styled-components';
+import styled from 'styled-components';
 import { colors } from 'ts/style/colors';
 
-export interface Props {
-    children: React.ReactNode;
-}
-
-export const UnorderedList: React.FunctionComponent<Props> = (props: Props) => (
-    <>
-        <Wrapper>{props.children}</Wrapper>
-    </>
-);
-
-UnorderedList.defaultProps = {};
-
-const Wrapper = styled.ul`
+export const UnorderedList = styled.ul`
     list-style-type: disc;
     margin-bottom: 1.875rem;
     padding-left: 1rem;
