db32e66f54c36ffb6b655f98ef428e69e9da134f
Piotr Janosz <janosz128@gmail.com>
2019-07-06 19:36:23 +0200

    Refactored rating_bar

--- packages/website/ts/components/docs/resource/rating_bar.tsx
+++ packages/website/ts/components/docs/resource/rating_bar.tsx
@@ -1,50 +1,40 @@
-import * as _ from 'lodash';
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-import { Paragraph } from 'ts/components/text';
 import { colors } from 'ts/style/colors';
 
-interface LabelInterface {
-    [key: number]: string;
-}
-
-interface RatingBarProps {
+interface IRatingBarProps {
     rating: number;
 }
 
-interface RatingBulletProps {
+interface IRatingBulletProps {
     isFilled: boolean;
 }
 
-export const RatingBar: React.StatelessComponent<RatingBarProps> = ({ rating }) => {
-    const id =
-        Math.random()
-            .toString(36)
-            .substring(2, 15) +
-        Math.random()
-            .toString(36)
-            .substring(2, 15);
+export const RatingBar: React.FC<IRatingBarProps> = ({ rating }) => {
+    const id = Math.random()
+        .toString(36)
+        .substring(2, 15);
     const ratingPlaceholders = Array.from(new Array(3), (value, index) => index + 1);
     const fillCheck = (currentIndex: number) => currentIndex <= rating;
 
     // TODO convert this to use a Container component
     return (
-        <Wrapper>
+        <RatingBarWrapper>
             {ratingPlaceholders.map((currentIndex: number) => (
                 <RatingBullet key={`${id}-${currentIndex}`} isFilled={fillCheck(currentIndex)} />
             ))}
-        </Wrapper>
+        </RatingBarWrapper>
     );
 };
 
-const Wrapper = styled.div`
+const RatingBarWrapper = styled.div`
     display: flex;
     align-items: center;
 `;
 
-const RatingBullet = styled.div<RatingBulletProps>`
-    background-color: ${props => (props.isFilled ? colors.brandDark : 'rgba(0, 56, 49, 0.2)')};
+const RatingBullet = styled.div<IRatingBulletProps>`
+    background-color: ${({ isFilled }) => (isFilled ? colors.brandDark : 'rgba(0, 56, 49, 0.2)')};
     border-radius: 50%;
     width: 10px;
     height: 10px;
