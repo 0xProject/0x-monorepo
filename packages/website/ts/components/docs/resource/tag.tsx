06744ee7fb0612d2cb50b23b5086ef7f5d3eaf0a
Piotr Janosz <janosz128@gmail.com>
2019-07-06 19:41:14 +0200

    Refactored resource and tag components

--- packages/website/ts/components/docs/resource/tag.tsx
+++ packages/website/ts/components/docs/resource/tag.tsx
@@ -1,30 +1,15 @@
-import { Link } from '@0x/react-shared';
-import * as _ from 'lodash';
-import * as React from 'react';
 import styled from 'styled-components';
+
 import { colors } from 'ts/style/colors';
 
-export interface TagProps {
-    children: React.ReactNode;
+interface ITagProps {
     isInverted?: boolean;
 }
 
-interface WrapperProps {
-    isInverted?: boolean;
-}
-
-export const Tag: React.FunctionComponent<TagProps> = ({ isInverted, children }: TagProps) => (
-    <Wrapper isInverted={isInverted}>{children}</Wrapper>
-);
-
-Tag.defaultProps = {
-    isInverted: false,
-};
-
-const Wrapper = styled.div<WrapperProps>`
-    background-color: ${props => (props.isInverted ? colors.brandDark : 'rgba(0, 56, 49, 0.1)')};
+export const Tag = styled.div<ITagProps>`
+    background-color: ${isInverted => (isInverted ? colors.brandDark : 'rgba(0, 56, 49, 0.1)')};
     border-radius: 4px;
-    color: ${props => (props.isInverted ? colors.white : colors.brandDark)};
+    color: ${isInverted => (isInverted ? colors.white : colors.brandDark)};
     font-size: 0.666666667rem;
     font-family: 'Formular Mono';
     font-weight: 400;
@@ -37,3 +22,7 @@ const Wrapper = styled.div<WrapperProps>`
         margin-left: 10px;
     }
 `;
+
+Tag.defaultProps = {
+    isInverted: false,
+};
