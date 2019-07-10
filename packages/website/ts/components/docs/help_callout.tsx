7eed3f6ac456fc71a9294e5f59a43f1ad4380adc
Piotr Janosz <janosz128@gmail.com>
2019-07-10 14:24:02 +0200

    Fixed styling for help callout

--- packages/website/ts/components/docs/help_callout.tsx
+++ packages/website/ts/components/docs/help_callout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Link } from '@0x/react-shared';
+
 import { Icon } from 'ts/components/icon';
 import { Heading, Paragraph } from 'ts/components/text';
 import { colors } from 'ts/style/colors';
@@ -12,14 +14,18 @@ export interface IHelpCalloutProps {
 }
 
 export const HelpCallout: React.FC<IHelpCalloutProps> = props => (
-    <HelpCalloutWrapper href={props.url}>
+    <HelpCalloutWrapper>
         <Icon color={colors.brandLight} name="help" size={38} margin={[0, 30, 0, 0]} />
         <div>
             <Heading size="small" marginBottom="8px">
                 {props.heading}
             </Heading>
             <Paragraph size="default" marginBottom="0">
-                {props.description}
+                Get in touch{' '}
+                <Link fontColor={colors.brandDark} textDecoration="underline" to={props.url}>
+                    here
+                </Link>{' '}
+                and we'll be happy to help
             </Paragraph>
         </div>
     </HelpCalloutWrapper>
@@ -27,13 +33,10 @@ export const HelpCallout: React.FC<IHelpCalloutProps> = props => (
 
 HelpCallout.defaultProps = {
     heading: 'Need some help?',
-    description: `Get in touch here and we’ll be happy to help.`,
     url: 'https://discordapp.com/invite/d3FTX3M',
 };
 
-const HelpCalloutWrapper = styled.a.attrs({
-    target: '_blank',
-})`
+const HelpCalloutWrapper = styled.div`
     display: flex;
     align-items: center;
     padding: 25px 30px;
