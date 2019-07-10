5eab80dbd10a32219b777256b9187a90b4f1eb41
xianny <xianny@gmail.com>
2019-07-10 08:25:35 -0700

    combine diff scripts

--- packages/abi-gen/diff.sh
+++ packages/abi-gen/diff.sh
@@ -1,4 +1,4 @@
-diff $1 $2
+diff -r $1 $2
 if [ $? -ne 0 ]; then
     echo "ERROR: Freshly generated output does not match expected output.  If you're confident that the expected output should be updated, copy it in there and commit it."
     exit 1
