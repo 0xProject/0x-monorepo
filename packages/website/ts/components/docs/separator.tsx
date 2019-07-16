89c8254dc4a5a8df95b508734ca8101fe3b0cddb
Piotr Janosz <janosz128@gmail.com>
2019-07-16 23:43:31 +0200

    Fixed docs whitespace for mobile / tablet

--- packages/website/ts/components/docs/separator.tsx
+++ packages/website/ts/components/docs/separator.tsx
@@ -1,8 +1,20 @@
 import styled from 'styled-components';
 
-export const Separator = styled.hr`
+interface ISeparatorProps {
+    margin?: string;
+}
+
+export const Separator = styled.hr<ISeparatorProps>`
     border-width: 0 0 1px;
     border-color: #e4e4e4;
     height: 0;
-    margin: 30px 0;
+    margin: ${({ margin }) => margin};
+
+    @media (max-width: 768px) {
+        margin: 30px 0;
+    }
 `;
+
+Separator.defaultProps = {
+    margin: '30px 0',
+};
