01d54591e19b2c35778d43e59e9954e2062394a4
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-07-16 18:17:28 -0400

    abi-gen: customizable param validation for Python

--- packages/abi-gen/test-cli/expected-output/python/abi_gen_dummy/__init__.py
+++ packages/abi-gen/test-cli/expected-output/python/abi_gen_dummy/__init__.py
@@ -4,6 +4,7 @@
 
 import json
 from typing import (  # pylint: disable=unused-import
+    Any,
     List,
     Optional,
     Tuple,
@@ -17,6 +18,43 @@ from web3.providers.base import BaseProvider
 
 from zero_ex.contract_wrappers._base_contract_wrapper import BaseContractWrapper
 from zero_ex.contract_wrappers.tx_params import TxParams
+
+
+class AbiGenDummyValidatorBase:
+    """Base class for validating inputs to AbiGenDummy methods."""
+    def __init__(
+        self,
+        provider: BaseProvider,
+        contract_address: str,
+        private_key: str = None,
+    ):
+        """Initialize the instance."""
+
+    def assert_valid(
+        self, method_name: str, parameter_name: str, argument_value: Any
+    ):
+        """Raise an exception if method input is not valid.
+
+        :param method_name: Name of the method whose input is to be validated.
+        :param parameter_name: Name of the parameter whose input is to be
+            validated.
+        :param argument_value: Value of argument to parameter to be validated.
+        """
+
+
+# Try to import a custom validator class definition; if there isn't one,
+# declare one that we can instantiate for the default argument to the
+# constructor for AbiGenDummy below.
+try:
+    # both mypy and pylint complain about what we're doing here, but this
+    # works just fine, so their messages have been disabled here.
+    from . import (  # type: ignore # pylint: disable=import-self
+        AbiGenDummyValidator,
+    )
+except ImportError:
+
+    class AbiGenDummyValidator(AbiGenDummyValidatorBase):  # type: ignore
+        """No-op input validator."""
 
 
 class Tuple0xc9bdd2d5(TypedDict):
@@ -76,6 +114,7 @@ class AbiGenDummy(BaseContractWrapper):
         self,
         provider: BaseProvider,
         contract_address: str,
+        validator: AbiGenDummyValidator = None,
         private_key: str = None,
     ):
         """Get an instance of wrapper for smart contract.
@@ -91,6 +130,11 @@ class AbiGenDummy(BaseContractWrapper):
             contract_address=contract_address,
             private_key=private_key,
         )
+
+        if not validator:
+            validator = AbiGenDummyValidator(provider, contract_address, private_key)
+
+        self.validator = validator
 
     def _get_contract_instance(self, token_address):
         """Get an instance of the smart contract at a specific address.
@@ -130,6 +174,11 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='acceptsAnArrayOfBytes',
+            parameter_name='a',
+            argument_value=a,
+        )
         a = [
             bytes.fromhex(a_element.decode("utf-8"))
             for a_element in a
@@ -158,6 +207,26 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='ecrecoverFn',
+            parameter_name='hash',
+            argument_value=_hash,
+        )
+        self.validator.assert_valid(
+            method_name='ecrecoverFn',
+            parameter_name='v',
+            argument_value=v,
+        )
+        self.validator.assert_valid(
+            method_name='ecrecoverFn',
+            parameter_name='r',
+            argument_value=r,
+        )
+        self.validator.assert_valid(
+            method_name='ecrecoverFn',
+            parameter_name='s',
+            argument_value=s,
+        )
         func = self._get_contract_instance(
             self.contract_address
         ).functions.ecrecoverFn(
@@ -182,6 +251,11 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='acceptsBytes',
+            parameter_name='a',
+            argument_value=a,
+        )
         a = bytes.fromhex(a.decode("utf-8"))
         func = self._get_contract_instance(
             self.contract_address
@@ -280,6 +354,11 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='structInput',
+            parameter_name='s',
+            argument_value=s,
+        )
         func = self._get_contract_instance(
             self.contract_address
         ).functions.structInput(
@@ -301,6 +380,11 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='simplePureFunctionWithInput',
+            parameter_name='x',
+            argument_value=x,
+        )
         # safeguard against fractional inputs
         x = int(x)
         func = self._get_contract_instance(
@@ -343,6 +427,11 @@ class AbiGenDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='nestedStructInput',
+            parameter_name='n',
+            argument_value=n,
+        )
         func = self._get_contract_instance(
             self.contract_address
         ).functions.nestedStructInput(
--- packages/abi-gen/test-cli/expected-output/python/test_lib_dummy/__init__.py
+++ packages/abi-gen/test-cli/expected-output/python/test_lib_dummy/__init__.py
@@ -4,6 +4,7 @@
 
 import json
 from typing import (  # pylint: disable=unused-import
+    Any,
     List,
     Optional,
     Tuple,
@@ -19,6 +20,43 @@ from zero_ex.contract_wrappers._base_contract_wrapper import BaseContractWrapper
 from zero_ex.contract_wrappers.tx_params import TxParams
 
 
+class TestLibDummyValidatorBase:
+    """Base class for validating inputs to TestLibDummy methods."""
+    def __init__(
+        self,
+        provider: BaseProvider,
+        contract_address: str,
+        private_key: str = None,
+    ):
+        """Initialize the instance."""
+
+    def assert_valid(
+        self, method_name: str, parameter_name: str, argument_value: Any
+    ):
+        """Raise an exception if method input is not valid.
+
+        :param method_name: Name of the method whose input is to be validated.
+        :param parameter_name: Name of the parameter whose input is to be
+            validated.
+        :param argument_value: Value of argument to parameter to be validated.
+        """
+
+
+# Try to import a custom validator class definition; if there isn't one,
+# declare one that we can instantiate for the default argument to the
+# constructor for TestLibDummy below.
+try:
+    # both mypy and pylint complain about what we're doing here, but this
+    # works just fine, so their messages have been disabled here.
+    from . import (  # type: ignore # pylint: disable=import-self
+        TestLibDummyValidator,
+    )
+except ImportError:
+
+    class TestLibDummyValidator(TestLibDummyValidatorBase):  # type: ignore
+        """No-op input validator."""
+
+
 
 
 
@@ -30,6 +68,7 @@ class TestLibDummy(BaseContractWrapper):
         self,
         provider: BaseProvider,
         contract_address: str,
+        validator: TestLibDummyValidator = None,
         private_key: str = None,
     ):
         """Get an instance of wrapper for smart contract.
@@ -45,6 +84,11 @@ class TestLibDummy(BaseContractWrapper):
             contract_address=contract_address,
             private_key=private_key,
         )
+
+        if not validator:
+            validator = TestLibDummyValidator(provider, contract_address, private_key)
+
+        self.validator = validator
 
     def _get_contract_instance(self, token_address):
         """Get an instance of the smart contract at a specific address.
@@ -65,6 +109,11 @@ class TestLibDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='publicAddConstant',
+            parameter_name='x',
+            argument_value=x,
+        )
         # safeguard against fractional inputs
         x = int(x)
         func = self._get_contract_instance(
@@ -88,6 +137,11 @@ class TestLibDummy(BaseContractWrapper):
         :param tx_params: transaction parameters
 
         """
+        self.validator.assert_valid(
+            method_name='publicAddOne',
+            parameter_name='x',
+            argument_value=x,
+        )
         # safeguard against fractional inputs
         x = int(x)
         func = self._get_contract_instance(
