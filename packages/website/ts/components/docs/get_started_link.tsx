33320fd7586384359784c007ade6d2babb6e6fb4
Piotr Janosz <janosz128@gmail.com>
2019-07-16 23:29:09 +0200

    Extracted get started links from home

---
+++ packages/website/ts/components/docs/get_started_link.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Button } from 'ts/components/button';
+import { Paragraph } from 'ts/components/text';
+
+import { colors } from 'ts/style/colors';
+
+export interface IGetStartedLinkProps {
+    heading: string;
+    description: string;
+    url: string;
+}
+
+export const GetStartedLink: React.FC<IGetStartedLinkProps> = props => (
+    <>
+        <GetStartedButton color={colors.brandDark} href={props.url} isWithArrow={true}>
+            {props.heading}
+        </GetStartedButton>
+        <GetStartedParagraph color={colors.textDarkPrimary} isMuted={1}>
+            {props.description}
+        </GetStartedParagraph>
+    </>
+);
+
+const GetStartedButton = styled(Button)`
+    margin-bottom: 12px;
+
+    @media (max-width: 500px) {
+        font-size: 16px !important;
+
+        svg {
+            display: none;
+        }
+    }
+`;
+
+const GetStartedParagraph = styled(Paragraph)`
+    @media (max-width: 500px) {
+        font-size: 14px;
+    }
+`;
