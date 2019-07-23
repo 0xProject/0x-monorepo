984305d483f73d63f8988e535121946e60ca79f9
Piotr Janosz <janosz128@gmail.com>
2019-07-23 16:46:46 +0200

    Small cleanup for docs

--- packages/website/ts/pages/docs/guides.tsx
+++ packages/website/ts/pages/docs/guides.tsx
@@ -10,49 +10,26 @@ import { Section } from 'ts/components/newLayout';
 
 import { documentConstants } from 'ts/utils/document_meta_constants';
 
-import { ClearRefinements, Hits, InstantSearch } from 'react-instantsearch-dom';
+import { Hits, InstantSearch } from 'react-instantsearch-dom';
 
 import algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch('39X6WOJZKW', '6acba761a34d99781628c6178af1e16c');
 
-const resources = [
-    {
-        heading: '0x Mesh - your gateway to networked liquidity',
-        description:
-            'Learn about the 0x peer-to-peer network for sharing orders and how you can use it to tap into networked liquidity.',
-        tags: ['Relayer'],
-        url: 'https://0x.org',
-        isCommunity: true,
-    },
-    {
-        heading: '0x Mesh - your gateway to networked liquidity',
-        description:
-            'The Radar Relay SDK is a software development kit that simplifies the interactions with Radar Relay’s APIs',
-        tags: ['Api explorer', 'Relayer'],
-        url: 'https://0x.org',
-    },
-];
-
-export const DocsGuides: React.FC = () => {
-    return (
-        <SiteWrap theme="light">
-            <DocumentTitle {...documentConstants.DOCS} />
-            <Hero title="Guides" />
-            <Section maxWidth={'1030px'} isPadded={false} padding="0 0">
-                <InstantSearch searchClient={searchClient} indexName="0x_guides_test">
-                    <ClearRefinements />
-                    <Columns>
-                        <Filters filters={filters} />
-                        <article>
-                            <Hits hitComponent={Resource} />
-                        </article>
-                    </Columns>
-                </InstantSearch>
-            </Section>
-        </SiteWrap>
-    );
-};
+export const DocsGuides: React.FC = () => (
+    <SiteWrap theme="light">
+        <DocumentTitle {...documentConstants.DOCS} />
+        <Hero title="Guides" />
+        <Section maxWidth="1030px" isPadded={false}>
+            <InstantSearch searchClient={searchClient} indexName="0x_guides_test">
+                <Columns>
+                    <Filters filters={filters} />
+                    <Hits hitComponent={Resource} />
+                </Columns>
+            </InstantSearch>
+        </Section>
+    </SiteWrap>
+);
 
 const Columns = styled.div`
     display: grid;
