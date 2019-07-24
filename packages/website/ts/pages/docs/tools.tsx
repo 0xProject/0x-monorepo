3407e09a66936f5e9ee2de60f6a9bc009ae04767
Piotr Janosz <janosz128@gmail.com>
2019-07-24 14:07:49 +0200

    Created a reusable page wrapper for docs pages

--- packages/website/ts/pages/docs/tools.tsx
+++ packages/website/ts/pages/docs/tools.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { FeatureLink } from 'ts/components/docs/feature_link';
-import { Hero } from 'ts/components/docs/hero';
+import { DocsPageLayout } from 'ts/components/docs/layout/docs_page_layout';
 import { Resource } from 'ts/components/docs/resource/resource';
 import { Filters } from 'ts/components/docs/sidebar/filters';
-import { SiteWrap } from 'ts/components/docs/siteWrap';
-import { DocumentTitle } from 'ts/components/document_title';
-import { Section } from 'ts/components/newLayout';
 import { Heading } from 'ts/components/text';
-
-import { documentConstants } from 'ts/utils/document_meta_constants';
 
 import { Hits, InstantSearch } from 'react-instantsearch-dom';
 
@@ -20,17 +15,14 @@ const searchClient = algoliasearch('39X6WOJZKW', '6acba761a34d99781628c6178af1e1
 
 export const DocsTools: React.FC = () => {
     return (
-        <SiteWrap theme="light">
-            <DocumentTitle {...documentConstants.DOCS} />
-            <Hero title="Tools" />
-            <Section maxWidth="1030px" isPadded={false}>
-                <InstantSearch searchClient={searchClient} indexName="0x_tools_test">
-                    <Columns>
-                        <Filters filters={filters} />
-                        <article>
-                            <Hits hitComponent={Resource} />
+        <DocsPageLayout title="Tools">
+            <InstantSearch searchClient={searchClient} indexName="0x_tools_test">
+                <Columns>
+                    <Filters filters={filters} />
+                    <article>
+                        <Hits hitComponent={Resource} />
 
-                            {/* <FeaturedToolsWrapper>
+                        {/* <FeaturedToolsWrapper>
                                 <Heading asElement="h2" size="default">
                                     Featured Tools
                                 </Heading>
@@ -45,7 +37,7 @@ export const DocsTools: React.FC = () => {
                                 ))}
                             </FeaturedToolsWrapper> */}
 
-                            {/* <ResourcesWrapper>
+                        {/* <ResourcesWrapper>
                                 <Heading asElement="h2" size="default">
                                     Docker Images
                                 </Heading>
@@ -54,7 +46,7 @@ export const DocsTools: React.FC = () => {
                                     <Resource key={`resource-${index}`} {...resource} />
                                 ))}
                             </ResourcesWrapper> */}
-                            {/* 
+                        {/* 
                             <ResourcesWrapper>
                                 <Heading asElement="h2" size="default">
                                     TypeScript Libraries
@@ -64,11 +56,10 @@ export const DocsTools: React.FC = () => {
                                     <Resource key={`resource-${index}`} {...resource} />
                                 ))}
                             </ResourcesWrapper> */}
-                        </article>
-                    </Columns>
-                </InstantSearch>
-            </Section>
-        </SiteWrap>
+                    </article>
+                </Columns>
+            </InstantSearch>
+        </DocsPageLayout>
     );
 };
 
@@ -109,24 +100,6 @@ const featuredLinks = [
         heading: '0x Code Sandbox',
         description: 'A description could possibly go here but could be tight.',
         icon: 'flexibleIntegration',
-        url: 'https://0x.org',
-    },
-];
-
-const resources = [
-    {
-        heading: '0x Mesh - your gateway to networked liquidity',
-        description:
-            'Learn about the 0x peer-to-peer network for sharing orders and how you can use it to tap into networked liquidity.',
-        tags: ['Relayer', 'Dogs', 'Bells and whistles', 'Interstellar', 'Maharaji'],
-        url: 'https://0x.org',
-        isCommunity: true,
-    },
-    {
-        heading: '0x Mesh - your gateway to networked liquidity',
-        description:
-            'The Radar Relay SDK is a software development kit that simplifies the interactions with Radar Relay’s APIs',
-        tags: ['Api explorer', 'Relayer'],
         url: 'https://0x.org',
     },
 ];
