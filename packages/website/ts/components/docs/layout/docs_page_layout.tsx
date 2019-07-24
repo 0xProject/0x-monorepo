5f11ce51d8a5037c0935a20c4adbbe582341c2c1
Piotr Janosz <janosz128@gmail.com>
2019-07-24 19:26:52 +0200

    Added scroll to top arrow for docs pages

--- packages/website/ts/components/docs/layout/docs_page_layout.tsx
+++ packages/website/ts/components/docs/layout/docs_page_layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import styled from 'styled-components';
 
 import CircularProgress from 'material-ui/CircularProgress';
 
 import { Hero } from 'ts/components/docs/hero';
+import { ScrollTopArrow } from 'ts/components/docs/layout/scroll_top_arrow';
 import { SiteWrap } from 'ts/components/docs/siteWrap';
 import { DocumentTitle } from 'ts/components/document_title';
 import { Section } from 'ts/components/newLayout';
@@ -21,19 +23,25 @@ interface IDocsPageLayoutProps {
 }
 
 const SECTION_MIN_HEIGHT = '50vh';
+const SECTION_WIDTH = '1150px';
 
 export const DocsPageLayout: React.FC<IDocsPageLayoutProps> = props => {
     return (
         <SiteWrap theme="light">
             <DocumentTitle {...documentConstants.DOCS} />
             <Hero title={props.title} subtitle={props.subtitle} isHome={props.isHome} />
-            <Section maxWidth="1150px" minHeight={SECTION_MIN_HEIGHT} isPadded={false} overflow="visible">
+            <Section maxWidth={SECTION_WIDTH} minHeight={SECTION_MIN_HEIGHT} isPadded={false} overflow="visible">
                 {props.loading ? (
                     <LoaderWrapper>
                         <CircularProgress size={40} thickness={2} color={colors.brandLight} />
                     </LoaderWrapper>
                 ) : (
                     props.children
+                )}
+                {!props.isHome && (
+                    <MediaQuery maxWidth={900}>
+                        <ScrollTopArrow />
+                    </MediaQuery>
                 )}
             </Section>
         </SiteWrap>
