44113b89c62d6f9e27b908792910f41c70c47091
Piotr Janosz <janosz128@gmail.com>
2019-07-26 15:52:23 +0200

    Finished work on the collapsible sidebar for filters / table of contents in docs

---
+++ packages/website/ts/components/docs/sidebar/collapse.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { colors } from 'ts/style/colors';
+import { useDimensions } from 'ts/style/dimensions';
+
+interface ICollapseProps {
+    children: React.ReactNode;
+    title?: string;
+}
+
+interface ICollapse {
+    isActive: boolean;
+    height?: number;
+}
+
+export const Collapse: React.FC<ICollapseProps> = props => {
+    const [isActive, setIsActive] = useState<boolean>(true);
+    // @ts-ignore
+    const [contentRef, { height }] = useDimensions();
+
+    const toggleCollapse = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <>
+            <CollapseToggle isActive={isActive} onClick={toggleCollapse}>
+                <p>On this page</p>
+                <svg width="12" height="8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 7l5-5 5 5" stroke={colors.brandDark} strokeWidth="1.5" />
+                </svg>
+            </CollapseToggle>
+            <CollapseWrapper height={height} isActive={isActive}>
+                <CollapseContent isActive={isActive}>
+                    <div ref={contentRef}>{props.children}</div>
+                </CollapseContent>
+            </CollapseWrapper>
+        </>
+    );
+};
+
+const CollapseWrapper = styled.div<ICollapse>`
+    height: ${props => (props.isActive ? props.height : 0)}px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const CollapseToggle = styled.button<ICollapse>`
+    display: flex;
+    align-items: center;
+    border: none;
+    background: none;
+    cursor: pointer;
+    color: ${colors.brandDark};
+    padding: 0;
+    font-size: 0.89rem;
+    margin-bottom: ${props => (props.isActive ? 1 : 0)}rem;
+
+    svg {
+        margin-left: 10px;
+        transform: rotate(${props => (props.isActive ? 0 : 180)}deg);
+        transition: transform 300ms ease-out;
+    }
+`;
+
+const CollapseContent = styled.div<ICollapse>`
+    overflow: hidden;
+    transition: flex 300ms ease-out;
+    flex: ${props => (props.isActive ? 1 : 0)};
+`;
--- packages/website/ts/components/docs/sidebar/sidebar_wrapper.tsx
+++ packages/website/ts/components/docs/sidebar/sidebar_wrapper.tsx
@@ -1,69 +1,32 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import MediaQuery from 'react-responsive';
 import styled from 'styled-components';
 
-import { colors } from 'ts/style/colors';
-import { useDimensions } from 'ts/style/dimensions';
+import { Collapse } from 'ts/components/docs/sidebar/collapse';
 
-interface IAccordionProps {
+interface ISidebarWrapperProps {
     children: React.ReactNode;
-    title?: string;
 }
 
-export const Accordion: React.FC<IAccordionProps> = props => {
-    const [isActive, setIsActive] = useState<boolean>(false);
-    // @ts-ignore
-    const [contentRef, { height }] = useDimensions();
-
-    const toggleAccordion = () => {
-        setIsActive(!isActive);
-    };
-
+export const SidebarWrapper: React.FC<ISidebarWrapperProps> = ({ children }) => {
     return (
-        <AccordionWrapper height={height}>
-            <AccordionToggle isActive={isActive} onClick={toggleAccordion}>
-                <p>On this page</p>
-                <svg width="12" height="8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 7l5-5 5 5" stroke={colors.brandDark} strokeWidth="1.5" />
-                </svg>
-            </AccordionToggle>
-            <AccordionContent height={height} isActive={isActive}>
-                <div ref={contentRef}>{props.children}</div>
-            </AccordionContent>
-        </AccordionWrapper>
+        <SidebarAside>
+            <SidebarContent>
+                <MediaQuery maxWidth={900}>
+                    <Collapse>{children}</Collapse>
+                </MediaQuery>
+
+                <MediaQuery minWidth={901}>{children}</MediaQuery>
+            </SidebarContent>
+        </SidebarAside>
     );
 };
 
-const AccordionWrapper = styled.div<{ height: number }>`
-    height: ${props => props.height}px;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2.22em;
-
-    @media (max-width: 900px) {
-        margin-bottom: 30px;
-    }
+const SidebarAside = styled.aside`
+    position: relative;
 `;
 
-const AccordionToggle = styled.button<{ isActive: boolean }>`
-    display: flex;
-    align-items: center;
-    border: none;
-    background: none;
-    color: ${colors.brandDark};
-    padding: 0;
-    font-size: 0.89rem;
-    margin-bottom: 1rem;
-
-    svg {
-        margin-left: 10px;
-        transform: rotate(${props => (props.isActive ? 0 : 180)}deg);
-        transition: transform 300ms ease-out;
-    }
-`;
-
-const AccordionContent = styled.div<{ isActive: boolean; height: number }>`
-    overflow: hidden;
-    height: auto;
-    transition: flex 300ms ease-out;
-    flex: ${props => (props.isActive ? 1 : 0)};
+const SidebarContent = styled.div`
+    position: sticky;
+    top: 116px; /* To make space for the header (react-headroom) when clicking on links */
 `;
