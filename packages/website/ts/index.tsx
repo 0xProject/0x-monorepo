6cf9fb094964403ffe5667b257de5b41eeb6f95b
Piotr Janosz <janosz128@gmail.com>
2019-07-28 23:10:16 +0200

    [WIP] porting indexing to typescript, ts-node

--- packages/website/ts/index.tsx
+++ packages/website/ts/index.tsx
@@ -220,11 +220,10 @@ render(
                                     path={`${WebsitePaths.AssetSwapper}/:version?`}
                                     component={LazyAssetSwapperDocumentation}
                                 />
+                                <Route path={`${WebsitePaths.Docs}/:type/:page`} component={DocsView as any} />
                                 <Route exact={true} path={WebsitePaths.DocsGuides} component={DocsGuides as any} />
                                 <Route exact={true} path={WebsitePaths.DocsTools} component={DocsTools as any} />
-                                <Route path={`${WebsitePaths.DocsGuides}/:page`} component={DocsView as any} />
-                                <Route path={`${WebsitePaths.DocsTools}/:page`} component={DocsView as any} />
-                                <Route path={WebsitePaths.Docs} component={DocsHome as any} />
+                                <Route exact={true} path={WebsitePaths.Docs} component={DocsHome as any} />
                                 {/* Legacy endpoints */}
                                 <Route
                                     path={`${WebsiteLegacyPaths.ZeroExJs}/:version?`}
