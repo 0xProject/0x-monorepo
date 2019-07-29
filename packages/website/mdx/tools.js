71e3430f5f46811c7bbd104a33886267d5099af2
Piotr Janosz <janosz128@gmail.com>
2019-07-29 14:55:30 +0200

    Further polish of algolia indexing

--- packages/website/mdx/tools.js
+++ packages/website/mdx/tools.js
@@ -1,13 +1,8 @@
-const { adminClient, sharedSettings } = require('./constants');
-const { indexFiles, setIndexSettings } = require('./helpers');
+import { adminClient, searchIndex, settings } from '../ts/utils/algolia_search';
+import { indexFilesAsync, setIndexSettings } from './helpers';
 
-const index = adminClient.initIndex('0x_tools_test');
-const dirName = 'tools';
+const indexName = 'tools';
+const index = adminClient.initIndex(searchIndex[indexName]);
 
-const settings = {
-    ...sharedSettings,
-    attributesForFaceting: ['type', 'tags', 'difficulty', 'isCommunity'],
-};
-
-setIndexSettings(index, settings);
-indexFiles(index, dirName);
+setIndexSettings(index, settings[indexName]);
+indexFilesAsync(index, indexName);
