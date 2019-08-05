5c128a4aacfd82b47e1fce2b87d10d50e4979264
Alex Towle <jalextowle@gmail.com>
2019-08-04 22:37:32 -0700

    Update glabal-hooks so that sol-coverage and sol-profiler works again

--- contracts/exchange/test/global_hooks.ts
+++ contracts/exchange/test/global_hooks.ts
@@ -1,18 +1,28 @@
-import { env, EnvVars } from '@0x/dev-utils';
-
 import { coverage, profiler, provider } from '@0x/contracts-test-utils';
+import { env, EnvVars } from '@0x/dev-utils';
+import { prependSubprovider } from '@0x/subproviders';
 import { providerUtils } from '@0x/utils';
+
+const coverageSubprovider = coverage.getCoverageSubproviderSingleton();
+const profilerSubprovider = profiler.getProfilerSubproviderSingleton();
+
+if (env.parseBoolean(EnvVars.SolidityCoverage)) {
+    prependSubprovider(provider, coverageSubprovider);
+    provider.stop();
+}
+if (env.parseBoolean(EnvVars.SolidityProfiler)) {
+    prependSubprovider(provider, profilerSubprovider);
+    provider.stop();
+}
 
 before('start web3 provider', () => {
     providerUtils.startProviderEngine(provider);
 });
 after('generate coverage report', async () => {
     if (env.parseBoolean(EnvVars.SolidityCoverage)) {
-        const coverageSubprovider = coverage.getCoverageSubproviderSingleton();
         await coverageSubprovider.writeCoverageAsync();
     }
     if (env.parseBoolean(EnvVars.SolidityProfiler)) {
-        const profilerSubprovider = profiler.getProfilerSubproviderSingleton();
         await profilerSubprovider.writeProfilerOutputAsync();
     }
     provider.stop();
