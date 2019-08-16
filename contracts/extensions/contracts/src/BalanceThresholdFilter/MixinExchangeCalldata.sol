c366a4bd83bbda617877021b08df4bc14eef517d
Amir Bandeali <abandeali1@gmail.com>
2019-08-16 14:00:09 -0700

    Update copyright dates

--- contracts/extensions/contracts/src/BalanceThresholdFilter/MixinExchangeCalldata.sol
+++ contracts/extensions/contracts/src/BalanceThresholdFilter/MixinExchangeCalldata.sol
@@ -1,7 +1,7 @@
     
     /*
 
-  Copyright 2018 ZeroEx Intl.
+  Copyright 2019 ZeroEx Intl.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
