635b80440a07d4beb450d7a9b736e722274329e8
Piotr Janosz <janosz128@gmail.com>
2019-08-20 18:01:27 +0200

    Moved sorting filters to filter group to sort difficulty filters in a custom wat

--- packages/website/ts/components/docs/sidebar/filters.tsx
+++ packages/website/ts/components/docs/sidebar/filters.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import _ from 'lodash';
-
 import { FiltersClear } from 'ts/components/docs/sidebar/filters_clear';
 import { FiltersGroup } from 'ts/components/docs/sidebar/filters_group';
 import { SidebarWrapper } from 'ts/components/docs/sidebar/sidebar_wrapper';
@@ -13,16 +11,15 @@ interface IFiltersGroupProps {
     attribute: string;
     heading: string;
     customLabel?: string;
-    operator?: string;
 }
 
-const transformItems = (items: IFiltersGroupProps[]) => _.orderBy(items, 'label', 'asc');
-
-export const Filters: React.FC<IFiltersProps> = ({ filters }) => (
-    <SidebarWrapper>
-        {filters.map((filter: IFiltersGroupProps, index: number) => (
-            <FiltersGroup key={`filter-${index}`} operator="and" transformItems={transformItems} {...filter} />
-        ))}
-        <FiltersClear />
-    </SidebarWrapper>
-);
+export const Filters: React.FC<IFiltersProps> = ({ filters }) => {
+    return (
+        <SidebarWrapper>
+            {filters.map((filter: IFiltersGroupProps, index: number) => (
+                <FiltersGroup key={`filter-${index}`} operator="and" {...filter} />
+            ))}
+            <FiltersClear />
+        </SidebarWrapper>
+    );
+};
