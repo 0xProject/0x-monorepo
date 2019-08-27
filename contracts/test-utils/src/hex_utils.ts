0999805b3ae773fb569bbb95fd087f26d895c65b
Lawrence Forman <lawrence@0xproject.com>
2019-08-27 16:52:52 -0400

    `@0x/contracts-test-utils`: Add `toHex()`, `hexInvert()`, `hexLeftPad()`, and `hexRightPad()` hex utils.

--- contracts/test-utils/src/hex_utils.ts
+++ contracts/test-utils/src/hex_utils.ts
@@ -1,5 +1,10 @@
+import { BigNumber } from '@0x/utils';
 import * as crypto from 'crypto';
 import * as ethUtil from 'ethereumjs-util';
+
+import { constants } from './constants';
+
+const { WORD_LENGTH } = constants;
 
 /**
  * Concatenate all arguments as a hex string.
@@ -7,9 +12,52 @@ import * as ethUtil from 'ethereumjs-util';
 export function hexConcat(...args: Array<string | number | Buffer>): string {
     return ethUtil.bufferToHex(Buffer.concat(args.map(h => ethUtil.toBuffer(h))));
 }
+
 /**
  * Generate a random hex string.
  */
-export function hexRandom(size: number = 32): string {
+export function hexRandom(size: number = WORD_LENGTH): string {
     return ethUtil.bufferToHex(crypto.randomBytes(size));
+}
+
+/**
+ * Left-pad a hex number to a number of bytes.
+ */
+export function hexLeftPad(n: string | BigNumber | number, size: number = WORD_LENGTH): string {
+    return ethUtil.bufferToHex(ethUtil.setLengthLeft(toHex(n), size));
+}
+
+/**
+ * Right-pad a hex number to a number of bytes.
+ */
+export function hexRightPad(n: string | BigNumber | number, size: number = WORD_LENGTH): string {
+    return ethUtil.bufferToHex(ethUtil.setLengthRight(toHex(n), size));
+}
+
+/**
+ * Inverts a hex word.
+ */
+export function hexInvert(n: string | BigNumber | number, size: number = WORD_LENGTH): string {
+    const buf = ethUtil.setLengthLeft(toHex(n), size);
+    // tslint:disable-next-line: no-bitwise
+    return ethUtil.bufferToHex(Buffer.from(buf.map(b => ~b)));
+}
+
+/**
+ * Convert a string, a number, or a BigNumber into a hex string.
+ */
+export function toHex(n: string | BigNumber | number): string {
+    let hex = '0x00';
+    if (typeof(n) === 'number') {
+        hex = `0x${n.toString(16)}`;
+    } else if (BigNumber.isBigNumber(n)) {
+        hex = `0x${n.toString(16)}`;
+    } else {
+        if (/^0x/.test(n)) {
+            hex = n;
+        } else {
+            hex = `0x${parseInt(n, 10).toString(16)}`;
+        }
+    }
+    return hex;
 }
