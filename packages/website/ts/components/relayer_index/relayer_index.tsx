2a09f9c84a6f0ea210faa677124b1198cde41508
Piotr Janosz <janosz128@gmail.com>
2019-08-27 17:13:36 +0200

    Refactored deprecated componentWillMount in relayer_index to hooks / dropped the class

--- packages/website/ts/components/relayer_index/relayer_index.tsx
+++ packages/website/ts/components/relayer_index/relayer_index.tsx
@@ -31,96 +31,83 @@ const NUMBER_OF_COLUMNS_MEDIUM = 2;
 const NUMBER_OF_COLUMNS_SMALL = 2;
 const GRID_PADDING = 20;
 
-export class RelayerIndex extends React.Component<RelayerIndexProps, RelayerIndexState> {
-    private _isUnmounted: boolean;
-    constructor(props: RelayerIndexProps) {
-        super(props);
-        this._isUnmounted = false;
-        this.state = {
-            relayerInfos: undefined,
-            error: undefined,
-        };
-    }
-    public componentWillMount(): void {
+export const RelayerIndex: React.FC<RelayerIndexProps> = props => {
+    const [state, setState] = React.useState<RelayerIndexState>({ error: undefined, relayerInfos: undefined });
+    const isUnmounted = React.useRef(false);
+
+    React.useEffect(() => {
         // tslint:disable-next-line:no-floating-promises
-        this._fetchRelayerInfosAsync();
-    }
-    public componentWillUnmount(): void {
-        this._isUnmounted = true;
-    }
-    public render(): React.ReactNode {
-        const isReadyToRender = this.state.error === undefined && this.state.relayerInfos !== undefined;
-        if (!isReadyToRender) {
-            return (
-                // TODO: consolidate this loading component with the one in portal and OpenPositions
-                // TODO: possibly refactor into a generic loading container with spinner and retry UI
-                <div className="center">
-                    {this.state.error === undefined ? (
-                        <CircularProgress size={40} thickness={5} />
-                    ) : (
-                        <Retry onRetry={this._fetchRelayerInfosAsync.bind(this)} />
-                    )}
-                </div>
-            );
-        } else {
-            const numberOfRelayers = this.state.relayerInfos.length;
-            const numberOfColumns = Math.min(
-                numberOfRelayers,
-                this._numberOfColumnsForScreenWidth(this.props.screenWidth),
-            );
-            const isExpanded = this.props.cellStyle === RelayerIndexCellStyle.Expanded;
-            const cellHeight = isExpanded ? CELL_HEIGHT_EXPANDED : CELL_HEIGHT_MINIMIZED;
-            const gridTileStyle = isExpanded ? RelayerGridTileStyle.Expanded : RelayerGridTileStyle.Minimized;
-            return (
-                <GridList
-                    cellHeight={cellHeight}
-                    cols={numberOfColumns}
-                    padding={GRID_PADDING}
-                    style={{ marginTop: -10, marginBottom: 0 }}
-                >
-                    {this.state.relayerInfos.map((relayerInfo: WebsiteBackendRelayerInfo, index) => (
-                        <RelayerGridTile
-                            key={index}
-                            relayerInfo={relayerInfo}
-                            networkId={this.props.networkId}
-                            style={gridTileStyle}
-                        />
-                    ))}
-                </GridList>
-            );
-        }
-    }
-    private async _fetchRelayerInfosAsync(): Promise<void> {
+        fetchRelayerInfosAsync();
+
+        return () => (isUnmounted.current = true);
+    }, []);
+
+    const fetchRelayerInfosAsync = async (): Promise<void> => {
         try {
-            if (!this._isUnmounted) {
-                this.setState({
-                    relayerInfos: undefined,
-                    error: undefined,
-                });
+            if (!isUnmounted.current) {
+                setState({ relayerInfos: undefined, error: undefined });
             }
+
             const relayerInfos = await backendClient.getRelayerInfosAsync();
-            if (!this._isUnmounted) {
-                this.setState({
-                    relayerInfos,
-                });
+
+            if (!isUnmounted.current) {
+                setState({ ...state, relayerInfos });
             }
         } catch (error) {
-            if (!this._isUnmounted) {
-                this.setState({
-                    error,
-                });
+            if (!isUnmounted.current) {
+                setState({ ...state, error });
             }
         }
+    };
+
+    const isReadyToRender = state.error === undefined && state.relayerInfos !== undefined;
+
+    if (!isReadyToRender) {
+        return (
+            // TODO: consolidate this loading component with the one in portal and OpenPositions
+            // TODO: possibly refactor into a generic loading container with spinner and retry UI
+            <div className="center">
+                {state.error === undefined ? (
+                    <CircularProgress size={40} thickness={5} />
+                ) : (
+                    <Retry onRetry={fetchRelayerInfosAsync} />
+                )}
+            </div>
+        );
+    } else {
+        const numberOfRelayers = state.relayerInfos.length;
+        const numberOfColumns = Math.min(numberOfRelayers, numberOfColumnsForScreenWidth(props.screenWidth));
+        const isExpanded = props.cellStyle === RelayerIndexCellStyle.Expanded;
+        const cellHeight = isExpanded ? CELL_HEIGHT_EXPANDED : CELL_HEIGHT_MINIMIZED;
+        const gridTileStyle = isExpanded ? RelayerGridTileStyle.Expanded : RelayerGridTileStyle.Minimized;
+        return (
+            <GridList
+                cellHeight={cellHeight}
+                cols={numberOfColumns}
+                padding={GRID_PADDING}
+                style={{ marginTop: -10, marginBottom: 0 }}
+            >
+                {state.relayerInfos.map((relayerInfo: WebsiteBackendRelayerInfo, index) => (
+                    <RelayerGridTile
+                        key={index}
+                        relayerInfo={relayerInfo}
+                        networkId={props.networkId}
+                        style={gridTileStyle}
+                    />
+                ))}
+            </GridList>
+        );
     }
-    private _numberOfColumnsForScreenWidth(screenWidth: ScreenWidths): number {
-        switch (screenWidth) {
-            case ScreenWidths.Md:
-                return NUMBER_OF_COLUMNS_MEDIUM;
-            case ScreenWidths.Sm:
-                return NUMBER_OF_COLUMNS_SMALL;
-            case ScreenWidths.Lg:
-            default:
-                return NUMBER_OF_COLUMNS_LARGE;
-        }
+};
+
+function numberOfColumnsForScreenWidth(screenWidth: ScreenWidths): number {
+    switch (screenWidth) {
+        case ScreenWidths.Md:
+            return NUMBER_OF_COLUMNS_MEDIUM;
+        case ScreenWidths.Sm:
+            return NUMBER_OF_COLUMNS_SMALL;
+        case ScreenWidths.Lg:
+        default:
+            return NUMBER_OF_COLUMNS_LARGE;
     }
 }
