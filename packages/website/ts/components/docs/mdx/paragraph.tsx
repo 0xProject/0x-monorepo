775a41a789dd5e78c817bf5b51b46bc5c3511c24
Piotr Janosz <janosz128@gmail.com>
2019-08-29 23:04:33 +0200

    Changed line-height to 1.6 for paragraph

--- packages/website/ts/components/docs/mdx/paragraph.tsx
+++ packages/website/ts/components/docs/mdx/paragraph.tsx
@@ -6,7 +6,7 @@ export const Paragraph = styled.p`
     color: ${docs.textColor};
     font-size: ${docs.fontSize.desktop};
     font-weight: 300;
-    line-height: 1.4;
+    line-height: 1.6;
     margin-bottom: 30px;
 
     @media (max-width: 900px) {
