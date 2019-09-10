8a9c184b3477a131da6d0221bae2601fd245007b
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-09-10 14:46:17 -0400

    Update __init__.py

--- python-packages/contract_addresses/src/zero_ex/contract_addresses/__init__.py
+++ python-packages/contract_addresses/src/zero_ex/contract_addresses/__init__.py
@@ -83,7 +83,7 @@ NETWORK_TO_ADDRESSES: Dict[NetworkId, ContractAddresses] = {
         coordinator_registry="0x45797531b873fd5e519477a070a955764c1a5b07",
         coordinator="0xa14857e8930acd9a882d33ec20559beb5479c8a6",
         erc1155_proxy="0x7eefbd48fd63d441ec7435d024ec7c5131019add",
-        dev_utils: '0x92d9a4d50190ae04e03914db2ee650124af844e6',
+        dev_utils="0x92d9a4d50190ae04e03914db2ee650124af844e6",
     ),
     NetworkId.ROPSTEN: ContractAddresses(  # nosec
         erc20_proxy="0xb1408f4c245a23c31b98d2c626777d4c0d766caa",
@@ -97,7 +97,7 @@ NETWORK_TO_ADDRESSES: Dict[NetworkId, ContractAddresses] = {
         coordinator_registry="0x403cc23e88c17c4652fb904784d1af640a6722d9",
         coordinator="0x2ba02e03ee0029311e0f43715307870a3e701b53",
         erc1155_proxy="0x19bb6caa3bc34d39e5a23cedfa3e6c7e7f3c931d",
-        dev_utils: '0x3e0b46bad8e374e4a110c12b832cb120dbe4a479',
+        dev_utils="0x3e0b46bad8e374e4a110c12b832cb120dbe4a479",
     ),
     NetworkId.RINKEBY: ContractAddresses(  # nosec
         exchange="0xbff9493f92a3df4b0429b6d00743b3cfb4c85831",
@@ -111,7 +111,7 @@ NETWORK_TO_ADDRESSES: Dict[NetworkId, ContractAddresses] = {
         coordinator_registry="0x1084b6a398e47907bae43fec3ff4b677db6e4fee",
         coordinator="0x2ba02e03ee0029311e0f43715307870a3e701b53",
         erc1155_proxy="0x19bb6caa3bc34d39e5a23cedfa3e6c7e7f3c931d",
-        dev_utils: '0x2d4a9abda7b8b3605c8dbd34e3550a7467c78287',
+        dev_utils="0x2d4a9abda7b8b3605c8dbd34e3550a7467c78287'",
     ),
     NetworkId.KOVAN: ContractAddresses(  # nosec
         erc20_proxy="0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e",
@@ -125,7 +125,7 @@ NETWORK_TO_ADDRESSES: Dict[NetworkId, ContractAddresses] = {
         coordinator_registry="0x09fb99968c016a3ff537bf58fb3d9fe55a7975d5",
         coordinator="0x2ba02e03ee0029311e0f43715307870a3e701b53",
         erc1155_proxy="0x64517fa2b480ba3678a2a3c0cf08ef7fd4fad36f",
-        dev_utils: '0x1e3616bc5144362f95d72de41874395567697e93',
+        dev_utils="0x1e3616bc5144362f95d72de41874395567697e93",
     ),
     NetworkId.GANACHE: ContractAddresses(  # nosec
         exchange="0x48bacb9266a570d521063ef5dd96e61686dbe788",
@@ -139,7 +139,7 @@ NETWORK_TO_ADDRESSES: Dict[NetworkId, ContractAddresses] = {
         order_validator="0x4d3d5c850dd5bd9d6f4adda3dd039a3c8054ca29",
         coordinator_registry="0x1941ff73d1154774d87521d2d0aaad5d19c8df60",
         coordinator="0x0d8b0dd11f5d34ed41d556def5f841900d5b1c6b",
-        dev_utils: '0x38ef19fdf8e8415f18c307ed71967e19aac28ba1',
+        dev_utils="0x38ef19fdf8e8415f18c307ed71967e19aac28ba1",
     ),
 }
 """A mapping from instances of NetworkId to instances of ContractAddresses.
