3fef6cd2732c8974e17f379fc1c027f1a9613290
Michael Zhu <mzhu25@stanford.edu>
2019-09-13 10:17:47 -0700

    remove unnecessary import and asyncs

--- contracts/staking/test/unit_tests/lib_safe_downcast_test.ts
+++ contracts/staking/test/unit_tests/lib_safe_downcast_test.ts
@@ -1,6 +1,5 @@
 import { blockchainTests, expect, Numberish } from '@0x/contracts-test-utils';
 import { BigNumber, SafeMathRevertErrors } from '@0x/utils';
-import * as _ from 'lodash';
 
 import { artifacts, TestLibSafeDowncastContract } from '../../src/';
 
@@ -20,7 +19,7 @@ blockchainTests('LibSafeDowncast', env => {
     const MAX_UINT_96 = new BigNumber(2).pow(96).minus(1);
     const MAX_UINT_256 = new BigNumber(2).pow(256).minus(1);
 
-    describe('downcastToUint96', async () => {
+    describe('downcastToUint96', () => {
         async function verifyCorrectDowncastAsync(n: Numberish): Promise<void> {
             const actual = await testContract.downcastToUint96.callAsync(new BigNumber(n));
             expect(actual).to.bignumber.eq(n);
@@ -31,27 +30,27 @@ blockchainTests('LibSafeDowncast', env => {
                 new BigNumber(n),
             );
             const tx = testContract.downcastToUint96.callAsync(new BigNumber(n));
-            await expect(tx).to.revertWith(expectedError);
+            expect(tx).to.revertWith(expectedError);
         }
 
         it('correctly downcasts 0', async () => {
-            await verifyCorrectDowncastAsync(0);
+            return verifyCorrectDowncastAsync(0);
         });
         it('correctly downcasts 1337', async () => {
-            await verifyCorrectDowncastAsync(1337);
+            return verifyCorrectDowncastAsync(1337);
         });
         it('correctly downcasts MAX_UINT_96', async () => {
-            await verifyCorrectDowncastAsync(MAX_UINT_96);
+            return verifyCorrectDowncastAsync(MAX_UINT_96);
         });
         it('reverts on MAX_UINT_96 + 1', async () => {
-            await verifyDowncastErrorAsync(MAX_UINT_96.plus(1));
+            return verifyDowncastErrorAsync(MAX_UINT_96.plus(1));
         });
         it('reverts on MAX_UINT_256', async () => {
-            await verifyDowncastErrorAsync(MAX_UINT_256);
+            return verifyDowncastErrorAsync(MAX_UINT_256);
         });
     });
 
-    describe('downcastToUint64', async () => {
+    describe('downcastToUint64', () => {
         async function verifyCorrectDowncastAsync(n: Numberish): Promise<void> {
             const actual = await testContract.downcastToUint64.callAsync(new BigNumber(n));
             expect(actual).to.bignumber.eq(n);
@@ -62,23 +61,23 @@ blockchainTests('LibSafeDowncast', env => {
                 new BigNumber(n),
             );
             const tx = testContract.downcastToUint64.callAsync(new BigNumber(n));
-            await expect(tx).to.revertWith(expectedError);
+            expect(tx).to.revertWith(expectedError);
         }
 
         it('correctly downcasts 0', async () => {
-            await verifyCorrectDowncastAsync(0);
+            return verifyCorrectDowncastAsync(0);
         });
         it('correctly downcasts 1337', async () => {
-            await verifyCorrectDowncastAsync(1337);
+            return verifyCorrectDowncastAsync(1337);
         });
         it('correctly downcasts MAX_UINT_64', async () => {
-            await verifyCorrectDowncastAsync(MAX_UINT_64);
+            return verifyCorrectDowncastAsync(MAX_UINT_64);
         });
         it('reverts on MAX_UINT_64 + 1', async () => {
-            await verifyDowncastErrorAsync(MAX_UINT_64.plus(1));
+            return verifyDowncastErrorAsync(MAX_UINT_64.plus(1));
         });
         it('reverts on MAX_UINT_256', async () => {
-            await verifyDowncastErrorAsync(MAX_UINT_256);
+            return verifyDowncastErrorAsync(MAX_UINT_256);
         });
     });
 });
