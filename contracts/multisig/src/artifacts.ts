a7b2f5ed3cb1d44cafc57154f988a1d57e0d98bd
Amir Bandeali <abandeali1@gmail.com>
2019-09-13 12:45:57 -0400

    Update AssetProxyOwner tests

--- contracts/multisig/src/artifacts.ts
+++ contracts/multisig/src/artifacts.ts
@@ -6,6 +6,7 @@
 import { ContractArtifact } from 'ethereum-types';
 
 import * as AssetProxyOwner from '../generated-artifacts/AssetProxyOwner.json';
+import * as ContractCallReceiver from '../generated-artifacts/ContractCallReceiver.json';
 import * as MultiSigWallet from '../generated-artifacts/MultiSigWallet.json';
 import * as MultiSigWalletWithTimeLock from '../generated-artifacts/MultiSigWalletWithTimeLock.json';
 import * as TestAssetProxyOwner from '../generated-artifacts/TestAssetProxyOwner.json';
@@ -14,6 +15,7 @@ export const artifacts = {
     AssetProxyOwner: AssetProxyOwner as ContractArtifact,
     MultiSigWallet: MultiSigWallet as ContractArtifact,
     MultiSigWalletWithTimeLock: MultiSigWalletWithTimeLock as ContractArtifact,
+    ContractCallReceiver: ContractCallReceiver as ContractArtifact,
     TestAssetProxyOwner: TestAssetProxyOwner as ContractArtifact,
     TestRejectEther: TestRejectEther as ContractArtifact,
 };
