93d57873a75aece50e23f551003ce9c1cbde44cf
Michael Zhu <mzhu25@stanford.edu>
2019-09-16 10:25:41 -0700

    MixinVaultCore unit tests

--- contracts/staking/src/artifacts.ts
+++ contracts/staking/src/artifacts.ts
@@ -49,6 +49,7 @@ import * as TestLibFixedMath from '../generated-artifacts/TestLibFixedMath.json'
 import * as TestLibProxy from '../generated-artifacts/TestLibProxy.json';
 import * as TestLibProxyReceiver from '../generated-artifacts/TestLibProxyReceiver.json';
 import * as TestLibSafeDowncast from '../generated-artifacts/TestLibSafeDowncast.json';
+import * as TestMixinVaultCore from '../generated-artifacts/TestMixinVaultCore.json';
 import * as TestProtocolFees from '../generated-artifacts/TestProtocolFees.json';
 import * as TestProtocolFeesERC20Proxy from '../generated-artifacts/TestProtocolFeesERC20Proxy.json';
 import * as TestStaking from '../generated-artifacts/TestStaking.json';
@@ -101,6 +102,7 @@ export const artifacts = {
     TestLibProxy: TestLibProxy as ContractArtifact,
     TestLibProxyReceiver: TestLibProxyReceiver as ContractArtifact,
     TestLibSafeDowncast: TestLibSafeDowncast as ContractArtifact,
+    TestMixinVaultCore: TestMixinVaultCore as ContractArtifact,
     TestProtocolFees: TestProtocolFees as ContractArtifact,
     TestProtocolFeesERC20Proxy: TestProtocolFeesERC20Proxy as ContractArtifact,
     TestStaking: TestStaking as ContractArtifact,
