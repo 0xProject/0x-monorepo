2569cd85cad7b173aed70a4a9218d732d00b5aeb
Alex Towle <jalextowle@gmail.com>
2019-09-17 14:23:45 -0700

    `@0x:contracts-staking` Refactored the storage layout test into a function

--- contracts/staking/contracts/src/Staking.sol
+++ contracts/staking/contracts/src/Staking.sol
@@ -19,11 +19,11 @@
 pragma solidity ^0.5.9;
 pragma experimental ABIEncoderV2;
 
+import "./fees/MixinExchangeFees.sol";
 import "./interfaces/IStaking.sol";
-import "./sys/MixinParams.sol";
 import "./stake/MixinStake.sol";
 import "./staking_pools/MixinStakingPool.sol";
-import "./fees/MixinExchangeFees.sol";
+import "./sys/MixinParams.sol";
 
 
 contract Staking is
@@ -33,112 +33,12 @@ contract Staking is
     MixinStake,
     MixinExchangeFees
 {
+    /// @dev This constructor will ensure that the storage layout of this contract
+    ///      is compatible with the original staking storage layout.
     constructor()
         public
     {
-        assembly {
-            let storage_offset := 0x0
-
-            /// Ownable
-
-            if iszero(eq(owner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            /// MixinStorage
-
-            if iszero(eq(wethAssetProxy_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(stakingContract_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(readOnlyProxy_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(readOnlyProxyCallee_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_activeStakeByOwner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_inactiveStakeByOwner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_delegatedStakeByOwner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_delegatedStakeToPoolByOwner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_delegatedStakeByPoolId_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_withdrawableStakeByOwner_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(nextPoolId_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(poolJoinedByMakerAddress_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(numMakersByPoolId_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(currentEpoch_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(currentEpochStartTimeInSeconds_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(protocolFeesThisEpochByPool_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(activePoolsThisEpoch_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_cumulativeRewardsByPool_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_cumulativeRewardsByPoolReferenceCounter_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(_cumulativeRewardsByPoolLastStored_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(validExchanges_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(zrxVault_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(ethVault_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(rewardVault_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(epochDurationInSeconds_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(rewardDelegatedStakeWeight_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(minimumPoolStake_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(maximumMakersInPool_slot, storage_offset)) { revert(0x0, 0x0) }
-            storage_offset := add(storage_offset, 0x1)
-
-            if iszero(eq(cobbDouglasAlphaNumerator_slot, storage_offset)) { revert(0x0, 0x0) }
-            if iszero(eq(cobbDouglasAlphaNumerator_offset, 0x0)) { revert(0x0, 0x0) }
-
-            // This number will be tightly packed into the previous values storage slot since
-            // they are both `uint32`. Because of this tight packing, the offset of this value
-            // must be 4, since the previous value is a 4 byte number.
-            if iszero(eq(cobbDouglasAlphaDenominator_slot, storage_offset)) { revert(0x0, 0x0) }
-            if iszero(eq(cobbDouglasAlphaDenominator_offset, 0x4)) { revert(0x0, 0x0) }
-        }
+        _assertStorageLayout();
     }
 
     // this contract can receive ETH
@@ -173,5 +73,299 @@ contract Staking is
             _rewardVaultAddress,
             _zrxVaultAddress
         );
+    }
+
+    /// @dev This function will fail if the storage layout of this contract deviates from
+    ///      the original staking contract's storage. The use of this function provides assurance
+    ///      that regressions from the original storage layout will not occur.
+    function _assertStorageLayout()
+        internal
+        pure
+    {
+        assembly {
+            let slot := 0x0
+            let offset := 0x0
+
+            /// Ownable
+
+            assertSlotAndOffset(
+                owner_slot,
+                owner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            /// MixinStorage
+
+            assertSlotAndOffset(
+                wethAssetProxy_slot,
+                wethAssetProxy_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                stakingContract_slot,
+                stakingContract_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                readOnlyProxy_slot,
+                readOnlyProxy_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                readOnlyProxyCallee_slot,
+                readOnlyProxyCallee_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _activeStakeByOwner_slot,
+                _activeStakeByOwner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _inactiveStakeByOwner_slot,
+                _inactiveStakeByOwner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _delegatedStakeByOwner_slot,
+                _delegatedStakeByOwner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _delegatedStakeToPoolByOwner_slot,
+                _delegatedStakeToPoolByOwner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _delegatedStakeByPoolId_slot,
+                _delegatedStakeByPoolId_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _withdrawableStakeByOwner_slot,
+                _withdrawableStakeByOwner_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                nextPoolId_slot,
+                nextPoolId_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                poolJoinedByMakerAddress_slot,
+                poolJoinedByMakerAddress_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                numMakersByPoolId_slot,
+                numMakersByPoolId_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                currentEpoch_slot,
+                currentEpoch_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                currentEpochStartTimeInSeconds_slot,
+                currentEpochStartTimeInSeconds_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                protocolFeesThisEpochByPool_slot,
+                protocolFeesThisEpochByPool_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                activePoolsThisEpoch_slot,
+                activePoolsThisEpoch_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _cumulativeRewardsByPool_slot,
+                _cumulativeRewardsByPool_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _cumulativeRewardsByPoolReferenceCounter_slot,
+                _cumulativeRewardsByPoolReferenceCounter_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                _cumulativeRewardsByPoolLastStored_slot,
+                _cumulativeRewardsByPoolLastStored_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                validExchanges_slot,
+                validExchanges_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                zrxVault_slot,
+                zrxVault_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                ethVault_slot,
+                ethVault_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                rewardVault_slot,
+                rewardVault_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                epochDurationInSeconds_slot,
+                epochDurationInSeconds_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                rewardDelegatedStakeWeight_slot,
+                rewardDelegatedStakeWeight_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                minimumPoolStake_slot,
+                minimumPoolStake_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                maximumMakersInPool_slot,
+                maximumMakersInPool_offset,
+                slot,
+                offset
+            )
+            slot := add(slot, 0x1)
+
+            assertSlotAndOffset(
+                cobbDouglasAlphaNumerator_slot,
+                cobbDouglasAlphaNumerator_offset,
+                slot,
+                offset
+            )
+            offset := add(offset, 0x4)
+
+            // This number will be tightly packed into the previous values storage slot since
+            // they are both `uint32`. Because of this tight packing, the offset of this value
+            // must be 4, since the previous value is a 4 byte number.
+            assertSlotAndOffset(
+                cobbDouglasAlphaDenominator_slot,
+                cobbDouglasAlphaDenominator_offset,
+                slot,
+                offset
+            )
+
+            // This assembly function will assert that the actual values for `_slot` and `_offset` are
+            // correct and will revert with a rich error if they are different than the expected values.
+            function assertSlotAndOffset(
+                actual_slot,
+                actual_offset,
+                expected_slot,
+                expected_offset
+            ) {
+                // If expected_slot is not equal to actual_slot, revert with a rich error.
+                if iszero(eq(expected_slot, actual_slot)) {
+                    mstore(0x0, 0x213eb13400000000000000000000000000000000000000000000000000000000) // Rich error selector
+                    mstore(0x4, 0x0)                                                                // Unexpected slot error code
+                    mstore(0x24, expected_slot)                                                     // Expected slot
+                    mstore(0x44, actual_slot)                                                       // Actual slot
+                    revert(0x0, 0x64)
+                }
+
+                // If expected_offset is not equal to actual_offset, revert with a rich error.
+                if iszero(eq(expected_offset, actual_offset)) {
+                    mstore(0x0, 0x213eb13400000000000000000000000000000000000000000000000000000000) // Rich error selector
+                    mstore(0x4, 0x1)                                                                // Unexpected offset error code
+                    mstore(0x24, expected_offset)                                                   // Expected offset
+                    mstore(0x44, actual_offset)                                                     // Actual offset
+                    revert(0x0, 0x64)
+                }
+            }
+        }
     }
 }
