3732428f3d0550268803acf8c8246148584b5a04
Lawrence Forman <me@merklejerk.com>
2019-09-19 06:15:25 -0400

    `@0x/contracts-test-utils`: Ran prettier.

--- contracts/test-utils/src/index.ts
+++ contracts/test-utils/src/index.ts
@@ -28,16 +28,7 @@ export { bytes32Values, testCombinatoriallyWithReferenceFunc, uint256Values } fr
 export { TransactionFactory } from './transaction_factory';
 export { MutatorContractFunction, TransactionHelper } from './transaction_helper';
 export { testWithReferenceFuncAsync } from './test_with_reference';
-export {
-    hexConcat,
-    hexHash,
-    hexLeftPad,
-    hexInvert,
-    hexSlice,
-    hexRandom,
-    hexRightPad,
-    toHex,
-} from './hex_utils';
+export { hexConcat, hexHash, hexLeftPad, hexInvert, hexSlice, hexRandom, hexRightPad, toHex } from './hex_utils';
 export {
     BatchMatchOrder,
     ContractName,
