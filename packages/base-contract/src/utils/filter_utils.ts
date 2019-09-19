d5bbbe802b06c6124c57f204deddbb71f9381bcf
Lawrence Forman <me@merklejerk.com>
2019-09-19 03:21:34 -0400

    `@0x/base-contract`: Fix linter errors.

--- packages/base-contract/src/utils/filter_utils.ts
+++ packages/base-contract/src/utils/filter_utils.ts
@@ -52,11 +52,13 @@ export const filterUtils = {
                 // Null is a wildcard topic in a JSON-RPC call
                 topics.push(null);
             } else {
+                // tslint:disable: no-unnecessary-type-assertion
                 let value = indexFilterValues[eventInput.name] as any;
                 if (BigNumber.isBigNumber(value)) {
                     // tslint:disable-next-line custom-no-magic-numbers
                     value = ethUtil.fromSigned(value.toString(10) as any);
                 }
+                // tslint:enable: no-unnecessary-type-assertion
                 const buffer = ethUtil.toBuffer(value);
                 const paddedBuffer = ethUtil.setLengthLeft(buffer, TOPIC_LENGTH);
                 const topic = ethUtil.bufferToHex(paddedBuffer);
