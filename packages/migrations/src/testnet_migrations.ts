76724a6c73c2d3711d5da614f347512bcbbd7cfd
Amir Bandeali <abandeali1@gmail.com>
2019-09-29 23:27:32 -0700

    Implement initial 3.0 migrations script

--- packages/migrations/src/testnet_migrations.ts
+++ packages/migrations/src/testnet_migrations.ts
@@ -1,21 +1,249 @@
 import { getContractAddressesForNetworkOrThrow } from '@0x/contract-addresses';
-import { AssetProxyOwnerContract, ERC20ProxyContract } from '@0x/contract-wrappers';
-import { artifacts, AssetProxyOwnerContract as GovernanceMultisigContract } from '@0x/contracts-multisig';
-import { AbiEncoder, BigNumber, providerUtils } from '@0x/utils';
+import { artifacts as exchangeArtifacts, ExchangeContract } from '@0x/contracts-exchange';
+import { artifacts, AssetProxyOwnerContract, AssetProxyOwnerSubmissionEventArgs } from '@0x/contracts-multisig';
+import {
+    artifacts as stakingArtifacts,
+    ReadOnlyProxyContract,
+    StakingContract,
+    StakingProxyContract,
+    ZrxVaultContract,
+} from '@0x/contracts-staking';
+import { IAuthorizableContract, IOwnableContract } from '@0x/contracts-utils';
+import { AbiEncoder, BigNumber, logUtils, providerUtils } from '@0x/utils';
 import { Web3Wrapper } from '@0x/web3-wrapper';
-import { MethodAbi, SupportedProvider, TxData } from 'ethereum-types';
+import { LogWithDecodedArgs, SupportedProvider, TxData } from 'ethereum-types';
+
+import { constants } from './utils/constants';
+import { providerFactory } from './utils/provider_factory';
+
+export async function submitAndExecuteTransactionAsync(
+    assetProxyOwner: AssetProxyOwnerContract,
+    destination: string,
+    data: string,
+): Promise<void> {
+    const txReceipt = await assetProxyOwner.submitTransaction.awaitTransactionSuccessAsync(
+        destination,
+        constants.ZERO_AMOUNT,
+        data,
+    );
+    const txId = (txReceipt.logs[0] as LogWithDecodedArgs<AssetProxyOwnerSubmissionEventArgs>).args.transactionId;
+    logUtils.log(`${txId} submitted`);
+    await assetProxyOwner.executeTransaction.awaitTransactionSuccessAsync(txId);
+    logUtils.log(`${txId} executed`);
+}
 
 export async function runMigrationsAsync(supportedProvider: SupportedProvider, txDefaults: TxData): Promise<void> {
     const provider = providerUtils.standardizeOrThrow(supportedProvider);
     const web3Wrapper = new Web3Wrapper(provider);
-    const chainId = new BigNumber(await providerUtils.getChainIdAsync(provider));
     const networkId = await web3Wrapper.getNetworkIdAsync();
+    const chainId = new BigNumber(await providerUtils.getChainIdAsync(provider));
     const deployedAddresses = getContractAddressesForNetworkOrThrow(networkId);
-    // const noTimeLockMethods = [{destination: }]
-    const governanceMultisig = await GovernanceMultisigContract.deployFrom0xArtifactAsync(
+
+    const zrxVault = new ZrxVaultContract(deployedAddresses.zrxVault, provider, txDefaults);
+
+    const stakingLogic = await StakingContract.deployFrom0xArtifactAsync(
+        stakingArtifacts.Staking,
+        provider,
+        txDefaults,
+        stakingArtifacts,
+    );
+
+    const exchange = await ExchangeContract.deployFrom0xArtifactAsync(
+        exchangeArtifacts.Exchange,
+        provider,
+        txDefaults,
+        exchangeArtifacts,
+        chainId,
+    );
+
+    const readOnlyProxy = await ReadOnlyProxyContract.deployFrom0xArtifactAsync(
+        stakingArtifacts.ReadOnlyProxy,
+        provider,
+        txDefaults,
+        stakingArtifacts,
+    );
+
+    const stakingProxy = await StakingProxyContract.deployFrom0xArtifactAsync(
+        stakingArtifacts.StakingProxy,
+        provider,
+        txDefaults,
+        stakingArtifacts,
+        stakingLogic.address,
+        readOnlyProxy.address,
+    );
+
+    const authorizableInterface = new IAuthorizableContract(constants.NULL_ADDRESS, provider, txDefaults);
+    const ownableInterface = new IOwnableContract(constants.NULL_ADDRESS, provider, txDefaults);
+
+    const customTimeLocks = [
+        {
+            destination: deployedAddresses.erc20Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddress.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.erc20Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddressAtIndex.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.erc721Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddress.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.erc721Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddressAtIndex.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.erc1155Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddress.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.erc1155Proxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddressAtIndex.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.multiAssetProxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddress.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.multiAssetProxy,
+            functionSelector: authorizableInterface.removeAuthorizedAddressAtIndex.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.stakingProxy,
+            functionSelector: stakingProxy.setReadOnlyMode.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.zrxVault,
+            functionSelector: zrxVault.enterCatastrophicFailure.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT,
+        },
+        {
+            destination: deployedAddresses.stakingProxy,
+            functionSelector: stakingProxy.attachStakingContract.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT, // 3 epochs on mainnet
+        },
+        {
+            destination: deployedAddresses.stakingProxy,
+            functionSelector: stakingLogic.setParams.getSelector(),
+            secondsTimeLocked: constants.ZERO_AMOUNT, // 1 epoch on mainnet
+        },
+    ];
+
+    const assetProxyOwner = await AssetProxyOwnerContract.deployFrom0xArtifactAsync(
         artifacts.AssetProxyOwner,
         provider,
         txDefaults,
         artifacts,
+        customTimeLocks.map(timeLockInfo => timeLockInfo.functionSelector),
+        customTimeLocks.map(timeLockInfo => timeLockInfo.destination),
+        customTimeLocks.map(timeLockInfo => timeLockInfo.secondsTimeLocked),
+        constants.ASSET_PROXY_OWNER_OWNERS,
+        constants.ASSET_PROXY_OWNER_CONFIRMATIONS,
+        constants.ASSET_PROXY_OWNER_TIMELOCK,
     );
+
+    logUtils.log('Configuring Exchange...');
+    await exchange.setProtocolFeeCollectorAddress.awaitTransactionSuccessAsync(stakingProxy.address);
+    await exchange.setProtocolFeeMultiplier.awaitTransactionSuccessAsync(new BigNumber(150000));
+    await exchange.registerAssetProxy.awaitTransactionSuccessAsync(deployedAddresses.erc20Proxy);
+    await exchange.registerAssetProxy.awaitTransactionSuccessAsync(deployedAddresses.erc721Proxy);
+    await exchange.registerAssetProxy.awaitTransactionSuccessAsync(deployedAddresses.erc1155Proxy);
+    await exchange.registerAssetProxy.awaitTransactionSuccessAsync(deployedAddresses.multiAssetProxy);
+    await exchange.registerAssetProxy.awaitTransactionSuccessAsync(deployedAddresses.staticCallProxy);
+    await exchange.transferOwnership.awaitTransactionSuccessAsync(assetProxyOwner.address);
+    logUtils.log('Exchange configured!');
+
+    logUtils.log('Configuring ZrxVault...');
+    await zrxVault.addAuthorizedAddress.awaitTransactionSuccessAsync(assetProxyOwner.address);
+    logUtils.log('ZrxVault configured!');
+
+    logUtils.log('Configuring StakingProxy...');
+    await stakingProxy.addAuthorizedAddress.awaitTransactionSuccessAsync(assetProxyOwner.address);
+    logUtils.log('StakingProxy configured!');
+
+    logUtils.log('Transfering ownership of 2.0 contracts...');
+    const oldAssetProxyOwner = new AssetProxyOwnerContract(deployedAddresses.assetProxyOwner, provider, txDefaults);
+    await submitAndExecuteTransactionAsync(
+        oldAssetProxyOwner,
+        deployedAddresses.exchange, // Exchange 2.1 address
+        ownableInterface.transferOwnership.getABIEncodedTransactionData(assetProxyOwner.address),
+    );
+    await submitAndExecuteTransactionAsync(
+        oldAssetProxyOwner,
+        deployedAddresses.erc20Proxy,
+        ownableInterface.transferOwnership.getABIEncodedTransactionData(assetProxyOwner.address),
+    );
+    await submitAndExecuteTransactionAsync(
+        oldAssetProxyOwner,
+        deployedAddresses.erc721Proxy,
+        ownableInterface.transferOwnership.getABIEncodedTransactionData(assetProxyOwner.address),
+    );
+    await submitAndExecuteTransactionAsync(
+        oldAssetProxyOwner,
+        deployedAddresses.erc1155Proxy,
+        ownableInterface.transferOwnership.getABIEncodedTransactionData(assetProxyOwner.address),
+    );
+    await submitAndExecuteTransactionAsync(
+        oldAssetProxyOwner,
+        deployedAddresses.multiAssetProxy,
+        ownableInterface.transferOwnership.getABIEncodedTransactionData(assetProxyOwner.address),
+    );
+    logUtils.log('Ownership transferred!');
+
+    const functionCalls = [
+        {
+            destination: zrxVault.address,
+            data: zrxVault.setStakingProxy.getABIEncodedTransactionData(stakingProxy.address),
+        },
+        {
+            destination: stakingProxy.address,
+            data: stakingLogic.addExchangeAddress.getABIEncodedTransactionData(exchange.address),
+        },
+        {
+            destination: deployedAddresses.erc20Proxy,
+            data: authorizableInterface.addAuthorizedAddress.getABIEncodedTransactionData(exchange.address),
+        },
+        {
+            destination: deployedAddresses.erc20Proxy,
+            data: authorizableInterface.addAuthorizedAddress.getABIEncodedTransactionData(zrxVault.address),
+        },
+        {
+            destination: deployedAddresses.erc721Proxy,
+            data: authorizableInterface.addAuthorizedAddress.getABIEncodedTransactionData(exchange.address),
+        },
+        {
+            destination: deployedAddresses.erc1155Proxy,
+            data: authorizableInterface.addAuthorizedAddress.getABIEncodedTransactionData(exchange.address),
+        },
+        {
+            destination: deployedAddresses.multiAssetProxy,
+            data: authorizableInterface.addAuthorizedAddress.getABIEncodedTransactionData(exchange.address),
+        },
+    ];
+    const batchTransactionEncoder = AbiEncoder.create('(bytes[],address[],uint256[])');
+    const batchTransactionData = batchTransactionEncoder.encode([
+        functionCalls.map(item => item.data),
+        functionCalls.map(item => item.destination),
+        functionCalls.map(() => constants.ZERO_AMOUNT),
+    ]);
+    await submitAndExecuteTransactionAsync(assetProxyOwner, assetProxyOwner.address, batchTransactionData);
 }
+
+(async () => {
+    const networkId = 3;
+    const rpcUrl = 'https://ropsten.infura.io/v3/';
+    const provider = await providerFactory.getLedgerProviderAsync(networkId, rpcUrl);
+    await runMigrationsAsync(provider, { from: constants.ASSET_PROXY_OWNER_OWNERS[0], gasPrice: 40000000000 });
+})().catch(err => {
+    logUtils.log(err);
+    process.exit(1);
+});
--- packages/migrations/src/utils/provider_factory.ts
+++ packages/migrations/src/utils/provider_factory.ts
@@ -5,8 +5,6 @@ import Eth from '@ledgerhq/hw-app-eth';
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
 import { Provider } from 'ethereum-types';
 
-import { constants } from './constants';
-
 async function ledgerEthereumNodeJsClientFactoryAsync(): Promise<LedgerEthereumClient> {
     const ledgerConnection = await TransportNodeHid.create();
     const ledgerEthClient = new Eth(ledgerConnection);
@@ -22,18 +20,6 @@ export const providerFactory = {
         const ledgerSubprovider = new LedgerSubprovider(ledgerWalletConfigs);
         provider.addProvider(ledgerSubprovider);
         provider.addProvider(new RPCSubprovider(rpcUrl));
-        providerUtils.startProviderEngine(provider);
-        return provider;
-    },
-    async getMainnetLedgerProviderAsync(): Promise<Provider> {
-        const provider = new Web3ProviderEngine();
-        const ledgerWalletConfigs = {
-            networkId: constants.MAINNET_NETWORK_ID,
-            ledgerEthereumClientFactoryAsync: ledgerEthereumNodeJsClientFactoryAsync,
-        };
-        const ledgerSubprovider = new LedgerSubprovider(ledgerWalletConfigs);
-        provider.addProvider(ledgerSubprovider);
-        provider.addProvider(new RPCSubprovider(constants.MAINNET_RPC_URL));
         providerUtils.startProviderEngine(provider);
         return provider;
     },
