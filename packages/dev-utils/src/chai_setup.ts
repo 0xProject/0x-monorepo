2badaf455acbf89367d14d1a5920f050cfb575b5
Lawrence Forman <lawrence@0xproject.com>
2019-10-02 11:11:41 -0700

    `@0x/dev-utils`: Inline the `Chai` namespace extensions as a global declaratation in `chai_setup.ts`.

--- packages/dev-utils/src/chai_setup.ts
+++ packages/dev-utils/src/chai_setup.ts
@@ -1,9 +1,18 @@
+import { RevertError } from '@0x/utils';
 import * as chai from 'chai';
 import chaiAsPromised = require('chai-as-promised');
 import ChaiBigNumber = require('chai-bignumber');
 import * as dirtyChai from 'dirty-chai';
 
 import { revertErrorHelper } from './chai_revert_error';
+
+declare global {
+    namespace Chai {
+        export interface Assertion {
+            revertWith: (expected: string | RevertError) => Promise<void>;
+        }
+    }
+}
 
 export const chaiSetup = {
     configure(): void {
