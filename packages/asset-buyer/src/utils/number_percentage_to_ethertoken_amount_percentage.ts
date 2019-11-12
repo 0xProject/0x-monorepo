dd00f2016f074ff223c6271410fc75e8465d2483
David Sun <ffstudios1@gmail.com>
2019-11-12 16:31:08 -0500

    removed asset-buyer

--- packages/asset-buyer/src/utils/number_percentage_to_ethertoken_amount_percentage.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber } from '@0x/utils';
-import { Web3Wrapper } from '@0x/web3-wrapper';
-
-// HACK (xianny): copied from asset-swapper, which will replace this package
-const ONE_AMOUNT = new BigNumber(1);
-const ETHER_TOKEN_DECIMALS = 18;
-export const numberPercentageToEtherTokenAmountPercentage = (percentage: number): BigNumber => {
-    return Web3Wrapper.toBaseUnitAmount(ONE_AMOUNT, ETHER_TOKEN_DECIMALS).multipliedBy(percentage);
-};
