117f4a282dfc4874a7fe236bcf2d7cda6752b9e8
fabioberger <me@fabioberger.com>
2019-11-16 08:21:10 +0000

    Add EnvVars type

--- packages/dev-utils/src/index.ts
+++ packages/dev-utils/src/index.ts
@@ -1,7 +1,7 @@
 export { BlockchainLifecycle } from './blockchain_lifecycle';
 export { web3Factory, Web3Config } from './web3_factory';
 export { constants as devConstants } from './constants';
-export { env } from './env';
+export { env, EnvVars } from './env';
 export { callbackErrorReporter } from './callback_error_reporter';
 export { chaiSetup } from './chai_setup';
 export { tokenUtils } from './token_utils';
