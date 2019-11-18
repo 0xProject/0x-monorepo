16b13f976849f5df5806572993d68ca6b94bba86
fabioberger <me@fabioberger.com>
2019-11-18 22:32:47 +0100

    Fix exports from exchange

--- contracts/exchange/src/index.ts
+++ contracts/exchange/src/index.ts
@@ -17,24 +17,9 @@ export {
 } from './wrappers';
 export import ExchangeRevertErrors = require('./revert_errors');
 export { exchangeDataEncoder } from './exchange_data_encoder';
-export {
-    TokenOwnersByName,
-    TokenContractsByName,
-    TokenIds,
-    ERC1155TokenIds,
-    ERC721TokenIds,
-    TokenData,
-    Named,
-} from './balance_stores/types';
 export { SignedOrder } from '@0x/types';
 export {
-    Numberish,
-    TokenBalances,
     ExchangeFunctionName,
-    ERC1155Holdings,
-    ERC20BalancesByOwner,
-    ERC721TokenIdsByOwner,
-    EthBalancesByOwner,
 } from '@0x/contracts-test-utils';
 export { DevUtilsContract } from '@0x/contracts-dev-utils';
 export {
