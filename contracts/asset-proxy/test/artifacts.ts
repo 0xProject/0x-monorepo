54afc8a4a19b815ed6789691ed148ce85bfa88db
Greg Hysen <greg.hysen@gmail.com>
2019-12-03 16:01:30 -0800

    Fixed merge conflicts

--- contracts/asset-proxy/test/artifacts.ts
+++ contracts/asset-proxy/test/artifacts.ts
@@ -5,12 +5,8 @@
  */
 import { ContractArtifact } from 'ethereum-types';
 
-<<<<<<< HEAD
 import * as ChaiBridge from '../test/generated-artifacts/ChaiBridge.json';
-import * as DyDxBridge from '../test/generated-artifacts/DyDxBridge.json';
-=======
 import * as DydxBridge from '../test/generated-artifacts/DydxBridge.json';
->>>>>>> ee82a0f67... Consistent capitalization for "dydx"`
 import * as ERC1155Proxy from '../test/generated-artifacts/ERC1155Proxy.json';
 import * as ERC20BridgeProxy from '../test/generated-artifacts/ERC20BridgeProxy.json';
 import * as ERC20Proxy from '../test/generated-artifacts/ERC20Proxy.json';
@@ -20,16 +16,9 @@ import * as IAssetData from '../test/generated-artifacts/IAssetData.json';
 import * as IAssetProxy from '../test/generated-artifacts/IAssetProxy.json';
 import * as IAssetProxyDispatcher from '../test/generated-artifacts/IAssetProxyDispatcher.json';
 import * as IAuthorizable from '../test/generated-artifacts/IAuthorizable.json';
-<<<<<<< HEAD
 import * as IChai from '../test/generated-artifacts/IChai.json';
-import * as IDyDx from '../test/generated-artifacts/IDyDx.json';
-=======
 import * as IDydx from '../test/generated-artifacts/IDydx.json';
-<<<<<<< HEAD
->>>>>>> ee82a0f67... Consistent capitalization for "dydx"`
-=======
 import * as IDydxBridge from '../test/generated-artifacts/IDydxBridge.json';
->>>>>>> 5e2de489d... Updating bridge to work w/o MAP
 import * as IERC20Bridge from '../test/generated-artifacts/IERC20Bridge.json';
 import * as IEth2Dai from '../test/generated-artifacts/IEth2Dai.json';
 import * as IKyberNetworkProxy from '../test/generated-artifacts/IKyberNetworkProxy.json';
@@ -42,6 +31,7 @@ import * as MultiAssetProxy from '../test/generated-artifacts/MultiAssetProxy.js
 import * as Ownable from '../test/generated-artifacts/Ownable.json';
 import * as StaticCallProxy from '../test/generated-artifacts/StaticCallProxy.json';
 import * as TestChaiBridge from '../test/generated-artifacts/TestChaiBridge.json';
+import * as TestDydxBridge from '../test/generated-artifacts/TestDydxBridge.json';
 import * as TestERC20Bridge from '../test/generated-artifacts/TestERC20Bridge.json';
 import * as TestEth2DaiBridge from '../test/generated-artifacts/TestEth2DaiBridge.json';
 import * as TestKyberBridge from '../test/generated-artifacts/TestKyberBridge.json';
@@ -58,12 +48,8 @@ export const artifacts = {
     ERC721Proxy: ERC721Proxy as ContractArtifact,
     MultiAssetProxy: MultiAssetProxy as ContractArtifact,
     StaticCallProxy: StaticCallProxy as ContractArtifact,
-<<<<<<< HEAD
     ChaiBridge: ChaiBridge as ContractArtifact,
-    DyDxBridge: DyDxBridge as ContractArtifact,
-=======
     DydxBridge: DydxBridge as ContractArtifact,
->>>>>>> ee82a0f67... Consistent capitalization for "dydx"`
     Eth2DaiBridge: Eth2DaiBridge as ContractArtifact,
     KyberBridge: KyberBridge as ContractArtifact,
     UniswapBridge: UniswapBridge as ContractArtifact,
@@ -71,22 +57,16 @@ export const artifacts = {
     IAssetProxy: IAssetProxy as ContractArtifact,
     IAssetProxyDispatcher: IAssetProxyDispatcher as ContractArtifact,
     IAuthorizable: IAuthorizable as ContractArtifact,
-<<<<<<< HEAD
     IChai: IChai as ContractArtifact,
-    IDyDx: IDyDx as ContractArtifact,
-=======
     IDydx: IDydx as ContractArtifact,
-<<<<<<< HEAD
->>>>>>> ee82a0f67... Consistent capitalization for "dydx"`
-=======
     IDydxBridge: IDydxBridge as ContractArtifact,
->>>>>>> 5e2de489d... Updating bridge to work w/o MAP
     IERC20Bridge: IERC20Bridge as ContractArtifact,
     IEth2Dai: IEth2Dai as ContractArtifact,
     IKyberNetworkProxy: IKyberNetworkProxy as ContractArtifact,
     IUniswapExchange: IUniswapExchange as ContractArtifact,
     IUniswapExchangeFactory: IUniswapExchangeFactory as ContractArtifact,
     TestChaiBridge: TestChaiBridge as ContractArtifact,
+    TestDydxBridge: TestDydxBridge as ContractArtifact,
     TestERC20Bridge: TestERC20Bridge as ContractArtifact,
     TestEth2DaiBridge: TestEth2DaiBridge as ContractArtifact,
     TestKyberBridge: TestKyberBridge as ContractArtifact,
