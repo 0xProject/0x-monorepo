9a2fca73ff044de0f1e5746457db4b022869661d
Lawrence Forman <lawrence@0xproject.com>
2019-12-10 15:37:43 -0800

    Fix more broken contracts.

--- contracts/erc20-bridge-sampler/contracts/test/TestERC20BridgeSampler.sol
+++ contracts/erc20-bridge-sampler/contracts/test/TestERC20BridgeSampler.sol
@@ -336,9 +336,8 @@ contract TestERC20BridgeSampler is
         bytes32 orderHash = keccak256(abi.encode(order.salt));
         // Everything else is derived from the hash.
         orderInfo.orderHash = orderHash;
-        orderInfo.orderStatus = uint8(uint256(orderHash) % uint8(-1));
-        orderInfo.orderTakerAssetFilledAmount =
-            uint256(orderHash) % order.takerAssetAmount;
+        orderInfo.orderStatus = LibOrder.OrderStatus(uint256(orderHash) % uint8(-1));
+        orderInfo.orderTakerAssetFilledAmount = uint256(orderHash) % order.takerAssetAmount;
         fillableTakerAssetAmount =
             order.takerAssetAmount - orderInfo.orderTakerAssetFilledAmount;
         isValidSignature = uint256(orderHash) % 2 == 1;
