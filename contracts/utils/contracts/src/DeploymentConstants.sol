bec1f236161d31d04c51ceec7913661e12334a81
Greg Hysen <greg.hysen@gmail.com>
2019-12-13 11:06:16 -0800

    Fixed merge conflicts

--- contracts/utils/contracts/src/DeploymentConstants.sol
+++ contracts/utils/contracts/src/DeploymentConstants.sol
@@ -42,7 +42,7 @@ contract DeploymentConstants {
     address constant private DEV_UTILS_ADDRESS = 0xcCc2431a7335F21d9268bA62F0B32B0f2EFC463f;
     /// @dev Kyber ETH pseudo-address.
     address constant internal KYBER_ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    /// @dev Mainnet address of the DyDx contract.
+    /// @dev Mainnet address of the dYdX contract.
     address constant private DYDX_ADDRESS = 0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e;
 
     /// @dev Overridable way to get the `KyberNetworkProxy` address.
@@ -127,7 +127,7 @@ contract DeploymentConstants {
 
     /// @dev Overridable way to get the DyDx contract.
     /// @return exchange The DyDx exchange contract.
-    function _getDyDxAddress()
+    function _getDydxAddress()
         internal
         view
         returns (address dydxAddress)
