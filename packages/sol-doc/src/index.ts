70cb550f4a30a3622058dd405ce72ef4b67cde47
Lawrence Forman <me@merklejerk.com>
2019-12-27 02:44:06 -0500

    `@0x/sol-doc`: Export all relevant types and functions.

--- packages/sol-doc/src/index.ts
+++ packages/sol-doc/src/index.ts
@@ -1,1 +1,20 @@
-export { extractDocsAsync, SolidityDocs } from './extract_docs';
+export {
+    ContractDocs,
+    ContractKind,
+    EnumValueDocs,
+    EnumValueDocsMap,
+    EventDocs,
+    extractDocsAsync,
+    FunctionKind,
+    MethodDocs,
+    ParamDocs,
+    ParamDocsMap,
+    SolidityDocs,
+    StorageLocation,
+    StructDocs,
+    Visibility,
+} from './extract_docs';
+
+export { transformDocs, TransformOpts } from './transform_docs';
+
+export { generateMarkdownFromDocs, MarkdownOpts } from './gen_md';
