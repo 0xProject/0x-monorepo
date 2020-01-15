4ad40b16428f187f15fe2f3d03a09466b05308a7
Lawrence Forman <lawrence@0xproject.com>
2020-01-15 18:09:42 -0500

    `@0x/contract-wrappers`: Update `IERC20BridgeSampler` wrapper.

--- packages/contract-wrappers/src/generated-wrappers/i_erc20_bridge_sampler.ts
+++ packages/contract-wrappers/src/generated-wrappers/i_erc20_bridge_sampler.ts
@@ -850,8 +850,18 @@ export class IERC20BridgeSamplerContract extends BaseContract {
             },
         };
     }
+    /**
+     * Query batches of native orders and sample buy quotes on multiple DEXes at once.
+     * @param orders Batches of Native orders to query.
+     * @param orderSignatures Batches of Signatures for each respective order in
+     *     `orders`.
+     * @param sources Address of each DEX. Passing in an unsupported DEX will
+     *     throw.
+     * @param makerTokenAmounts Batches of Maker token sell amount for each sample.
+     * @returns ordersAndSamples How much taker asset can be filled         by each order in &#x60;orders&#x60;. Taker amounts sold for each source at         each maker token amount. First indexed by source index, then sample         index
+     */
     public queryBatchOrdersAndSampleBuys(
-        orders: Array<Array<{
+        orders: Array<{
             makerAddress: string;
             takerAddress: string;
             feeRecipientAddress: string;
@@ -866,7 +876,7 @@ export class IERC20BridgeSamplerContract extends BaseContract {
             takerAssetData: string;
             makerFeeAssetData: string;
             takerFeeAssetData: string;
-        }>>,
+        }>[],
         orderSignatures: string[][],
         sources: string[],
         makerTokenAmounts: BigNumber[][],
@@ -904,8 +914,18 @@ export class IERC20BridgeSamplerContract extends BaseContract {
             },
         };
     }
+    /**
+     * Query batches of native orders and sample sell quotes on multiple DEXes at once.
+     * @param orders Batches of Native orders to query.
+     * @param orderSignatures Batches of Signatures for each respective order in
+     *     `orders`.
+     * @param sources Address of each DEX. Passing in an unsupported DEX will
+     *     throw.
+     * @param takerTokenAmounts Batches of Taker token sell amount for each sample.
+     * @returns ordersAndSamples How much taker asset can be filled         by each order in &#x60;orders&#x60;. Maker amounts bought for each source at         each taker token amount. First indexed by source index, then sample         index.
+     */
     public queryBatchOrdersAndSampleSells(
-        orders: Array<Array<{
+        orders: Array<{
             makerAddress: string;
             takerAddress: string;
             feeRecipientAddress: string;
@@ -920,7 +940,7 @@ export class IERC20BridgeSamplerContract extends BaseContract {
             takerAssetData: string;
             makerFeeAssetData: string;
             takerFeeAssetData: string;
-        }>>,
+        }>[],
         orderSignatures: string[][],
         sources: string[],
         takerTokenAmounts: BigNumber[][],
