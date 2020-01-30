8effcf63a213e1f2fa32e0688fa0a0a24633ee1b
Lawrence Forman <me@merklejerk.com>
2020-01-30 05:13:43 -0500

    `@0x/contracts-asset-proxy`: Add asset data decoding and encoding functions

--- contracts/asset-proxy/src/index.ts
+++ contracts/asset-proxy/src/index.ts
@@ -24,6 +24,7 @@ export { ERC1155ProxyWrapper } from './erc1155_proxy_wrapper';
 export { ERC1155MintableContract, Erc1155Wrapper } from '@0x/contracts-erc1155';
 export { DummyERC20TokenContract } from '@0x/contracts-erc20';
 export { DummyERC721TokenContract } from '@0x/contracts-erc721';
+export { AssetProxyId } from '@0x/types';
 export {
     ERC1155HoldingsByOwner,
     ERC20BalancesByOwner,
@@ -54,6 +55,7 @@ export {
     OutputField,
     ParamDescription,
     EvmBytecodeOutput,
+    EvmBytecodeOutputLinkReferences,
     AbiDefinition,
     FunctionAbi,
     EventAbi,
@@ -67,4 +69,21 @@ export {
     TupleDataItem,
     StateMutability,
 } from 'ethereum-types';
+
+export {
+    decodeERC1155AssetData,
+    decodeERC20AssetData,
+    decodeERC20BridgeAssetData,
+    decodeERC721AssetData,
+    decodeMultiAssetData,
+    decodeStaticCallAssetData,
+    encodeERC1155AssetData,
+    encodeERC20AssetData,
+    encodeERC20BridgeAssetData,
+    encodeERC721AssetData,
+    encodeMultiAssetData,
+    encodeStaticCallAssetData,
+    getAssetDataProxyId,
+} from './asset_data';
+
 export * from './dydx_bridge_encoder';
