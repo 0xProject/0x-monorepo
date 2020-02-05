db1e601f5ed99815d234d53f08b9b6315e429a1d
Michael Zhu <mchl.zhu.96@gmail.com>
2020-02-05 15:39:55 -0800

    Contracts for Chainlink stop-limit orders

---
+++ contracts/integrations/contracts/src/ChainlinkStopLimit.sol
@@ -0,0 +1,60 @@
+/*
+
+  Copyright 2019 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.9;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-utils/contracts/src/LibSafeMath.sol";
+import "./interfaces/IChainlinkAggregator.sol";
+
+
+contract ChainlinkStopLimit {
+
+    using LibSafeMath for uint256;
+
+    /// @dev Checks that the price returned by the encoded Chainlink reference contract is
+    ///      within the encoded price range, and the price was recorded recently enough.
+    /// @param stopLimitData Encodes the address of the Chainlink reference contract, the
+    ///        valid price range, and the requisite "freshness" of the oracle's price data.
+    function checkStopLimit(bytes calldata stopLimitData)
+        external
+        view
+    {
+        (
+            address oracle,
+            int256 stopPrice,
+            int256 limitPrice,
+            uint256 priceFreshness
+        ) = abi.decode(
+            stopLimitData,
+            (address, int256, int256, uint256)
+        );
+
+        int256 latestPrice = IChainlinkAggregator(oracle).latestAnswer();
+        require(
+            latestPrice >= stopPrice && latestPrice <= limitPrice,
+            "ChainlinkStopLimitOracle/OUT_OF_PRICE_RANGE"
+        );
+
+        uint256 latestTimestamp = IChainlinkAggregator(oracle).latestTimestamp();
+        require(
+            now.safeSub(latestTimestamp) <= priceFreshness, // solhint-disable-line not-rely-on-time
+            "ChainlinkStopLimitOracle/PRICE_DATA_TOO_OLD"
+        );
+    }
+}
