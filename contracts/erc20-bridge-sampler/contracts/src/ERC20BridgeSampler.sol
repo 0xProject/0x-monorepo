76491fa6322430510f1df2482bb57e2949ac37ce
Michael Zhu <mchl.zhu.96@gmail.com>
2020-02-20 20:13:39 -0800

    address comments

--- contracts/erc20-bridge-sampler/contracts/src/ERC20BridgeSampler.sol
+++ contracts/erc20-bridge-sampler/contracts/src/ERC20BridgeSampler.sol
@@ -48,7 +48,7 @@ contract ERC20BridgeSampler is
     /// @dev Base gas limit for Curve calls. Some Curves have multiple tokens
     ///      So a reasonable ceil is 150k per token. Biggest Curve has 4 tokens.
     uint256 constant internal CURVE_CALL_GAS = 600e3; // 600k
-    /// @dev Defaukt gas limit for liquidity provider calls.
+    /// @dev Default gas limit for liquidity provider calls.
     uint256 constant internal DEFAULT_CALL_GAS = 200e3; // 200k
 
     /// @dev Call multiple public functions on this contract in a single transaction.
@@ -435,7 +435,6 @@ contract ERC20BridgeSampler is
     }
 
     /// @dev Sample sell quotes from an arbitrary on-chain liquidity provider.
-    ///      Calls the provider contract with the default gas limit (200k).
     /// @param providerAddress Address of the liquidity provider contract.
     /// @param takerToken Address of the taker token (what to sell).
     /// @param makerToken Address of the maker token (what to buy).
@@ -452,40 +451,11 @@ contract ERC20BridgeSampler is
         view
         returns (uint256[] memory makerTokenAmounts)
     {
-        return sampleSellsFromLiquidityProvider(
-            providerAddress,
-            takerToken,
-            makerToken,
-            takerTokenAmounts,
-            DEFAULT_CALL_GAS
-        );
-    }
-
-    /// @dev Sample sell quotes from an arbitrary on-chain liquidity provider.
-    /// @param providerAddress Address of the liquidity provider contract.
-    /// @param takerToken Address of the taker token (what to sell).
-    /// @param makerToken Address of the maker token (what to buy).
-    /// @param takerTokenAmounts Taker token sell amount for each sample.
-    /// @param callGasLimit Gas limit to use for calls to the provider's
-    ///        `getSellQuote` method.
-    /// @return makerTokenAmounts Maker amounts bought at each taker token
-    ///         amount.
-    function sampleSellsFromLiquidityProvider(
-        address providerAddress,
-        address takerToken,
-        address makerToken,
-        uint256[] memory takerTokenAmounts,
-        uint256 callGasLimit
-    )
-        public
-        view
-        returns (uint256[] memory makerTokenAmounts)
-    {
         uint256 numSamples = takerTokenAmounts.length;
         makerTokenAmounts = new uint256[](numSamples);
         for (uint256 i = 0; i < numSamples; i++) {
             (bool didSucceed, bytes memory resultData) =
-                providerAddress.staticcall.gas(callGasLimit)(
+                providerAddress.staticcall.gas(DEFAULT_CALL_GAS)(
                     abi.encodeWithSelector(
                         ILiquidityProvider(0).getSellQuote.selector,
                         takerToken,
@@ -496,6 +466,7 @@ contract ERC20BridgeSampler is
             if (didSucceed) {
                 buyAmount = abi.decode(resultData, (uint256));
             } else {
+                // Exit early if the amount is too high for the liquidity provider to serve
                 break;
             }
             makerTokenAmounts[i] = buyAmount;
@@ -503,7 +474,6 @@ contract ERC20BridgeSampler is
     }
 
     /// @dev Sample buy quotes from an arbitrary on-chain liquidity provider.
-    ///      Calls the provider contract with the default gas limit (200k).
     /// @param providerAddress Address of the liquidity provider contract.
     /// @param takerToken Address of the taker token (what to sell).
     /// @param makerToken Address of the maker token (what to buy).
@@ -520,41 +490,11 @@ contract ERC20BridgeSampler is
         view
         returns (uint256[] memory takerTokenAmounts)
     {
-        return sampleBuysFromLiquidityProvider(
-            providerAddress,
-            takerToken,
-            makerToken,
-            makerTokenAmounts,
-            DEFAULT_CALL_GAS
-        );
-    }
-
-    /// @dev Sample buy quotes from an arbitrary on-chain liquidity provider.
-    ///      Calls the provider contract with the default gas limit (200k).
-    /// @param providerAddress Address of the liquidity provider contract.
-    /// @param takerToken Address of the taker token (what to sell).
-    /// @param makerToken Address of the maker token (what to buy).
-    /// @param makerTokenAmounts Maker token buy amount for each sample.
-    /// @param callGasLimit Gas limit to use for calls to the provider's
-    ///        `getBuyQuote` method.
-    /// @return takerTokenAmounts Taker amounts sold at each maker token
-    ///         amount.
-    function sampleBuysFromLiquidityProvider(
-        address providerAddress,
-        address takerToken,
-        address makerToken,
-        uint256[] memory makerTokenAmounts,
-        uint256 callGasLimit
-    )
-        public
-        view
-        returns (uint256[] memory takerTokenAmounts)
-    {
         uint256 numSamples = makerTokenAmounts.length;
         takerTokenAmounts = new uint256[](numSamples);
         for (uint256 i = 0; i < numSamples; i++) {
             (bool didSucceed, bytes memory resultData) =
-                providerAddress.staticcall.gas(callGasLimit)(
+                providerAddress.staticcall.gas(DEFAULT_CALL_GAS)(
                     abi.encodeWithSelector(
                         ILiquidityProvider(0).getBuyQuote.selector,
                         takerToken,
@@ -565,6 +505,7 @@ contract ERC20BridgeSampler is
             if (didSucceed) {
                 sellAmount = abi.decode(resultData, (uint256));
             } else {
+                // Exit early if the amount is too high for the liquidity provider to serve
                 break;
             }
             takerTokenAmounts[i] = sellAmount;
