1916694dc626b4b3e379fbdbc09d333c60ab93fb
Lawrence Forman <lawrence@0xproject.com>
2020-04-01 17:21:43 -0400

    delete instant

--- packages/instant/src/style/z_index.ts
+++
@@ -1,9 +0,0 @@
-export const zIndex = {
-    errorPopBehind: 10,
-    mainContainer: 20,
-    dropdownItems: 30,
-    panel: 40,
-    containerOverlay: 45,
-    errorPopup: 50,
-    overlayDefault: 100,
-};
