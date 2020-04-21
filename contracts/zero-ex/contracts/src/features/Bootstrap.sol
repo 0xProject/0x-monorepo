80787456fa3e68a9e21a9a803a256c9532489600
Lawrence Forman <lawrence@0xproject.com>
2020-04-20 23:37:19 -0400

    `@0x/contracts-zero-ex`: Fix comments.

--- contracts/zero-ex/contracts/src/features/Bootstrap.sol
+++ contracts/zero-ex/contracts/src/features/Bootstrap.sol
@@ -56,7 +56,7 @@ contract Bootstrap is
     ///      into `target`. Before exiting the `bootstrap()` function will
     ///      deregister itself from the proxy to prevent being called again.
     /// @param target The bootstrapper contract address.
-    /// @param callData The call data to execute on `_bootstrapper`.
+    /// @param callData The call data to execute on `target`.
     function bootstrap(address target, bytes calldata callData) external override {
         // Only the bootstrap caller can call this function.
         if (msg.sender != _bootstrapCaller) {
--- contracts/zero-ex/contracts/src/features/IBootstrap.sol
+++ contracts/zero-ex/contracts/src/features/IBootstrap.sol
@@ -24,9 +24,9 @@ pragma experimental ABIEncoderV2;
 interface IBootstrap {
 
     /// @dev Bootstrap the initial feature set of this contract by delegatecalling
-    ///      into `_bootstrapper`. Before exiting the `bootstrap()` function will
+    ///      into `target`. Before exiting the `bootstrap()` function will
     ///      deregister itself from the proxy to prevent being called again.
     /// @param target The bootstrapper contract address.
-    /// @param callData The call data to execute on `_bootstrapper`.
+    /// @param callData The call data to execute on `target`.
     function bootstrap(address target, bytes calldata callData) external;
 }
