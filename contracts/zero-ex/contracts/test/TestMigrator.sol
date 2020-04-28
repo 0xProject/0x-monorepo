f1de64dcafa9f86ad57c7e38501ba8e9425d3e8e
Lawrence Forman <lawrence@0xproject.com>
2020-04-28 12:07:09 -0400

    `@0x/contracts-zero-ex`: Make `Ownable` feature conform to `IOwnable`.

--- contracts/zero-ex/contracts/test/TestMigrator.sol
+++ contracts/zero-ex/contracts/test/TestMigrator.sol
@@ -32,7 +32,7 @@ contract TestMigrator {
     function succeedingMigrate() external returns (bytes4 success) {
         emit TestMigrateCalled(
             msg.data,
-            IOwnable(address(this)).getOwner()
+            IOwnable(address(this)).owner()
         );
         return LibMigrate.MIGRATE_SUCCESS;
     }
@@ -40,7 +40,7 @@ contract TestMigrator {
     function failingMigrate() external returns (bytes4 success) {
         emit TestMigrateCalled(
             msg.data,
-            IOwnable(address(this)).getOwner()
+            IOwnable(address(this)).owner()
         );
         return 0xdeadbeef;
     }
