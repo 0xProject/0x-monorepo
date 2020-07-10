6f0acbbf2f45257ddc2d7fe72440b7d0bef17e8b
Jacob Evans <jacob@dekz.net>
2020-07-10 21:37:56 +1000

    hack: use overrides instead of forking in ganache

--- contracts/integrations/test/bridge_sampler/bridge_sampler_mainnet_test.ts
+++ contracts/integrations/test/bridge_sampler/bridge_sampler_mainnet_test.ts
@@ -1,6 +1,7 @@
 import { artifacts, ERC20BridgeSamplerContract } from '@0x/contracts-erc20-bridge-sampler';
-import { blockchainTests, constants, describe, expect, toBaseUnitAmount } from '@0x/contracts-test-utils';
-import { BigNumber } from '@0x/utils';
+import { blockchainTests, describe, expect, toBaseUnitAmount, Web3ProviderEngine } from '@0x/contracts-test-utils';
+import { RPCSubprovider } from '@0x/dev-utils/node_modules/@0x/subproviders';
+import { BigNumber, providerUtils } from '@0x/utils';
 
 export const VB = '0x6cc5f688a315f3dc28a7781717a9a798a59fda7b';
 blockchainTests.configure({
@@ -11,25 +12,52 @@ blockchainTests.configure({
 
 blockchainTests.fork.resets('Mainnet Sampler Tests', env => {
     let testContract: ERC20BridgeSamplerContract;
+    const fakeSamplerAddress = '0x1111111111111111111111111111111111111111';
     before(async () => {
-        testContract = await ERC20BridgeSamplerContract.deployFrom0xArtifactAsync(
-            artifacts.ERC20BridgeSampler,
-            env.provider,
-            { ...env.txDefaults, from: VB },
-            {},
-            constants.NULL_ADDRESS,
-        );
+        const provider = new Web3ProviderEngine();
+        provider.addProvider(new RPCSubprovider(process.env.FORK_RPC_URL!));
+        providerUtils.startProviderEngine(provider);
+        testContract = new ERC20BridgeSamplerContract(fakeSamplerAddress, provider, {
+            ...env.txDefaults,
+            from: VB,
+        });
+        /// testContract = await ERC20BridgeSamplerContract.deployFrom0xArtifactAsync(
+        ///    artifacts.ERC20BridgeSampler,
+        ///    env.provider,
+        ///    { ...env.txDefaults, from: VB },
+        ///    {},
+        ///    constants.NULL_ADDRESS,
+        /// );
     });
     describe.only('Kyber', () => {
+        const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+        const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
         describe('sampleSellsFromKyberNetwork()', () => {
+            it('samples sells from Kyber WETH->DAI', async () => {
+                const samples = await testContract
+                    .sampleSellsFromKyberNetwork(WETH, DAI, [toBaseUnitAmount(1)])
+                    .callAsync({
+                        overrides: {
+                            [fakeSamplerAddress]: {
+                                code: artifacts.ERC20BridgeSampler.compilerOutput.evm.deployedBytecode.object,
+                            },
+                        },
+                    });
+                console.log(samples);
+                expect(samples.length).to.be.bignumber.greaterThan(0);
+                expect(samples[0]).to.be.bignumber.greaterThan(0);
+            });
             it('samples sells from Kyber DAI->USDC', async () => {
                 const samples = await testContract
-                    .sampleSellsFromKyberNetwork(
-                        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-                        '0x6b175474e89094c44da98b954eedeac495271d0f',
-                        [toBaseUnitAmount(1)],
-                    )
-                    .callAsync();
+                    .sampleSellsFromKyberNetwork(DAI, USDC, [toBaseUnitAmount(1)])
+                    .callAsync({
+                        overrides: {
+                            [fakeSamplerAddress]: {
+                                code: artifacts.ERC20BridgeSampler.compilerOutput.evm.deployedBytecode.object,
+                            },
+                        },
+                    });
                 console.log(samples);
                 expect(samples.length).to.be.bignumber.greaterThan(0);
                 expect(samples[0]).to.be.bignumber.greaterThan(0);
