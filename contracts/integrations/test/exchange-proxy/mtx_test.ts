e27e6baabecd8a7dcb64570ecf0c2cc63e0b2636
Lawrence Forman <me@merklejerk.com>
2020-08-17 22:08:25 -0400

    `@0x/contracts-integrations`: Add EP mtx tests.

---
+++ contracts/integrations/test/exchange-proxy/mtx_test.ts
@@ -0,0 +1,11 @@
+import { blockchainTests, describe } from '@0x/contracts-test-utils';
+
+blockchainTests.resets('exchange proxy - meta-transactions', env => {
+    before(async () => {
+
+    });
+
+    it('can call `transformERC20()` with signed calldata', async () => {
+
+    });
+});
