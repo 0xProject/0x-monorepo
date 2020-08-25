e9b0ac8820921e3c6a36e87405c6d0380e883779
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-08-25 17:42:02 -0400

    asset-swapper: Stop req'ing takerAddr for RFQT ind
    Stop requiring takerAddress for indicative RFQ-T quotes.

--- packages/asset-swapper/src/utils/quote_requestor.ts
+++ packages/asset-swapper/src/utils/quote_requestor.ts
@@ -39,18 +39,6 @@ function getTokenAddressOrThrow(assetData: string): string {
     throw new Error(`Decoded asset data (${JSON.stringify(decodedAssetData)}) does not contain a token address`);
 }
 
-function assertTakerAddressOrThrow(takerAddress: string | undefined): void {
-    if (
-        takerAddress === undefined ||
-        takerAddress === '' ||
-        takerAddress === '0x' ||
-        !takerAddress ||
-        takerAddress === constants.NULL_ADDRESS
-    ) {
-        throw new Error('RFQ-T requires the presence of a taker address');
-    }
-}
-
 function inferQueryParams(
     marketOperation: MarketOperation,
     makerAssetData: string,
@@ -137,7 +125,15 @@ export class QuoteRequestor {
         options: RfqtRequestOpts,
     ): Promise<RFQTFirmQuote[]> {
         const _opts: RfqtRequestOpts = { ...constants.DEFAULT_RFQT_REQUEST_OPTS, ...options };
-        assertTakerAddressOrThrow(_opts.takerAddress);
+        if (
+            _opts.takerAddress === undefined ||
+            _opts.takerAddress === '' ||
+            _opts.takerAddress === '0x' ||
+            !_opts.takerAddress ||
+            _opts.takerAddress === constants.NULL_ADDRESS
+        ) {
+            throw new Error('RFQ-T firm quotes require the presence of a taker address');
+        }
 
         const firmQuoteResponses = await this._getQuotesAsync<RFQTFirmQuote>( // not yet BigNumber
             makerAssetData,
@@ -217,7 +213,6 @@ export class QuoteRequestor {
         options: RfqtRequestOpts,
     ): Promise<RFQTIndicativeQuote[]> {
         const _opts: RfqtRequestOpts = { ...constants.DEFAULT_RFQT_REQUEST_OPTS, ...options };
-        assertTakerAddressOrThrow(_opts.takerAddress);
 
         const responsesWithStringInts = await this._getQuotesAsync<RFQTIndicativeQuote>( // not yet BigNumber
             makerAssetData,
