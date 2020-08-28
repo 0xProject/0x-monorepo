6c54d4c18e08d16c26d2e9ec61715f3234733ed5
Michael Zhu <mchl.zhu.96@gmail.com>
2020-08-28 13:25:23 -0700

    AsmUniswap test

---
+++ contracts/zero-ex/contracts/src/features/IUniswapV2Pair.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.6.5;
+
+interface IUniswapV2Pair {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+}
