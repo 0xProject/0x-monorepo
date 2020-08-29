3d0969229f1b53722b0b683fa20d9daef79dd26d
Remco Bloemen <remco@0x.org>
2020-08-28 23:44:03 -0700

    Implement ordering

--- contracts/zero-ex/contracts/src/features/AsmUniswap.sol
+++ contracts/zero-ex/contracts/src/features/AsmUniswap.sol
@@ -44,66 +44,79 @@ contract AsmUniswap {
             let haveToken := calldataload(0x24)
             let wantToken := calldataload(0x44)
             let haveAmount := calldataload(0x64)
+            let order := lt(haveToken, wantToken)
 
-            if lt(haveToken, wantToken) {
-                // Compute the UniswapV2Pair address
+            // Compute the UniswapV2Pair address
+            switch order
+            case 0 {
+                mstore(0x00, wantToken)
+                mstore(0x20, haveToken)
+            }
+            default {
                 mstore(0x00, haveToken)
                 mstore(0x20, wantToken)
-                let salt := keccak256(0x00, 0x40)
-                mstore(0x00, FF_FACTORY)
-                mstore(0x15, salt)
-                mstore(0x35, CODE_HASH)
-                let pair := and(ADDRESS_MASK, keccak256(0, 0x55))
-                // TODO: Fix pair computation
-                pair := 0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11
+            }
+            let salt := keccak256(0x00, 0x40)
+            mstore(0x00, FF_FACTORY)
+            mstore(0x15, salt)
+            mstore(0x35, CODE_HASH)
+            let pair := and(ADDRESS_MASK, keccak256(0, 0x55))
+            // TODO: Fix pair computation
+            pair := 0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11
 
-                // Call ALLOWANCE_TARGET.executeCall(HAVE_TOKEN,
-                //  "transferFrom(msg.sender, PAIR, haveAmount)")
-                // TODO: Use a big CODECOPY to create templates for all calls?
-                mstore(0x00, EXECUTE_CALL_SELECTOR)
-                mstore(0x04, haveToken)
-                mstore(0x24, 0x40)
-                mstore(0x44, 0x64)
-                mstore(0x64, TRANSFER_FROM_SELECTOR)
-                mstore(0x68, caller()) 
-                mstore(0x88, pair)
-                mstore(0xA8, haveAmount)
-                if iszero(call(gas(), ALLOWANCE_TARGET, 0, 0, 0xE4, 0, 0)) {
-                    bubbleRevert()
-                }
-                // No need to check result, if transfer failed the UniswapV2Pair will
-                // reject our trade (or it may succeed if somehow the reserve was out of sync)
-                // this is fine for the taker.
+            // Call ALLOWANCE_TARGET.executeCall(HAVE_TOKEN,
+            //  "transferFrom(msg.sender, PAIR, haveAmount)")
+            // TODO: Use a big CODECOPY to create templates for all calls?
+            mstore(0x00, EXECUTE_CALL_SELECTOR)
+            mstore(0x04, haveToken)
+            mstore(0x24, 0x40)
+            mstore(0x44, 0x64)
+            mstore(0x64, TRANSFER_FROM_SELECTOR)
+            mstore(0x68, caller()) 
+            mstore(0x88, pair)
+            mstore(0xA8, haveAmount)
+            if iszero(call(gas(), ALLOWANCE_TARGET, 0, 0, 0xE4, 0, 0)) {
+                bubbleRevert()
+            }
+            // No need to check result, if transfer failed the UniswapV2Pair will
+            // reject our trade (or it may succeed if somehow the reserve was out of sync)
+            // this is fine for the taker.
 
-                // Call PAIR.getReserves()
-                // Call never fails (PAIR is trusted)
-                // Results are in range (0, 2¹¹²) stored in:
-                // haveToken = mload(0x00)
-                // wantToken = mload(0x20)
-                mstore(0x00, GET_RESERVES_SELECTOR)
-                if iszero(call(gas(), pair, 0, 0, 4, 0, 0x40)) {
-                    bubbleRevert()
-                }
+            // Call PAIR.getReserves()
+            // Call never fails (PAIR is trusted)
+            // Results are in range (0, 2¹¹²) stored in:
+            // haveToken = mload(0x00)
+            // wantToken = mload(0x20)
+            mstore(0x00, GET_RESERVES_SELECTOR)
+            if iszero(call(gas(), pair, 0, 0, 4, 0, 0x40)) {
+                bubbleRevert()
+            }
 
-                // Call PAIR.swap(0, wantAmount, to, new bytes(0))
-                let haveAmountWithFee := mul(haveAmount, 997)
-                mstore(0x40, SWAP_SELECTOR)
+            // Call PAIR.swap(0, wantAmount, to, new bytes(0))
+            let haveAmountWithFee := mul(haveAmount, 997)
+            mstore(0x40, SWAP_SELECTOR)
+            switch order
+            case 0 {
+                mstore(0x44, div(
+                    mul(haveAmountWithFee, mload(0x00)),
+                    add(haveAmountWithFee, mul(mload(0x20), 1000))
+                ))
+                mstore(0x64, 0)
+            }
+            default {
                 mstore(0x44, 0)
                 mstore(0x64, div(
                     mul(haveAmountWithFee, mload(0x20)),
                     add(haveAmountWithFee, mul(mload(0x00), 1000))
                 ))
-                mstore(0x84, to)
-                mstore(0xA4, 0x80)
-                mstore(0xC4, 0)
-                if iszero(call(gas(), pair, 0, 0x40, 0xF4, 0, 0)) {
-                    bubbleRevert()
-                }
-                stop()
             }
-
-            // TODO haveToken > wantToken
-            revert(0, 0)
+            mstore(0x84, to)
+            mstore(0xA4, 0x80)
+            mstore(0xC4, 0)
+            if iszero(call(gas(), pair, 0, 0x40, 0xF4, 0, 0)) {
+                bubbleRevert()
+            }
+            stop()
 
             function bubbleRevert() {
                 returndatacopy(0, 0, returndatasize())
