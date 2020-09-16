79e0a9ef3701f8bdb2e28dd4a6b83bce88019472
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-09-16 17:34:20 -0400

    Add and use constant ONE_MINUTE_MS
    Addresses review comments
    https://github.com/0xProject/0x-monorepo/pull/2706#discussion_r489610605
    and
    https://github.com/0xProject/0x-monorepo/pull/2706#discussion_r489614731

--- packages/asset-swapper/src/utils/rfq_maker_blacklist.ts
+++ packages/asset-swapper/src/utils/rfq_maker_blacklist.ts
@@ -2,9 +2,10 @@
  * Tracks a maker's history of timely responses, and manages whether a given
  * maker should be avoided for being too latent.
  */
+
+import { constants } from '../constants';
+
 export class RfqMakerBlacklist {
-    // tslint:disable-next-line:custom-no-magic-numbers
-    private static readonly _msPerMinute = 1000 * 60;
     private readonly _makerTimeoutStreakLength: { [makerUrl: string]: number } = {};
     private readonly _makerBlacklistedUntilDate: { [makerUrl: string]: number } = {};
     constructor(private readonly _blacklistDurationMinutes: number, private readonly _timeoutStreakThreshold: number) {}
@@ -16,8 +17,7 @@ export class RfqMakerBlacklist {
             this._makerTimeoutStreakLength[makerUrl] += 1;
             if (this._makerTimeoutStreakLength[makerUrl] > this._timeoutStreakThreshold) {
                 this._makerBlacklistedUntilDate[makerUrl] =
-                    Date.now() + this._blacklistDurationMinutes * RfqMakerBlacklist._msPerMinute;
-                this._makerTimeoutStreakLength[makerUrl] = 0;
+                    Date.now() + this._blacklistDurationMinutes * constants.ONE_MINUTE_MS;
             }
         } else {
             this._makerTimeoutStreakLength[makerUrl] = 0;
